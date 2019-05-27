573906cf46de2d0da679e58d8cb7054767a76fd6
osdnk <micosa97@gmail.com>
2019-05-27 16:42:16 +0200

    remove unrelated things

--- dever.sh
+++
@@ -1,3 +0,0 @@
-cp -R android Example/node_modules/react-native-screens/.
-cp -R ios Example/node_modules/react-native-screens/.
-cp -R src Example/node_modules/react-native-screens/.
--- proder.sh
+++
@@ -1,3 +0,0 @@
-cp -R Example/node_modules/react-native-screens/android .
-cp -R Example/node_modules/react-native-screens/ios .
-cp -R Example/node_modules/react-native-screens/src .
