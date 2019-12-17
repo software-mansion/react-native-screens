56ec62528a8f453acb7312d9ead6012a45a4228a
Michał Osadnik <micosa97@gmail.com>
2019-12-17 17:21:40 +0100

    Update RNSScreen.m

--- ios/RNSScreen.m
+++ ios/RNSScreen.m
@@ -31,12 +31,6 @@
   return self;
 }
 
-- (void)reactSetFrame:(CGRect)frame
-{
-  // ignore setFrame call from react, the frame of this view
-  // is controlled by the UIViewController it is contained in
-}
-
 - (void)updateBounds
 {
   [_bridge.uiManager setSize:self.bounds.size forView:self];
