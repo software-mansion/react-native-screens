software-mansion/react-native-screens

commit 22264c08133f171cf4317786bac3e3cc37e515de

Implement largeTitle backgroundColor and hideShadow on iOS (#333)
New props allowing to:
- define the background color 
- hide the shadow

of the header when the edge of any scrollable content reaches the matching edge of the navigation bar.

See [scrollEdgeAppearance](https://developer.apple.com/documentation/uikit/uinavigationitem/3198041-scrolledgeappearance)

ios/RNSScreenStackHeaderConfig.m
@@ -203,7 +203,7 @@
         RCTImageSource *source = imageView.imageSources[0];
         [imageView reactSetFrame:CGRectMake(imageView.frame.origin.x,
                                             imageView.frame.origin.y,
-                                            source.size.width, 
+                                            source.size.width,
                                             source.size.height)];
       }
       UIImage *image = imageView.image;
@@ -255,6 +255,79 @@
 {
   [self updateViewController:vc withConfig:config animated:animated];
 }
+
+#ifdef __IPHONE_13_0
++ (UINavigationBarAppearance*)buildAppearance:(UIViewController *)vc withConfig:(RNSScreenStackHeaderConfig *)config
+{
+  UINavigationBarAppearance *appearance = [UINavigationBarAppearance new];
+
+  if (config.backgroundColor && CGColorGetAlpha(config.backgroundColor.CGColor) == 0.) {
+    // transparent background color
+    [appearance configureWithTransparentBackground];
+  } else {
+    // non-transparent background or default background
+    if (config.translucent) {
+      [appearance configureWithDefaultBackground];
+    } else {
+      [appearance configureWithOpaqueBackground];
+    }
+
+    // set background color if specified
+    if (config.backgroundColor) {
+      appearance.backgroundColor = config.backgroundColor;
+    }
+  }
+
+  if (config.backgroundColor && CGColorGetAlpha(config.backgroundColor.CGColor) == 0.) {
+    appearance.backgroundColor = config.backgroundColor;
+  }
+
+  if (config.hideShadow) {
+    appearance.shadowColor = nil;
+  }
+
+  if (config.titleFontFamily || config.titleFontSize || config.titleColor) {
+    NSMutableDictionary *attrs = [NSMutableDictionary new];
+
+    if (config.titleColor) {
+      attrs[NSForegroundColorAttributeName] = config.titleColor;
+    }
+
+    CGFloat size = config.titleFontSize ? [config.titleFontSize floatValue] : 17;
+    if (config.titleFontFamily) {
+      attrs[NSFontAttributeName] = [UIFont fontWithName:config.titleFontFamily size:size];
+    } else {
+      attrs[NSFontAttributeName] = [UIFont boldSystemFontOfSize:size];
+    }
+    appearance.titleTextAttributes = attrs;
+  }
+
+  if (config.largeTitleFontFamily || config.largeTitleFontSize || config.largeTitleColor || config.titleColor) {
+    NSMutableDictionary *largeAttrs = [NSMutableDictionary new];
+
+    if (config.largeTitleColor || config.titleColor) {
+      largeAttrs[NSForegroundColorAttributeName] = config.largeTitleColor ? config.largeTitleColor : config.titleColor;
+    }
+
+    CGFloat largeSize = config.largeTitleFontSize ? [config.largeTitleFontSize floatValue] : 34;
+    if (config.largeTitleFontFamily) {
+      largeAttrs[NSFontAttributeName] = [UIFont fontWithName:config.largeTitleFontFamily size:largeSize];
+    } else {
+      largeAttrs[NSFontAttributeName] = [UIFont boldSystemFontOfSize:largeSize];
+    }
+
+    appearance.largeTitleTextAttributes = largeAttrs;
+  }
+
+  UIImage *backButtonImage = [self loadBackButtonImageInViewController:vc withConfig:config];
+  if (backButtonImage) {
+    [appearance setBackIndicatorImage:backButtonImage transitionMaskImage:backButtonImage];
+  } else if (appearance.backIndicatorImage) {
+    [appearance setBackIndicatorImage:nil transitionMaskImage:nil];
+  }
+  return appearance;
+}
+#endif
 
 + (void)updateViewController:(UIViewController *)vc withConfig:(RNSScreenStackHeaderConfig *)config animated:(BOOL)animated
 {
@@ -311,76 +384,16 @@
   }
 #ifdef __IPHONE_13_0
   if (@available(iOS 13.0, *)) {
-    UINavigationBarAppearance *appearance = [UINavigationBarAppearance new];
-
-    if (config.backgroundColor && CGColorGetAlpha(config.backgroundColor.CGColor) == 0.) {
-      // transparent background color
-      [appearance configureWithTransparentBackground];
-    } else {
-      // non-transparent background or default background
-      if (config.translucent) {
-        [appearance configureWithDefaultBackground];
-      } else {
-        [appearance configureWithOpaqueBackground];
-      }
-
-      // set background color if specified
-      if (config.backgroundColor) {
-        appearance.backgroundColor = config.backgroundColor;
-      }
-    }
-
-    if (config.backgroundColor && CGColorGetAlpha(config.backgroundColor.CGColor) == 0.) {
-      appearance.backgroundColor = config.backgroundColor;
-    }
-
-    if (config.hideShadow) {
-      appearance.shadowColor = nil;
-    }
-
-    if (config.titleFontFamily || config.titleFontSize || config.titleColor) {
-      NSMutableDictionary *attrs = [NSMutableDictionary new];
-
-      if (config.titleColor) {
-        attrs[NSForegroundColorAttributeName] = config.titleColor;
-      }
-
-      CGFloat size = config.titleFontSize ? [config.titleFontSize floatValue] : 17;
-      if (config.titleFontFamily) {
-        attrs[NSFontAttributeName] = [UIFont fontWithName:config.titleFontFamily size:size];
-      } else {
-        attrs[NSFontAttributeName] = [UIFont boldSystemFontOfSize:size];
-      }
-      appearance.titleTextAttributes = attrs;
-    }
-
-    if (config.largeTitleFontFamily || config.largeTitleFontSize || config.largeTitleColor || config.titleColor) {
-      NSMutableDictionary *largeAttrs = [NSMutableDictionary new];
-
-      if (config.largeTitleColor || config.titleColor) {
-        largeAttrs[NSForegroundColorAttributeName] = config.largeTitleColor ? config.largeTitleColor : config.titleColor;
-      }
-
-      CGFloat largeSize = config.largeTitleFontSize ? [config.largeTitleFontSize floatValue] : 34;
-      if (config.largeTitleFontFamily) {
-        largeAttrs[NSFontAttributeName] = [UIFont fontWithName:config.largeTitleFontFamily size:largeSize];
-      } else {
-        largeAttrs[NSFontAttributeName] = [UIFont boldSystemFontOfSize:largeSize];
-      }
-
-      appearance.largeTitleTextAttributes = largeAttrs;
-    }
-
-    UIImage *backButtonImage = [self loadBackButtonImageInViewController:vc withConfig:config];
-    if (backButtonImage) {
-      [appearance setBackIndicatorImage:backButtonImage transitionMaskImage:backButtonImage];
-    } else if (appearance.backIndicatorImage) {
-      [appearance setBackIndicatorImage:nil transitionMaskImage:nil];
-    }
-
+    UINavigationBarAppearance *appearance = [self buildAppearance: vc withConfig:config];
     navitem.standardAppearance = appearance;
     navitem.compactAppearance = appearance;
-    navitem.scrollEdgeAppearance = appearance;
+
+    UINavigationBarAppearance *scrollEdgeAppearance = [self buildAppearance: vc withConfig:config];
+    scrollEdgeAppearance.backgroundColor = config.largeTitleBackgroundColor;
+    if (config.largeTitleHideShadow) {
+        scrollEdgeAppearance.shadowColor = nil;
+    }
+    navitem.scrollEdgeAppearance = scrollEdgeAppearance;
   } else
 #endif
   {
@@ -474,6 +487,8 @@ RCT_EXPORT_VIEW_PROPERTY(largeTitle, BOOL)
 RCT_EXPORT_VIEW_PROPERTY(largeTitleFontFamily, NSString)
 RCT_EXPORT_VIEW_PROPERTY(largeTitleFontSize, NSNumber)
 RCT_EXPORT_VIEW_PROPERTY(largeTitleColor, UIColor)
+RCT_EXPORT_VIEW_PROPERTY(largeTitleBackgroundColor, UIColor)
+RCT_EXPORT_VIEW_PROPERTY(largeTitleHideShadow, BOOL)
 RCT_EXPORT_VIEW_PROPERTY(hideBackButton, BOOL)
 RCT_EXPORT_VIEW_PROPERTY(hideShadow, BOOL)
 // `hidden` is an UIView property, we need to use different name internally

ios/RNSScreenStackHeaderConfig.h
@@ -20,6 +20,8 @@
 @property (nonatomic) BOOL largeTitle;
 @property (nonatomic, retain) NSString *largeTitleFontFamily;
 @property (nonatomic, retain) NSNumber *largeTitleFontSize;
+@property (nonatomic, retain) UIColor *largeTitleBackgroundColor;
+@property (nonatomic) BOOL largeTitleHideShadow;
 @property (nonatomic, retain) UIColor *largeTitleColor;
 @property (nonatomic) BOOL hideBackButton;
 @property (nonatomic) BOOL hideShadow;

src/screens.d.ts
@@ -139,6 +139,14 @@ declare module 'react-native-screens' {
      */
     largeTitleFontSize?: number;
     /**
+     *@description Controlls the color of the navigation header when the edge of any scrollable content reaches the matching edge of the navigation bar.
+     */
+    largeTitleBackgroundColor?: string;
+    /**
+     * @description Boolean that allows for disabling drop shadow under navigation header when the edge of any scrollable content reaches the matching edge of the navigation bar.
+     */
+    largeTitleHideShadow?: boolean;
+    /**
      * @host (iOS only)
      * @description Customize the color to be used for the large title. By default uses the titleColor property.
      */

commit 18bc749a39307268e4ea7341f6b229c752a09e22

Fix large header background when largeTitleBackgroundColor is not set (#435)
Fix a regression caused by #333

When `largeTitleBackgroundColor` we want to keep the current background color, setting to nil will cause it to become transparent.

Fixes #433 
Fixes #431

ios/RNSScreenStackHeaderConfig.m
@@ -389,7 +389,9 @@
     navitem.compactAppearance = appearance;
 
     UINavigationBarAppearance *scrollEdgeAppearance = [[UINavigationBarAppearance alloc] initWithBarAppearance:appearance];
-    scrollEdgeAppearance.backgroundColor = config.largeTitleBackgroundColor;
+    if (config.largeTitleBackgroundColor != nil) {
+      scrollEdgeAppearance.backgroundColor = config.largeTitleBackgroundColor;
+    }
     if (config.largeTitleHideShadow) {
         scrollEdgeAppearance.shadowColor = nil;
     }