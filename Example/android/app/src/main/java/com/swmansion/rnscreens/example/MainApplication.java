a0b34bd104951b4e474abcae2738c3b9084dc5c7
WoLewicki <wojciech.lewicki@swmansion.com>
2020-03-27 19:45:24 +0100

    feat: add working android example

--- Example/android/app/src/main/java/com/swmansion/rnscreens/example/MainApplication.java
+++ Example/android/app/src/main/java/com/swmansion/rnscreens/example/MainApplication.java
@@ -2,21 +2,25 @@ package com.swmansion.rnscreens.example;
 
 import android.app.Application;
 
-import com.facebook.react.ReactInstanceManager;
+
+import android.app.Application;
+import android.content.Context;
 import com.facebook.react.PackageList;
-import com.facebook.hermes.reactexecutor.HermesExecutorFactory;
-import com.facebook.react.bridge.JavaScriptExecutorFactory;
 import com.facebook.react.ReactApplication;
-import com.facebook.react.bridge.NativeModule;
-import com.facebook.react.bridge.ReactApplicationContext;
-import com.facebook.react.uimanager.ViewManager;
+import com.facebook.react.ReactInstanceManager;
 import com.facebook.react.ReactNativeHost;
 import com.facebook.react.ReactPackage;
 import com.facebook.soloader.SoLoader;
+import java.lang.reflect.InvocationTargetException;
+import java.util.List;
+import com.facebook.hermes.reactexecutor.HermesExecutorFactory;
+import com.facebook.react.bridge.JavaScriptExecutorFactory;
+import com.facebook.react.bridge.NativeModule;
+import com.facebook.react.bridge.ReactApplicationContext;
+import com.facebook.react.uimanager.ViewManager;
 
 import java.util.Arrays;
 import java.util.Collections;
-import java.util.List;
 
 public class MainApplication extends Application implements ReactApplication {
 
