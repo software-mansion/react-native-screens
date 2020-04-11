61130cc6bc70107a98216184f207f3f57dd5f693
Satyajit Sahoo <satyajit.happy@gmail.com>
2020-04-11 20:32:33 +0200

    Simplify the implementation on web
    Use `Animated.createComponent` on web instead of adding listeners manually. This means:
    
    - No extra listeners
    - One less dependency (`debounce`)
    - Smaller component hierarchy since `Animated.View` used the HOC anyway
    
    This also fixes some issues on web:
    
    - Fix `NativeScreen` ignoring the `active` prop
    - Fix `enableScreens(false)` not respected
    - Fix custom `display` value not being respected since it was set to `flex`
    - Fix view not re-appearing sometimes when `active` state changes

--- src/screens.web.js
+++ src/screens.web.js
@@ -1,113 +1,35 @@
-import debounce from 'debounce';
 import React from 'react';
 import { Animated, View } from 'react-native';
 
-let _shouldEnableScreens = true;
+let ENABLE_SCREENS = true;
 
 export function enableScreens(shouldEnableScreens = true) {
-  if (shouldEnableScreens) {
-    console.warn(
-      'react-native-screens is not fully supported on this platform yet.'
-    );
-  }
-  _shouldEnableScreens = shouldEnableScreens;
+  ENABLE_SCREENS = shouldEnableScreens;
 }
 
 export function screensEnabled() {
-  return _shouldEnableScreens;
+  return ENABLE_SCREENS;
 }
 
-function isAnimatedValue(value) {
-  return value && value.__getValue && value.addListener;
-}
-
-function isPropTruthy(prop) {
-  let activeValue = prop;
-  if (isAnimatedValue(prop)) {
-    activeValue = prop.__getValue();
-  }
-
-  return !!activeValue;
-}
-
-export class Screen extends React.Component {
+export class NativeScreen extends React.Component {
   static defaultProps = {
     active: true,
   };
 
-  listenerId = null;
-
-  constructor(props) {
-    super(props);
-
-    this._onAnimatedValueUpdated = debounce(this._onAnimatedValueUpdated, 10);
-    this._addListener(props.active);
-  }
-
-  componentWillUnmount() {
-    this._removeListener(this.props.active);
-  }
-
-  _addListener = possibleListener => {
-    if (this.listenerId)
-      throw new Error(
-        'Screen: Attempting to observe an animated value while another value is already observed.'
-      );
-    if (isAnimatedValue(possibleListener)) {
-      this.listenerId = possibleListener.addListener(
-        this._onAnimatedValueUpdated
-      );
-    }
-  };
-
-  _removeListener = possibleListener => {
-    if (isAnimatedValue(possibleListener)) {
-      possibleListener.removeListener(this.listenerId);
-      this.listenerId = null;
-    }
-  };
-
-  shouldComponentUpdate({ active: nextActive }) {
-    const { active } = this.props;
-    if (nextActive !== active) {
-      this._removeListener(active);
-      this._addListener(nextActive);
-      this._updateDisplay(isPropTruthy(nextActive));
-      return false;
-    }
-    return true;
-  }
-
-  _onAnimatedValueUpdated = ({ value }) => {
-    this._updateDisplay(!!value);
-  };
-
-  _updateDisplay = isActive => {
-    if (isActive === undefined) {
-      isActive = isPropTruthy(this.props.active);
-    }
-    const display = isActive ? 'flex' : 'none';
-    this.setNativeProps({ style: { display } });
-  };
-
-  setNativeProps = nativeProps => {
-    if (this._view) {
-      this._view.setNativeProps(nativeProps);
-    }
-  };
-
-  _setRef = view => {
-    this._view = view;
-    this._updateDisplay();
-  };
-
   render() {
-    return <Animated.View {...this.props} ref={this._setRef} />;
+    const { active, style, ...rest } = this.props;
+
+    return (
+      <View
+        style={[style, ENABLE_SCREENS && !active ? { display: 'none' } : null]}
+        {...rest}
+      />
+    );
   }
 }
 
-export const ScreenContainer = View;
+export const Screen = Animated.createAnimatedComponent(NativeScreen);
 
-export const NativeScreen = View;
+export const ScreenContainer = View;
 
 export const NativeScreenContainer = View;
