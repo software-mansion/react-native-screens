ccab0f3098090d42cd54a6a3e6f929cbb711c571
WoLewicki <wojciech.lewicki@swmansion.com>
2020-08-20 16:41:19 +0200

    fix: webview in stack

--- android/src/main/java/com/swmansion/rnscreens/Screen.java
+++ android/src/main/java/com/swmansion/rnscreens/Screen.java
@@ -8,6 +8,7 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.view.WindowManager;
 import android.view.inputmethod.InputMethodManager;
+import android.webkit.WebView;
 import android.widget.TextView;
 
 import androidx.annotation.Nullable;
@@ -151,7 +152,22 @@ public class Screen extends ViewGroup {
       return;
     }
     mTransitioning = transitioning;
-    super.setLayerType(transitioning ? View.LAYER_TYPE_HARDWARE : View.LAYER_TYPE_NONE, null);
+    boolean isWebViewInScreen = findWebView(this);
+    super.setLayerType(transitioning && !isWebViewInScreen ? View.LAYER_TYPE_HARDWARE : View.LAYER_TYPE_NONE, null);
+  }
+
+  private boolean findWebView(ViewGroup viewGroup) {
+    for(int i = 0; i < viewGroup.getChildCount(); i++) {
+      View child = viewGroup.getChildAt(i);
+      if (child instanceof WebView) {
+        return true;
+      } else if (child instanceof ViewGroup) {
+         if (findWebView((ViewGroup) child)) {
+           return true;
+         }
+      }
+    }
+    return false;
   }
 
   public void setStackPresentation(StackPresentation stackPresentation) {
