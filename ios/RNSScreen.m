a53dbce86227e452c0efad6898d7b7b64c0ebebe
Eli Zibin <1131641+zibs@users.noreply.github.com>
2020-11-09 10:47:21 -0800

    Update RNSScreen.m
    Update to 0.5 from 0.1

--- ios/RNSScreen.m
+++ ios/RNSScreen.m
@@ -420,7 +420,7 @@
 
 - (void)ensureHeaderButtonsVisible
 {
-  double delayInSeconds = 0.1;
+  double delayInSeconds = 0.5;
   dispatch_time_t popTime = dispatch_time(DISPATCH_TIME_NOW, (int64_t)(delayInSeconds * NSEC_PER_SEC));
   dispatch_after(popTime, dispatch_get_main_queue(), ^(void){
     [self.view.reactSubviews enumerateObjectsUsingBlock:^(UIView * _Nonnull view, NSUInteger idx, BOOL * _Nonnull stop) {
