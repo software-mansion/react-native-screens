7cef065e5e0b9afae9eb2b5ef0fe960128c0e8ce
WoLewicki <wojciech.lewicki@swmansion.com>
2020-11-10 16:49:01 +0100

    feat: add some changes

--- Example/metro.config.js
+++ Example/metro.config.js
@@ -1,10 +1,30 @@
-/**
- * Metro configuration for React Native
- * https://github.com/facebook/react-native
- *
- * @format
- */
+const blacklist = require('metro-config/src/defaults/blacklist');
+const path = require('path');
+
+const glob = require('glob-to-regexp');
+
+function getBlacklist() {
+  const nodeModuleDirs = [
+    glob(`${path.resolve(__dirname, '..')}/node_modules/*`),
+    glob(
+      `${path.resolve(
+        __dirname
+      )}/node_modules/*/node_modules/hoist-non-react-statics/*`
+    ),
+    glob(
+      `${path.resolve(
+        __dirname
+      )}/node_modules/react-native/node_modules/@babel/*`
+    ),
+  ];
+  return blacklist(nodeModuleDirs);
+}
+
 module.exports = {
+  resolver: {
+    blacklistRE: getBlacklist(),
+  },
+  watchFolders: [path.resolve(__dirname, '..')],
   transformer: {
     getTransformOptions: async () => ({
       transform: {
