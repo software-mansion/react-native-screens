940f0ae322bafe937558885908094807d2337dbc
Wojciech Lewicki <wojciech.lewicki@swmansion.com>
2020-11-13 15:08:53 +0100

    feat: change screens resolving logic (#698)
    Changed Android and iOS files to get the native code from the project files, updated babel and metro configs to get the JS code from the project files too.

--- Example/metro.config.js
+++ Example/metro.config.js
@@ -1,10 +1,25 @@
-/**
- * Metro configuration for React Native
- * https://github.com/facebook/react-native
- *
- * @format
- */
+const blacklist = require('metro-config/src/defaults/blacklist');
+const path = require('path');
+
+const glob = require('glob-to-regexp');
+
+function getBlacklist() {
+  const nodeModuleDirs = [
+    glob(`${path.resolve(__dirname, '..')}/node_modules/*`),
+    glob(
+      `${path.resolve(
+        __dirname
+      )}/node_modules/react-native/node_modules/@babel/*`
+    ),
+  ];
+  return blacklist(nodeModuleDirs);
+}
+
 module.exports = {
+  resolver: {
+    blacklistRE: getBlacklist(),
+  },
+  watchFolders: [path.resolve(__dirname, '..')],
   transformer: {
     getTransformOptions: async () => ({
       transform: {
