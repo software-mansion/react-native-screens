a5d451bc5e3a6b1e1228f09d701a48621d2281c1
jiong-shen-cb <70784786+jiong-shen-cb@users.noreply.github.com>
2020-11-19 18:46:10 -0800

    Update RNSScreen.m
    update slide prop name

--- ios/RNSScreen.m
+++ ios/RNSScreen.m
@@ -517,8 +517,8 @@ RCT_ENUM_CONVERTER(RNSScreenStackAnimation, (@{
                                                   @"none": @(RNSScreenStackAnimationNone),
                                                   @"fade": @(RNSScreenStackAnimationFade),
                                                   @"flip": @(RNSScreenStackAnimationFlip),
-                                                  @"slide": @(RNSScreenStackAnimationDefault),
-                                                  @"slide_rtl": @(RNSScreenStackAnimationDefault),
+                                                  @"slide_from_right": @(RNSScreenStackAnimationDefault),
+                                                  @"slide_from_left": @(RNSScreenStackAnimationDefault),
                                                   }), RNSScreenStackAnimationDefault, integerValue)
 
 RCT_ENUM_CONVERTER(RNSScreenReplaceAnimation, (@{
