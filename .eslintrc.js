32a99f6197f6525dbcbbced5a3c10d77060782c2
Wojciech Lewicki <wojciech.lewicki@swmansion.com>
2020-12-05 20:28:58 +0100

    fix: add native-stack to ts and eslint (#731)
    ## Description
    
    Added `native-stack` path to `tsconfig` and `.eslintrc.js` to properly resolve it in `TestsExample` project.
    
    ## Changes
    
    Updated `tsconfig.json` and `.eslintrc.js`.
    
    ## Test code and steps to reproduce
    
    Spot no eslint error in files of `TestsExample` project.
    
    ## Checklist
    
    - [x] Ensured that CI passes

--- .eslintrc.js
+++ .eslintrc.js
@@ -16,7 +16,7 @@ module.exports = {
   },
 
   settings: {
-    'import/core-modules': ['react-native-screens'],
+    'import/core-modules': ['react-native-screens', 'react-native-screens/native-stack'],
     'import/ignore': ['node_modules/react-native/index\\.js$'],
     'import/resolver': {
       node: {
