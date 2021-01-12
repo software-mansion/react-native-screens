2be6f19a0de24a1f68add11a7df7e2a45f5701e6
WoLewicki <wojciech.lewicki@swmansion.com>
2021-01-12 11:44:07 +0100

    feat: add setting frame in notifyWillAppear

--- TestsExample/App.js
+++ TestsExample/App.js
@@ -23,6 +23,7 @@ import Test691 from './src/Test691';
 import Test702 from './src/Test702';
 import Test706 from './src/Test706';
 import Test713 from './src/Test713';
+import Test750 from './src/Test750';
 
 enableScreens();
 
