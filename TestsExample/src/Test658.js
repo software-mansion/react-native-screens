c990d8148718b601ebaf3a5abca9a6a0de70a9d2
Henning Hall <henning.hall@hotmail.com>
2021-01-22 21:37:46 +0100

    fix linting and added routing options

--- TestsExample/src/Test658.js
+++ TestsExample/src/Test658.js
@@ -1,7 +1,7 @@
-import { NavigationContainer } from '@react-navigation/native';
+import {NavigationContainer} from '@react-navigation/native';
 import * as React from 'react';
-import { Button, Dimensions, Text, View } from 'react-native';
-import { createNativeStackNavigator } from 'react-native-screens/native-stack';
+import {Button, Dimensions, Text, View} from 'react-native';
+import {createNativeStackNavigator} from 'react-native-screens/native-stack';
 
 const Stack = createNativeStackNavigator();
 
@@ -9,31 +9,64 @@ export default function App() {
   return (
     <NavigationContainer>
       <Stack.Navigator>
-        <Stack.Screen name="First" component={TransparentModal} options={{  
-          stackPresentation: 'transparentModal',
-          headerMode: 'none',
-          headerShown: false,
-
-        }}/>
+        <Stack.Screen
+          name="TransparentModal"
+          component={Screen}
+          options={{
+            stackPresentation: 'transparentModal',
+            headerMode: 'none',
+            headerShown: false,
+          }}
+        />
+        <Stack.Screen
+          name="Modal"
+          component={Screen}
+          options={{
+            stackPresentation: 'modal',
+            headerMode: 'none',
+            headerShown: false,
+          }}
+        />
       </Stack.Navigator>
     </NavigationContainer>
   );
 }
 
-function TransparentModal({navigation}) {
+function Screen({navigation}) {
   const addedRoutes = navigation.dangerouslyGetState().routes.length - 1;
   const margin = addedRoutes * 20;
-  const width = Dimensions.get("screen").width - addedRoutes * 40;
-  const backgroundColor = colors[addedRoutes % colors.length]
+  const width = Dimensions.get('screen').width - addedRoutes * 40;
+  const backgroundColor = colors[addedRoutes % colors.length];
   return (
-    <View style={[{width, margin, backgroundColor, height: '100%', borderWidth: 2}]}>
+    <View
+      style={[
+        {width, margin, backgroundColor, height: '100%', borderWidth: 2},
+      ]}>
       <Button
         title="Open transparent modal"
-        onPress={() => navigation.push('First')}
+        onPress={() => navigation.push('TransparentModal')}
       />
-            <Text style={{padding: 10}}>For each modal you open, all previous modals should be visible underneath</Text> 
+      <Button title="Open modal" onPress={() => navigation.push('Modal')} />
+      {addedRoutes > 0 && (
+        <Button title="Back" onPress={() => navigation.goBack()} />
+      )}
+      <Text style={{padding: 10}}>
+        For each transparent modal you open, all previously visible screens
+        should be visible underneath.
+      </Text>
+      <Text style={{padding: 10}}>
+        For each new (non-transparent) modal you open, all previously visible
+        screens should be hidden.
+      </Text>
     </View>
   );
 }
 
-const colors = ['darkviolet', 'slateblue', 'mediumseagreen', 'khaki', 'orange', 'indianred']
+const colors = [
+  'darkviolet',
+  'slateblue',
+  'mediumseagreen',
+  'khaki',
+  'orange',
+  'indianred',
+];
