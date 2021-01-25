9369a24856386553cb4a7cd5334b5172c52f18a4
Satyajit Sahoo <satyajit.happy@gmail.com>
2021-01-25 12:17:25 +0100

    chore: fix babel and metro config

--- TestsExample/babel.config.js
+++ TestsExample/babel.config.js
@@ -1,18 +1,11 @@
 module.exports = {
   presets: ['babel-preset-expo'],
   plugins: [
-    '@babel/plugin-transform-modules-commonjs',
     [
       'module-resolver',
       {
         alias: {
-          'react-native-screens': '../src',
-          'react': './node_modules/react',
-          'react-native': './node_modules/react-native',
-          '@babel': './node_modules/@babel',
-          '@react-navigation/native': './node_modules/@react-navigation/native/src',
-          'react-navigation': './node_modules/react-navigation/src',
-          'react-navigation-stack': './node_modules/react-navigation-stack/src',
+          'react-native-screens': '../src'
         },
       },
     ],
