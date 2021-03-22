3d2870e2ffe56635be5e5bf148359d1262360450
kacperkapusciak <kapusciak.k@gmail.com>
2021-03-22 12:42:07 +0100

    chore: code cleanup

--- Example/android/app/src/main/java/com/swmansion/rnscreens/example/MainApplication.java
+++ Example/android/app/src/main/java/com/swmansion/rnscreens/example/MainApplication.java
@@ -5,7 +5,6 @@ import android.app.Application;
 import android.content.Context;
 import com.facebook.react.PackageList;
 import com.facebook.react.ReactApplication;
-import com.reactnativerestart.RestartPackage;
 import com.facebook.react.ReactInstanceManager;
 import com.facebook.react.ReactNativeHost;
 import com.facebook.react.ReactPackage;
