e36bb591120d1cb25cb81b4391bb95133129b3ab
kacperkapusciak <kapusciak.k@gmail.com>
2021-03-31 12:12:46 +0200

    feat: add test example
    Co-authored-by: tomtargosz

--- TestsExample/App.js
+++ TestsExample/App.js
@@ -34,6 +34,7 @@ import Test817 from './src/Test817';
 import Test831 from './src/Test831';
 import Test844 from './src/Test844';
 import Test861 from './src/Test861';
+import Test865 from './src/Test865';
 
 enableScreens();
 
