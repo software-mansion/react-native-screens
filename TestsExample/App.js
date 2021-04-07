4b657f14011a27b45aed1d8c3be3482f9216846f
Kacper Kapuściak <39658211+kacperkapusciak@users.noreply.github.com>
2021-04-07 15:28:32 +0200

    fix: add missing import in App.js (#879)
    Added missing import statement in App.js for Test852 in TestsExample project.

--- TestsExample/App.js
+++ TestsExample/App.js
@@ -33,6 +33,7 @@ import Test791 from './src/Test791';
 import Test817 from './src/Test817';
 import Test831 from './src/Test831';
 import Test844 from './src/Test844';
+import Test852 from './src/Test852';
 import Test861 from './src/Test861';
 import Test865 from './src/Test865';
 
