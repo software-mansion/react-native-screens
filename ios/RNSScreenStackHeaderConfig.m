5105c406e22ad01005f90f986dfe672aa3c8b897
Abe Fettig <abe@fettig.net>
2021-05-07 04:25:40 -0400

    Fix broken layout for header subviews in iOS (#902)
    This PR overrides `reactSetFrame` inside `RNSScreenStackHeaderSubview` which manually set the `frame.origin.x` and `.y` of each RNSScreenStackHeaderConfig to zero because they're meant to be managed by a `UINavigationBar`, and instead the RN layout gives them wrong origin e.g. on rotation.

--- ios/RNSScreenStackHeaderConfig.m
+++ ios/RNSScreenStackHeaderConfig.m
@@ -42,6 +42,13 @@
   return self;
 }
 
+- (void) reactSetFrame:(CGRect)frame
+{
+  // Block any attempt to set coordinates on RNSScreenStackHeaderSubview. This
+  // makes UINavigationBar the only one to control the position of header content.
+  [super reactSetFrame:CGRectMake(0, 0, frame.size.width, frame.size.height)];
+}
+
 @end
 
 @implementation RNSScreenStackHeaderConfig {
