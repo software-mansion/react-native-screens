d81dacc914f8065faaa8962b86f3f51e048b8741
WoLewicki <wojciech.lewicki@swmansion.com>
2021-05-10 13:12:14 +0200

    feat: add same for v4

--- src/createNativeStackNavigator.tsx
+++ src/createNativeStackNavigator.tsx
@@ -439,6 +439,7 @@ class StackView extends React.Component<Props> {
     return (
       <Screen
         key={`screen_${route.key}`}
+        enabled={true}
         style={[StyleSheet.absoluteFill, options.cardStyle]}
         stackAnimation={stackAnimation}
         stackPresentation={stackPresentation}
