37049ef7d0584c0fed688204e09d1449c5c23938
WoLewicki <wojciech.lewicki@swmansion.com>
2021-06-16 13:51:31 +0200

    fix: make sure we only call onDetachedFromWindow once

--- TestsExample/App.js
+++ TestsExample/App.js
@@ -45,6 +45,7 @@ import Test861 from './src/Test861';
 import Test865 from './src/Test865';
 import Test881 from './src/Test881';
 import Test898 from './src/Test898';
+import Test913 from './src/Test913';
 
 export default function App() {
   return <Test42 />;
