6b4d5978a12c828b0ade2f400d953fce38c33334
goodspark <spark.did.a.code@gmail.com>
2021-07-22 12:30:51 -0700

    Add include to iOS screen stack animator header
    Was getting compile errors:
    
    ```
    In file included from external/RNScreens/ios/RNSScreenStackAnimator.m:1:
    external/RNScreens/ios/RNSScreenStackAnimator.h:1:47: error: no type or protocol named 'UIViewControllerAnimatedTransitioning'
    @interface RNSScreenStackAnimator : NSObject <UIViewControllerAnimatedTransitioning>
                                                  ^
    external/RNScreens/ios/RNSScreenStackAnimator.h:1:37: error: cannot find interface declaration for 'NSObject', superclass of 'RNSScreenStackAnimator'
    @interface RNSScreenStackAnimator : NSObject <UIViewControllerAnimatedTransitioning>
    ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~   ^
    external/RNScreens/ios/RNSScreenStackAnimator.h:2:36: error: expected a type
    - (instancetype)initWithOperation:(UINavigationControllerOperation)operation;
                                       ^
    external/RNScreens/ios/RNSScreenStackAnimator.m:12:15: error: 'RNSScreenStackAnimator' cannot use 'super' because it is a root class
      if (self = [super init]) {
                  ^
    4 errors generated.
    ```

--- ios/RNSScreenStackAnimator.h
+++ ios/RNSScreenStackAnimator.h
@@ -1,3 +1,5 @@
+#include "RNSScreenStack.h"
+
 @interface RNSScreenStackAnimator : NSObject <UIViewControllerAnimatedTransitioning>
 - (instancetype)initWithOperation:(UINavigationControllerOperation)operation;
 @end
