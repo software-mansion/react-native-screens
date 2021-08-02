03bdce2db4ef5b0c431dc9f2876e49dfc31fa271
Wojciech Lewicki <wojciech.lewicki@swmansion.com>
2021-08-02 14:48:03 +0200

    Apply suggestions from code review
    Co-authored-by: Kacper Kapuściak <39658211+kacperkapusciak@users.noreply.github.com>

--- src/native-stack/utils/getDefaultHeaderHeight.tsx
+++ src/native-stack/utils/getDefaultHeaderHeight.tsx
@@ -4,35 +4,25 @@ type Layout = { width: number; height: number };
 
 export default function getDefaultHeaderHeight(
   layout: Layout,
-  modalPresentation: boolean,
+  isModalPresentation: boolean,
   statusBarHeight: number
 ): number {
-  let headerHeight;
+  let headerHeight = 64;
 
   const isLandscape = layout.width > layout.height;
 
   if (Platform.OS === 'ios') {
     if (Platform.isPad) {
-      if (modalPresentation) {
-        headerHeight = 56;
-      } else {
-        headerHeight = 50;
-      }
+      headerHeight = isModalPresentation ? 56 : 50;
     } else {
       if (isLandscape) {
         headerHeight = 32;
       } else {
-        if (modalPresentation) {
-          headerHeight = 56;
-        } else {
-          headerHeight = 44;
-        }
+        headerHeight = isModalPresentation ? 56 : 44;
       }
     }
   } else if (Platform.OS === 'android') {
     headerHeight = 56;
-  } else {
-    headerHeight = 64;
   }
 
   return headerHeight + statusBarHeight;
