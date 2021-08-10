d3d87e3fac467c42a2a27d5b5d8c4ebbdedd39fb
kacperkapusciak <kapusciak.k@gmail.com>
2021-08-10 15:16:57 +0200

    refactor: convert StackFinishTransitioningEvent to Kotlin

---
+++ android/src/main/java/com/swmansion/rnscreens/StackFinishTransitioningEvent.kt
@@ -0,0 +1,26 @@
+package com.swmansion.rnscreens
+
+import com.facebook.react.bridge.Arguments
+import com.facebook.react.uimanager.events.Event
+import com.swmansion.rnscreens.ScreenAppearEvent
+import com.swmansion.rnscreens.StackFinishTransitioningEvent
+import com.facebook.react.uimanager.events.RCTEventEmitter
+
+class StackFinishTransitioningEvent(viewId: Int) : Event<ScreenAppearEvent?>(viewId) {
+    override fun getEventName(): String {
+        return EVENT_NAME
+    }
+
+    override fun getCoalescingKey(): Short {
+        // All events for a given view can be coalesced.
+        return 0
+    }
+
+    override fun dispatch(rctEventEmitter: RCTEventEmitter) {
+        rctEventEmitter.receiveEvent(viewTag, eventName, Arguments.createMap())
+    }
+
+    companion object {
+        const val EVENT_NAME = "topFinishTransitioning"
+    }
+}
