244e511443603bcd07680db0d4f7d114c2032a76
kacperkapusciak <kapusciak.k@gmail.com>
2021-08-10 15:13:24 +0200

    fix: make ScreenWillAppearEvent more idiomatic

--- android/src/main/java/com/swmansion/rnscreens/ScreenWillAppearEvent.kt
+++ android/src/main/java/com/swmansion/rnscreens/ScreenWillAppearEvent.kt
@@ -2,19 +2,13 @@ package com.swmansion.rnscreens
 
 import com.facebook.react.bridge.Arguments
 import com.facebook.react.uimanager.events.Event
-import com.swmansion.rnscreens.ScreenAppearEvent
-import com.swmansion.rnscreens.ScreenWillAppearEvent
 import com.facebook.react.uimanager.events.RCTEventEmitter
 
-class ScreenWillAppearEvent(viewId: Int) : Event<ScreenAppearEvent?>(viewId) {
-    override fun getEventName(): String {
-        return EVENT_NAME
-    }
+class ScreenWillAppearEvent(viewId: Int) : Event<ScreenWillAppearEvent>(viewId) {
+    override fun getEventName() = EVENT_NAME
 
-    override fun getCoalescingKey(): Short {
-        // All events for a given view can be coalesced.
-        return 0
-    }
+    // All events for a given view can be coalesced.
+    override fun getCoalescingKey(): Short = 0
 
     override fun dispatch(rctEventEmitter: RCTEventEmitter) {
         rctEventEmitter.receiveEvent(viewTag, eventName, Arguments.createMap())
