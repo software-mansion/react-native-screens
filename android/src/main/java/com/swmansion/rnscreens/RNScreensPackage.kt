616375023c3e49783c8a81a0b59f1835c847ff98
kacperkapusciak <kapusciak.k@gmail.com>
2021-08-10 14:59:10 +0200

    fix: make RNSScreensPackage more idiomatic

--- android/src/main/java/com/swmansion/rnscreens/RNScreensPackage.kt
+++ android/src/main/java/com/swmansion/rnscreens/RNScreensPackage.kt
@@ -1,28 +1,20 @@
 package com.swmansion.rnscreens
 
 import com.facebook.react.ReactPackage
-import com.facebook.react.bridge.ReactApplicationContext
 import com.facebook.react.bridge.NativeModule
+import com.facebook.react.bridge.ReactApplicationContext
 import com.facebook.react.uimanager.ViewManager
-import com.swmansion.rnscreens.ScreenContainerViewManager
-import com.swmansion.rnscreens.ScreenViewManager
-import com.swmansion.rnscreens.ScreenStackViewManager
-import com.swmansion.rnscreens.ScreenStackHeaderConfigViewManager
-import com.swmansion.rnscreens.ScreenStackHeaderSubviewManager
-import java.util.*
 
 class RNScreensPackage : ReactPackage {
-    override fun createNativeModules(reactContext: ReactApplicationContext): List<NativeModule> {
-        return emptyList()
-    }
+    override fun createNativeModules(reactContext: ReactApplicationContext) =
+        emptyList<NativeModule>()
 
-    override fun createViewManagers(reactContext: ReactApplicationContext): List<ViewManager<*, *>> {
-        return Arrays.asList<ViewManager<*, *>>(
+    override fun createViewManagers(reactContext: ReactApplicationContext) =
+        listOf<ViewManager<*, *>>(
             ScreenContainerViewManager(),
             ScreenViewManager(),
             ScreenStackViewManager(),
             ScreenStackHeaderConfigViewManager(),
             ScreenStackHeaderSubviewManager()
         )
-    }
 }
