65242425cc3f04aa495c63243a9ce9d262d6473f
kacperkapusciak <kapusciak.k@gmail.com>
2021-08-10 15:15:51 +0200

    fix: make ScreenWillDisappearEvent more idiomatic

--- android/src/main/java/com/swmansion/rnscreens/ScreenWillDisappearEvent.kt
+++ android/src/main/java/com/swmansion/rnscreens/ScreenWillDisappearEvent.kt
@@ -2,19 +2,13 @@ package com.swmansion.rnscreens
 
 import com.facebook.react.bridge.Arguments
 import com.facebook.react.uimanager.events.Event
-import com.swmansion.rnscreens.ScreenAppearEvent
-import com.swmansion.rnscreens.ScreenWillDisappearEvent
 import com.facebook.react.uimanager.events.RCTEventEmitter
 
-class ScreenWillDisappearEvent(viewId: Int) : Event<ScreenAppearEvent?>(viewId) {
-    override fun getEventName(): String {
-        return EVENT_NAME
-    }
+class ScreenWillDisappearEvent(viewId: Int) : Event<ScreenWillDisappearEvent>(viewId) {
+    override fun getEventName() = EVENT_NAME
 
-    override fun getCoalescingKey(): Short {
-        // All events for a given view can be coalesced.
-        return 0
-    }
+    // All events for a given view can be coalesced.
+    override fun getCoalescingKey(): Short = 0
 
     override fun dispatch(rctEventEmitter: RCTEventEmitter) {
         rctEventEmitter.receiveEvent(viewTag, eventName, Arguments.createMap())
