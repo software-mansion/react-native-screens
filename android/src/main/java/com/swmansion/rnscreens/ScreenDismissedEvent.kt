859b2c76db5cb6c9666871de5027921bdaf3bcd8
kacperkapusciak <kapusciak.k@gmail.com>
2021-08-10 14:52:21 +0200

    fix: make ScreenDismissedEvent more idiomatic

--- android/src/main/java/com/swmansion/rnscreens/ScreenDismissedEvent.kt
+++ android/src/main/java/com/swmansion/rnscreens/ScreenDismissedEvent.kt
@@ -1,20 +1,14 @@
 package com.swmansion.rnscreens
 
 import com.facebook.react.bridge.Arguments
-import com.swmansion.rnscreens.ScreenDismissedEvent
-import com.facebook.react.uimanager.events.RCTEventEmitter
-import com.facebook.react.bridge.WritableMap
 import com.facebook.react.uimanager.events.Event
+import com.facebook.react.uimanager.events.RCTEventEmitter
 
-class ScreenDismissedEvent(viewId: Int) : Event<ScreenDismissedEvent?>(viewId) {
-    override fun getEventName(): String {
-        return EVENT_NAME
-    }
+class ScreenDismissedEvent(viewId: Int) : Event<ScreenDismissedEvent>(viewId) {
+    override fun getEventName() = EVENT_NAME
 
-    override fun getCoalescingKey(): Short {
-        // All events for a given view can be coalesced.
-        return 0
-    }
+    // All events for a given view can be coalesced.
+    override fun getCoalescingKey(): Short = 0
 
     override fun dispatch(rctEventEmitter: RCTEventEmitter) {
         val args = Arguments.createMap()
