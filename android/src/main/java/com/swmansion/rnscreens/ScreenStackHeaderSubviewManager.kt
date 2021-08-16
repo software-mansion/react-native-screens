0f5641b77cb673acac42d72fe4e65fea994cce16
WoLewicki <wojciech.lewicki@swmansion.com>
2021-08-16 17:15:31 +0200

    feat: convert ScreenStackHeaderSubviewManager

---
+++ android/src/main/java/com/swmansion/rnscreens/ScreenStackHeaderSubviewManager.kt
@@ -0,0 +1,35 @@
+package com.swmansion.rnscreens
+
+import com.facebook.react.module.annotations.ReactModule
+import com.facebook.react.uimanager.ThemedReactContext
+import com.facebook.react.uimanager.annotations.ReactProp
+import com.facebook.react.views.view.ReactViewGroup
+import com.facebook.react.views.view.ReactViewManager
+
+@ReactModule(name = ScreenStackHeaderSubviewManager.REACT_CLASS)
+class ScreenStackHeaderSubviewManager : ReactViewManager() {
+    override fun getName(): String {
+        return REACT_CLASS
+    }
+
+    override fun createViewInstance(context: ThemedReactContext): ReactViewGroup {
+        return ScreenStackHeaderSubview(context)
+    }
+
+    @ReactProp(name = "type")
+    fun setType(view: ScreenStackHeaderSubview, type: String) {
+        if ("left" == type) {
+            view.type = ScreenStackHeaderSubview.Type.LEFT
+        } else if ("center" == type) {
+            view.type = ScreenStackHeaderSubview.Type.CENTER
+        } else if ("right" == type) {
+            view.type = ScreenStackHeaderSubview.Type.RIGHT
+        } else if ("back" == type) {
+            view.type = ScreenStackHeaderSubview.Type.BACK
+        }
+    }
+
+    companion object {
+        const val REACT_CLASS = "RNSScreenStackHeaderSubview"
+    }
+}
