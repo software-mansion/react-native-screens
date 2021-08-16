8fdbd2e65b14c48e549be2572b93ca5f7a2e15ce
Ubax <jakub.tkacz@swmansion.com>
2021-08-16 13:21:27 +0300

    chore: Adds configuration for latest rns

--- TestsExampleTV/babel.config.js
+++ TestsExampleTV/babel.config.js
@@ -1,3 +1,14 @@
 module.exports = {
-  presets: ['module:metro-react-native-babel-preset'],
+  presets: ['babel-preset-expo'],
+  plugins: [
+    [
+      'module-resolver',
+      {
+        alias: {
+          'react-native-screens': '../src',
+        },
+      },
+    ],
+    'react-native-reanimated/plugin',
+  ],
 };
