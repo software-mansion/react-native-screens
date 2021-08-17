192aa82dc36a5e399f55696f0a53f77db7d43c2c
kacperkapusciak <kapusciak.k@gmail.com>
2021-08-17 16:36:05 +0200

    fix: downgrade ktlint, remove null coalesing

--- scripts/format-android.js
+++ scripts/format-android.js
@@ -10,11 +10,12 @@ const { exec } = require("child_process");
 const ktlintFormatCommand = "./android/gradlew -p android ktlintFormat -q -Pfile="
 
 // takes file as parameter passed by lint-staged (optional)
-const fileName = process.argv[2] ?? '';
+const fileName = process.argv[2] !== undefined ? process.argv[2] : '';
 
 // executes command with file parameter without space between them
 exec(`${ktlintFormatCommand}${fileName}`, (error, stdout) => {
   if (error) {
+    console.log(error);
     console.log(stdout);
     return exit(1);
   }
