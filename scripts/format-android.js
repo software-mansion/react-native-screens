6f148c916760f75541040e109e476a8701986629
kacperkapusciak <kapusciak.k@gmail.com>
2021-08-17 15:31:26 +0200

    fix: remove redundant code, pre-commit ordering

--- scripts/format-android.js
+++ scripts/format-android.js
@@ -1,13 +1,18 @@
+/* 
+ * This script is a wrapper for gradle & ktlintFormat to make
+ * it work properly with lint-staged.
+ */
+
 const { exit } = require("process");
 const { exec } = require("child_process");
 
 // ktlintFormat task in android/build.gradle
 const ktlintFormatCommand = "./android/gradlew -p android ktlintFormat -q -Pfile="
 
-// takes file as parameter optionally supplied by lint-staged
+// takes file as parameter passed by lint-staged (optional)
 const fileName = process.argv[2] ?? '';
 
-// executes command with file parameter
+// executes command with file parameter without space between them
 exec(`${ktlintFormatCommand}${fileName}`, (error, stdout) => {
   if (error) {
     console.log(stdout);
