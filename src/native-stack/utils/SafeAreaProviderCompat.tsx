4e97a0cedcc73c00167a4bf88bb3ecab981f5063
WoLewicki <wojciech.lewicki@swmansion.com>
2021-08-18 14:14:21 +0200

    feat: add comment and the newest code

--- src/native-stack/utils/SafeAreaProviderCompat.tsx
+++ src/native-stack/utils/SafeAreaProviderCompat.tsx
@@ -1,5 +1,14 @@
+// code taken from
+// https://github.com/react-navigation/react-navigation/blob/ec0d113eb25c39ef9defb6c7215640f44e3569ae/packages/elements/src/SafeAreaProviderCompat.tsx
 import * as React from 'react';
-import { Dimensions, Platform } from 'react-native';
+import {
+  Dimensions,
+  Platform,
+  StyleProp,
+  StyleSheet,
+  View,
+  ViewStyle,
+} from 'react-native';
 import {
   initialWindowMetrics,
   SafeAreaInsetsContext,
@@ -8,6 +17,7 @@ import {
 
 type Props = {
   children: React.ReactNode;
+  style?: StyleProp<ViewStyle>;
 };
 
 const { width = 0, height = 0 } = Dimensions.get('window');
@@ -23,7 +33,7 @@ const initialMetrics =
       }
     : initialWindowMetrics;
 
-export default function SafeAreaProviderCompat({ children }: Props) {
+export default function SafeAreaProviderCompat({ children, style }: Props) {
   return (
     <SafeAreaInsetsContext.Consumer>
       {(insets) => {
@@ -31,11 +41,11 @@ export default function SafeAreaProviderCompat({ children }: Props) {
           // If we already have insets, don't wrap the stack in another safe area provider
           // This avoids an issue with updates at the cost of potentially incorrect values
           // https://github.com/react-navigation/react-navigation/issues/174
-          return children;
+          return <View style={[styles.container, style]}>{children}</View>;
         }
 
         return (
-          <SafeAreaProvider initialMetrics={initialMetrics}>
+          <SafeAreaProvider initialMetrics={initialMetrics} style={style}>
             {children}
           </SafeAreaProvider>
         );
@@ -45,3 +55,9 @@ export default function SafeAreaProviderCompat({ children }: Props) {
 }
 
 SafeAreaProviderCompat.initialMetrics = initialMetrics;
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+});
