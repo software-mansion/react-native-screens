e4dc15d1ea26527f3a432a2cdc1da6cd5f3fdff7
kacperkapusciak <kapusciak.k@gmail.com>
2021-08-20 13:11:09 +0200

    chore: bump react-native to 0.65 in TestsExample

--- TestsExample/babel.config.js
+++ TestsExample/babel.config.js
@@ -1,14 +1,14 @@
 module.exports = {
-  presets: ['babel-preset-expo'],
+  presets: ['babel-preset-expo', '@babel/preset-typescript'],
   plugins: [
     [
       'module-resolver',
       {
         alias: {
-          'react-native-screens': '../src'
+          'react-native-screens': '../src',
         },
       },
     ],
     'react-native-reanimated/plugin',
-  ]
+  ],
 };
