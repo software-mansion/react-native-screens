020a989d731a7c8f8969ad483e036a329adc3e35
Wojciech Lewicki <wojciech.lewicki@swmansion.com>
2021-08-24 17:32:48 +0200

    fix: restore deleted tests and update TestsExample (#1073)
    PR fixing deleted by mistake files from #890 and updating Podfile.lock of TestsExample.

--- TestsExample/App.js
+++ TestsExample/App.js
@@ -50,6 +50,10 @@ import Test881 from './src/Test881';
 import Test887 from './src/Test887';
 import Test898 from './src/Test898';
 import Test913 from './src/Test913';
+import Test999 from './src/Test999';
+import Test1031 from './src/Test1031';
+import Test1032 from './src/Test1032';
+import Test1036 from './src/Test1036';
 
 export default function App() {
   return (
