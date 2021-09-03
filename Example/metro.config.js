bf75ac9072b2b317e588034cfea0a34ce0c9a5d3
kacperkapusciak <kapusciak.k@gmail.com>
2021-09-03 15:53:33 +0200

    feat: add windows to ScreensExample

--- Example/metro.config.js
+++ Example/metro.config.js
@@ -1,7 +1,7 @@
 /* eslint-disable import/no-commonjs */
 
 const path = require('path');
-const blacklist = require('metro-config/src/defaults/blacklist');
+const exclusionList = require('metro-config/src/defaults/exclusionList');
 const escape = require('escape-string-regexp');
 const pack = require('../package.json');
 
@@ -15,19 +15,25 @@ const modules = [
   ...Object.keys(pack.peerDependencies),
 ];
 
+// We need to make sure that only one version is loaded for peerDependencies
+// So we blacklist them at the root, and alias them to the versions in example's node_modules
+const modulesToExclude = modules.map(
+  (m) => new RegExp(`^${escape(path.join(root, 'node_modules', m))}\\/.*$`)
+)
+
 module.exports = {
   projectRoot: __dirname,
   watchFolders: [root],
-
-  // We need to make sure that only one version is loaded for peerDependencies
-  // So we blacklist them at the root, and alias them to the versions in example's node_modules
   resolver: {
-    blacklistRE: blacklist(
-      modules.map(
-        (m) =>
-          new RegExp(`^${escape(path.join(root, 'node_modules', m))}\\/.*$`)
-      )
-    ),
+    blockList: exclusionList([
+      ...modulesToExclude,
+      // This stops "react-native run-windows" from causing the metro server to crash if its already running
+      new RegExp(
+        `${path.resolve(__dirname, 'windows').replace(/[/\\]/g, '/')}.*`,
+      ),
+      // This prevents "react-native run-windows" from hitting: EBUSY: resource busy or locked, open msbuild.ProjectImports.zip
+      /.*\.ProjectImports\.zip/,
+    ]),
 
     extraNodeModules: modules.reduce((acc, name) => {
       acc[name] = path.join(__dirname, 'node_modules', name);
---
+++ Example/windows/ScreensExample/MainPage.h
@@ -0,0 +1,19 @@
+﻿#pragma once
+#include "MainPage.g.h"
+#include <winrt/Microsoft.ReactNative.h>
+
+namespace winrt::ScreensExample::implementation
+{
+    struct MainPage : MainPageT<MainPage>
+    {
+        MainPage();
+    };
+}
+
+namespace winrt::ScreensExample::factory_implementation
+{
+    struct MainPage : MainPageT<MainPage, implementation::MainPage>
+    {
+    };
+}
+
