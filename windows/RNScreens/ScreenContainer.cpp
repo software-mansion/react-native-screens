b72479d0414bde823c1a4fb61403f803be0c303e
kacperkapusciak <kapusciak.k@gmail.com>
2021-09-16 11:44:45 +0200

    break CI harder (┛ಠ_ಠ)┛彡┻━┻

--- windows/RNScreens/ScreenContainer.cpp
+++ windows/RNScreens/ScreenContainer.cpp
@@ -1,7 +1,7 @@
+#include "pch.h"
 #include "ScreenContainer.h"
 #include "JSValueXaml.h"
 #include "NativeModules.h"
-#include "pch.h"
 
 namespace winrt {
 using namespace Microsoft::ReactNative;
@@ -17,33 +17,5 @@ ScreenContainer::ScreenContainer(
     winrt::Microsoft::ReactNative::IReactContext reactContext)
     : m_reactContext(reactContext),
       m_children(
-          {winrt::single_threaded_vector<Windows::UI::Xaml::UIElement>()}) {}
 
-void ScreenContainer::addScreen(Screen &screen, int64_t) {
-  auto uiElement = screen.try_as<UIElement>();
-  if (!uiElement)
-    return;
-
-  m_children.Append(uiElement);
-  Content(uiElement);
-}
-
-void ScreenContainer::removeAllChildren() {
-  Content(nullptr);
-  m_children.Clear();
-}
-
-void ScreenContainer::removeChildAt(int64_t index) {
-  m_children.RemoveAt(static_cast<uint32_t>(index));
-}
-
-void ScreenContainer::replaceChild(
-    winrt::Windows::UI::Xaml::UIElement oldChild,
-    winrt::Windows::UI::Xaml::UIElement newChild) {
-  uint32_t index;
-  if (!m_children.IndexOf(oldChild, index))
-    return;
-
-  m_children.SetAt(index, newChild);
-}
 } // namespace winrt::RNScreens::implementation
