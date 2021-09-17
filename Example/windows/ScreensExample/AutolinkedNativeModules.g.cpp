155618f2b9f6bd596b12493583c753c83964d0d9
kacperkapusciak <kapusciak.k@gmail.com>
2021-09-17 11:30:17 +0200

    fix: link RNScreens to Example windows

--- Example/windows/ScreensExample/AutolinkedNativeModules.g.cpp
+++ Example/windows/ScreensExample/AutolinkedNativeModules.g.cpp
@@ -3,6 +3,9 @@
 #include "pch.h"
 #include "AutolinkedNativeModules.g.h"
 
+// Includes from react-native-screens
+#include <winrt/RNScreens.h>
+
 // Includes from react-native-webview
 #include <winrt/ReactNativeWebView.h>
 
@@ -11,6 +14,8 @@ namespace winrt::Microsoft::ReactNative
 
 void RegisterAutolinkedNativeModulePackages(winrt::Windows::Foundation::Collections::IVector<winrt::Microsoft::ReactNative::IReactPackageProvider> const& packageProviders)
 { 
+    // IReactPackageProviders from react-native-screens
+    packageProviders.Append(winrt::RNScreens::ReactPackageProvider());
     // IReactPackageProviders from react-native-webview
     packageProviders.Append(winrt::ReactNativeWebView::ReactPackageProvider());
 }
