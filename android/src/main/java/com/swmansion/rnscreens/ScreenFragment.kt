89a7e84f5da5828e35095c151c6f9f9b5c2e8630
WoLewicki <wojciech.lewicki@swmansion.com>
2021-11-19 16:01:45 +0100

    fix: remove unused import

--- android/src/main/java/com/swmansion/rnscreens/ScreenFragment.kt
+++ android/src/main/java/com/swmansion/rnscreens/ScreenFragment.kt
@@ -9,7 +9,6 @@ import android.view.View
 import android.view.ViewGroup
 import android.view.ViewParent
 import android.widget.FrameLayout
-import androidx.coordinatorlayout.widget.CoordinatorLayout
 import androidx.fragment.app.Fragment
 import com.facebook.react.bridge.ReactContext
 import com.facebook.react.bridge.UiThreadUtil
