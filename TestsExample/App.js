3df9e40ee4039c9ff5d2744d3b1b0445d251fd5a
WoLewicki <wojciech.lewicki@swmansion.com>
2022-01-05 16:30:20 +0100

    feat: add test number

--- TestsExample/App.js
+++ TestsExample/App.js
@@ -73,14 +73,14 @@ import Test1209 from './src/Test1209';
 import Test1214 from './src/Test1214';
 import Test1227 from './src/Test1227';
 import Test1228 from './src/Test1228';
-import Test from './src/Test';
+import Test1259 from './src/Test1259';
 
 enableFreeze(true);
 
 export default function App() {
   return (
     <ReanimatedScreenProvider>
-      <Test />
+      <Test42 />
     </ReanimatedScreenProvider>
   );
 }
