a3fdbd1d3c09823e87ce4956c348b58d4b8015e9
WoLewicki <wojciech.lewicki@swmansion.com>
2022-01-05 17:47:25 +0100

    fix: revert changes in 556

--- TestsExample/src/Test556.js
+++ TestsExample/src/Test556.js
@@ -1,14 +1,14 @@
 import * as React from 'react';
 import {Button} from 'react-native';
-import {NavigationContainer, ParamListBase} from '@react-navigation/native';
-import {createNativeStackNavigator, NativeStackNavigationProp} from 'react-native-screens/native-stack';
+import {NavigationContainer} from '@react-navigation/native';
+import {createNativeStackNavigator} from 'react-native-screens/native-stack';
 
 const Stack = createNativeStackNavigator();
 
 export default function App() {
   return (
     <NavigationContainer>
-      <Stack.Navigator screenOptions={{transitionDuration: 2.0, stackAnimation: 'simple_push'}}>
+      <Stack.Navigator>
         <Stack.Screen name="First" component={First} />
         <Stack.Screen
           name="Second"
@@ -19,14 +19,14 @@ export default function App() {
   );
 }
 
-function First({navigation}: {navigation: NativeStackNavigationProp<ParamListBase>}) {
+function First({navigation}) {
   return (
     <Button title="Tap me for second screen" onPress={() => navigation.navigate('Second')} />
 
   );
 }
 
-function Second({navigation}: {navigation: NativeStackNavigationProp<ParamListBase>}) {
+function Second({navigation}) {
   return (
     <Button title="Tap me for second screen" onPress={() => navigation.navigate('First')} />
   );
