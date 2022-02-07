74462ff25e5cd3e8e1d81ce9fda5b6eaaecf4ecf
Ubax <ubax7k@gmail.com>
2022-02-07 10:11:10 +0200

    Improves demo on Android

--- FabricExample/src/HeaderDemo.js
+++ FabricExample/src/HeaderDemo.js
@@ -1,5 +1,5 @@
 import React, {useState, useLayoutEffect} from 'react';
-import {ScrollView, StyleSheet, Text} from 'react-native';
+import {Platform, ScrollView, StyleSheet, Text} from 'react-native';
 import {BLACK, PRIMARY, SECONDARY, WHITE} from './colors';
 import {
   SettingsInput,
@@ -141,7 +141,7 @@ export default function HeaderDemo({navigation}) {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    paddingTop: 30,
+    paddingTop: Platform.OS === 'ios' ? 30 : 100,
     backgroundColor: WHITE,
   },
   heading: {
