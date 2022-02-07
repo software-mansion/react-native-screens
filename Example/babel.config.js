11b9e0affadb996367df49e99db4823d7bd740d8
Ubax <ubax7k@gmail.com>
2022-02-07 15:10:45 +0200

    Adds screens as tgz dependency to example app

--- Example/babel.config.js
+++ Example/babel.config.js
@@ -1,13 +1,13 @@
 module.exports = {
   presets: ['babel-preset-expo'],
-  plugins: [
-    [
-      'module-resolver',
-      {
-        alias: {
-          'react-native-screens': '../src',
-        },
-      },
-    ],
-  ],
+  // plugins: [
+  //   [
+  //     'module-resolver',
+  //     {
+  //       alias: {
+  //         'react-native-screens': '../src',
+  //       },
+  //     },
+  //   ],
+  // ],
 };
