69e63aefe1b97ee43ceda57ea67e1272db062eee
Ubax <ubax7k@gmail.com>
2022-02-11 16:14:49 +0200

    Adds flex to ScreenStack

--- src/fabric/ScreenStack.js
+++ src/fabric/ScreenStack.js
@@ -2,7 +2,9 @@ import * as React from 'react';
 import ScreenStackNativeComponent from './ScreenStackNativeComponent';
 
 const ScreenStack = function (props) {
-  return <ScreenStackNativeComponent {...props} />;
+  return (
+    <ScreenStackNativeComponent {...props} style={[{ flex: 1 }, props.style]} />
+  );
 };
 
 export default ScreenStack;
