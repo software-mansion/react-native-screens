22fd87da42004ce47db0f865801cc20f890758e7
kacperkapusciak <kacper.kapusciak@swmansion.com>
2022-03-01 14:58:58 +0100

    fix: cleanup test

--- src/__tests__/index.native.test.tsx
+++ src/__tests__/index.native.test.tsx
@@ -4,15 +4,17 @@ import { Text } from 'react-native';
 import { render } from '@testing-library/react-native';
 import { Screen, ScreenContainer } from 'react-native-screens';
 
-test('screen renders content', () => {
-  const text = 'Hello, world!';
-  const screen = render(
-    <ScreenContainer>
-      <Screen>
-        <Text>{text}</Text>
-      </Screen>
-    </ScreenContainer>
-  );
-  const textEl = screen.queryByText(text);
-  expect(textEl).toHaveTextContent(text);
+describe('[ScreenContainer]', () => {
+  it('renders a screen with content', () => {
+    const text = 'Hello, world!';
+    const scene = render(
+      <ScreenContainer>
+        <Screen>
+          <Text>{text}</Text>
+        </Screen>
+      </ScreenContainer>
+    );
+    const textEl = scene.queryByText(text);
+    expect(textEl).toHaveTextContent(text);
+  });
 });
