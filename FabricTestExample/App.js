28c42d33e1a64abd1dc5b97d530d2172d35c775e
Kacper Kafara <kacperkafara@gmail.com>
2022-03-25 15:31:00 +0100

    feat: Add Test1072 (fullScreenSwipeEnabled) & update App.js

--- FabricTestExample/App.js
+++ FabricTestExample/App.js
@@ -3,46 +3,10 @@ import {createNativeStackNavigator} from '@react-navigation/native-stack';
 import {NavigationContainer} from '@react-navigation/native';
 import {View, Button, Text} from 'react-native';
 
-const Stack = createNativeStackNavigator();
+import Test1072 from './src/Test1072';
 
-function First({navigation}) {
-  const nav = navigation;
-  return (
-    <View>
-      <Text>First screen</Text>
-      <Button
-        title="Navigate to second"
-        onPress={() => nav.navigate('Second')}
-      />
-    </View>
-  );
-}
-
-function Second() {
-  return (
-    <View>
-      <Text>Second screen</Text>
-    </View>
-  );
-}
-
-const App = () => (
-  <NavigationContainer>
-    <Stack.Navigator
-      screenOptions={{
-        fullScreenGestureEnabled: true,
-      }}>
-      <Stack.Screen
-        name="First"
-        options={{
-          title: 'Fabric Example',
-          headerShown: true,
-        }}
-        component={First}
-      />
-      <Stack.Screen name="Second" component={Second} />
-    </Stack.Navigator>
-  </NavigationContainer>
-);
+const App = () => {
+  return <Test1072 />;
+};
 
 export default App;
