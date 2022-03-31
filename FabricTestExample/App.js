4876e83df1ebd8ff21118bcf9644c0f41d8d3cb7
Kacper Kafara <kacperkafara@gmail.com>
2022-03-31 08:57:32 +0200

    chore: Update App.js

--- FabricTestExample/App.js
+++ FabricTestExample/App.js
@@ -3,10 +3,12 @@ import {createNativeStackNavigator} from '@react-navigation/native-stack';
 import {NavigationContainer} from '@react-navigation/native';
 import {View, Button, Text} from 'react-native';
 
+import Test42 from './src/Test42';
+import Test642 from './src/Test642';
 import Test1072 from './src/Test1072';
 
 const App = () => {
-  return <Test1072 />;
+  return <Test642 />;
 };
 
 export default App;
