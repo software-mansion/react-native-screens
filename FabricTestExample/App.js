145dae6b27c507ec7713956820a6fbd97e6e3e8f
Wojciech Lewicki <wojciech.lewicki@swmansion.com>
2022-03-31 10:54:33 +0200

    chore: bump react-native version

--- FabricTestExample/App.js
+++ FabricTestExample/App.js
@@ -1,7 +1,4 @@
 import React from 'react';
-import {createNativeStackNavigator} from '@react-navigation/native-stack';
-import {NavigationContainer} from '@react-navigation/native';
-import {View, Button, Text} from 'react-native';
 
 import Test42 from './src/Test42';
 import Test642 from './src/Test642';
@@ -9,7 +6,7 @@ import Test780 from './src/Test780';
 import Test1072 from './src/Test1072';
 
 const App = () => {
-  return <Test642 />;
+  return <Test42 />;
 };
 
 export default App;
