8b1f8df5cf44732aaa2518499ff226f9312ed03b
Kacper Kafara <kacperkafara@gmail.com>
2022-04-07 10:02:49 +0200

    refract: Merge RNSScreen & RNSScreenController interfaces

--- ios/RNSScreenController.h
+++ ios/RNSScreenController.h
@@ -1,12 +1,24 @@
 #import <UIKit/UIKit.h>
 #import "RNSScreenContainer.h"
 
+//@interface RNSScreen : UIViewController <RNScreensViewControllerDelegate>
+//
+//- (instancetype)initWithView:(UIView *)view;
+//- (void)notifyFinishTransitioning;
+//- (UIViewController *)findChildVCForConfigAndTrait:(RNSWindowTrait)trait includingModals:(BOOL)includingModals;
+//
+//@end
+
 @interface RNSScreenController : UIViewController <RNScreensViewControllerDelegate>
 
 - (instancetype)initWithView:(UIView *)view;
+- (UIViewController *)findChildVCForConfigAndTrait:(RNSWindowTrait)trait includingModals:(BOOL)includingModals;
+
+#if RN_FABRIC_ENABLED
 - (void)setViewToSnapshot:(UIView *)snapshot;
 - (void)resetViewToScreen;
-
-- (UIViewController *)findChildVCForConfigAndTrait:(RNSWindowTrait)trait includingModals:(BOOL)includingModals;
+#else
+- (void)notifyFinishTransitioning;
+#endif
 
 @end
