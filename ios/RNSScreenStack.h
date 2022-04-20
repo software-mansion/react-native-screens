a7e3012132f294b5fcb4cd06b69a6808c9970047
Kacper Kafara <kacperkafara@gmail.com>
2022-04-20 11:24:05 +0200

    chore: merge ScreenStack headers

--- ios/RNSScreenStack.h
+++ ios/RNSScreenStack.h
@@ -1,5 +1,10 @@
+#ifdef RN_FABRIC_ENABLED
+#import <React/RCTViewComponentView.h>
+#import <UIKit/UIKit.h>
+#else
 #import <React/RCTUIManagerObserverCoordinator.h>
 #import <React/RCTViewManager.h>
+#endif
 
 #import "RNSScreenContainer.h"
 
@@ -7,15 +12,24 @@
 
 @end
 
-@interface RNSScreenStackView : UIView <RNSScreenContainerDelegate, RCTInvalidating>
+@interface RNSScreenStackView :
+#ifdef RN_FABRIC_ENABLED
+    RCTViewComponentView
+#else
+    UIView <RNSScreenContainerDelegate, RCTInvalidating>
+#endif
 
+#ifndef RN_FABRIC_ENABLED
 @property (nonatomic, copy) RCTDirectEventBlock onFinishTransitioning;
 
 - (void)markChildUpdated;
 - (void)didUpdateChildren;
+#endif
 
 @end
 
+#ifndef RN_FABRIC_ENABLED
 @interface RNSScreenStackManager : RCTViewManager <RCTInvalidating>
 
 @end
+#endif
