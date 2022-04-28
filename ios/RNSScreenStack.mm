68435f27ca31c9b9f4452ae2e0efb25155ecc92f
Kacper Kafara <kacperkafara@gmail.com>
2022-04-28 11:02:42 +0200

    refact: linter suggestion

--- ios/RNSScreenStack.mm
+++ ios/RNSScreenStack.mm
@@ -1,11 +1,11 @@
 #ifdef RN_FABRIC_ENABLED
 #import <React/RCTMountingTransactionObserving.h>
+#import <React/RCTSurfaceTouchHandler.h>
 #import <React/UIView+React.h>
 #import <react/renderer/components/rnscreens/ComponentDescriptors.h>
 #import <react/renderer/components/rnscreens/EventEmitters.h>
 #import <react/renderer/components/rnscreens/Props.h>
 #import <react/renderer/components/rnscreens/RCTComponentViewHelpers.h>
-#import <React/RCTSurfaceTouchHandler.h>
 
 #import "RCTFabricComponentsPlugins.h"
 
