af198611918b4b541f45c664c4c880d29fbcb9cf
Kacper Kafara <kacperkafara@gmail.com>
2022-05-07 16:22:49 +0200

    chore: export ScreenContainer from src/fabric

--- src/fabric/index.js
+++ src/fabric/index.js
@@ -2,10 +2,12 @@ import Screen from './Screen';
 import ScreenStackHeaderSubview from './ScreenStackHeaderSubview';
 import ScreenStackHeaderConfig from './ScreenStackHeaderConfigNativeComponent';
 import ScreenStack from './ScreenStack';
+import ScreenContainer from './ScreenContainer';
 
 export {
   Screen,
   ScreenStackHeaderSubview,
   ScreenStackHeaderConfig,
   ScreenStack,
+  ScreenContainer,
 };
