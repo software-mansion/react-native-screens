cad48e87534aaa092d878303a6a270e8fe1947b2
Kacper Kafara <kacperkafara@gmail.com>
2022-05-13 13:52:10 +0200

    chore: unify business logic between archs (8) (#1446)
    * chore: unify RNSScreen#vieDidLayoutSubviews
    
    * chore: make RNSScreenStack#hitTest:withEvent: method shared
    
    https://github.com/software-mansion/react-native-screens/pull/1416#discussion_r856175275
    
    * chore: make RNSScreenStackView#isScrollViewPanGestureRecongnizer & some
    helper methods shared
    
    https://github.com/software-mansion/react-native-screens/pull/1416#discussion_r856301097
    
    * chore: move backButtonInCustomView to shared sectionin
    RNSScreenStackHeaderConfig.h
    
    * chore: move backButtonInCustomView prop to implemented section in JS
    
    * chore: update `backButtonInCustomView` for Fabric
    
    * chore: unify
    RNSScreenStackHeaderConfigView#willShowViewController:animated:withConfig
    
    * refact: linter suggestion
    
    * fix: RNSScreen viewDidLayoutSubviews
    
    * chore: copy interfaces to android/src/paper directory
    
    * fix: commit address in comment
    
    It changed after I rebased

--- android/src/paper/java/com/facebook/react/viewmanagers/RNSScreenManagerInterface.java
+++ android/src/paper/java/com/facebook/react/viewmanagers/RNSScreenManagerInterface.java
@@ -11,9 +11,13 @@ package com.facebook.react.viewmanagers;
 
 import android.view.View;
 import androidx.annotation.Nullable;
+import com.facebook.react.bridge.ReadableMap;
 
 public interface RNSScreenManagerInterface<T extends View> {
+  void setCustomAnimationOnSwipe(T view, boolean value);
   void setFullScreenSwipeEnabled(T view, boolean value);
+  void setHomeIndicatorHidden(T view, boolean value);
+  void setPreventNativeDismiss(T view, boolean value);
   void setGestureEnabled(T view, boolean value);
   void setStatusBarColor(T view, @Nullable Integer value);
   void setStatusBarHidden(T view, boolean value);
@@ -21,12 +25,14 @@ public interface RNSScreenManagerInterface<T extends View> {
   void setStatusBarAnimation(T view, @Nullable String value);
   void setStatusBarStyle(T view, @Nullable String value);
   void setStatusBarTranslucent(T view, boolean value);
+  void setGestureResponseDistance(T view, @Nullable ReadableMap value);
   void setStackPresentation(T view, @Nullable String value);
   void setStackAnimation(T view, @Nullable String value);
   void setTransitionDuration(T view, int value);
   void setReplaceAnimation(T view, @Nullable String value);
+  void setHideKeyboardOnSwipe(T view, boolean value);
+  void setActivityState(T view, int value);
   void setNavigationBarColor(T view, @Nullable Integer value);
   void setNavigationBarHidden(T view, boolean value);
   void setNativeBackButtonDismissalEnabled(T view, boolean value);
-  void setActivityState(T view, int value);
 }
