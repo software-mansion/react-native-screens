473b3c9089127303f51c69f31e90a3f67f8ff8cf
Kacper Kafara <kacperkafara@gmail.com>
2022-06-01 11:54:33 +0200

    chore: add updating of the prop for Fabric

--- ios/RNSScreen.mm
+++ ios/RNSScreen.mm
@@ -471,6 +471,8 @@
 
   [self setActivityStateOrNil:[NSNumber numberWithInt:newScreenProps.activityState]];
   
+  [self setSwipeDirection:[RNSConvert RNSScreenSwipeDirectionFromCppEquivalent:newScreenProps.swipeDirection]];
+
 #if !TARGET_OS_TV
   if (newScreenProps.statusBarHidden != oldScreenProps.statusBarHidden) {
     [self setStatusBarHidden:newScreenProps.statusBarHidden];
@@ -508,7 +510,7 @@
   if (newScreenProps.replaceAnimation != oldScreenProps.replaceAnimation) {
     [self setReplaceAnimation:[RNSConvert RNSScreenReplaceAnimationFromCppEquivalent:newScreenProps.replaceAnimation]];
   }
-  
+
   [super updateProps:props oldProps:oldProps];
 }
 
