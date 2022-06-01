17dd0087196010e567dc0aa54db09a122c07890c
Kacper Kafara <kacperkafara@gmail.com>
2022-06-01 17:25:42 +0200

    refact: rename test case to Test1463

--- FabricTestExample/App.js
+++ FabricTestExample/App.js
@@ -8,10 +8,10 @@ import Test860 from './src/Test860';
 import Test1072 from './src/Test1072';
 import Test1190 from './src/Test1190';
 import Test1419 from './src/Test1419';
-import Test from './src/Test';
+import Test1463 from './src/Test1463';
 
 const App = () => {
-  return <Test />;
+  return <Test1463 />;
 };
 
 export default App;
