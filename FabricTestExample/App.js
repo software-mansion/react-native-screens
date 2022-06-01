adb773656603c90a9154bbb602ad5a2595b7c923
Kacper Kafara <kacperkafara@gmail.com>
2022-06-01 17:28:17 +0200

    refact: cleanup in FabricTesetExample/App.js
    refact: remove not related test files
    
    refact: remove not related test files (followup)
    
    refact: restore TestsExample/App.js

--- FabricTestExample/App.js
+++ FabricTestExample/App.js
@@ -2,12 +2,9 @@ import React from 'react';
 
 import Test42 from './src/Test42';
 import Test642 from './src/Test642';
-// import Test702 from './src/Test702';
 import Test780 from './src/Test780';
 import Test860 from './src/Test860';
 import Test1072 from './src/Test1072';
-import Test1190 from './src/Test1190';
-import Test1419 from './src/Test1419';
 import Test1463 from './src/Test1463';
 
 const App = () => {
