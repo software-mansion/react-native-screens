225db19377db917d8ca7b18eaa4a83c04bbd77a4
Kacper Kafara <kacperkafara@gmail.com>
2022-06-02 10:42:29 +0200

    chore: add event types to codegen

--- src/fabric/SearchBarNativeComponent.js
+++ src/fabric/SearchBarNativeComponent.js
@@ -6,9 +6,20 @@
 import codegenNativeComponent from 'react-native/Libraries/Utilities/codegenNativeComponent';
 import type { ViewProps } from 'react-native/Libraries/Components/View/ViewPropTypes';
 import type { HostComponent } from 'react-native/Libraries/Renderer/shims/ReactNativeTypes';
+import type {
+  WithDefault,
+  BubblingEventHandler,
+} from 'react-native/Libraries/Types/CodegenTypes';
+
+type SearchBarEvent = $ReadOnly<{||}>;
 
 type NativeProps = $ReadOnly<{|
   ...ViewProps,
+  onFocus?: ?BubblingEventHandler<SearchBarEvent>,
+  onBlur?: ?BubblingEventHandler<SearchBarEvent>,
+  onSearchButtonPress?: ?BubblingEventHandler<SearchBarEvent>,
+  onCancelButtonPress?: ?BubblingEventHandler<SearchBarEvent>,
+  onChangeText?: ?BubblingEventHandler<SearchBarEvent>,
 |}>;
 
 type ComponentType = HostComponent<NativeProps>;
