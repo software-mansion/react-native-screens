1c2d41af7d86b229646a3768699dfd2445d0af26
Kacper Kafara <kacperkafara@gmail.com>
2022-06-27 16:26:23 +0200

    chore: add missing import in App.js
    This test was introduced in #1419

--- FabricTestExample/App.js
+++ FabricTestExample/App.js
@@ -10,6 +10,7 @@ import Test780 from './src/Test780';
 import Test860 from './src/Test860';
 import Test887 from './src/Test887';
 import Test1072 from './src/Test1072';
+import Test1153 from './src/Test1153';
 import Test1260 from './src/Test1260';
 import Test1463 from './src/Test1463';
 
