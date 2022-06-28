0433b38b23a553047dc5a24e90ba5350468753c9
Kacper Kafara <kacperkafara@gmail.com>
2022-06-28 14:47:33 +0200

    chore: initial adaptation of RNSFullWindowOverlay implementation
    WIP

--- ios/RNSFullWindowOverlay.mm
+++ ios/RNSFullWindowOverlay.mm
@@ -2,7 +2,11 @@
 
 #import "RNSFullWindowOverlay.h"
 
+#ifdef RN_FABRIC_ENABLED
+#import <React/RCTSurfaceTouchHandler.h>
+#else
 #import <React/RCTTouchHandler.h>
+#endif // RN_FABRIC_ENABLED
 
 @implementation RNSFullWindowOverlayContainer
 
@@ -22,19 +26,36 @@
   __weak RCTBridge *_bridge;
   RNSFullWindowOverlayContainer *_container;
   CGRect _reactFrame;
+#ifdef RN_FABRIC_ENABLED
+  RCTSurfaceTouchHandler *_touchHandler;
+#else
   RCTTouchHandler *_touchHandler;
+#endif // RN_FABRIC_ENABLED
+}
+
+- (instancetype)init
+{
+  if (self = [super init]) {
+    [self _initCommon];
+  }
+  return self;
 }
 
 - (instancetype)initWithBridge:(RCTBridge *)bridge
 {
   if (self = [super init]) {
     _bridge = bridge;
-    _reactFrame = CGRectNull;
-    _container = self.container;
-    [self show];
+    [self _initCommon];
   }
 
   return self;
+}
+
+- (void)_initCommon
+{
+  _reactFrame = CGRectNull;
+  _container = self.container;
+  [self show];
 }
 
 - (void)reactSetFrame:(CGRect)frame
@@ -72,17 +93,29 @@
     }
   } else {
     if (_touchHandler == nil) {
+#ifdef RN_FABRIC_ENABLED
+      _touchHandler = [RCTSurfaceTouchHandler new];
+#else
       _touchHandler = [[RCTTouchHandler alloc] initWithBridge:_bridge];
+#endif
     }
     [_touchHandler attachToView:_container];
   }
 }
+
+#ifdef RN_FABRIC_ENABLED
+#pragma mark - Fabric Specific
+
+#else
+#pragma mark - Paper specific
 
 - (void)invalidate
 {
   [_container removeFromSuperview];
   _container = nil;
 }
+
+#endif // RN_FABRIC_ENABLED
 
 @end
 
