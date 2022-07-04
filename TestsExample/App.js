893ca9750078afbb42c69ada7661456325c919ee
kacperkapusciak <kacper.kapusciak@swmansion.com>
2022-07-04 11:43:29 +0200

    chore: add example
    Co-authored-by: Wojciech Lewicki <wojciech.lewicki@swmansion.com>

--- TestsExample/App.js
+++ TestsExample/App.js
@@ -78,13 +78,14 @@ import Test1260 from './src/Test1260';
 import Test1296 from './src/Test1296';
 import Test1299 from './src/Test1299';
 import Test1419 from './src/Test1419';
+import Test from './src/Test';
 
 enableFreeze(true);
 
 export default function App() {
   return (
     <ReanimatedScreenProvider>
-      <Test42 />
+      <Test />
     </ReanimatedScreenProvider>
   );
 }
