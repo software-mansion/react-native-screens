c3a62ac2c17f5143203b925e8d7eb5bd80e99e81
kacperkapusciak <kacper.kapusciak@swmansion.com>
2022-07-20 10:09:23 +0200

    chore: add reproduction

--- TestsExample/App.js
+++ TestsExample/App.js
@@ -81,6 +81,7 @@ import Test1419 from './src/Test1419';
 import Test1473 from './src/Test1473';
 import Test1476 from './src/Test1476';
 import Test1509 from './src/Test1509';
+import Test1539 from './src/Test1539';
 
 enableFreeze(true);
 
