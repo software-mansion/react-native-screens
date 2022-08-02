c887d58020ba548c1506a06b2a381266cb14a792
Kacper Kafara <kacperkafara@gmail.com>
2022-08-02 10:37:23 +0200

    fix: follow library installation instructions for FabricTestExample

--- FabricTestExample/android/app/src/main/java/com/fabrictestexample/MainActivity.java
+++ FabricTestExample/android/app/src/main/java/com/fabrictestexample/MainActivity.java
@@ -1,10 +1,17 @@
 package com.fabrictestexample;
 
+import android.os.Bundle;
+
 import com.facebook.react.ReactActivity;
 import com.facebook.react.ReactActivityDelegate;
 import com.facebook.react.ReactRootView;
 
 public class MainActivity extends ReactActivity {
+
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(null);
+  }
 
   /**
    * Returns the name of the main component registered from JavaScript. This is used to schedule
