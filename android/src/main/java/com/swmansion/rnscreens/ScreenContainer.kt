9a0eb32bbc06d9d050bc4fd9f940eff5cd08c2fe
Kacper Kafara <kacperkafara@gmail.com>
2022-08-16 13:39:28 +0200

    fix: handle React Native loaded on Fragment (#1553)
    * chore: add method for resolving appropriate fragment manager
    
    * fix: resolve appropriate fragment manager
    
    * refact: change method name
    
    * chore: make return type non nullable
    
    * chore: clarify comments & simplify logic
    
    * chore: refactor code & add checks
    
    * fix: typo
    
    * refact: another typo
    
    * chore: take advantage of FragmentManager#findFragment method
    
    Also add some comments that describe the changes
    
    * chore: add link to the issue
    
    * refact: use checkNotNull in place of explicit comparision against null
    
    * style: Kotlin
    
    * chore: fallback to activity's FM

--- android/src/main/java/com/swmansion/rnscreens/ScreenContainer.kt
+++ android/src/main/java/com/swmansion/rnscreens/ScreenContainer.kt
@@ -135,6 +135,41 @@ open class ScreenContainer<T : ScreenFragment>(context: Context?) : ViewGroup(co
         performUpdatesNow()
     }
 
+    private fun findFragmentManagerForReactRootView(rootView: ReactRootView): FragmentManager {
+        var context = rootView.context
+
+        // ReactRootView is expected to be initialized with the main React Activity as a context but
+        // in case of Expo the activity is wrapped in ContextWrapper and we need to unwrap it
+        while (context !is FragmentActivity && context is ContextWrapper) {
+            context = context.baseContext
+        }
+
+        check(context is FragmentActivity) {
+            "In order to use RNScreens components your app's activity need to extend ReactActivity"
+        }
+
+        // In case React Native is loaded on a Fragment (not directly in activity) we need to find
+        // fragment manager whose fragment's view is ReactRootView. As of now, we detect such case by
+        // checking whether any fragments are attached to activity which hosts ReactRootView.
+        // See: https://github.com/software-mansion/react-native-screens/issues/1506 on why the cases
+        // must be treated separately.
+        return if (context.supportFragmentManager.fragments.isEmpty()) {
+            // We are in standard React Native application w/o custom native navigation based on fragments.
+            context.supportFragmentManager
+        } else {
+            // We are in some custom setup & we want to use the closest fragment manager in hierarchy.
+            // `findFragment` method throws IllegalStateException when it fails to resolve appropriate
+            // fragment. It might happen when e.g. React Native is loaded directly in Activity
+            // but some custom fragments are still used. Such use case seems highly unlikely
+            // so, as for now we fallback to activity's FragmentManager in hope for the best.
+            try {
+                FragmentManager.findFragment<Fragment>(rootView).childFragmentManager
+            } catch (ex: IllegalStateException) {
+                context.supportFragmentManager
+            }
+        }
+    }
+
     private fun setupFragmentManager() {
         var parent: ViewParent = this
         // We traverse view hierarchy up until we find screen parent or a root view
@@ -146,28 +181,22 @@ open class ScreenContainer<T : ScreenFragment>(context: Context?) : ViewGroup(co
         // If parent is of type Screen it means we are inside a nested fragment structure.
         // Otherwise we expect to connect directly with root view and get root fragment manager
         if (parent is Screen) {
-            val screenFragment = parent.fragment
-            check(screenFragment != null) { "Parent Screen does not have its Fragment attached" }
-            mParentScreenFragment = screenFragment
-            screenFragment.registerChildScreenContainer(this)
-            setFragmentManager(screenFragment.childFragmentManager)
-            return
+            checkNotNull(
+                parent.fragment?.let { screenFragment ->
+                    mParentScreenFragment = screenFragment
+                    screenFragment.registerChildScreenContainer(this)
+                    setFragmentManager(screenFragment.childFragmentManager)
+                }
+            ) { "Parent Screen does not have its Fragment attached" }
+        } else {
+            // we expect top level view to be of type ReactRootView, this isn't really necessary but in
+            // order to find root view we test if parent is null. This could potentially happen also when
+            // the view is detached from the hierarchy and that test would not correctly indicate the root
+            // view. So in order to make sure we indeed reached the root we test if it is of a correct type.
+            // This allows us to provide a more descriptive error message for the aforementioned case.
+            check(parent is ReactRootView) { "ScreenContainer is not attached under ReactRootView" }
+            setFragmentManager(findFragmentManagerForReactRootView(parent))
         }
-
-        // we expect top level view to be of type ReactRootView, this isn't really necessary but in
-        // order to find root view we test if parent is null. This could potentially happen also when
-        // the view is detached from the hierarchy and that test would not correctly indicate the root
-        // view. So in order to make sure we indeed reached the root we test if it is of a correct type.
-        // This allows us to provide a more descriptive error message for the aforementioned case.
-        check(parent is ReactRootView) { "ScreenContainer is not attached under ReactRootView" }
-        // ReactRootView is expected to be initialized with the main React Activity as a context but
-        // in case of Expo the activity is wrapped in ContextWrapper and we need to unwrap it
-        var context = parent.context
-        while (context !is FragmentActivity && context is ContextWrapper) {
-            context = context.baseContext
-        }
-        check(context is FragmentActivity) { "In order to use RNScreens components your app's activity need to extend ReactActivity" }
-        setFragmentManager(context.supportFragmentManager)
     }
 
     protected fun createTransaction(): FragmentTransaction {
