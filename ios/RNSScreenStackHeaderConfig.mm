b16fceabfbea8c474fddffde2f009f10e123fabb
Kacper Kafara <kacperkafara@gmail.com>
2022-08-17 10:58:23 +0200

    fix: invalid header background colour  (#1560)
    * chore: add reproduction to TestsExample
    
    * fix: add checks for null value
    
    * style: remove reduntant comments
    
    * chore: add Test1391 to FabricTestExample
    
    * chore: simplify condition for isPresentingVC

--- ios/RNSScreenStackHeaderConfig.mm
+++ ios/RNSScreenStackHeaderConfig.mm
@@ -118,7 +118,7 @@
   }
 
   // we want updates sent to the VC below modal too since it is also visible
-  BOOL isPresentingVC = vc.presentedViewController == nextVC;
+  BOOL isPresentingVC = nextVC != nil && vc.presentedViewController == nextVC;
 
   BOOL isInFullScreenModal = nav == nil && _screenView.stackPresentation == RNSScreenStackPresentationFullScreenModal;
   // if nav is nil, it means we can be in a fullScreen modal, so there is no nextVC, but we still want to update
