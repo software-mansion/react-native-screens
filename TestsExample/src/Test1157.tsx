b9f3d3ea7c28d18afb14ecfb5911db63e7ce83cd
Kacper Kafara <kacperkafara@gmail.com>
2022-09-10 10:24:52 +0200

    style: linter suggestions in Test1157

--- TestsExample/src/Test1157.tsx
+++ TestsExample/src/Test1157.tsx
@@ -1,7 +1,12 @@
+/* eslint-disable react/display-name */
+/* eslint-disable react-native/no-inline-styles */
 import * as React from 'react';
 import {Button, View, TouchableOpacity} from 'react-native';
 import {NavigationContainer, ParamListBase} from '@react-navigation/native';
-import {createNativeStackNavigator, NativeStackNavigationProp} from 'react-native-screens/native-stack';
+import {
+  createNativeStackNavigator,
+  NativeStackNavigationProp,
+} from 'react-native-screens/native-stack';
 
 function First({
   navigation,
@@ -25,28 +30,59 @@ function Second({
 }) {
   return (
     <View style={{flex: 1, backgroundColor: 'yellow'}}>
-      <Button title="Tap me for first screen" onPress={() => navigation.goBack()} />
+      <Button
+        title="Tap me for first screen"
+        onPress={() => navigation.goBack()}
+      />
     </View>
   );
 }
 
+console.log("Test1157 module")
+
 const Stack = createNativeStackNavigator();
 
 const ButtonWithBiggerChild = () => (
-  <TouchableOpacity style={{width: 30, height: 30, backgroundColor: 'red'}} onPress={() => console.log("hello")}>
-      <View style={{width: 30, height: 30, backgroundColor: 'yellow', marginLeft: -15}}/>
+  <TouchableOpacity
+    style={{width: 30, height: 30, backgroundColor: 'red'}}
+    onPress={() => console.log('hello')}>
+    <View
+      style={{
+        width: 30,
+        height: 30,
+        backgroundColor: 'yellow',
+        marginLeft: -15,
+      }}
+    />
   </TouchableOpacity>
 );
 
 export default function App() {
   return (
     <NavigationContainer>
-      <Stack.Navigator screenOptions={{stackPresentation: 'modal'}}>
-        <Stack.Screen name="First" component={First} 
-          options={{headerShown: true,
+      <Stack.Navigator screenOptions={{
+        stackPresentation: 'modal',
+        backButtonImage: require("../assets/backButton.png"),
+        // headerBackTitleVisible: false
+        }}>
+        <Stack.Screen
+          name="First"
+          component={First}
+          options={{
+            // headerShown: true,
             // headerLeft: () => <TouchableOpacity onPress={() => console.log("hello")} style={{width: 30, height: 30, backgroundColor: 'red', marginLeft: -15}}/>,
-            headerLeft: () => <ButtonWithBiggerChild />,
-            headerRight: () => <TouchableOpacity onPress={() => console.log("there")} style={{width: 30, height: 30, backgroundColor: 'red', marginRight: -15}}/>,
+            // backButtonImage: require('../assets/backButton.png'),
+            // headerLeft: () => <ButtonWithBiggerChild />,
+            headerRight: () => (
+              <TouchableOpacity
+                onPress={() => console.log('there')}
+                style={{
+                  width: 30,
+                  height: 30,
+                  backgroundColor: 'red',
+                  marginRight: -15,
+                }}
+              />)
             }}
         />
         <Stack.Screen
