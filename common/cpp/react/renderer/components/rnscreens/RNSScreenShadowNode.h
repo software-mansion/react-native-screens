882f032fb9bd68411a41a32f75af48e14b4afb62
Kacper Kafara <kacperkafara@gmail.com>
2022-09-12 11:56:00 +0200

    chore: add JSI_EXPORT macros to shadow node decl

--- common/cpp/react/renderer/components/rnscreens/RNSScreenShadowNode.h
+++ common/cpp/react/renderer/components/rnscreens/RNSScreenShadowNode.h
@@ -4,13 +4,14 @@
 #include <react/renderer/components/rnscreens/EventEmitters.h>
 #include <react/renderer/components/rnscreens/Props.h>
 #include <react/renderer/components/view/ConcreteViewShadowNode.h>
+#include <jsi/jsi.h>
 
 namespace facebook {
 namespace react {
 
-extern const char RNSScreenComponentName[];
+JSI_EXPORT extern const char RNSScreenComponentName[];
 
-class RNSScreenShadowNode final : public ConcreteViewShadowNode<
+class JSI_EXPORT RNSScreenShadowNode final : public ConcreteViewShadowNode<
                                           RNSScreenComponentName,
                                           RNSScreenProps,
                                           RNSScreenEventEmitter,
