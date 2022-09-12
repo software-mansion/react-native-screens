0ccc2eaeb22e8bc76b3783f8d91b91c89e03b312
Kacper Kafara <kacperkafara@gmail.com>
2022-09-12 14:12:36 +0200

    fix: add all components descriptors & specify cmakeListsPath
    Setting `componentDescriptors` causes RNCLI to not search for
    autogenerated ones -> we need to specify all of them manually

--- react-native.config.js
+++ react-native.config.js
@@ -2,8 +2,18 @@ module.exports = {
   dependency: {
     platforms: {
       android: {
-        componentDescriptors: ['RNSScreenComponentDescriptor'],
-        androidMkPath: "common/cpp/Android.mk"
+        componentDescriptors: [
+          "RNSFullWindowOverlayComponentDescriptor",
+          "RNSScreenContainerComponentDescriptor",
+          "RNSScreenNavigationContainerComponentDescriptor",
+          "RNSScreenStackHeaderConfigComponentDescriptor",
+          "RNSScreenStackHeaderSubviewComponentDescriptor",
+          "RNSScreenStackComponentDescriptor",
+          "RNSSearchBarComponentDescriptor",
+          'RNSScreenComponentDescriptor'
+        ],
+        androidMkPath: "../common/cpp/Android.mk",
+        cmakeListsPath: "../common/cpp/CMakeLists.txt"
       },
     },
   },
