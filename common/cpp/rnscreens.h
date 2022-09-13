de99905d8b4eec2d95af906fb6d925bb31636cfc
Kacper Kafara <kacperkafara@gmail.com>
2022-09-13 22:55:48 +0200

    chore: make main project use custom rnscreens.h file

--- common/cpp/rnscreens.h
+++ common/cpp/rnscreens.h
@@ -12,11 +12,10 @@
 #include <ReactCommon/JavaTurboModule.h>
 #include <ReactCommon/TurboModule.h>
 #include <jsi/jsi.h>
+#include "react/renderer/components/rnscreens/RNSScreenComponentDescriptor.h"
 
 namespace facebook {
 namespace react {
-
-
 
 JSI_EXPORT
 std::shared_ptr<TurboModule> rnscreens_ModuleProvider(const std::string &moduleName, const JavaTurboModule::InitParams &params);
