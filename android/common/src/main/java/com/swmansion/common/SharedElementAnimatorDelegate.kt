59ff2c72bc6ecd6d0581158f9705c81089326862
Krzysztof Piaskowy <krzysztof.piaskowy@swmansion.com>
2022-09-16 17:00:34 +0200

    Update gradle

--- android/common/src/main/java/com/swmansion/common/SharedElementAnimatorDelegate.kt
+++ android/common/src/main/java/com/swmansion/common/SharedElementAnimatorDelegate.kt
@@ -1,5 +1,8 @@
 package com.swmansion.common
 
+import android.view.View
+
 interface SharedElementAnimatorDelegate {
     fun testMethod(number: Int)
+    fun runTransition(view1: View, view2: View)
 }
--- android/src/main/java/com/swmansion/rnscreens/Screen.kt
+++ android/src/main/java/com/swmansion/rnscreens/Screen.kt
@@ -5,6 +5,7 @@ import android.content.pm.ActivityInfo
 import android.graphics.Paint
 import android.os.Parcelable
 import android.util.SparseArray
+import android.view.View
 import android.view.ViewGroup
 import android.view.WindowManager
 import android.webkit.WebView
