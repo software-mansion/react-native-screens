0585cdac0480f52262ee2f4ad4bf276d0cedb485
Kacper Kafara <kacperkafara@gmail.com>
2022-09-26 15:43:31 +0200

    chore: add search bar imp api test screen

--- TestsExample/src/Test1097.tsx
+++ TestsExample/src/Test1097.tsx
@@ -1,5 +1,6 @@
+/* eslint-disable @typescript-eslint/ban-ts-comment */
 import * as React from 'react';
-import {Button, NativeSyntheticEvent, ScrollView} from 'react-native';
+import {Button, NativeSyntheticEvent, ScrollView, View} from 'react-native';
 import {
   NavigationContainer,
   NavigationProp,
@@ -23,6 +24,7 @@ export default function App(): JSX.Element {
         }}>
         <AppStack.Screen name="First" component={First} />
         <AppStack.Screen name="Second" component={Second} />
+        <AppStack.Screen name="Third" component={Third} />
       </AppStack.Navigator>
     </NavigationContainer>
   );
@@ -81,6 +83,10 @@ function First({navigation}: NativeStackScreenProps<ParamListBase>) {
         onPress={() => navigation.navigate('Second')}
       />
       <Button
+        title="Tap me for third screen"
+        onPress={() => navigation.navigate('Third')}
+      />
+      <Button
         title="Tap me for ref"
         onPress={() => (searchBarRef.current as any).focus()}
       />
@@ -108,6 +114,68 @@ function Second({navigation}: {navigation: NavigationProp<ParamListBase>}) {
         title="Tap me for first screen"
         onPress={() => navigation.navigate('First')}
       />
+      <Button
+        title="Tap me for third screen"
+        onPress={() => navigation.navigate('Third')}
+      />
     </ScrollView>
   );
+}
+
+function Third({navigation}: {navigation: NavigationProp<ParamListBase>}) {
+  const searchBarRef = React.useRef();
+
+  const searchBarProps: SearchBarProps = {
+    // @ts-ignore
+    ref: searchBarRef,
+    barTintColor: 'powderblue',
+    hideWhenScrolling: true,
+    obscureBackground: false,
+    hideNavigationBar: false,
+    autoCapitalize: 'sentences',
+    placeholder: 'Some text',
+    onChangeText: (e: NativeSyntheticEvent<{text: string}>) => console.warn(`Text changed to ${e.nativeEvent.text}`),
+    onCancelButtonPress: () => console.warn('Cancel button pressed'),
+    onSearchButtonPress: () => console.warn('Search button pressed'),
+    onFocus: () => console.warn('onFocus event'),
+    onBlur: () => console.warn('onBlur event'),
+  }
+
+  React.useEffect(() => {
+    navigation.setOptions({
+      searchBar: searchBarProps
+    })
+  }, [navigation]);
+
+
+  return (
+    <ScrollView
+      contentInsetAdjustmentBehavior="automatic"
+      keyboardDismissMode="on-drag">
+      <Button
+        title="Tap me for the first screen"
+        onPress={() => navigation.navigate('First')}
+      />
+      <Button
+        title="Focus search bar"
+        onPress={() => (searchBarRef.current as any).focus()}
+      />
+      <Button 
+        title="Remove focus from search bar"
+        onPress={() => (searchBarRef.current as any).blur()}
+      />
+      <Button
+        title="Clear text in search bar"
+        onPress={() => (searchBarRef.current as any).clearText()}
+      />
+      <Button
+        title="Show cancel button"
+        onPress={() => (searchBarRef.current as any).toggleCancelButton(true)}
+      />
+      <Button
+        title="Hide cancel button"
+        onPress={() => (searchBarRef.current as any).toggleCancelButton(false)}
+      />
+    </ScrollView>
+  )
 }
