22372776ad9b5a3caf80960d16c34d425f2976ac
Kacper Kafara <kacperkafara@gmail.com>
2022-09-27 10:03:34 +0200

    chore: TS for Screen

--- src/fabric/ScreenNativeComponent.ts
+++ src/fabric/ScreenNativeComponent.ts
@@ -1,36 +1,31 @@
-/**
- * @flow strict-local
- * @format
- */
-/* eslint-disable */
 import codegenNativeComponent from 'react-native/Libraries/Utilities/codegenNativeComponent';
-import type { ViewProps } from 'react-native/Libraries/Components/View/ViewPropTypes';
-import type { HostComponent } from 'react-native/Libraries/Renderer/shims/ReactNativeTypes';
-import type { ColorValue } from 'react-native/Libraries/StyleSheet/StyleSheet';
+import type { ViewProps, ColorValue } from 'react-native';
 import type {
   BubblingEventHandler,
   WithDefault,
   Int32,
+  Float,
+  Double,
 } from 'react-native/Libraries/Types/CodegenTypes';
 
-type ScreenEvent = $ReadOnly<{||}>;
+type ScreenEvent = Readonly<{}>;
 
-type ScreenDismissedEvent = $ReadOnly<{|
+type ScreenDismissedEvent = Readonly<{
   dismissCount: Int32,
-|}>;
+}>;
 
-type TransitionProgressEvent = $ReadOnly<{|
+type TransitionProgressEvent = Readonly<{
   progress: Double,
   closing: Int32,
   goingForward: Int32,
-|}>;
+}>;
 
-type GestureResponseDistanceType = $ReadOnly<{|
+type GestureResponseDistanceType = Readonly<{
   start: Float,
   end: Float,
   top: Float,
   bottom: Float,
-|}>;
+}>;
 
 type StackPresentation =
   | 'push'
@@ -58,15 +53,15 @@ type ReplaceAnimation = 'pop' | 'push';
 
 type SheetDetentTypes = 'large' | 'medium' | 'all';
 
-export type NativeProps = $ReadOnly<{|
-  ...ViewProps,
-  onAppear?: ?BubblingEventHandler<ScreenEvent>,
-  onDisappear?: ?BubblingEventHandler<ScreenEvent>,
-  onDismissed?: ?BubblingEventHandler<ScreenDismissedEvent>,
-  onNativeDismissCancelled?: ?BubblingEventHandler<ScreenDismissedEvent>,
-  onWillAppear?: ?BubblingEventHandler<ScreenEvent>,
-  onWillDisappear?: ?BubblingEventHandler<ScreenEvent>,
-  onTransitionProgress?: ?BubblingEventHandler<TransitionProgressEvent>,
+
+export interface NativeProps extends ViewProps {
+  onAppear?: BubblingEventHandler<ScreenEvent>,
+  onDisappear?: BubblingEventHandler<ScreenEvent>,
+  onDismissed?: BubblingEventHandler<ScreenDismissedEvent>,
+  onNativeDismissCancelled?: BubblingEventHandler<ScreenDismissedEvent>,
+  onWillAppear?: BubblingEventHandler<ScreenEvent>,
+  onWillDisappear?: BubblingEventHandler<ScreenEvent>,
+  onTransitionProgress?: BubblingEventHandler<TransitionProgressEvent>,
   sheetAllowedDetents?: WithDefault<SheetDetentTypes, 'large'>,
   sheetLargestUndimmedDetent?: WithDefault<SheetDetentTypes, 'all'>,
   sheetGrabberVisible?: WithDefault<boolean, false>,
@@ -94,11 +89,9 @@ export type NativeProps = $ReadOnly<{|
   navigationBarColor?: ColorValue,
   navigationBarHidden?: boolean,
   nativeBackButtonDismissalEnabled?: boolean,
-  onHeaderBackButtonClicked?: ?BubblingEventHandler<ScreenEvent>,
-|}>;
+  onHeaderBackButtonClicked?: BubblingEventHandler<ScreenEvent>,
+};
 
-type ComponentType = HostComponent<NativeProps>;
-
-export default (codegenNativeComponent<NativeProps>('RNSScreen', {
+export default codegenNativeComponent<NativeProps>('RNSScreen', {
   interfaceOnly: true,
-}): ComponentType);
+});
