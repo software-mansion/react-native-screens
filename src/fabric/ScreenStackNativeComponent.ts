cea4e7adc035f4a0f28d2317d459cb818f6bb2ec
Kacper Kafara <kacperkafara@gmail.com>
2022-09-27 09:44:47 +0200

    chore: TS for ScreenStackNativeComponent

--- src/fabric/ScreenStackNativeComponent.ts
+++ src/fabric/ScreenStackNativeComponent.ts
@@ -1,23 +1,11 @@
-/**
- * @flow strict-local
- * @format
- */
-/* eslint-disable */
 import codegenNativeComponent from 'react-native/Libraries/Utilities/codegenNativeComponent';
-import type { ViewProps } from 'react-native/Libraries/Components/View/ViewPropTypes';
-import type { HostComponent } from 'react-native/Libraries/Renderer/shims/ReactNativeTypes';
+import type { ViewProps } from 'react-native';
 import type { DirectEventHandler } from 'react-native/Libraries/Types/CodegenTypes';
 
-type FinishTransitioningEvent = $ReadOnly<{||}>;
+type FinishTransitioningEvent = Readonly<{}>;
 
-type NativeProps = $ReadOnly<{|
-  ...ViewProps,
-  onFinishTransitioning?: ?DirectEventHandler<FinishTransitioningEvent>,
-|}>;
+interface NativeProps extends ViewProps {
+  onFinishTransitioning?: DirectEventHandler<FinishTransitioningEvent>,
+}
 
-type ComponentType = HostComponent<NativeProps>;
-
-export default (codegenNativeComponent<NativeProps>(
-  'RNSScreenStack',
-  {}
-): ComponentType);
+export default codegenNativeComponent<NativeProps>('RNSScreenStack', {});
