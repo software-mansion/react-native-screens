51da830bed703beb1eeaad57e5de3516adff0270
Kacper Kafara <kacper.kafara@swmansion.com>
2022-10-24 13:40:26 +0200

    fix: FabricTestExample fails to start due to new `react-native.config.js` (#1624)
    ## Description
    
    #1615 introduced a check for RNCLI version inside library, to avoid
    crashes when client project uses version < 9.0.0, however due to [how
    node resolution algorithm
    works](https://nodejs.org/api/modules.html#loading-from-node_modules-folders),
    when launching example Fabric applications [the
    line](https://github.com/software-mansion/react-native-screens/blob/49cc61e1fa9926a671d32bbb634a2c43c999e122/react-native.config.js#L4)]:
    
    ```js
    require('@react-native-community/cli-platform-android/package.json').version;
    ```
    
    resolved version from library's `node_modules` instead of that of
    application. This to our custom component descriptors not being
    registered, as `react-native-screens` still depends on
    `react-native@0.63`, thus resolved RNCLI version is 4.x.x
    
    I've tried writing custom module resolver for metro, but it is bypassed
    as the `react-native.config.js` [is evaluated directly via
    `node`](https://github.com/react-native-community/cli/blob/aed2572ccf03552d5ad82e104682f86291255f8a/packages/cli-platform-android/native_modules.gradle#L425-L451)
    - I was not able to intercept this `require` call in custom metro module
    resolver.
    
    Hence I decided to add direct **`devDependency`** (it should not impact
    client applications) and override any other version by using [yarn
    "resolutions"
    mechanism](https://classic.yarnpkg.com/lang/en/docs/selective-version-resolutions/).
    
    ## Changes
    
    Added direct dev dependency on:
    
    ```json
    "@react-native-community/cli": "^9.0.0",
    "@react-native-community/cli-platform-android": "^9.0.0",
    "@react-native-community/cli-platform-ios": "^9.0.0",
    ```
    
    and overrided any other version with resolutions mechanism.
    
    
    ## Test code and steps to reproduce
    
    Build & run `FabricTestExample`.
    
    I tested the change on `FabricTestExample` & fresh RN applications
    (0.68, 0.69, 0.70)
    
    ## Checklist
    
    - [ ] Ensured that CI passes

--- FabricTestExample/metro.config.js
+++ FabricTestExample/metro.config.js
@@ -27,8 +27,8 @@ module.exports = {
     blacklistRE: exclusionList(
       modules.map(
         (m) =>
-          new RegExp(`^${escape(path.join(root, 'node_modules', m))}\\/.*$`)
-      )
+          new RegExp(`^${escape(path.join(root, 'node_modules', m))}\\/.*$`),
+      ),
     ),
 
     extraNodeModules: modules.reduce((acc, name) => {
