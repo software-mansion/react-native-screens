99cfa245abaaad98c1bd808af1869be2ed076536
Kacper Kafara <kacperkafara@gmail.com>
2023-02-01 18:02:56 +0100

    Run whole test suite

--- Example/e2e/jest.config.js
+++ Example/e2e/jest.config.js
@@ -1,8 +1,7 @@
 /** @type {import('@jest/types').Config.InitialOptions} */
 module.exports = {
   rootDir: '..',
-  // testMatch: ['<rootDir>/e2e/**/*.e2e.ts'],
-  testMatch: ['<rootDir>/e2e/examplesTests/events.e2e.ts'],
+  testMatch: ['<rootDir>/e2e/**/*.e2e.ts'],
   testTimeout: 120000,
   maxWorkers: 1,
   globalSetup: 'detox/runners/jest/globalSetup',
