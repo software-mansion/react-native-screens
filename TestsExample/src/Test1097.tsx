af621f8e359a24b8b36bae152f39b2082751b28f
Kacper Kafara <kacperkafara@gmail.com>
2023-02-20 15:16:26 +0100

    Cleanup examples

--- TestsExample/src/Test1097.tsx
+++ TestsExample/src/Test1097.tsx
@@ -1,6 +1,6 @@
 /* eslint-disable @typescript-eslint/ban-ts-comment */
 import * as React from 'react';
-import {Button, NativeSyntheticEvent, ScrollView, View} from 'react-native';
+import {Button, NativeSyntheticEvent, ScrollView} from 'react-native';
 import {
   NavigationContainer,
   NavigationProp,
@@ -10,7 +10,7 @@ import {
   createNativeStackNavigator,
   NativeStackScreenProps,
 } from 'react-native-screens/native-stack';
-import {SearchBarProps} from 'react-native-screens';
+import {SearchBarProps, SearchBarRef} from 'react-native-screens';
 
 const AppStack = createNativeStackNavigator();
 
@@ -31,7 +31,7 @@ export default function App(): JSX.Element {
 }
 
 function First({navigation}: NativeStackScreenProps<ParamListBase>) {
-  const searchBarRef = React.useRef();
+  const searchBarRef = React.useRef<SearchBarRef>();
 
   React.useEffect(() => {
     navigation.setOptions({
@@ -88,7 +88,7 @@ function First({navigation}: NativeStackScreenProps<ParamListBase>) {
       />
       <Button
         title="Tap me for ref"
-        onPress={() => (searchBarRef.current as any).focus()}
+        onPress={() => searchBarRef.current?.focus()}
       />
       {items
         .filter(
@@ -113,7 +113,7 @@ function First({navigation}: NativeStackScreenProps<ParamListBase>) {
       />
       <Button
         title="Tap me for ref"
-        onPress={() => (searchBarRef.current as any).focus()}
+        onPress={() => searchBarRef.current?.focus()}
       />
     </ScrollView>
   );
@@ -135,7 +135,7 @@ function Second({navigation}: {navigation: NavigationProp<ParamListBase>}) {
 }
 
 function Third({navigation}: {navigation: NavigationProp<ParamListBase>}) {
-  const searchBarRef = React.useRef();
+  const searchBarRef = React.useRef<SearchBarRef>();
 
   const searchBarProps: SearchBarProps = {
     // @ts-ignore
@@ -170,23 +170,23 @@ function Third({navigation}: {navigation: NavigationProp<ParamListBase>}) {
       />
       <Button
         title="Focus search bar"
-        onPress={() => (searchBarRef.current as any).focus()}
+        onPress={() => searchBarRef.current?.focus()}
       />
       <Button 
         title="Remove focus from search bar"
-        onPress={() => (searchBarRef.current as any).blur()}
+        onPress={() => searchBarRef.current?.blur()}
       />
       <Button
         title="Clear text in search bar"
-        onPress={() => (searchBarRef.current as any).clearText()}
+        onPress={() => searchBarRef.current?.clearText()}
       />
       <Button
         title="Show cancel button"
-        onPress={() => (searchBarRef.current as any).toggleCancelButton(true)}
+        onPress={() => searchBarRef.current?.toggleCancelButton(true)}
       />
       <Button
         title="Hide cancel button"
-        onPress={() => (searchBarRef.current as any).toggleCancelButton(false)}
+        onPress={() => searchBarRef.current?.toggleCancelButton(false)}
       />
       <Button
         title="Tap me for the first screen"
@@ -194,7 +194,7 @@ function Third({navigation}: {navigation: NavigationProp<ParamListBase>}) {
       />
       <Button
         title="Focus search bar"
-        onPress={() => (searchBarRef.current as any).focus()}
+        onPress={() => searchBarRef.current?.focus()}
       />
     </ScrollView>
   )
