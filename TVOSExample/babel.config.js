801210f644856e954e53d81c909152149e722929
Kacper Kafara <kacperkafara@gmail.com>
2023-02-21 16:06:44 +0100

    Change way of linking screens in TVOSExample & udpate deps & reduce vulnerabilities

--- TVOSExample/babel.config.js
+++ TVOSExample/babel.config.js
@@ -1,14 +1,4 @@
 module.exports = {
-  presets: ['babel-preset-expo'],
-  plugins: [
-    [
-      'module-resolver',
-      {
-        alias: {
-          'react-native-screens': '../src',
-        },
-      },
-    ],
-    'react-native-reanimated/plugin',
-  ],
+  presets: ['module:metro-react-native-babel-preset'],
+  plugins: [],
 };
