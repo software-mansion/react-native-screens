21fd1d07c4c5e0999c7b8f92908251e5341848fc
Kacper Kafara <kacperkafara@gmail.com>
2023-03-13 12:28:05 +0100

    Override onCreate method in MainActivity in example applications
    This override was accidentaly removed during bumping examples to RN 0.71
    (in some cases it was never added, but it should be)

--- FabricTestExample/android/app/src/main/java/com/fabrictestexample/MainActivity.java
+++ FabricTestExample/android/app/src/main/java/com/fabrictestexample/MainActivity.java
@@ -1,5 +1,6 @@
 package com.fabrictestexample;
 
+import android.os.Bundle;
 import com.facebook.react.ReactActivity;
 import com.facebook.react.ReactActivityDelegate;
 import com.facebook.react.defaults.DefaultNewArchitectureEntryPoint;
@@ -14,6 +15,11 @@ public class MainActivity extends ReactActivity {
   @Override
   protected String getMainComponentName() {
     return "FabricTestExample";
+  }
+
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(null);
   }
 
   /**
--- TestsExample/android/app/src/main/java/com/testsexample/MainActivity.java
+++ TestsExample/android/app/src/main/java/com/testsexample/MainActivity.java
@@ -1,5 +1,6 @@
 package com.testsexample;
 
+import android.os.Bundle;
 import com.facebook.react.ReactActivity;
 import com.facebook.react.ReactActivityDelegate;
 import com.facebook.react.defaults.DefaultNewArchitectureEntryPoint;
@@ -14,6 +15,11 @@ public class MainActivity extends ReactActivity {
   @Override
   protected String getMainComponentName() {
     return "TestsExample";
+  }
+
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(null);
   }
 
   /**
