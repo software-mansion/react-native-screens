c043a6844e6cdad4e4a8842ad80f705b6c506f43
Wojciech Lewicki <wojciech.lewicki@swmansion.com>
2023-06-14 12:29:29 +0200

    fix: proper handling of header events on Fabric and bumping examples to 0.72 (#1783)
    - Fixing the way header events are dispatched in order to make it work on Fabric. Used the implementation analogous to how it is done for Screen events.
    - Bumping examples to RN 0.72.0-rc.5

--- FabricTestExample/android/app/src/main/java/com/fabrictestexample/MainActivity.java
+++ FabricTestExample/android/app/src/main/java/com/fabrictestexample/MainActivity.java
@@ -1,6 +1,5 @@
 package com.fabrictestexample;
 
-import android.os.Bundle;
 import com.facebook.react.ReactActivity;
 import com.facebook.react.ReactActivityDelegate;
 import com.facebook.react.defaults.DefaultNewArchitectureEntryPoint;
@@ -17,11 +16,6 @@ public class MainActivity extends ReactActivity {
     return "FabricTestExample";
   }
 
-  @Override
-  protected void onCreate(Bundle savedInstanceState) {
-    super.onCreate(null);
-  }
-
   /**
    * Returns the instance of the {@link ReactActivityDelegate}. Here we use a util class {@link
    * DefaultReactActivityDelegate} which allows you to easily enable Fabric and Concurrent React
@@ -33,9 +27,6 @@ public class MainActivity extends ReactActivity {
         this,
         getMainComponentName(),
         // If you opted-in for the New Architecture, we enable the Fabric Renderer.
-        DefaultNewArchitectureEntryPoint.getFabricEnabled(), // fabricEnabled
-        // If you opted-in for the New Architecture, we enable Concurrent React (i.e. React 18).
-        DefaultNewArchitectureEntryPoint.getConcurrentReactEnabled() // concurrentRootEnabled
-        );
+        DefaultNewArchitectureEntryPoint.getFabricEnabled());
   }
 }
