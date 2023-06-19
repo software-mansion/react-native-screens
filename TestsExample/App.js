software-mansion/react-native-screens

commit e871092d3ddf0b34ccc89efa988fe78fcbb79a1a

fix: fix back button menu for headerBackTitleVisible prop (#1646)
## Description

In the native-stack, there's a `headerBackTitleVisible` prop which
allows hiding the back button title while keeping the default `<` arrow.
It achieves it by passing [an empty string to the `backTitle` header
prop](https://github.com/react-navigation/react-navigation/blob/%40react-navigation/native-stack%406.9.2/packages/native-stack/src/views/HeaderConfig.tsx#L171).

Although it was a proper way of hiding the back button title prior to
iOS 14 it has the negative effect of hiding all elements in the back
button menu in >=iOS 14.

This can be fixed by a relatively new `backButtonDisplayMode` API by
setting its value to `minimal`.

Fixes react-navigation/react-navigation/issues/11015

## Screenshots / GIFs

### Before


https://user-images.githubusercontent.com/39658211/203362522-29afafe9-033e-4214-bfd1-9897b10941c1.mov


### After


https://user-images.githubusercontent.com/39658211/203359936-020bf4db-dede-4e9c-8c57-c042f1880711.mov

### No effect on iOS 13 (still uses an empty string as there's no back
button menu available)


https://user-images.githubusercontent.com/39658211/203523164-6508ec6b-662b-45ce-9df0-46e81d4c900d.mov




## Test code and steps to reproduce

TestsExample/Test1646.tsx

<details>
<summary>Code example</summary>
<br>

```tsx
import {NavigationContainer, useRoute} from '@react-navigation/native';
import {createNativeStackNavigator} from '@react-navigation/native-stack';
import * as React from 'react';
import {Button, View} from 'react-native';

const Stack = createNativeStackNavigator();

export default function App() {
  return (
    <NavigationContainer>
      <Stack.Navigator
        screenOptions={{
          headerBackTitleVisible: false,
        }}>
        <Stack.Screen
          name="Screen"
          component={Screen}

TestsExample/App.js
@@ -82,6 +82,7 @@ import Test1473 from './src/Test1473';
 import Test1476 from './src/Test1476';
 import Test1509 from './src/Test1509';
 import Test1539 from './src/Test1539';
+import Test1646 from './src/Test1646';
 
 enableFreeze(true);
 

FabricTestExample/App.js
@@ -80,6 +80,7 @@ import Test1391 from './src/Test1391';
 import Test1419 from './src/Test1419';
 import Test1473 from './src/Test1473';
 import Test1476 from './src/Test1476';
+import Test1646 from './src/Test1646';
 
 enableFreeze(true);
 

FabricTestExample/src/Test1646.tsx
@@ -1,0 +1,46 @@
+import {NavigationContainer, useRoute} from '@react-navigation/native';
+import {createNativeStackNavigator} from '@react-navigation/native-stack';
+import * as React from 'react';
+import {Button, View} from 'react-native';
+
+const Stack = createNativeStackNavigator();
+
+export default function App() {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator
+        screenOptions={{
+          headerBackTitleVisible: false,
+        }}>
+        <Stack.Screen
+          name="Screen"
+          component={Screen}
+          options={({route}: any) => ({title: route.params?.title ?? 'Hello'})}
+        />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+}
+
+function Screen({navigation}: any) {
+  const route = useRoute<any>();
+  const count = route.params?.count ?? 0;
+
+  // React.useEffect(() => {
+  //   navigation.setOptions({headerBackTitleVisible: count % 2 === 0});
+  // }, [count, navigation]);
+
+  return (
+    <View>
+      <Button
+        onPress={() =>
+          navigation.push(route.name, {
+            title: `Hello ${count + 1}`,
+            count: count + 1,
+          })
+        }
+        title="Push a route"
+      />
+    </View>
+  );
+}

TestsExample/src/Test1646.tsx
@@ -1,0 +1,46 @@
+import {NavigationContainer, useRoute} from '@react-navigation/native';
+import {createNativeStackNavigator} from '@react-navigation/native-stack';
+import * as React from 'react';
+import {Button, View} from 'react-native';
+
+const Stack = createNativeStackNavigator();
+
+export default function App() {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator
+        screenOptions={{
+          headerBackTitleVisible: false,
+        }}>
+        <Stack.Screen
+          name="Screen"
+          component={Screen}
+          options={({route}: any) => ({title: route.params?.title ?? 'Hello'})}
+        />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+}
+
+function Screen({navigation}: any) {
+  const route = useRoute<any>();
+  const count = route.params?.count ?? 0;
+
+  // React.useEffect(() => {
+  //   navigation.setOptions({headerBackTitleVisible: count % 2 === 0});
+  // }, [count, navigation]);
+
+  return (
+    <View>
+      <Button
+        onPress={() =>
+          navigation.push(route.name, {
+            title: `Hello ${count + 1}`,
+            count: count + 1,
+          })
+        }
+        title="Push a route"
+      />
+    </View>
+  );
+}

ios/RNSScreenStackHeaderConfig.mm
@@ -460,7 +460,23 @@
   }
 
 #if !TARGET_OS_TV
-  if (config.backTitle != nil || config.backTitleFontFamily || config.backTitleFontSize ||
+  // Fix for github.com/react-navigation/react-navigation/issues/11015
+  // It allows to hide back button title and use back button menu as normal.
+  // Back button display mode and back button menu are available since iOS 14.
+  if (@available(iOS 14.0, *)) {
+    // Make sure to set display mode to default.
+    // This line resets back button display mode - especially needed on the Fabric architecture.
+    navitem.backButtonDisplayMode = UINavigationItemBackButtonDisplayModeDefault;
+
+    NSString *trimmedBackTitle =
+        [config.backTitle stringByTrimmingCharactersInSet:[NSCharacterSet whitespaceCharacterSet]];
+
+    // When an whitespace only back title is passed set back button mode to minimal.
+    if (config.backTitle != nil && [trimmedBackTitle length] == 0) {
+      navitem.backButtonDisplayMode = UINavigationItemBackButtonDisplayModeMinimal;
+    }
+  } else if (
+      config.backTitle != nil || config.backTitleFontFamily || config.backTitleFontSize ||
       config.disableBackButtonMenu) {
     RNSUIBarButtonItem *backBarButtonItem = [[RNSUIBarButtonItem alloc] initWithTitle:config.backTitle ?: prevItem.title
                                                                                 style:UIBarButtonItemStylePlain

commit d481b3ee7e2ac829bf5a7c0cdfeccd4e3d868a4c

fix: add internal isNewBackTitleImplementation const (#1791)
## Description

Because of a bug introduced in
https://github.com/software-mansion/react-native-screens/pull/1646
`react-native-screens` v3.21 changed how header's backTitle handles
whitespace strings in
https://github.com/software-mansion/react-native-screens/pull/1726

To allow for backwards compatibility in @react-navigation/native-stack
we need have a way to check if this version or newer is used

See https://github.com/react-navigation/react-navigation/pull/11423 for
more context.

## Changes

Added new `isNewBackTitleImplementation` internal constant that can be
used in `@react-navigation/native-stack`.

## Screenshots / GIFs

#### This change &
https://github.com/react-navigation/react-navigation/pull/11423 applied:


https://github.com/software-mansion/react-native-screens/assets/39658211/e2409b46-0725-473d-962b-1acc9deaa469


#### Without this change and
https://github.com/react-navigation/react-navigation/pull/11423:


https://github.com/software-mansion/react-native-screens/assets/39658211/ec65fd5d-f8e9-4d88-b442-6bfc68e9ee9c


## Test code and steps to reproduce

Test1791.tsx

You need to apply changes introduced in
https://github.com/react-navigation/react-navigation/pull/11423 to
`@react-navigation/native-stack` to test these canges.

## Checklist

- [x] Included code example that can be used to test this change

TestsExample/App.js
@@ -87,6 +87,7 @@ import Test1646 from './src/Test1646';
 import Test1649 from './src/Test1649';
 import Test1683 from './src/Test1683';
 import Test1726 from './src/Test1726';
+import Test1791 from './src/Test1791';
 
 enableFreeze(true);
 

TestsExample/src/Test1791.tsx
@@ -1,0 +1,63 @@
+import {NavigationContainer} from '@react-navigation/native';
+
+import {createNativeStackNavigator} from '@react-navigation/native-stack';
+
+import React from 'react';
+import {Button, View} from 'react-native';
+
+const Stack = createNativeStackNavigator();
+
+const Screen1 = ({navigation}) => (
+  <View style={{flex: 1}}>
+    <Button onPress={() => navigation.navigate('Screen2')} title="Next" />
+  </View>
+);
+
+const Screen2 = ({navigation}) => (
+  <View style={{flex: 1}}>
+    <Button onPress={() => navigation.navigate('Screen3')} title="Next" />
+  </View>
+);
+
+const Screen3 = ({navigation}) => (
+  <View style={{flex: 1}}>
+    <Button onPress={() => navigation.navigate('Screen4')} title="Next" />
+  </View>
+);
+
+const Screen4 = () => <View style={{flex: 1}} />;
+
+const App = () => {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen component={Screen1} name="Screen1" />
+        <Stack.Screen
+          component={Screen2}
+          name="Screen2"
+          options={{
+            headerBackTitleVisible: false,
+            headerBackTitle: 'Custom title in back button menu',
+          }}
+        />
+        <Stack.Screen
+          component={Screen3}
+          name="Screen3"
+          options={{
+            headerBackTitle: 'Small title',
+            headerBackTitleStyle: {fontSize: 8},
+          }}
+        />
+        <Stack.Screen
+          component={Screen4}
+          name="Screen4"
+          options={{
+            headerBackTitle: 'Custom title',
+          }}
+        />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+};
+
+export default App;

src/index.native.tsx
@@ -34,6 +34,7 @@ import {
 } from './types';
 import {
   isSearchBarAvailableForCurrentPlatform,
+  isNewBackTitleImplementation,
   executeNativeBackPress,
 } from './utils';
 
@@ -592,5 +593,6 @@ module.exports = {
   useTransitionProgress,
 
   isSearchBarAvailableForCurrentPlatform,
+  isNewBackTitleImplementation,
   executeNativeBackPress,
 };

src/index.tsx
@@ -13,6 +13,7 @@ export * from './types';
 export { default as useTransitionProgress } from './useTransitionProgress';
 export {
   isSearchBarAvailableForCurrentPlatform,
+  isNewBackTitleImplementation,
   executeNativeBackPress,
 } from './utils';
 

src/utils.ts
@@ -10,3 +10,9 @@ export function executeNativeBackPress() {
   BackHandler.exitApp();
   return true;
 }
+
+// Because of a bug introduced in https://github.com/software-mansion/react-native-screens/pull/1646
+// react-native-screens v3.21 changed how header's backTitle handles whitespace strings in https://github.com/software-mansion/react-native-screens/pull/1726
+// To allow for backwards compatibility in @react-navigation/native-stack we need a way to check if this version or newer is used.
+// See https://github.com/react-navigation/react-navigation/pull/11423 for more context.
+export const isNewBackTitleImplementation = true;