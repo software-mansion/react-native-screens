6bdc32abb5073929f4ddf0d1d9b8ebfef8df1317
Kacper Kafara <kacperkafara@gmail.com>
2023-07-19 14:07:21 +0200

    Add configuration for TestExample

--- TestsExample/.eslintrc.js
+++ TestsExample/.eslintrc.js
@@ -1,4 +1,12 @@
 module.exports = {
   root: true,
   extends: '@react-native',
+  overrides: [
+    {
+      files: ['*.tsx', '*.js'],
+      rules: {
+        'react-native/no-inline-styles': 'off',
+      },
+    },
+  ],
 };
