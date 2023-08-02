ed8874e2e7f96e5441396bdbe2edb3422da5e42c
Kacper Kafara <kacperkafara@gmail.com>
2023-08-02 08:28:26 +0200

    Set default for `sheetCustomDetents` in `InnerScreen`

--- src/index.native.tsx
+++ src/index.native.tsx
@@ -271,7 +271,7 @@ class InnerScreen extends React.Component<ScreenProps> {
       sheetGrabberVisible = false,
       sheetCornerRadius = -1.0,
       sheetExpandsWhenScrolledToEdge = true,
-      sheetHeight = [0.5, 0.7],
+      sheetCustomDetents = [],
     } = rest;
 
     if (enabled && isPlatformSupported) {
@@ -321,7 +321,7 @@ class InnerScreen extends React.Component<ScreenProps> {
             sheetGrabberVisible={sheetGrabberVisible}
             sheetCornerRadius={sheetCornerRadius}
             sheetExpandsWhenScrolledToEdge={sheetExpandsWhenScrolledToEdge}
-            sheetHeight={sheetHeight}
+            sheetCustomDetents={sheetCustomDetents}
             gestureResponseDistance={{
               start: gestureResponseDistance?.start ?? -1,
               end: gestureResponseDistance?.end ?? -1,
