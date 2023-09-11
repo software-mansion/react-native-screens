920a1a05104f7da345307866197671c765d6a6d0
Kacper Kafara <kacperkafara@gmail.com>
2023-09-11 11:59:07 +0200

    Add activity & context resolving methods to ScreenFragmentWrapper

--- android/src/main/java/com/swmansion/rnscreens/ScreenFragmentWrapper.kt
+++ android/src/main/java/com/swmansion/rnscreens/ScreenFragmentWrapper.kt
@@ -1,7 +1,14 @@
 package com.swmansion.rnscreens
 
+import android.app.Activity
+import com.facebook.react.bridge.ReactContext
+
 interface ScreenFragmentWrapper : FragmentHolder {
     var screen: Screen
 
+    // Communication with container
     fun onContainerUpdate()
+
+    fun tryGetActivity(): Activity?
+    fun tryGetContext(): ReactContext?
 }
