732a0ecd206f14f5c08d8ce7f915b48de824f1c6
Kacper Kafara <kacperkafara@gmail.com>
2023-09-11 15:31:09 +0200

    Start separating ScreenStackFragmentWrapper interface

---
+++ android/src/main/java/com/swmansion/rnscreens/ScreenStackFragmentWrapper.kt
@@ -0,0 +1,11 @@
+package com.swmansion.rnscreens
+
+import androidx.appcompat.widget.Toolbar
+
+interface ScreenStackFragmentWrapper : ScreenFragmentWrapper {
+    // Toolbar management
+    fun removeToolbar()
+    fun setToolbar(toolbar: Toolbar)
+    fun setToolbarShadowHidden(hidden: Boolean)
+    fun setToolbarTranslucent(translucent: Boolean)
+}
