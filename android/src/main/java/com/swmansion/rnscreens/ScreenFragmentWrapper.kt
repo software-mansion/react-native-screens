108bdfa2219d12ca9f3e9dc086e4cf0ccb8b81ad
Kacper Kafara <kacperkafara@gmail.com>
2023-09-11 12:40:13 +0200

    Add more communication-with-continer-related methods to ScreenFragmentWrapper

--- android/src/main/java/com/swmansion/rnscreens/ScreenFragmentWrapper.kt
+++ android/src/main/java/com/swmansion/rnscreens/ScreenFragmentWrapper.kt
@@ -5,11 +5,18 @@ import com.facebook.react.bridge.ReactContext
 
 interface ScreenFragmentWrapper : FragmentHolder, ScreenEventDispatcher {
     var screen: Screen
-    val childScreenContainers: List<ScreenContainer<*>>
 
     // Communication with container
+    val childScreenContainers: List<ScreenContainer<*>>
+    fun addChildContainer(container: ScreenContainer<*>)
+    fun removeChildContainer(container: ScreenContainer<*>)
     fun onContainerUpdate()
 
+    // Animation phase callbacks
+    fun onViewAnimationStart()
+    fun onViewAnimationEnd()
+
+    // Helpers
     fun tryGetActivity(): Activity?
     fun tryGetContext(): ReactContext?
 }
