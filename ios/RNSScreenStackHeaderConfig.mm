40df2fdab3cfd753dc7263246515ca6d449e0321
tboba <tymoteusz.boba@gmail.com>
2023-10-11 18:07:41 +0200

    Remove (RNSScreen *) leftover

--- ios/RNSScreenStackHeaderConfig.mm
+++ ios/RNSScreenStackHeaderConfig.mm
@@ -149,7 +149,7 @@ namespace react = facebook::react;
     [RNSScreenStackHeaderConfig updateViewController:self.screenView.controller withConfig:self animated:YES];
     // As the header might have change in `updateViewController` we need to ensure that header height
     // returned by the `onHeaderHeightChange` event is correct.
-    [(RNSScreen *)self.screenView.controller calculateAndNotifyHeaderHeightChangeIsModal:NO];
+    [self.screenView.controller calculateAndNotifyHeaderHeightChangeIsModal:NO];
   }
 }
 
