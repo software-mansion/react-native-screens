a1b5e8964270cb1751498292fd1b0774c6d195a7
Joe Mun <12057449+joemun@users.noreply.github.com>
2023-10-26 09:23:41 -0700

    change null check
    Co-authored-by: Tymoteusz Boba <Tymoteusz.Boba@gmail.com>

--- android/src/main/java/com/swmansion/rnscreens/ScreenViewManager.kt
+++ android/src/main/java/com/swmansion/rnscreens/ScreenViewManager.kt
@@ -42,9 +42,9 @@ class ScreenViewManager : ViewGroupManager<Screen>(), RNSScreenManagerInterface<
         props: ReactStylesDiffMap?,
         stateWrapper: StateWrapper?
     ): Any? {
-        if (BuildConfig.IS_NEW_ARCHITECTURE_ENABLED && view.fabricViewStateManager != null) {
+        if (BuildConfig.IS_NEW_ARCHITECTURE_ENABLED) {
             // fabricViewStateManager should never be null in Fabric. The null check is only for Paper's empty impl.
-            view.fabricViewStateManager!!.setStateWrapper(stateWrapper)
+            view.fabricViewStateManager?.setStateWrapper(stateWrapper)
         }
         return super.updateState(view, props, stateWrapper)
     }
