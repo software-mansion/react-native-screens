95fc91687156e8e1306c10fdf2ab47081cd160b2
Kacper Kafara <kacperkafara@gmail.com>
2023-11-03 14:58:41 +0100

    Typo in MainApplication

--- FabricTestExample/android/app/src/main/java/com/fabrictestexample/MainApplication.kt
+++ FabricTestExample/android/app/src/main/java/com/fabrictestexample/MainApplication.kt
@@ -25,7 +25,7 @@ class MainApplication : Application(), ReactApplication {
         override fun getJSMainModuleName(): String = "index"
 
         override val isNewArchEnabled: Boolean = BuildConfig.IS_NEW_ARCHITECTURE_ENABLED
-        override val isHermesEnabled: Boolean= BuildConfig.IS_HERMES_ENABLED
+        override val isHermesEnabled: Boolean = BuildConfig.IS_HERMES_ENABLED
     }
 
     override val reactHost: ReactHost
