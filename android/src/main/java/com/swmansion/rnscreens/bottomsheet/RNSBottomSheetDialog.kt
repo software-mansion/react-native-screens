89aaec14329e4f3e6a10984300f217b3117e9f68
Kacper Kafara <kacperkafara@gmail.com>
2023-11-20 14:00:39 +0100

    Add some important comments

--- android/src/main/java/com/swmansion/rnscreens/bottomsheet/RNSBottomSheetDialog.kt
+++ android/src/main/java/com/swmansion/rnscreens/bottomsheet/RNSBottomSheetDialog.kt
@@ -9,6 +9,10 @@ import java.lang.ref.WeakReference
 class RNSBottomSheetDialog(context: Context, fragment: ScreenModalFragment) : BottomSheetDialog(context) {
     private val fragmentRef: WeakReference<ScreenModalFragment> = WeakReference(fragment)
 
+    // There are various code paths leading to this method, however the one I'm concerned with
+    // is dismissal via swipe-down. If the sheet is dismissed we don't want the native dismiss logic
+    // to run, as this will lead to inconsistencies in ScreenStack state. Instead we intercept
+    // dismiss intention and run our logic.
     override fun cancel() {
         Log.d(TAG, "cancel")
         fragmentRef.get()!!.dismissFromContainer()
