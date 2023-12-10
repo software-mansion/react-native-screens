7092271eeeec1fadd8e5107f3dc163d17664dd3b
Jasonzza <jasonzza@outlook.com>
2023-12-11 00:53:01 +0800

    Update docs

--- src/types.tsx
+++ src/types.tsx
@@ -553,6 +553,7 @@ export interface SearchBarProps {
    * * `blur` - removes focus from the search bar
    * * `clearText` - removes any text present in the search bar input field
    * * `setText` - sets the search bar's content to given value
+   * * `cancelSearch` - cancel search in search bar.
    * * `toggleCancelButton` - depending on passed boolean value, hides or shows cancel button (iOS only)
    */
   ref?: React.RefObject<SearchBarCommands>;
