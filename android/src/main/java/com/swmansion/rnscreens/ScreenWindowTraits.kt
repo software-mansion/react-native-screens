d3dc8f9e54a4930fe10cec5a9fb790a3d21a9f0e
jiyong1 <jyong9591@gmail.com>
2023-12-12 12:42:07 +0900

    fix(Android): modify the decorFitsSystemWindow parameter in setNavigationBarHidden

--- android/src/main/java/com/swmansion/rnscreens/ScreenWindowTraits.kt
+++ android/src/main/java/com/swmansion/rnscreens/ScreenWindowTraits.kt
@@ -197,7 +197,7 @@ object ScreenWindowTraits {
         val screenForNavBarHidden = findScreenForTrait(screen, WindowTraits.NAVIGATION_BAR_HIDDEN)
         val hidden = screenForNavBarHidden?.isNavigationBarHidden ?: false
 
-        WindowCompat.setDecorFitsSystemWindows(window, hidden)
+        WindowCompat.setDecorFitsSystemWindows(window, !hidden)
         if (hidden) {
             WindowInsetsControllerCompat(window, window.decorView).let { controller ->
                 controller.hide(WindowInsetsCompat.Type.navigationBars())
