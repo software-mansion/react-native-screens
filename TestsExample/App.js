5af4b0c7a6d43d11a0e24a8238c597aebf7d533d
Kacper Kafara <kacper.kafara@swmansion.com>
2023-12-27 14:32:59 +0100

    fix(iOS): not working hitslop for headerRight/Left views (#1995)
    ## Description
    
    Since #1825 header config is no longer first child of a screen &
    `hitTest:withEvent:` method assumed this invariant to be true.
    
    Fixed that by using appropriate screen method instead of blind
    assumption.
    
    Fixes #1981
    
    ## Changes
    
    * Fixed `hitTest:withEvent:` method by using `findHeaderConfig`
    `RNSScreenView`'s method
    * Improved `findHeaderConfig` method itself
    
    ## Test code and steps to reproduce
    
    `Test1981`
    
    ## Checklist
    
    - [x] Included code example that can be used to test this change
    - [x] Ensured that CI passes

--- TestsExample/App.js
+++ TestsExample/App.js
@@ -93,6 +93,7 @@ import Test1802 from './src/Test1802';
 import Test1844 from './src/Test1844';
 import Test1864 from './src/Test1864';
 import Test1829 from './src/Test1829';
+import Test1981 from './src/Test1981';
 
 enableFreeze(true);
 
