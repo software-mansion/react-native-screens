2a6eedd1c027a51f328bab3034d3e5a3988cc6a8
tboba <tymoteusz.boba@gmail.com>
2024-01-03 12:22:13 +0100

    Change console.warn

--- src/components/FullWindowOverlay.tsx
+++ src/components/FullWindowOverlay.tsx
@@ -11,7 +11,7 @@ const NativeFullWindowOverlay: React.ComponentType<
 
 function FullWindowOverlay(props: { children: ReactNode }) {
   if (Platform.OS !== 'ios') {
-    console.warn('Importing FullWindowOverlay is only valid on iOS devices.');
+    console.warn('Using FullWindowOverlay is only valid on iOS devices.');
     return <View {...props} />;
   }
   return (
