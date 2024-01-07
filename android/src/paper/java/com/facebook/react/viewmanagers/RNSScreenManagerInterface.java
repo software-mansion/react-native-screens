7b4ed4027a23ced32d010166f7476ed0002af961
Kacper Kafara <kacperkafara@gmail.com>
2024-01-07 18:57:31 +0100

    Update view manager interfaces for paper

--- android/src/paper/java/com/facebook/react/viewmanagers/RNSScreenManagerInterface.java
+++ android/src/paper/java/com/facebook/react/viewmanagers/RNSScreenManagerInterface.java
@@ -11,11 +11,12 @@ package com.facebook.react.viewmanagers;
 
 import android.view.View;
 import androidx.annotation.Nullable;
+import com.facebook.react.bridge.ReadableArray;
 import com.facebook.react.bridge.ReadableMap;
 
 public interface RNSScreenManagerInterface<T extends View> {
-  void setSheetAllowedDetents(T view, @Nullable String value);
-  void setSheetLargestUndimmedDetent(T view, @Nullable String value);
+  void setSheetAllowedDetents(T view, @Nullable ReadableArray value);
+  void setSheetLargestUndimmedDetent(T view, int value);
   void setSheetGrabberVisible(T view, boolean value);
   void setSheetCornerRadius(T view, float value);
   void setSheetExpandsWhenScrolledToEdge(T view, boolean value);
