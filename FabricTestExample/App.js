b810be4e066a94f89f9d526443f0485ac62a2038
tboba <tymoteusz.boba@gmail.com>
2024-01-11 15:48:01 +0100

    Add tests to reproduce

--- FabricTestExample/App.js
+++ FabricTestExample/App.js
@@ -92,6 +92,7 @@ import Test1844 from './src/Test1844';
 import Test1864 from './src/Test1864';
 import TestScreenAnimation from './src/TestScreenAnimation';
 import Test1981 from './src/Test1981';
+import Test2008 from './src/Test2008';
 
 enableFreeze(true);
 
