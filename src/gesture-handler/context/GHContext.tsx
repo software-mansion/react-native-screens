eb3a6356796a2bf5f2aa39c738fa0ae2930fe4df
tboba <tymoteusz.boba@gmail.com>
2024-01-18 15:32:54 +0100

    Fix wrongly written context

--- src/gesture-handler/context/GHContext.tsx
+++ src/gesture-handler/context/GHContext.tsx
@@ -1,10 +1,7 @@
-import React from 'react';
+import React, { PropsWithChildren } from 'react';
 import { GestureProviderProps } from '../../native-stack/types';
 
 // context to be used when the user wants full screen swipe (see `gesture-handler` folder in repo)
 export const GHContext = React.createContext(
-  (
-    _props: GestureProviderProps,
-    { children }: { children: React.ReactNode }
-  ) => <>{children}</>
+  (props: PropsWithChildren<GestureProviderProps>) => <>{props.children}</>
 );
--- src/gesture-handler/context/GHContext.web.tsx
+++
@@ -1,6 +0,0 @@
-import React from 'react';
-import { GestureProviderProps } from '../../native-stack/types';
-
-export const GHContext = React.createContext(
-  (_props: GestureProviderProps): React.ReactElement => <></>
-);
