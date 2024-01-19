409d655b204389dc801eb24a46a98e5a508e2685
tboba <tymoteusz.boba@gmail.com>
2024-01-19 16:42:06 +0100

    Move GHContext from gesture-handler to native-stack

--- src/index.tsx
+++ src/index.tsx
@@ -53,9 +53,9 @@ export { default as FullWindowOverlay } from './components/FullWindowOverlay';
 export { default as NativeScreensModule } from './fabric/NativeScreensModule';
 
 /*
- * Gesture Handler
+ * Contexts
  */
-export { GHContext } from './gesture-handler/context/GHContext';
+export { GHContext } from './native-stack/contexts/GHContext';
 
 /*
  * Utils
