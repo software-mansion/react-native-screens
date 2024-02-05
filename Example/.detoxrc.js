8d335a05ce4eb22c9874947ad3f404267189a636
tboba <tymoteusz.boba@gmail.com>
2024-02-05 15:12:14 +0100

    Revert removing assembleAndroidTest, change macos-12 to macos-14

--- Example/.detoxrc.js
+++ Example/.detoxrc.js
@@ -27,13 +27,15 @@ module.exports = {
     'android.debug': {
       type: 'android.apk',
       binaryPath: 'android/app/build/outputs/apk/debug/app-debug.apk',
-      build: 'cd android && ./gradlew assembleDebug && cd ..',
+      build:
+        'cd android && ./gradlew assembleDebug assembleAndroidTest -DtestBuildType=debug && cd ..',
       reversePorts: [8081],
     },
     'android.release': {
       type: 'android.apk',
       binaryPath: 'android/app/build/outputs/apk/release/app-release.apk',
-      build: 'cd android && ./gradlew assembleRelease && cd ..',
+      build:
+        'cd android && ./gradlew assembleRelease assembleAndroidTest -DtestBuildType=release && cd ..',
     },
   },
   devices: {
