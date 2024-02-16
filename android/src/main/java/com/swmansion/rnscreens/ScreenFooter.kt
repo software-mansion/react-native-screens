a214f1993eaac2c3f19ab9ec51c55de71a1c84f3
Kacper Kafara <kacperkafara@gmail.com>
2024-02-16 13:38:48 +0100

    Add stub for ScreenFooter

---
+++ android/src/main/java/com/swmansion/rnscreens/ScreenFooter.kt
@@ -0,0 +1,9 @@
+package com.swmansion.rnscreens
+
+import android.annotation.SuppressLint
+import com.facebook.react.bridge.ReactContext
+import com.facebook.react.views.view.ReactViewGroup
+
+@SuppressLint("ViewConstructor")
+class ScreenFooter(reactContext: ReactContext) : ReactViewGroup(reactContext) {
+}
