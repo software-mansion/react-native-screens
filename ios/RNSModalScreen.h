fb139b185377b66e53af1ee1f8d4334d60c40677
Wojciech Lewicki <wojciech.lewicki@swmansion.com>
2024-03-15 14:15:58 +0100

    feat: remove not needed code for modal screen

--- ios/RNSModalScreen.h
+++ ios/RNSModalScreen.h
@@ -1,18 +1,5 @@
 #import "RNSScreen.h"
 
-@interface RCTConvert (RNSModalScreen)
-
-+ (RNSScreenStackPresentation)RNSScreenStackPresentation:(id)json;
-+ (RNSScreenStackAnimation)RNSScreenStackAnimation:(id)json;
-
-#if !TARGET_OS_TV
-+ (RNSStatusBarStyle)RNSStatusBarStyle:(id)json;
-+ (UIStatusBarAnimation)UIStatusBarAnimation:(id)json;
-+ (UIInterfaceOrientationMask)UIInterfaceOrientationMask:(id)json;
-#endif
-
-@end
-
 @interface RNSModalScreen : RNSScreenView
 @end
 
