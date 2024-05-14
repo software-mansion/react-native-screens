3371225d90b2f78beb70681d85d52cae8428c86e
alduzy <alduzy@gmail.com>
2024-05-14 16:57:08 +0200

    moved teststo common dir, removed duplicates

--- TestsExample/index.js
+++ TestsExample/index.js
@@ -1,6 +1,6 @@
 import 'react-native-gesture-handler';
 import { AppRegistry } from 'react-native';
-import App from './App';
+import App from '../tests';
 import { name as appName } from './app.json';
 
 AppRegistry.registerComponent(appName, () => App);
