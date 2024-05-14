add5b73234ef170af4c5645ad67c831106c6ee63
Kacper Kafara <kacperkafara@gmail.com>
2024-05-14 06:48:18 +0200

    Add modal screen to example

--- FabricTestExample/src/Test2118.tsx
+++ FabricTestExample/src/Test2118.tsx
@@ -1,14 +1,30 @@
-import React, { useEffect } from 'react';
+import React from 'react';
 import { View, Text, Button, Pressable, StyleSheet, Alert } from 'react-native';
-import { GestureHandlerRootView } from 'react-native-gesture-handler';
-
-const fill = { flex: 1 };
-
-import { NavigationContainer } from '@react-navigation/native';
+import { NavigationContainer, useNavigation } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
-import { createStackNavigator } from '@react-navigation/stack';
 
-function HomeScreen({ navigation }) {
+const Stack = createNativeStackNavigator(); // <-- change to createStackNavigator to see a difference
+
+const ModalStack = createNativeStackNavigator();
+
+export default function App() {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator screenOptions={{ animation: 'slide_from_left' }}>
+        <Stack.Screen name="Home" component={HomeScreen} />
+        <Stack.Screen name="DetailsStack" component={DetailsScreen} />
+        <Stack.Screen
+          name="StackInModal"
+          component={StackInModal}
+          options={{ presentation: 'modal' }}
+        />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+}
+
+function HomeScreen() {
+  const navigation = useNavigation();
   return (
     <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
       <Text>Home Screen</Text>
@@ -16,29 +32,20 @@ function HomeScreen({ navigation }) {
         title="Go to Details"
         onPress={() => navigation.navigate('DetailsStack')}
       />
+      <Button
+        title="Go to StackInModal"
+        onPress={() => navigation.navigate('StackInModal')}
+      />
     </View>
   );
 }
 
-const styles = StyleSheet.create({
-  wrapperCustom: {
-    width: '100%',
-    height: 100,
-    marginHorizontal: 30,
-    borderRadius: 10,
-    margin: 10,
-  },
-  text: {
-    fontSize: 20,
-    color: 'black',
-  },
-});
-
-function DetailsScreen({ navigation }) {
+function DetailsScreen() {
   return (
     <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
       {new Array(10).fill(0).map((_, i) => (
         <Pressable
+          key={i.toString()}
           onPress={() => {
             Alert.alert('Pressed!');
           }}
@@ -57,17 +64,25 @@ function DetailsScreen({ navigation }) {
   );
 }
 
-const Stack = createNativeStackNavigator(); // <-- change to createStackNavigator to see a difference
-
-function App() {
+function StackInModal() {
   return (
-      <NavigationContainer>
-        <Stack.Navigator screenOptions={{ animation: 'slide_from_left' }}>
-          <Stack.Screen name="Home" component={HomeScreen} />
-          <Stack.Screen name="DetailsStack" component={DetailsScreen} />
-        </Stack.Navigator>
-      </NavigationContainer>
+    <ModalStack.Navigator>
+      <ModalStack.Screen name="ModalHome" component={HomeScreen} />
+      <ModalStack.Screen name="ModalDetails" component={DetailsScreen} />
+    </ModalStack.Navigator>
   );
 }
 
-export default App;
+const styles = StyleSheet.create({
+  wrapperCustom: {
+    width: '100%',
+    height: 100,
+    marginHorizontal: 30,
+    borderRadius: 10,
+    margin: 10,
+  },
+  text: {
+    fontSize: 20,
+    color: 'black',
+  },
+});
