510594ae3d1a587b01757ad8c1ef3360e1977d7b
alduzy <alduzy@gmail.com>
2024-05-17 09:50:35 +0200

    cr fixes

--- Example/android/app/src/main/java/com/swmansion/rnscreens/example/MainActivity.kt
+++ Example/android/app/src/main/java/com/swmansion/rnscreens/example/MainActivity.kt
@@ -20,4 +20,8 @@ class MainActivity : ReactActivity() {
    */
   override fun createReactActivityDelegate(): ReactActivityDelegate =
       DefaultReactActivityDelegate(this, mainComponentName, fabricEnabled)
+
+  override fun onCreate(savedInstanceState: Bundle?) {
+    super.onCreate(null)
+  }
 }
