7ff2f6fabddd3badb5115d3092fe482ff82e3aa8
Kacper Kafara <kacperkafara@gmail.com>
2024-05-30 13:30:02 +0200

    Prettier in src/types.tsx

--- src/types.tsx
+++ src/types.tsx
@@ -219,7 +219,9 @@ export interface ScreenProps extends ViewProps {
   /**
    * A callback that gets called when the current screen is in `formSheet` presentation and its detent has changed.
    */
-  onSheetDetentChanged?: (e: NativeSyntheticEvent<{ index: number; isStable: boolean }>) => void;
+  onSheetDetentChanged?: (
+    e: NativeSyntheticEvent<{ index: number; isStable: boolean }>
+  ) => void;
   /**
    * An internal callback called every frame during the transition of screens of `native-stack`, used to feed transition context.
    */
