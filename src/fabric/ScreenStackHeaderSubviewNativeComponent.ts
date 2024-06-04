e0fd410e69fe83421ab39d6c51f21e842af9659e
evanbacon <bacon@expo.io>
2024-06-04 15:18:59 +0200

    add basic React Server Component support

--- src/fabric/ScreenStackHeaderSubviewNativeComponent.ts
+++ src/fabric/ScreenStackHeaderSubviewNativeComponent.ts
@@ -1,3 +1,5 @@
+'use client';
+
 import codegenNativeComponent from 'react-native/Libraries/Utilities/codegenNativeComponent';
 import type { ViewProps } from 'react-native';
 import type { WithDefault } from 'react-native/Libraries/Types/CodegenTypes';
--- src/native-stack/contexts/GHContext.tsx
+++ src/native-stack/contexts/GHContext.tsx
@@ -1,3 +1,5 @@
+'use client';
+
 import React, { PropsWithChildren } from 'react';
 import { GestureProviderProps } from '../types';
 
