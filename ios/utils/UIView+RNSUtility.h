aca213c145f0902ade43a135b95f3eef35f2f33c
Kacper Kafara <kacperkafara@gmail.com>
2024-06-18 16:49:16 +0200

    Add category on UIView with code for touch handler finding

---
+++ ios/utils/UIView+RNSUtility.h
@@ -0,0 +1,23 @@
+#import <Foundation/Foundation.h>
+
+#ifdef RCT_NEW_ARCH_ENABLED
+#import <React/RCTSurfaceTouchHandler.h>
+#else
+#import <React/RCTTouchHandler.h>
+#endif // RCT_NEW_ARCH_ENABLED
+
+#ifdef RCT_NEW_ARCH_ENABLED
+#define RNS_TOUCH_HANDLER_ARCH_TYPE RCTSurfaceTouchHandler
+#else
+#define RNS_TOUCH_HANDLER_ARCH_TYPE RCTTouchHandler
+#endif // RCT_NEW_ARCH_ENABLED
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface UIView (RNSUtility)
+
+- (nullable RNS_TOUCH_HANDLER_ARCH_TYPE *)rnscreens_findTouchHandlerInAncestorChain;
+
+@end
+
+NS_ASSUME_NONNULL_END
