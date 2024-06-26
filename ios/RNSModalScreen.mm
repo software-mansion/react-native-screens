17af80e13a09851cdd2b818686af9134eacd0931
Kacper Kafara <kacperkafara@gmail.com>
2024-06-26 10:52:24 +0200

    Add working PoC of the solution
    Needs to be cleaned up

--- ios/RNSModalScreen.mm
+++ ios/RNSModalScreen.mm
@@ -7,6 +7,28 @@
 
 @implementation RNSModalScreen
 
+- (void)traitCollectionDidChange:(UITraitCollection *)previousTraitCollection
+{
+  [super traitCollectionDidChange:previousTraitCollection];
+
+  // Describe following facts:
+  // 1. With full screen modal react root view is mounted in the initial view hierarchy
+  // 2. Thus the trait observation in react root view does not work anymore
+  // 3. we need to send theme-chanages notification our selves
+  // Describe this in-depth in PR and paste link here
+  if (RCTSharedApplication().applicationState == UIApplicationStateBackground ||
+      (self.stackPresentation != RNSScreenStackPresentationFullScreenModal)) {
+    return;
+  }
+
+  [[NSNotificationCenter defaultCenter]
+      postNotificationName:RCTUserInterfaceStyleDidChangeNotification
+                    object:self
+                  userInfo:@{
+                    RCTUserInterfaceStyleDidChangeNotificationTraitCollectionKey : self.traitCollection,
+                  }];
+}
+
 #ifdef RCT_NEW_ARCH_ENABLED
 + (react::ComponentDescriptorProvider)componentDescriptorProvider
 {
