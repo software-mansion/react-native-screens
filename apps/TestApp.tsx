1afa70a1a14943e67aaaba5d766b25e210ceb183
alduzy <alduzy@gmail.com>
2024-07-02 16:11:48 +0200

    updated switching between example and tests

--- apps/TestApp.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import { enableFreeze } from 'react-native-screens';
-import * as Test from './src/tests';
-
-enableFreeze(true);
-
-export default function App() {
-  return <Test.Test42 />;
-}
--- apps/index.ts
+++ apps/index.ts
@@ -1,4 +1,3 @@
 import App from './App';
-import TestApp from './TestApp';
 
 export default App;
