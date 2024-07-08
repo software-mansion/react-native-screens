2b31454829201b7f65bb7e64e92d6f4e424c31e4
Maciej Stosio <maciekstosio@users.noreply.github.com>
2024-07-08 15:05:39 +0200

    chore: test pipeline

--- src/fabric/ScreenStackHeaderConfigNativeComponent.ts
+++ src/fabric/ScreenStackHeaderConfigNativeComponent.ts
@@ -45,7 +45,6 @@ export interface NativeProps extends ViewProps {
   hideBackButton?: boolean;
   backButtonInCustomView?: boolean;
   // TODO: implement this props on iOS
-  topInsetEnabled?: boolean;
 }
 
 export default codegenNativeComponent<NativeProps>(
