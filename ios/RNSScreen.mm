5a1eee9ea05503bece616687bb617cccef9aab10
alduzy <alduzy@gmail.com>
2024-07-15 15:10:54 +0200

    Remove unwanted change

--- ios/RNSScreen.mm
+++ ios/RNSScreen.mm
@@ -797,6 +797,7 @@ namespace react = facebook::react;
   // subviews
   // Explanation taken from `reactSetFrame`, which is old arch equivalent of this code.
 }
+
 - (void)finalizeUpdates:(RNComponentViewUpdateMask)updateMask
 {
   [super finalizeUpdates:updateMask];
