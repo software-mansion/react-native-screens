3ec2afb34495528a1bc529e8da5b1be9797dfa48
Patrycja Kalińska <cysiak0412@gmail.com>
2024-07-16 11:36:25 +0200

    chore: update navbar to work with latest t-rex-ui

--- docs/src/theme/Navbar/index.js
+++ docs/src/theme/Navbar/index.js
@@ -3,13 +3,8 @@ import useBaseUrl from '@docusaurus/useBaseUrl';
 import { Navbar } from '@swmansion/t-rex-ui';
 
 export default function NavbarWrapper(props) {
-  const heroImages = {
-    logo: useBaseUrl('/img/logo.svg'),
-  };
-
   return (
     <Navbar
-      heroImages={heroImages}
       useLandingLogoDualVariant={true}
       isAlgoliaActive={false}
       {...props}
