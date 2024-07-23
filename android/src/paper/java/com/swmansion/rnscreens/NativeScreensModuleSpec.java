a2de4ed63402ab4a6eafdc7accd44b79468f2aa0
Maciej Stosio <maciekstosio@users.noreply.github.com>
2024-07-23 17:06:50 +0200

    fix: Fixes to sync archs scripts (#2259)
    ## Description
    
    Fixes to
    https://github.com/software-mansion/react-native-screens/pull/2224, that
    came out in other repos.
    
    
    ## Changes
    
    - Bump version of GitHub actions 
    - Fix warning when trying to copy non existing codegen interface
    
    ## Test code and steps to reproduce
    
    Duplicate
    `android/src/paper/java/com/facebook/react/viewmanagers/RNSSearchBarManagerDelegate.java`
    with name change for example with ` copy` at the end. Before it would
    fail because file wouldn't be found. Now there will be warning in logs.

--- android/src/paper/java/com/swmansion/rnscreens/NativeScreensModuleSpec.java
+++ android/src/paper/java/com/swmansion/rnscreens/NativeScreensModuleSpec.java
@@ -12,8 +12,10 @@
 
 package com.swmansion.rnscreens;
 
+import com.facebook.proguard.annotations.DoNotStrip;
 import com.facebook.react.bridge.ReactApplicationContext;
 import com.facebook.react.bridge.ReactContextBaseJavaModule;
+import com.facebook.react.bridge.ReactMethod;
 import com.facebook.react.bridge.ReactModuleWithSpec;
 import com.facebook.react.turbomodule.core.interfaces.TurboModule;
 import javax.annotation.Nonnull;
@@ -29,4 +31,6 @@ public abstract class NativeScreensModuleSpec extends ReactContextBaseJavaModule
   public @Nonnull String getName() {
     return NAME;
   }
+
+
 }
