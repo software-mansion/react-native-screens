725929a6ee9122b3169ef78373763340f5667153
Wojciech Lewicki <wojciech.lewicki@swmansion.com>
2024-07-31 10:39:39 +0200

    fix(Fabric): not working animations on second-top screen (#2270)
    PR fixing animations on Fabric.
    
    The problem started in RN 0.74, from which suspending the component triggers its useLayoutEffect. It has the consequences in Animated since detaching the native node happens then: facebook/react-native@a5b84b9/packages/react-native/Libraries/Animated/useAnimatedProps.js#L221-L238.

--- src/components/ScreenStack.tsx
+++ src/components/ScreenStack.tsx
@@ -7,6 +7,10 @@ import ScreenStackNativeComponent from '../fabric/ScreenStackNativeComponent';
 const NativeScreenStack: React.ComponentType<ScreenStackProps> =
   ScreenStackNativeComponent as any;
 
+function isFabric() {
+  return 'nativeFabricUIManager' in global;
+}
+
 function ScreenStack(props: ScreenStackProps) {
   const { children, gestureDetectorBridge, ...rest } = props;
   const ref = React.useRef(null);
@@ -19,8 +23,14 @@ function ScreenStack(props: ScreenStackProps) {
     const isFreezeEnabled =
       descriptor?.options?.freezeOnBlur ?? freezeEnabled();
 
+    // On Fabric, when screen is frozen, animated and reanimated values are not updated
+    // due to component being unmounted. To avoid this, we don't freeze the previous screen there
+    const freezePreviousScreen = isFabric()
+      ? size - index > 2
+      : size - index > 1;
+
     return (
-      <DelayedFreeze freeze={isFreezeEnabled && size - index > 1}>
+      <DelayedFreeze freeze={isFreezeEnabled && freezePreviousScreen}>
         {child}
       </DelayedFreeze>
     );
