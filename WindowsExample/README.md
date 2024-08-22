6e4f0c94669e696a904bfe3b53f89c15948aa5c3
adrianryt <adrianryt3@gmail.com>
2024-08-22 12:15:43 +0200

    Create example for Windows

---
+++ WindowsExample/README.md
@@ -0,0 +1,1 @@
+# React Native Screens example app for windows
---
+++ WindowsExample/windows/WindowsExample/MainPage.h
@@ -0,0 +1,19 @@
+﻿#pragma once
+#include "MainPage.g.h"
+#include <winrt/Microsoft.ReactNative.h>
+
+namespace winrt::WindowsExample::implementation
+{
+    struct MainPage : MainPageT<MainPage>
+    {
+        MainPage();
+    };
+}
+
+namespace winrt::WindowsExample::factory_implementation
+{
+    struct MainPage : MainPageT<MainPage, implementation::MainPage>
+    {
+    };
+}
+
