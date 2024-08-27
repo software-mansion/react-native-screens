80dcad47fee74787bdb37cc8afbd18fc99b988d2
Tymoteusz Boba <Tymoteusz.Boba@gmail.com>
2024-08-27 13:38:23 +0200

    fix(iOS): Change import of RectUtil.h file to react/renderer/components (#2319)
    ## Description
    
    In the newest version of screens, there's a bug where compiler can't
    find RectUtil.h file from `utils/RectFile.h` during the compile phase of
    RNSScreenComponentDescriptor file.
    This PR fixes this by changing the path of RectUtil.h file to
    <react/renderer/components/rnscreens/util/RectFile.h> file.
    
    Fixes #2306.
    
    ## Changes
    
    - Changed import of RectUtil.h file in RNSScreenComponentDescriptor.h
    file
    
    ## Test code and steps to reproduce
    
    - Download reproducer from
    https://github.com/jankosecki/react-native-upgrade-tester/tree/rn-0.75
    - Delete patches/react-native-screens+3.34.0.patch
    - Clean up node_modules in case patch already applied
    - Run yarn setup (it invokes "pod-install": "(cd ./ios ;
    RCT_NEW_ARCH_ENABLED=1 USE_FRAMEWORKS=static bundle exec pod install
    --repo-update)" which enables new architecture)
    - Open XCode to set up development team (provided repro contains None
    team in Signing & Capabilities)
    - Run yarn start
    - Run yarn ios or trigger build from XCode (yarn ios causes a massive
    error output with different "error" lines but XCode offers a clear
    reason.
    
    ## Checklist
    
    - [ ] Ensured that CI passes

--- common/cpp/react/renderer/components/rnscreens/RNSScreenComponentDescriptor.h
+++ common/cpp/react/renderer/components/rnscreens/RNSScreenComponentDescriptor.h
@@ -5,9 +5,9 @@
 #endif
 #include <react/debug/react_native_assert.h>
 #include <react/renderer/components/rnscreens/Props.h>
+#include <react/renderer/components/rnscreens/utils/RectUtil.h>
 #include <react/renderer/core/ConcreteComponentDescriptor.h>
 #include "RNSScreenShadowNode.h"
-#include "utils/RectUtil.h"
 
 namespace facebook {
 namespace react {
