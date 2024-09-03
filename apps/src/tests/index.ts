e3972d789da0e52f8df29aeacbafaeaae5adaa98
tboba <tymoteusz.boba@gmail.com>
2024-09-03 17:44:24 +0200

    Add test case

--- apps/src/tests/index.ts
+++ apps/src/tests/index.ts
@@ -111,6 +111,6 @@ export { default as Test2235 } from './Test2235';
 export { default as Test2252 } from './Test2252';
 export { default as Test2271 } from './Test2271';
 export { default as Test2282 } from './Test2282';
+export { default as Test2232 } from './Test2332';
 export { default as TestScreenAnimation } from './TestScreenAnimation';
 export { default as TestHeader } from './TestHeader';
-
