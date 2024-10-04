af3048964d59e83d0ad4d99e4f4dd07611ec8bad
Maksymilian Galas <maksymilian.galas@swmansion.com>
2024-10-04 12:15:20 +0200

    fix(iOS): full screen modal crash on paper (#2336)
    ## Description
    
    Full screen modal was crashing sometimes on Paper.
    Fixes #2317.
    
    ## Test code and steps to reproduce
    
    Added `Test2317` to tests.
    
    ## Checklist
    
    - [x] Included code example that can be used to test this change
    - [ ] Ensured that CI passes
    
    ---------
    
    Co-authored-by: Kacper Kafara <kacperkafara@gmail.com>

--- apps/src/tests/index.ts
+++ apps/src/tests/index.ts
@@ -111,6 +111,7 @@ export { default as Test2235 } from './Test2235';
 export { default as Test2252 } from './Test2252';
 export { default as Test2271 } from './Test2271';
 export { default as Test2282 } from './Test2282';
+export { default as Test2317 } from './Test2317';
 export { default as Test2332 } from './Test2332';
 export { default as TestScreenAnimation } from './TestScreenAnimation';
 export { default as TestHeader } from './TestHeader';
