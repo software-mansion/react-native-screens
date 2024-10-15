software-mansion/react-native-screens

commit cce7d7837511bc0da1c2bce5e090888660405901

feat: allow preload using activityState (#2389)
## Description

This PR is a rewrite of
https://github.com/software-mansion/react-native-screens/pull/2278 using
already existing `activityState` -> to avoid introducing redundant prop.

Connected PR to react-navigation:
https://github.com/react-navigation/react-navigation/pull/12189

## Test code and steps to reproduce

You can play with the preload feature in `TestPreload.tsx`

To test assertion of activityState progress go to
`TestActivityStateProgression.tsx` - keep in mind that if the Screen has
`isNativeStack` it will just run JS validation, remove the prop to test
native checks.

## Checklist

- [ ] Included code example that can be used to test this change
- [ ] Updated TS types
- [x] Updated documentation: <!-- For adding new props to native-stack
-->
- [x]
https://github.com/software-mansion/react-native-screens/blob/main/guides/GUIDE_FOR_LIBRARY_AUTHORS.md
- [ ]
https://github.com/software-mansion/react-native-screens/blob/main/native-stack/README.md
- [ ]
https://github.com/software-mansion/react-native-screens/blob/main/src/types.tsx
- [ ]
https://github.com/software-mansion/react-native-screens/blob/main/src/native-stack/types.tsx
- [ ] Ensured that CI passes

---------

Co-authored-by: Kacper Kafara <kacperkafara@gmail.com>

ios/RNSScreen.mm
@@ -304,6 +304,10 @@ constexpr NSInteger SHEET_LARGEST_UNDIMMED_DETENT_NONE = -1;
 {
   int activityState = [activityStateOrNil intValue];
   if (activityStateOrNil != nil && activityState != -1 && activityState != _activityState) {
+    if ([_controller.navigationController isKindOfClass:RNSNavigationController.class] &&
+        _activityState < activityState) {
+      RCTLogError(@"[RNScreens] activityState can only progress in NativeStack");
+    }
     _activityState = activityState;
     [_reactSuperview markChildUpdated];
   }

ios/RNSScreenStack.mm
@@ -673,7 +673,7 @@ namespace react = facebook::react;
   NSMutableArray<UIViewController *> *pushControllers = [NSMutableArray new];
   NSMutableArray<UIViewController *> *modalControllers = [NSMutableArray new];
   for (RNSScreenView *screen in _reactSubviews) {
-    if (!screen.dismissed && screen.controller != nil) {
+    if (!screen.dismissed && screen.controller != nil && screen.activityState != RNSActivityStateInactive) {
       if (pushControllers.count == 0) {
         // first screen on the list needs to be places as "push controller"
         [pushControllers addObject:screen.controller];
@@ -947,6 +947,7 @@ namespace react = facebook::react;
 - (void)markChildUpdated
 {
   // do nothing
+  [self updateContainer];
 }
 
 - (void)didUpdateChildren

android/src/main/java/com/swmansion/rnscreens/Screen.kt
@@ -234,6 +234,9 @@ class Screen(
         if (activityState == this.activityState) {
             return
         }
+        if (container is ScreenStack && this.activityState != null && activityState < this.activityState!!) {
+            throw IllegalStateException("[RNScreens] activityState can only progress in NativeStack")
+        }
         this.activityState = activityState
         container?.notifyChildUpdate()
     }

android/src/main/java/com/swmansion/rnscreens/ScreenStack.kt
@@ -104,7 +104,7 @@ class ScreenStack(
 
         for (i in screenWrappers.indices.reversed()) {
             val screenWrapper = getScreenFragmentWrapperAt(i)
-            if (!dismissedWrappers.contains(screenWrapper)) {
+            if (!dismissedWrappers.contains(screenWrapper) && screenWrapper.screen.activityState !== Screen.ActivityState.INACTIVE) {
                 if (newTop == null) {
                     newTop = screenWrapper
                 } else {
@@ -182,8 +182,16 @@ class ScreenStack(
                                 R.anim.rns_no_animation_medium,
                             )
                         StackAnimation.FADE_FROM_BOTTOM -> it.setCustomAnimations(R.anim.rns_fade_from_bottom, R.anim.rns_no_animation_350)
-                        StackAnimation.IOS_FROM_RIGHT -> it.setCustomAnimations(R.anim.rns_ios_from_right_foreground_open, R.anim.rns_ios_from_right_background_open)
-                        StackAnimation.IOS_FROM_LEFT -> it.setCustomAnimations(R.anim.rns_ios_from_left_foreground_open, R.anim.rns_ios_from_left_background_open)
+                        StackAnimation.IOS_FROM_RIGHT ->
+                            it.setCustomAnimations(
+                                R.anim.rns_ios_from_right_foreground_open,
+                                R.anim.rns_ios_from_right_background_open,
+                            )
+                        StackAnimation.IOS_FROM_LEFT ->
+                            it.setCustomAnimations(
+                                R.anim.rns_ios_from_left_foreground_open,
+                                R.anim.rns_ios_from_left_background_open,
+                            )
                     }
                 } else {
                     when (stackAnimation) {
@@ -221,8 +229,16 @@ class ScreenStack(
                                 R.anim.rns_slide_out_to_bottom,
                             )
                         StackAnimation.FADE_FROM_BOTTOM -> it.setCustomAnimations(R.anim.rns_no_animation_250, R.anim.rns_fade_to_bottom)
-                        StackAnimation.IOS_FROM_RIGHT -> it.setCustomAnimations(R.anim.rns_ios_from_right_background_close, R.anim.rns_ios_from_right_foreground_close)
-                        StackAnimation.IOS_FROM_LEFT -> it.setCustomAnimations(R.anim.rns_ios_from_left_background_close, R.anim.rns_ios_from_left_foreground_close)
+                        StackAnimation.IOS_FROM_RIGHT ->
+                            it.setCustomAnimations(
+                                R.anim.rns_ios_from_right_background_close,
+                                R.anim.rns_ios_from_right_foreground_close,
+                            )
+                        StackAnimation.IOS_FROM_LEFT ->
+                            it.setCustomAnimations(
+                                R.anim.rns_ios_from_left_background_close,
+                                R.anim.rns_ios_from_left_foreground_close,
+                            )
                     }
                 }
             }
@@ -258,7 +274,9 @@ class ScreenStack(
                     break
                 }
                 // detach all screens that should not be visible
-                if (fragmentWrapper !== newTop && !dismissedWrappers.contains(fragmentWrapper)) {
+                if ((fragmentWrapper !== newTop && !dismissedWrappers.contains(fragmentWrapper)) ||
+                    fragmentWrapper.screen.activityState === Screen.ActivityState.INACTIVE
+                ) {
                     it.remove(fragmentWrapper.fragment)
                 }
             }

apps/src/tests/TestActivityStateProgression.tsx
@@ -1,0 +1,69 @@
+import * as React from 'react';
+import { View, Text, Button } from 'react-native';
+import { Screen, ScreenStack } from '../../../src';
+
+function HomeScreen({
+  setActivityState,
+}: {
+  setActivityState: (state: 0 | 1 | 2) => void;
+}) {
+  console.log('Render home');
+  return (
+    <View
+      style={{
+        flex: 1,
+        gap: 8,
+        alignItems: 'center',
+        justifyContent: 'center',
+      }}>
+      <Text>Home!</Text>
+      <Button
+        title="Set Activity State to 2"
+        onPress={() => setActivityState(2)}
+      />
+    </View>
+  );
+}
+
+function ProfileScreen({
+  setActivityState,
+}: {
+  setActivityState: (state: 0 | 1 | 2) => void;
+}) {
+  console.log('Render profile');
+  return (
+    <View
+      style={{
+        flex: 1,
+        gap: 8,
+        alignItems: 'center',
+        justifyContent: 'center',
+      }}>
+      <Text>Profile!</Text>
+      <Button
+        title="Set Activity State to 0"
+        onPress={() => setActivityState(0)}
+      />
+    </View>
+  );
+}
+
+const App = () => {
+  const [activityState, setActivityState] = React.useState<0 | 1 | 2>(0);
+  console.log('activityState', activityState);
+
+  /**
+   * Remove isNativeStack if you want to test native checks
+   */
+  return (
+    <ScreenStack style={{ flex: 1 }}>
+      <Screen activityState={2} isNativeStack>
+        <HomeScreen setActivityState={setActivityState} />
+      </Screen>
+      <Screen activityState={activityState} isNativeStack>
+        <ProfileScreen setActivityState={setActivityState} />
+      </Screen>
+    </ScreenStack>
+  );
+};
+export default App;

apps/src/tests/TestPreload.tsx
@@ -1,0 +1,352 @@
+import * as React from 'react';
+
+import { View, Text, Button } from 'react-native';
+import {
+  useNavigation,
+  CommonActions,
+  NavigationContainer,
+} from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+
+function HomeScreen() {
+  const navigation = useNavigation();
+
+  return (
+    <View
+      style={{
+        flex: 1,
+        gap: 8,
+        alignItems: 'center',
+        justifyContent: 'center',
+      }}>
+      <Text>Home!</Text>
+      <Button
+        title="Navigate to Home"
+        onPress={() => {
+          navigation.dispatch(CommonActions.navigate('Home2'));
+        }}
+      />
+      <Button
+        title="Preload CardProfile"
+        onPress={() => {
+          navigation.dispatch(
+            CommonActions.preload('CardProfile', { user: 'jane' }),
+          );
+        }}
+      />
+      <Button
+        title="Navigate to CardProfile"
+        onPress={() => {
+          navigation.dispatch(
+            CommonActions.navigate('CardProfile', { user: 'jane' }),
+          );
+        }}
+      />
+      <Button
+        title="Preload ModalProfile"
+        onPress={() => {
+          navigation.dispatch(
+            CommonActions.preload('ModalProfile', { user: 'jane' }),
+          );
+        }}
+      />
+      <Button
+        title="Navigate to ModalProfile"
+        onPress={() => {
+          navigation.dispatch(
+            CommonActions.navigate('ModalProfile', { user: 'jane' }),
+          );
+        }}
+      />
+      <Button
+        title="Preload TabScreen"
+        onPress={() => {
+          navigation.dispatch(
+            CommonActions.preload('TabScreen', { user: 'jane' }),
+          );
+        }}
+      />
+      <Button
+        title="Navigate to TabScreen"
+        onPress={() => {
+          navigation.dispatch(
+            CommonActions.navigate('TabScreen', { user: 'jane' }),
+          );
+        }}
+      />
+    </View>
+  );
+}
+
+function ProfileScreen({ route }: any) {
+  const navigation = useNavigation();
+  const [startTime] = React.useState(Date.now());
+  const [endTime, setEndTime] = React.useState<number | null>(null);
+
+  React.useEffect(() => {
+    const unsubscribe = navigation.addListener('focus', () => {
+      setEndTime(Date.now());
+    });
+
+    return () => {
+      unsubscribe();
+    };
+  }, [navigation]);
+
+  return (
+    <View
+      style={{
+        flex: 1,
+        gap: 8,
+        alignItems: 'center',
+        justifyContent: 'center',
+      }}>
+      <Text>Profile!</Text>
+      <Text>{route.params.user}'s profile</Text>
+      <Text>Preloaded for: {endTime ? endTime - startTime : 'N/A'}ms</Text>
+    </View>
+  );
+}
+
+const Tab = createBottomTabNavigator();
+const StackTabA = createNativeStackNavigator();
+
+function TabA() {
+  return (
+    <StackTabA.Navigator>
+      <StackTabA.Screen name="TabA_1" component={TabA_1} />
+      <StackTabA.Screen name="TabA_2" component={TabA_2} />
+    </StackTabA.Navigator>
+  );
+}
+
+function TabA_1() {
+  const navigation = useNavigation();
+  const [startTime] = React.useState(Date.now());
+  const [endTime, setEndTime] = React.useState<number | null>(null);
+
+  React.useEffect(() => {
+    const unsubscribe = navigation.addListener('focus', () => {
+      setEndTime(Date.now());
+    });
+
+    return () => {
+      unsubscribe();
+    };
+  }, [navigation]);
+
+  return (
+    <View
+      style={{
+        flex: 1,
+        gap: 8,
+        alignItems: 'center',
+        justifyContent: 'center',
+      }}>
+      <Text>TabA_1!</Text>
+      <Text>Preloaded for: {endTime ? endTime - startTime : 'N/A'}ms</Text>
+      <Button
+        title="Preload TabA_2"
+        onPress={() => {
+          navigation.dispatch(
+            CommonActions.preload('TabA_2', { user: 'jane' }),
+          );
+        }}
+      />
+      <Button
+        title="Navigate to TabA_2"
+        onPress={() => {
+          navigation.dispatch(
+            CommonActions.navigate('TabA_2', { user: 'jane' }),
+          );
+        }}
+      />
+    </View>
+  );
+}
+
+function TabA_2() {
+  const navigation = useNavigation();
+  const [startTime] = React.useState(Date.now());
+  const [endTime, setEndTime] = React.useState<number | null>(null);
+
+  React.useEffect(() => {
+    const unsubscribe = navigation.addListener('focus', () => {
+      setEndTime(Date.now());
+    });
+
+    return () => {
+      unsubscribe();
+    };
+  }, [navigation]);
+
+  return (
+    <View
+      style={{
+        flex: 1,
+        gap: 8,
+        alignItems: 'center',
+        justifyContent: 'center',
+      }}>
+      <Text>TabA_2!</Text>
+      <Text>Preloaded for: {endTime ? endTime - startTime : 'N/A'}ms</Text>
+      <Button
+        title="Preload ModalProfile"
+        onPress={() => {
+          navigation.dispatch(
+            CommonActions.preload('ModalProfile', { user: 'jane' }),
+          );
+        }}
+      />
+      <Button
+        title="Navigate to ModalProfile"
+        onPress={() => {
+          navigation.dispatch(
+            CommonActions.navigate('ModalProfile', { user: 'jane' }),
+          );
+        }}
+      />
+    </View>
+  );
+}
+
+const StackTabB = createNativeStackNavigator();
+
+function TabB() {
+  return (
+    <StackTabA.Navigator>
+      <StackTabA.Screen name="TabB_1" component={TabB_1} />
+      <StackTabA.Screen name="TabB_2" component={TabB_2} />
+    </StackTabA.Navigator>
+  );
+}
+
+function TabB_1() {
+  const navigation = useNavigation();
+  const [startTime] = React.useState(Date.now());
+  const [endTime, setEndTime] = React.useState<number | null>(null);
+
+  React.useEffect(() => {
+    const unsubscribe = navigation.addListener('focus', () => {
+      setEndTime(Date.now());
+    });
+
+    return () => {
+      unsubscribe();
+    };
+  }, [navigation]);
+
+  return (
+    <View
+      style={{
+        flex: 1,
+        gap: 8,
+        alignItems: 'center',
+        justifyContent: 'center',
+      }}>
+      <Text>TabB_1!</Text>
+      <Text>Preloaded for: {endTime ? endTime - startTime : 'N/A'}ms</Text>
+      <Button
+        title="Preload TabB_2"
+        onPress={() => {
+          navigation.dispatch(
+            CommonActions.preload('TabB_2', { user: 'jane' }),
+          );
+        }}
+      />
+      <Button
+        title="Navigate to TabB_2"
+        onPress={() => {
+          navigation.dispatch(
+            CommonActions.navigate('TabB_2', { user: 'jane' }),
+          );
+        }}
+      />
+    </View>
+  );
+}
+
+function TabB_2() {
+  const navigation = useNavigation();
+  const [startTime] = React.useState(Date.now());
+  const [endTime, setEndTime] = React.useState<number | null>(null);
+
+  React.useEffect(() => {
+    const unsubscribe = navigation.addListener('focus', () => {
+      setEndTime(Date.now());
+    });
+
+    return () => {
+      unsubscribe();
+    };
+  }, [navigation]);
+
+  return (
+    <View
+      style={{
+        flex: 1,
+        gap: 8,
+        alignItems: 'center',
+        justifyContent: 'center',
+      }}>
+      <Text>TabB_2!</Text>
+      <Text>Preloaded for: {endTime ? endTime - startTime : 'N/A'}ms</Text>
+      <Button
+        title="Preload ModalProfile"
+        onPress={() => {
+          navigation.dispatch(
+            CommonActions.preload('ModalProfile', { user: 'jane' }),
+          );
+        }}
+      />
+      <Button
+        title="Navigate to ModalProfile"
+        onPress={() => {
+          navigation.dispatch(
+            CommonActions.navigate('ModalProfile', { user: 'jane' }),
+          );
+        }}
+      />
+    </View>
+  );
+}
+
+function TabScreen({ route }: any) {
+  console.log('route?.params?.user', route?.params?.user);
+
+  return (
+    <Tab.Navigator>
+      <Tab.Screen name="TabA" component={TabA} />
+      <Tab.Screen name="TabB" component={TabB} />
+    </Tab.Navigator>
+  );
+}
+
+const Stack = createNativeStackNavigator();
+
+const App = () => (
+  <NavigationContainer>
+    <Stack.Navigator>
+      <Stack.Screen name="Home" component={HomeScreen} />
+      <Stack.Screen
+        name="Home2"
+        component={HomeScreen}
+        options={{ presentation: 'card' }}
+      />
+      <Stack.Screen
+        name="CardProfile"
+        component={ProfileScreen}
+        options={{ presentation: 'card' }}
+      />
+      <Stack.Screen
+        name="ModalProfile"
+        component={ProfileScreen}
+        options={{ presentation: 'modal' }}
+      />
+      <Stack.Screen name="TabScreen" component={TabScreen} />
+    </Stack.Navigator>
+  </NavigationContainer>
+);
+
+export default App;

apps/src/tests/index.ts
@@ -115,5 +115,7 @@ export { default as Test2332 } from './Test2332';
 export { default as Test2395 } from './Test2395';
 export { default as TestScreenAnimation } from './TestScreenAnimation';
 export { default as TestHeader } from './TestHeader';
+export { default as TestPreload } from './TestPreload';
+export { default as TestActivityStateProgression } from './TestActivityStateProgression';
 export { default as TestHeaderTitle } from './TestHeaderTitle';
 export { default as TestModalNavigation } from './TestModalNavigation';

src/components/Screen.tsx
@@ -16,6 +16,7 @@ import {
 // Native components
 import ScreenNativeComponent from '../fabric/ScreenNativeComponent';
 import ModalScreenNativeComponent from '../fabric/ModalScreenNativeComponent';
+import { usePrevious } from './helpers/usePrevious';
 
 type NativeScreenProps = Omit<
   ScreenProps,
@@ -165,6 +166,7 @@ export const InnerScreen = React.forwardRef<View, ScreenProps>(
   function InnerScreen(props, ref) {
     const innerRef = React.useRef<ViewConfig | null>(null);
     React.useImperativeHandle(ref, () => innerRef.current!, []);
+    const prevActivityState = usePrevious(props.activityState);
 
     const setRef = (ref: ViewConfig) => {
       innerRef.current = ref;
@@ -237,6 +239,18 @@ export const InnerScreen = React.forwardRef<View, ScreenProps>(
           'It appears that you are using old version of react-navigation library. Please update @react-navigation/bottom-tabs, @react-navigation/stack and @react-navigation/drawer to version 5.10.0 or above to take full advantage of new functionality added to react-native-screens',
         );
         activityState = active !== 0 ? 2 : 0; // in the new version, we need one of the screens to have value of 2 after the transition
+      }
+
+      if (
+        isNativeStack &&
+        prevActivityState !== undefined &&
+        activityState !== undefined
+      ) {
+        if (prevActivityState > activityState) {
+          throw new Error(
+            '[RNScreens] activityState cannot be decreased in NativeStack',
+          );
+        }
       }
 
       const handleRef = (ref: ViewConfig) => {

commit 778c8ee8d9b835a43d23395a6468257e739c7f07

fix(iOS): `activityState` regression check false-positive (#2404)
## Description

We had an issue introduced in #2389 that added the preload. We
temporarily
have an assumption in native stack, that the `activityState` can only
progress when
`Screen` component is rendered (directly) inside native stack navigator.
This invariant
was enforced with appropriate checks on native side. It turns out that
we were wrong in our
way of checking whether the `Screen` belongs to native stack or not - we
have forgotten about
`RNSContainerNavigationController` class that is used when rendering
`ScreenContainer` with `hasTwoStates: true`
and which inherits from `RNSNavigationController`.

## Changes

~Added a `isNativeStackViewController` method to
`RNSNavigationController` and overrode it in subclass.~

Added `RNSScreenView#isNativeStackScreen` method, which checks whether
our direct react superview is a `RNSScreenStackView`.

I've found checking for `_controller.navigationController` not reliable,
as `navigationController` can be found arbitrarily high in the view
hierarchy.

## Test code and steps to reproduce

Reported by Expo. If you want to reproduce this: create a JS stack
navigator with bottom tabs inside (with `hasTwoStates: true`),
and then a regular JS stack inside & try to navigate to any screen. 

We have not caught that earlier, because we're not testing regular JS
stack at all.

## Checklist

- [ ] Included code example that can be used to test this change
- [x] Ensured that CI passes

ios/RNSScreen.mm
@@ -307,13 +307,23 @@ constexpr NSInteger SHEET_LARGEST_UNDIMMED_DETENT_NONE = -1;
 {
   int activityState = [activityStateOrNil intValue];
   if (activityStateOrNil != nil && activityState != -1 && activityState != _activityState) {
-    if ([_controller.navigationController isKindOfClass:RNSNavigationController.class] &&
-        _activityState < activityState) {
-      RCTLogError(@"[RNScreens] activityState can only progress in NativeStack");
-    }
+    [self maybeAssertActivityStateProgressionOldValue:_activityState newValue:activityState];
     _activityState = activityState;
     [_reactSuperview markChildUpdated];
   }
+}
+
+- (void)maybeAssertActivityStateProgressionOldValue:(int)oldValue newValue:(int)newValue
+{
+  if (self.isNativeStackScreen && newValue < oldValue) {
+    RCTLogError(@"[RNScreens] activityState can only progress in NativeStack");
+  }
+}
+
+/// Note that this method works only after the screen is actually mounted under a screen stack view.
+- (BOOL)isNativeStackScreen
+{
+  return [_reactSuperview isKindOfClass:RNSScreenStackView.class];
 }
 
 #if !TARGET_OS_TV && !TARGET_OS_VISION

ios/RNSScreenStack.mm
@@ -953,7 +953,7 @@ namespace react = facebook::react;
 
 - (void)markChildUpdated
 {
-  // do nothing
+  // In native stack this should be called only for `preload` purposes.
   [self updateContainer];
 }
 