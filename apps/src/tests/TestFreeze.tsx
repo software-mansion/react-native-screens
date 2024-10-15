d26aad7b2bf1adf86a097b0aa6274c4d227f64f8
alduzy <alduzy@gmail.com>
2024-10-15 14:29:32 +0200

    simplified test

--- apps/src/tests/TestFreeze.tsx
+++ apps/src/tests/TestFreeze.tsx
@@ -1,95 +1,32 @@
 import React, { useState, useEffect } from 'react';
 import { View, Text, Button, StyleSheet } from 'react-native';
-import { NavigationContainer, ParamListBase } from '@react-navigation/native';
+import {
+  NavigationContainer,
+  ParamListBase,
+  RouteProp,
+} from '@react-navigation/native';
 import {
   createNativeStackNavigator,
   NativeStackNavigationProp,
 } from '@react-navigation/native-stack';
-import {
-  BottomTabNavigationProp,
-  createBottomTabNavigator,
-} from '@react-navigation/bottom-tabs';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 
-function FirstTabScreen({
-  navigation,
-}: {
-  navigation: BottomTabNavigationProp<ParamListBase>;
-}) {
+function TabScreen({ route }: { route: RouteProp<ParamListBase> }) {
   const [state, setState] = useState(false);
-
-  console.log('[FIRST TAB] render');
-  return (
-    <View
-      style={styles.container}
-      onLayout={e => {
-        console.log(
-          '[FIRST TAB] screen onLayout layout:',
-          e.nativeEvent.layout,
-        );
-        setState(!state);
-      }}>
-      <Text style={styles.title}>First Tab</Text>
-      <Text style={styles.text}>
-        Rotate device - only [FIRST TAB] logs should appear
-      </Text>
-      <Button
-        title="Go to second tab"
-        onPress={() => navigation.navigate('Second Tab')}
-      />
-    </View>
-  );
-}
-
-function SecondTabScreen({
-  navigation,
-}: {
-  navigation: BottomTabNavigationProp<ParamListBase>;
-}) {
-  const [state, setState] = useState(false);
-
-  console.log('[SECOND TAB] render');
+  const name = '[' + route.name.toUpperCase() + ']';
+  console.log(name, 'render');
 
   return (
     <View
       style={styles.container}
       onLayout={e => {
-        console.log('[SECOND TAB] screen onLayout', e.nativeEvent.layout);
+        console.log(name, 'screen onLayout', e.nativeEvent.layout);
         setState(!state);
       }}>
-      <Text style={styles.title}>Second Tab</Text>
+      <Text style={styles.title}>{name}</Text>
       <Text style={styles.text}>
-        Rotate device - only [SECOND TAB] logs should appear
+        Rotate device - only "{name}" logs should appear
       </Text>
-      <Button
-        title="Go to third tab"
-        onPress={() => navigation.navigate('Third Tab')}
-      />
-      <Button title="Go back" onPress={navigation.goBack} />
-    </View>
-  );
-}
-
-function ThirdTabScreen({
-  navigation,
-}: {
-  navigation: BottomTabNavigationProp<ParamListBase>;
-}) {
-  const [state, setState] = useState(false);
-
-  console.log('[THIRD TAB] render');
-
-  return (
-    <View
-      style={styles.container}
-      onLayout={e => {
-        console.log('[THIRD TAB] screen onLayout', e.nativeEvent.layout);
-        setState(!state);
-      }}>
-      <Text style={styles.title}>Third Tab</Text>
-      <Text style={styles.text}>
-        Rotate device - only [THIRD TAB] logs should appear
-      </Text>
-      <Button title="Go back" onPress={navigation.goBack} />
     </View>
   );
 }
@@ -120,8 +57,8 @@ function StackScreen({
   navigation: NativeStackNavigationProp<ParamListBase>;
 }) {
   const value = useValue();
-  // only 1 'render' should appear at the time
   console.log('render', value);
+  
   return (
     <View style={styles.container}>
       <Text style={styles.title}>Stack Screen</Text>
@@ -162,9 +99,9 @@ export default function App() {
   return (
     <NavigationContainer>
       <Tab.Navigator screenOptions={{ freezeOnBlur: true }}>
-        <Tab.Screen name="First Tab" component={FirstTabScreen} />
-        <Tab.Screen name="Second Tab" component={SecondTabScreen} />
-        <Tab.Screen name="Third Tab" component={ThirdTabScreen} />
+        <Tab.Screen name="First Tab" component={TabScreen} />
+        <Tab.Screen name="Second Tab" component={TabScreen} />
+        <Tab.Screen name="Third Tab" component={TabScreen} />
         <Tab.Screen
           name="Native Stack"
           component={StackNavigator}
