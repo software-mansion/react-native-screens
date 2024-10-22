9331a629b862b672040dc7edfac431e89386bdf7
Kacper Kafara <kacperkafara@gmail.com>
2024-10-22 17:08:22 +0200

    Update reproduction

--- apps/src/tests/Test2424.tsx
+++ apps/src/tests/Test2424.tsx
@@ -7,6 +7,7 @@ import { Button, ScrollView, Text, View } from "react-native";
 type RouteParamList = {
   Home: undefined;
   Sheet: undefined;
+  ReproSheet: undefined;
 }
 
 const Stack = createNativeStackNavigator<RouteParamList>();
@@ -15,6 +16,7 @@ function Home({ navigation }: { navigation: NativeStackNavigationProp<RouteParam
   return (
     <View style={{ flex: 1, backgroundColor: 'lightgreen' }}>
       <Button title="Open Sheet" onPress={() => navigation.navigate('Sheet')} />
+      <Button title="Open reproduction sheet" onPress={() => navigation.navigate('ReproSheet')} />
     </View>
 
   )
@@ -22,10 +24,25 @@ function Home({ navigation }: { navigation: NativeStackNavigationProp<RouteParam
 
 function Sheet(): React.JSX.Element {
   return (
-    <View style={{backgroundColor: 'crimson' }} collapsable={false} >
-      <ScrollView>
+    <View style={{backgroundColor: 'crimson', flex: 1 }} collapsable={false} >
+      <ScrollView style={{ flex: 1 }}>
         {Array.from({ length: 90 }).map((_, index) => (<Text key={index}>I'm a long scrollview</Text>))}
         <View style={{ width: '100%', height: 50, backgroundColor: 'seagreen' }} />
+      </ScrollView>
+    </View>
+  )
+}
+
+function SheetFromReproduction(): React.JSX.Element {
+  return (
+    <View style={{ flex: 1 }} >
+      <ScrollView style={{ flex: 1 }}>
+        <Text>Modal Screen</Text>
+        <View style={{ height: 2000, backgroundColor: 'red' }}>
+          {Array.from({ length: 100 }).map((_, index) => (
+            <Text key={index}>I'm a long scrollview</Text>
+          ))}
+        </View>
       </ScrollView>
     </View>
   )
@@ -38,8 +55,16 @@ export default function App() {
         <Stack.Screen name="Home" component={Home} />
         <Stack.Screen name='Sheet' component={Sheet} options={{
           presentation: 'formSheet',
-          headerShown: false,
-          sheetAllowedDetents: [0.5, 1.0],
+          headerShown: true,
+          sheetAllowedDetents: [0.9],
+          unstable_screenStyle: {
+            backgroundColor: 'crimson'
+          }
+        }} />
+        <Stack.Screen name='ReproSheet' component={SheetFromReproduction} options={{
+          presentation: 'formSheet',
+          headerShown: true,
+          sheetAllowedDetents: [0.9],
           unstable_screenStyle: {
             backgroundColor: 'crimson'
           }
