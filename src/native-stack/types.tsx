1f929a3444ee55d27abcf7ae05ccf09b83aa6646
Maciej Stosio <maciekstosio@users.noreply.github.com>
2024-10-22 16:29:55 +0200

    chore: add info about workaround for formSheet

--- src/native-stack/types.tsx
+++ src/native-stack/types.tsx
@@ -103,6 +103,8 @@ export type NativeStackNavigationOptions = {
   backButtonInCustomView?: boolean;
   /**
    * Style object for the scene content.
+   *
+   * As a workaround to truncated sheet content, formSheet uses backgroundColor from contentStyle and applies it on Screen.
    */
   contentStyle?: StyleProp<ViewStyle>;
   /**
