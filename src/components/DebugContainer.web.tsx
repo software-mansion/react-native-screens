b62f479d4b1629c6003cf17e62b9cf530107e577
Kacper Kafara <kacperkafara@gmail.com>
2024-10-30 13:55:37 +0100

    Linter

--- src/components/DebugContainer.web.tsx
+++ src/components/DebugContainer.web.tsx
@@ -2,6 +2,6 @@ import * as React from 'react';
 import { type ViewProps } from 'react-native';
 import ScreenContentWrapper from './ScreenContentWrapper';
 
-export default function DebugContainer(props: ViewProps)  {
+export default function DebugContainer(props: ViewProps) {
   return <ScreenContentWrapper {...props} />;
 }
