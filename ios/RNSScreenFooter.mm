adf25def3dfdc8889da7a34eac395c05fd497c21
Kacper Kafara <kacperkafara@gmail.com>
2024-11-13 12:14:36 +0100

    Fix series of compile time warnings

--- ios/RNSScreenFooter.mm
+++ ios/RNSScreenFooter.mm
@@ -26,35 +26,34 @@
 - (void)willMoveToSuperview:(UIView *)newSuperview
 {
   [super willMoveToSuperview:newSuperview];
-  if ([newSuperview isKindOfClass:RNSScreenView.class]) {
-    RNSScreenView *screen = (RNSScreenView *)newSuperview;
-    _parent = (RNSScreenView *)newSuperview;
+  //  if ([newSuperview isKindOfClass:RNSScreenView.class]) {
+  //    RNSScreenView *screen = (RNSScreenView *)newSuperview;
+  //    _parent = (RNSScreenView *)newSuperview;
 
-    //    [NSLayoutConstraint activateConstraints:@[
-    //      [NSLayoutConstraint constraintWithItem:self attribute:NSLayoutAttributeBottom
-    //      relatedBy:NSLayoutRelationEqual toItem:screen attribute:NSLayoutAttributeBottom multiplier:1.0
-    //      constant:0.0], [NSLayoutConstraint constraintWithItem:self attribute:NSLayoutAttributeLeft
-    //      relatedBy:NSLayoutRelationEqual toItem:screen attribute:NSLayoutAttributeLeft multiplier:1.0 constant:0.0],
-    //      [NSLayoutConstraint constraintWithItem:self attribute:NSLayoutAttributeRight relatedBy:NSLayoutRelationEqual
-    //      toItem:screen attribute:NSLayoutAttributeRight multiplier:1.0 constant:0.0], [NSLayoutConstraint
-    //      constraintWithItem:self attribute:NSLayoutAttributeTop relatedBy:NSLayoutRelationEqual toItem:screen
-    //      attribute:NSLayoutAttributeTop multiplier:1.0 constant:0.0], [NSLayoutConstraint constraintWithItem:screen
-    //      attribute:NSLayoutAttributeBottom relatedBy:NSLayoutRelationEqual toItem:self
-    //      attribute:NSLayoutAttributeBottom multiplier:1.0 constant:0.0], [NSLayoutConstraint
-    //      constraintWithItem:screen attribute:NSLayoutAttributeLeft relatedBy:NSLayoutRelationEqual toItem:self
-    //      attribute:NSLayoutAttributeLeft multiplier:1.0 constant:0.0], [NSLayoutConstraint constraintWithItem:screen
-    //      attribute:NSLayoutAttributeRight relatedBy:NSLayoutRelationEqual toItem:self
-    //      attribute:NSLayoutAttributeRight multiplier:1.0 constant:0.0], [NSLayoutConstraint constraintWithItem:screen
-    //      attribute:NSLayoutAttributeTop relatedBy:NSLayoutRelationEqual toItem:self attribute:NSLayoutAttributeTop
-    //      multiplier:1.0 constant:0.0],
-    //    ]];
-    //    [self setNeedsLayout];
-  }
+  //    [NSLayoutConstraint activateConstraints:@[
+  //      [NSLayoutConstraint constraintWithItem:self attribute:NSLayoutAttributeBottom
+  //      relatedBy:NSLayoutRelationEqual toItem:screen attribute:NSLayoutAttributeBottom multiplier:1.0
+  //      constant:0.0], [NSLayoutConstraint constraintWithItem:self attribute:NSLayoutAttributeLeft
+  //      relatedBy:NSLayoutRelationEqual toItem:screen attribute:NSLayoutAttributeLeft multiplier:1.0 constant:0.0],
+  //      [NSLayoutConstraint constraintWithItem:self attribute:NSLayoutAttributeRight relatedBy:NSLayoutRelationEqual
+  //      toItem:screen attribute:NSLayoutAttributeRight multiplier:1.0 constant:0.0], [NSLayoutConstraint
+  //      constraintWithItem:self attribute:NSLayoutAttributeTop relatedBy:NSLayoutRelationEqual toItem:screen
+  //      attribute:NSLayoutAttributeTop multiplier:1.0 constant:0.0], [NSLayoutConstraint constraintWithItem:screen
+  //      attribute:NSLayoutAttributeBottom relatedBy:NSLayoutRelationEqual toItem:self
+  //      attribute:NSLayoutAttributeBottom multiplier:1.0 constant:0.0], [NSLayoutConstraint
+  //      constraintWithItem:screen attribute:NSLayoutAttributeLeft relatedBy:NSLayoutRelationEqual toItem:self
+  //      attribute:NSLayoutAttributeLeft multiplier:1.0 constant:0.0], [NSLayoutConstraint constraintWithItem:screen
+  //      attribute:NSLayoutAttributeRight relatedBy:NSLayoutRelationEqual toItem:self
+  //      attribute:NSLayoutAttributeRight multiplier:1.0 constant:0.0], [NSLayoutConstraint constraintWithItem:screen
+  //      attribute:NSLayoutAttributeTop relatedBy:NSLayoutRelationEqual toItem:self attribute:NSLayoutAttributeTop
+  //      multiplier:1.0 constant:0.0],
+  //    ]];
+  //    [self setNeedsLayout];
+  //  }
 }
 
 - (void)didMoveToSuperview
 {
-  NSLog(@"Adding constraints between %@ and %@", self, _parent);
   if (_parent != nil) {
     //    [NSLayoutConstraint activateConstraints:@[
     //      [NSLayoutConstraint constraintWithItem:self
