61886888e36378c098b78fd26a857c256e1427c7
Kacper Kafara <kacperkafara@gmail.com>
2024-12-06 16:39:21 +0100

    Fix for Paper

--- ios/RNSScreenStackHeaderConfig.mm
+++ ios/RNSScreenStackHeaderConfig.mm
@@ -456,7 +456,7 @@ RNS_IGNORE_SUPER_CALL_END
     UIImage *shadowImage = appearance.shadowImage;
     // transparent background color
     [appearance configureWithTransparentBackground];
-      
+
     if (!config.hideShadow) {
       appearance.shadowColor = shadowColor;
       appearance.shadowImage = shadowImage;
@@ -688,11 +688,13 @@ RNS_IGNORE_SUPER_CALL_END
     UINavigationBarAppearance *scrollEdgeAppearance =
         [[UINavigationBarAppearance alloc] initWithBarAppearance:appearance];
     if (config.largeTitleBackgroundColor != nil) {
-      // Add support for using a fully transparent bar when the backgroundColor is set to transparent. 
+      // Add support for using a fully transparent bar when the backgroundColor is set to transparent.
       if (CGColorGetAlpha(config.largeTitleBackgroundColor.CGColor) == 0.) {
-      // This will also remove the background blur effect in the large title which is otherwise inherited from the standard appearance.
+        // This will also remove the background blur effect in the large title which is otherwise inherited from the
+        // standard appearance.
         [scrollEdgeAppearance configureWithTransparentBackground];
-        // This must be set to nil otherwise a default view will be added to the navigation bar background with an opaque background.
+        // This must be set to nil otherwise a default view will be added to the navigation bar background with an
+        // opaque background.
         scrollEdgeAppearance.backgroundColor = nil;
       } else {
         scrollEdgeAppearance.backgroundColor = config.largeTitleBackgroundColor;
@@ -852,6 +854,8 @@ RNS_IGNORE_SUPER_CALL_BEGIN
 
   //  [_reactSubviews insertObject:(RNSScreenStackHeaderSubview *)childComponentView atIndex:index];
   [self insertReactSubview:(RNSScreenStackHeaderSubview *)childComponentView atIndex:index];
+
+  // TODO: This could be called only once per transaction.
   [self updateViewControllerIfNeeded];
 }
 
