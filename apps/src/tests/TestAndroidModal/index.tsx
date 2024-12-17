9b15d4ad8491ed26dee9559cf8cd52f8abd24354
Kacper Kafara <kacperkafara@gmail.com>
2024-12-17 03:50:23 +0100

    Add reproduction

---
+++ apps/src/tests/TestAndroidModal/index.tsx
@@ -0,0 +1,92 @@
+import React from 'react';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import { HeaderButton, Text } from '@react-navigation/elements';
+import {
+  createStaticNavigation,
+  StaticParamList,
+} from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+//import { Image } from "react-native";
+import { Home } from './screens/Home';
+import { Profile } from './screens/Profile';
+import { Settings } from './screens/Settings';
+import { Updates } from './screens/Updates';
+import { NotFound } from './screens/NotFound';
+
+const HomeTabs = createBottomTabNavigator({
+  screens: {
+    Home: {
+      screen: Home,
+      options: {
+        title: 'Feed',
+      },
+    },
+    Updates: {
+      screen: Updates,
+      options: {
+      },
+    },
+  },
+});
+
+const RootStack = createNativeStackNavigator({
+  screens: {
+    HomeTabs: {
+      screen: HomeTabs,
+      options: {
+        title: 'Home',
+        headerShown: false,
+      },
+    },
+    Profile: {
+      screen: Profile,
+      linking: {
+        path: ':user(@[a-zA-Z0-9-_]+)',
+        parse: {
+          user: (value) => value.replace(/^@/, ''),
+        },
+        stringify: {
+          user: (value) => `@${value}`,
+        },
+      },
+    },
+    Settings: {
+      screen: Settings,
+      options: ({ navigation }) => ({
+        presentation: 'modal',
+        animation: 'slide_from_bottom',
+        headerShown: true,
+        headerRight: () => (
+          <HeaderButton onPress={navigation.goBack}>
+            <Text>Close</Text>
+          </HeaderButton>
+        ),
+      }),
+    },
+    NotFound: {
+      screen: NotFound,
+      options: {
+        title: '404',
+      },
+      linking: {
+        path: '*',
+      },
+    },
+  },
+});
+
+const Navigation = createStaticNavigation(RootStack);
+
+export default function App() {
+  return (
+    <Navigation />
+  );
+}
+
+type RootStackParamList = StaticParamList<typeof RootStack>;
+
+declare global {
+  namespace ReactNavigation {
+    interface RootParamList extends RootStackParamList {}
+  }
+}
