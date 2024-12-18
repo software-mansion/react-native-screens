3d5271896b960171f2c74f8b82ad18637c0d46ac
Kacper Kafara <kacperkafara@gmail.com>
2024-12-18 12:13:25 +0100

    Code changes for FabricExample

--- FabricExample/metro.config.js
+++ FabricExample/metro.config.js
@@ -4,7 +4,7 @@ const { getDefaultConfig, mergeConfig } = require('@react-native/metro-config');
  * Metro configuration
  * https://reactnative.dev/docs/metro
  *
- * @type {import('metro-config').MetroConfig}
+ * @type {import('@react-native/metro-config').MetroConfig}
  */
 
 const fs = require('fs');
