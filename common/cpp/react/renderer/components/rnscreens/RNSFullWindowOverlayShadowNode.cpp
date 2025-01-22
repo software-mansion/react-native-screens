4e9889539c47a9a53f0be88636359d33f91546db
Kacper Kafara <kacperkafara@gmail.com>
2025-01-22 17:25:34 +0100

    Add some potentially useful comment

--- common/cpp/react/renderer/components/rnscreens/RNSFullWindowOverlayShadowNode.cpp
+++ common/cpp/react/renderer/components/rnscreens/RNSFullWindowOverlayShadowNode.cpp
@@ -16,6 +16,12 @@ void RNSFullWindowOverlayShadowNode::applyFrameCorrections() {
   ensureUnsealed();
 
   const auto &stateData = getStateData();
+
+  // During mounting phase due to view flattening the frame origin can be
+  // offsetted by non-zero point value, while we require FullWindowOverlay view
+  // to be always positioned at (0, 0). By adding this correction, the view will
+  // be positioned at (0, 0) after mounting stage finishes. See:
+  // https://github.com/software-mansion/react-native-screens/pull/2641
   layoutMetrics_.frame.origin -= stateData.contentOffset;
 }
 
