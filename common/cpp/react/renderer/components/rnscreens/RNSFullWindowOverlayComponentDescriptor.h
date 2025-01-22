0f291e6f131981336689b42d18e9f2dcc68f94e7
Kacper Kafara <kacperkafara@gmail.com>
2025-01-22 17:19:38 +0100

    Clear imports in component descriptor implementation

--- common/cpp/react/renderer/components/rnscreens/RNSFullWindowOverlayComponentDescriptor.h
+++ common/cpp/react/renderer/components/rnscreens/RNSFullWindowOverlayComponentDescriptor.h
@@ -3,15 +3,10 @@
 #ifdef ANDROID
 #include <fbjni/fbjni.h>
 #endif // ANDROID
-#include <react/debug/react_native_assert.h>
-#include <react/renderer/components/rnscreens/Props.h>
-#include <react/renderer/components/rnscreens/utils/RectUtil.h>
 #include <react/renderer/core/ConcreteComponentDescriptor.h>
 #include "RNSFullWindowOverlayShadowNode.h"
 
 namespace facebook::react {
-
-using namespace rnscreens;
 
 class RNSFullWindowOverlayComponentDescriptor final
     : public ConcreteComponentDescriptor<RNSFullWindowOverlayShadowNode> {
