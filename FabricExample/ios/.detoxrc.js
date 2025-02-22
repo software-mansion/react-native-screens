0b91a016bbb0ac4af386be019124847653c947c9
Kacper Kafara <kacperkafara@gmail.com>
2025-02-22 18:22:08 +0100

    Add Fabric detox configuration

---
+++ FabricExample/ios/.detoxrc.js
@@ -0,0 +1,97 @@
+/** @type {Detox.DetoxConfig} */
+module.exports = {
+  testRunner: {
+    args: {
+      $0: 'jest',
+      config: 'e2e/jest.config.js',
+    },
+    jest: {
+      setupTimeout: 360000,
+    },
+  },
+  apps: {
+    'ios.debug': {
+      type: 'ios.app',
+      binaryPath:
+        'ios/build/Build/Products/Debug-iphonesimulator/FabricExample.app',
+      build:
+        'xcodebuild -workspace ios/FabricExample.xcworkspace -UseNewBuildSystem=YES -scheme FabricExample -configuration Debug -sdk iphonesimulator -derivedDataPath ios/build -quiet | xcpretty',
+    },
+    'ios.release': {
+      type: 'ios.app',
+      binaryPath:
+        'ios/build/Build/Products/Release-iphonesimulator/FabricExample.app',
+      build:
+        'export RCT_NO_LAUNCH_PACKAGER=true && xcodebuild ONLY_ACTIVE_ARCH=YES -workspace ios/FabricExample.xcworkspace -UseNewBuildSystem=YES -scheme FabricExample -configuration Release -sdk iphonesimulator -derivedDataPath ios/build -quiet | xcpretty',
+    },
+    'android.debug': {
+      type: 'android.apk',
+      binaryPath: 'android/app/build/outputs/apk/debug/app-debug.apk',
+      build:
+        'cd android && ./gradlew assembleDebug assembleAndroidTest -DtestBuildType=debug -PreactNativeArchitectures=x86_64 && cd ..',
+      reversePorts: [8081],
+    },
+    'android.release': {
+      type: 'android.apk',
+      binaryPath: 'android/app/build/outputs/apk/release/app-release.apk',
+      build:
+        'cd android && ./gradlew assembleRelease assembleAndroidTest -DtestBuildType=release -PreactNativeArchitectures=x86_64 && cd ..',
+    },
+  },
+  devices: {
+    simulator: {
+      type: 'ios.simulator',
+      device: {
+        type: 'iPhone 15 Pro',
+      },
+    },
+    attached: {
+      type: 'android.attached',
+      device: {
+        adbName: 'e2e_emulator',
+      },
+      utilBinaryPaths: ['e2e/apps/test-butler-app-2.2.1.apk'],
+    },
+    emulator: {
+      type: 'android.emulator',
+      device: {
+        avdName: 'e2e_emulator',
+      },
+      utilBinaryPaths: ['e2e/apps/test-butler-app-2.2.1.apk'],
+    },
+  },
+  configurations: {
+    'ios.sim.debug': {
+      device: 'simulator',
+      app: 'ios.debug',
+    },
+    'ios.sim.release': {
+      device: 'simulator',
+      app: 'ios.release',
+    },
+    'ios.release': {
+      device: 'simulator',
+      app: 'ios.release',
+    },
+    'android.att.debug': {
+      device: 'attached',
+      app: 'android.debug',
+    },
+    'android.att.release': {
+      device: 'attached',
+      app: 'android.release',
+    },
+    'android.emu.debug': {
+      device: 'emulator',
+      app: 'android.debug',
+    },
+    'android.emu.release': {
+      device: 'emulator',
+      app: 'android.release',
+    },
+    'android.release': {
+      device: 'emulator',
+      app: 'android.release',
+    },
+  },
+};
