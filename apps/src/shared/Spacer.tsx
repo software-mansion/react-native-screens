3223baada505f10490bce29f5343e0ba806b324d
Kacper Kafara <kacperkafara@gmail.com>
2025-02-25 11:32:57 +0100

    Add TestFullWindowOverlay

--- apps/src/shared/Spacer.tsx
+++ apps/src/shared/Spacer.tsx
@@ -2,7 +2,7 @@ import React, { ReactNode } from 'react';
 import { View } from 'react-native';
 
 interface Props {
-  children: ReactNode;
+  children?: ReactNode;
 }
 
 export const Spacer = ({ children }: Props): React.JSX.Element => (
