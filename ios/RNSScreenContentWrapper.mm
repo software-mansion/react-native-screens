98a7274b4c6af464cd916e254be94ffdd92734c0
Kacper Kafara <kacperkafara@gmail.com>
2025-02-27 13:45:58 +0100

    Add new architecture specific fix

--- ios/RNSScreenContentWrapper.mm
+++ ios/RNSScreenContentWrapper.mm
@@ -125,9 +125,9 @@ namespace react = facebook::react;
   }
 
   if (self.subviews.count > 2) {
-    // TODO: Log some warning here
     RCTLogWarn(
-        @"[RNScreens] FormSheet with ScrollView expects at most 2 subviews. Got %ld. This might result in incorrect layout.",
+        @"[RNScreens] FormSheet with ScrollView expects at most 2 subviews. Got %ld. This might result in incorrect layout. \
+          If you want to display header alongside the scrollView, make sure to apply `collapsable: false` on your header component view.",
         self.subviews.count);
   }
 
@@ -139,12 +139,23 @@ namespace react = facebook::react;
     newFrame.size = size;
     scrollViewComponent.frame = newFrame;
     return YES;
-  } else if (scrollViewComponentIndex == 1) {
-    UIView *headerView = self.subviews[0];
+  }
 
+  // Case 2: There is a header - we adjust scrollview size by the header height.
+  if (scrollViewComponentIndex == 1) {
+    UIView *headerView = self.subviews[0];
     CGRect newFrame = scrollViewComponent.frame;
     newFrame.size = size;
     newFrame.size.height -= headerView.frame.size.height;
+#ifdef RCT_NEW_ARCH_ENABLED
+    // For some unknown yet reason on new architecture the scroll view
+    // is placed at (0, 0), which makes no sense given there
+    // is another child at lower index in flex layout.
+    // TODO: Research why this happens and solve this properly.
+    if (newFrame.origin.y == 0) {
+      newFrame.origin.y = headerView.frame.size.height;
+    }
+#endif // RCT_NEW_ARCH_ENABLED
     scrollViewComponent.frame = newFrame;
     return YES;
   }
