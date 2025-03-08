64b729943a88c50c486e6527bdedfb7ed2ce304e
Kacper Kafara <kacperkafara@gmail.com>
2025-03-08 19:55:14 +0100

    Move animation logic out to extension method

--- android/src/main/java/com/swmansion/rnscreens/ScreenStack.kt
+++ android/src/main/java/com/swmansion/rnscreens/ScreenStack.kt
@@ -9,6 +9,7 @@ import com.facebook.react.uimanager.UIManagerHelper
 import com.swmansion.rnscreens.Screen.StackAnimation
 import com.swmansion.rnscreens.bottomsheet.isSheetFitToContents
 import com.swmansion.rnscreens.events.StackFinishTransitioningEvent
+import com.swmansion.rnscreens.utils.setTweenAnimations
 import java.util.Collections
 import kotlin.collections.ArrayList
 
@@ -145,101 +146,7 @@ class ScreenStack(
         createTransaction().let {
             // animation logic start
             if (stackAnimation != null) {
-                if (shouldUseOpenAnimation) {
-                    when (stackAnimation) {
-                        StackAnimation.DEFAULT ->
-                            it.setCustomAnimations(
-                                R.anim.rns_default_enter_in,
-                                R.anim.rns_default_enter_out,
-                            )
-
-                        StackAnimation.NONE ->
-                            it.setCustomAnimations(
-                                R.anim.rns_no_animation_20,
-                                R.anim.rns_no_animation_20,
-                            )
-
-                        StackAnimation.FADE ->
-                            it.setCustomAnimations(
-                                R.anim.rns_fade_in,
-                                R.anim.rns_fade_out,
-                            )
-
-                        StackAnimation.SLIDE_FROM_RIGHT ->
-                            it.setCustomAnimations(
-                                R.anim.rns_slide_in_from_right,
-                                R.anim.rns_slide_out_to_left,
-                            )
-                        StackAnimation.SLIDE_FROM_LEFT ->
-                            it.setCustomAnimations(
-                                R.anim.rns_slide_in_from_left,
-                                R.anim.rns_slide_out_to_right,
-                            )
-                        StackAnimation.SLIDE_FROM_BOTTOM ->
-                            it.setCustomAnimations(
-                                R.anim.rns_slide_in_from_bottom,
-                                R.anim.rns_no_animation_medium,
-                            )
-                        StackAnimation.FADE_FROM_BOTTOM -> it.setCustomAnimations(R.anim.rns_fade_from_bottom, R.anim.rns_no_animation_350)
-                        StackAnimation.IOS_FROM_RIGHT ->
-                            it.setCustomAnimations(
-                                R.anim.rns_ios_from_right_foreground_open,
-                                R.anim.rns_ios_from_right_background_open,
-                            )
-                        StackAnimation.IOS_FROM_LEFT ->
-                            it.setCustomAnimations(
-                                R.anim.rns_ios_from_left_foreground_open,
-                                R.anim.rns_ios_from_left_background_open,
-                            )
-                    }
-                } else {
-                    when (stackAnimation) {
-                        StackAnimation.DEFAULT ->
-                            it.setCustomAnimations(
-                                R.anim.rns_default_exit_in,
-                                R.anim.rns_default_exit_out,
-                            )
-
-                        StackAnimation.NONE ->
-                            it.setCustomAnimations(
-                                R.anim.rns_no_animation_20,
-                                R.anim.rns_no_animation_20,
-                            )
-
-                        StackAnimation.FADE ->
-                            it.setCustomAnimations(
-                                R.anim.rns_fade_in,
-                                R.anim.rns_fade_out,
-                            )
-
-                        StackAnimation.SLIDE_FROM_RIGHT ->
-                            it.setCustomAnimations(
-                                R.anim.rns_slide_in_from_left,
-                                R.anim.rns_slide_out_to_right,
-                            )
-                        StackAnimation.SLIDE_FROM_LEFT ->
-                            it.setCustomAnimations(
-                                R.anim.rns_slide_in_from_right,
-                                R.anim.rns_slide_out_to_left,
-                            )
-                        StackAnimation.SLIDE_FROM_BOTTOM ->
-                            it.setCustomAnimations(
-                                R.anim.rns_no_animation_medium,
-                                R.anim.rns_slide_out_to_bottom,
-                            )
-                        StackAnimation.FADE_FROM_BOTTOM -> it.setCustomAnimations(R.anim.rns_no_animation_250, R.anim.rns_fade_to_bottom)
-                        StackAnimation.IOS_FROM_RIGHT ->
-                            it.setCustomAnimations(
-                                R.anim.rns_ios_from_right_background_close,
-                                R.anim.rns_ios_from_right_foreground_close,
-                            )
-                        StackAnimation.IOS_FROM_LEFT ->
-                            it.setCustomAnimations(
-                                R.anim.rns_ios_from_left_background_close,
-                                R.anim.rns_ios_from_left_foreground_close,
-                            )
-                    }
-                }
+                it.setTweenAnimations(stackAnimation, shouldUseOpenAnimation)
             }
             // animation logic end
             goingForward = shouldUseOpenAnimation
