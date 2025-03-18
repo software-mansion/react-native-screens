4af3875d3ee6d6128ef25d189de33544521841e7
Krzysztof Ligarski <krzysztof.ligarski@swmansion.com>
2025-03-18 11:42:06 +0100

    progress on Test432

--- FabricExample/e2e/examplesTests/tests.e2e.ts
+++ FabricExample/e2e/examplesTests/tests.e2e.ts
@@ -1,8 +1,27 @@
-import { device } from 'detox';
+import { device, expect, element, by } from 'detox';
 
-describe('Tests from PRs and issues', () => {
-  beforeEach(async () => {
+describe('Test432', () => {
+  beforeAll(async () => {
     await device.reloadReactNative();
   });
+
+  it('should Test432 exist', async () => {
+    await waitFor(element(by.id('root-screen-tests-Test432')))
+    .toBeVisible()
+    .whileElement(by.id('root-screen-examples-scrollview'))
+    .scroll(100, 'down', NaN, 0.85);
+
+    await expect(element(by.id('root-screen-tests-Test432'))).toBeVisible();
+    await element(by.id('root-screen-tests-Test432')).tap();
+  });
+
+
+  // home home-square
+  // details details-red-square | click | details-green-square | click | details-red-square | back
+  // info info-green-square-1 | click | info-red-square info-green-square-1 info-green-square-2 | click | info-green-square-1 | back
+  // settings settings-square
+  // za kazdym powrotem do home sprawdzic czarny square
+
+
 
 });
--- apps/src/shared/Square.tsx
+++ apps/src/shared/Square.tsx
@@ -4,11 +4,13 @@ import { View } from 'react-native';
 interface Props {
   color?: string;
   size?: number;
+  testID?: string;
 }
 
 export const Square = ({
   size = 100,
   color = 'red',
+  testID,
 }: Props): React.JSX.Element => (
-  <View style={{ width: size, height: size, backgroundColor: color }} />
+  <View style={{ width: size, height: size, backgroundColor: color }} testID={testID} />
 );
