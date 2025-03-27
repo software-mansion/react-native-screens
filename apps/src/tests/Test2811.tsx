software-mansion/react-native-screens

commit e741b8e721e0b1ad7bcdad8797a70e3c6c036c2a

fix: clicking on Pressable located in screen header (#2466)
## Description

This PR fixes clicking on Pressables that are added to the native
header. Previously, Yoga had incorrect information about the location of
the content in the header.

The first step was to remove `top: "-100%"` style from the
`ScreenStackHeaderConfig` which made Yoga think, that the content is
pushed up by the size of its parent (Screen size).

The second step involved updating `layoutMetrics` of the
`RNSScreenStackHeaderConfigShadowNode`. The entire app content is pushed
down by the size of the header, so it also has an impact on the header
config in Yoga metrics. To mitigate this, the origin of
`RNSScreenStackHeaderConfigShadowNode` is decreased by the size of the
header which will zero out eventually leaving the header content in the
desired position. On iOS this position is actually moved by the top
inset size, so we also have to take it into account when setting header
config layout metrics.

Fixes #2219 

## Changes

Updated `ScreenShadowNode` to decrease `origin.y` of the
`HeaderConfigShadowNode` by the size of the header. Added `paddingTop`
to `HeaderConfigState` and set it as origin offset on iOS.

<!--
Please describe things you've changed here, make a **high level**
overview, if change is simple you can omit this section.

For example:

- Updated `about.md` docs

-->

<!--


-->

## Screenshots / GIFs

### Before

| iOS       | Android        |
|--------------|--------------|
| <video
src="https://github.com/user-attachments/assets/aa054d20-58e5-4153-924b-a1b74879ae6b">
| <video
src="https://github.com/user-attachments/assets/dd9d2732-10c5-40ca-b384-1570c08410cf">
|

### After

| iOs       | Android        |
|--------------|--------------|
| <video
src="https://github.com/user-attachments/assets/6953c9e5-b418-41b1-8b99-27f70c5df688">
| <video
src="https://github.com/user-attachments/assets/f5762f46-36b2-423a-93da-9f61032b54bf">
|



## Test code and steps to reproduce

common/cpp/react/renderer/components/rnscreens/RNSScreenStackHeaderConfigComponentDescriptor.h
@@ -34,8 +34,18 @@ class RNSScreenStackHeaderConfigComponentDescriptor final
         shadowNode.getState());
     auto stateData = state->getData();
 
-    layoutableShadowNode.setPadding(
-        {stateData.getPaddingStart(), 0, stateData.getPaddingEnd(), 0});
+    if (stateData.frameSize.width != 0 && stateData.frameSize.height != 0) {
+      layoutableShadowNode.setSize(
+          {stateData.frameSize.width, stateData.frameSize.height});
+#ifdef ANDROID
+      layoutableShadowNode.setPadding({
+          stateData.paddingStart,
+          0,
+          stateData.paddingEnd,
+          0,
+      });
+#endif // ANDROID
+    }
 
     ConcreteComponentDescriptor::adopt(shadowNode);
 #if !defined(ANDROID) && !defined(NDEBUG)

apps/src/tests/index.ts
@@ -116,6 +116,7 @@ export { default as Test2320 } from './Test2320';
 export { default as Test2332 } from './Test2332';
 export { default as Test2379 } from './Test2379';
 export { default as Test2395 } from './Test2395';
+export { default as Test2466 } from './Test2466';
 export { default as Test2552 } from './Test2552';
 export { default as TestScreenAnimation } from './TestScreenAnimation';
 export { default as TestScreenAnimationV5 } from './TestScreenAnimationV5';

android/src/fabric/java/com/swmansion/rnscreens/FabricEnabledHeaderConfigViewGroup.kt
@@ -14,6 +14,8 @@ abstract class FabricEnabledHeaderConfigViewGroup(
 ) : ViewGroup(context) {
     private var mStateWrapper: StateWrapper? = null
 
+    private var lastWidth = 0f
+    private var lastHeight = 0f
     private var lastPaddingStart = 0f
     private var lastPaddingEnd = 0f
 
@@ -21,36 +23,55 @@ abstract class FabricEnabledHeaderConfigViewGroup(
         mStateWrapper = wrapper
     }
 
-    fun updatePaddingsFabric(
+    fun updatePaddings(
         paddingStart: Int,
         paddingEnd: Int,
     ) {
-        updateState(paddingStart, paddingEnd)
+        // Do nothing on Fabric. This method is used only on Paper.
+    }
+
+    fun updateHeaderConfigState(
+        width: Int,
+        height: Int,
+        paddingStart: Int,
+        paddingEnd: Int,
+    ) {
+        updateState(width, height, paddingStart, paddingEnd)
     }
 
     @UiThread
     fun updateState(
+        width: Int,
+        height: Int,
         paddingStart: Int,
         paddingEnd: Int,
     ) {
-        val paddingStartDip: Float = PixelUtil.toDIPFromPixel(paddingStart.toFloat())
-        val paddingEndDip: Float = PixelUtil.toDIPFromPixel(paddingEnd.toFloat())
+        val realWidth: Float = PixelUtil.toDIPFromPixel(width.toFloat())
+        val realHeight: Float = PixelUtil.toDIPFromPixel(height.toFloat())
+        val realPaddingStart: Float = PixelUtil.toDIPFromPixel(paddingStart.toFloat())
+        val realPaddingEnd: Float = PixelUtil.toDIPFromPixel(paddingEnd.toFloat())
 
         // Check incoming state values. If they're already the correct value, return early to prevent
         // infinite UpdateState/SetState loop.
-        if (abs(lastPaddingStart - paddingStart) < DELTA &&
-            abs(lastPaddingEnd - paddingEnd) < DELTA
+        if (abs(lastWidth - realWidth) < DELTA &&
+            abs(lastHeight - realHeight) < DELTA &&
+            abs(lastPaddingStart - realPaddingStart) < DELTA &&
+            abs(lastPaddingEnd - realPaddingEnd) < DELTA
         ) {
             return
         }
 
-        lastPaddingStart = paddingStartDip
-        lastPaddingEnd = paddingEndDip
+        lastWidth = realWidth
+        lastHeight = realHeight
+        lastPaddingStart = realPaddingStart
+        lastPaddingEnd = realPaddingEnd
 
         val map: WritableMap =
             WritableNativeMap().apply {
-                putDouble("paddingStart", paddingStartDip.toDouble())
-                putDouble("paddingEnd", paddingEndDip.toDouble())
+                putDouble("frameWidth", realWidth.toDouble())
+                putDouble("frameHeight", realHeight.toDouble())
+                putDouble("paddingStart", realPaddingStart.toDouble())
+                putDouble("paddingEnd", realPaddingEnd.toDouble())
             }
         mStateWrapper?.updateState(map)
     }

android/src/fabric/java/com/swmansion/rnscreens/FabricEnabledHeaderSubviewGroup.kt
@@ -1,0 +1,51 @@
+package com.swmansion.rnscreens
+
+import android.content.Context
+import android.view.ViewGroup
+import androidx.annotation.UiThread
+import com.facebook.react.bridge.WritableMap
+import com.facebook.react.bridge.WritableNativeMap
+import com.facebook.react.uimanager.PixelUtil
+import com.facebook.react.uimanager.StateWrapper
+
+abstract class FabricEnabledHeaderSubviewGroup(
+    context: Context?,
+) : ViewGroup(context) {
+    private var mStateWrapper: StateWrapper? = null
+
+    fun setStateWrapper(wrapper: StateWrapper?) {
+        mStateWrapper = wrapper
+    }
+
+    protected fun updateSubviewFrameState(
+        width: Int,
+        height: Int,
+        offsetX: Int,
+        offsetY: Int,
+    ) {
+        updateState(width, height, offsetX, offsetY)
+    }
+
+    @UiThread
+    fun updateState(
+        width: Int,
+        height: Int,
+        offsetX: Int,
+        offsetY: Int,
+    ) {
+        val realWidth: Float = PixelUtil.toDIPFromPixel(width.toFloat())
+        val realHeight: Float = PixelUtil.toDIPFromPixel(height.toFloat())
+        val offsetXDip: Float = PixelUtil.toDIPFromPixel(offsetX.toFloat())
+        val offsetYDip: Float = PixelUtil.toDIPFromPixel(offsetY.toFloat())
+
+        val map: WritableMap =
+            WritableNativeMap().apply {
+                putDouble("frameWidth", realWidth.toDouble())
+                putDouble("frameHeight", realHeight.toDouble())
+                putDouble("contentOffsetX", offsetXDip.toDouble())
+                putDouble("contentOffsetY", offsetYDip.toDouble())
+            }
+
+        mStateWrapper?.updateState(map)
+    }
+}

android/src/main/java/com/swmansion/rnscreens/CustomToolbar.kt
@@ -68,8 +68,18 @@ open class CustomToolbar(
     ) {
         super.onLayout(changed, l, t, r, b)
 
-        // our children are already laid out
+        if (!changed) {
+            return
+        }
+
         val contentInsetStart = if (navigationIcon != null) contentInsetStartWithNavigation else contentInsetStart
-        config.updatePaddingsFabric(contentInsetStart, contentInsetEnd)
+        if (BuildConfig.IS_NEW_ARCHITECTURE_ENABLED) {
+            val width = r - l;
+            val height = b - t;
+            config.updateHeaderConfigState(width, height, contentInsetStart, contentInsetEnd)
+        } else {
+            // our children are already laid out
+            config.updatePaddings(contentInsetStart, contentInsetEnd)
+        }
     }
 }

android/src/main/java/com/swmansion/rnscreens/ScreenStackHeaderSubview.kt
@@ -8,7 +8,7 @@ import com.facebook.react.views.view.ReactViewGroup
 @SuppressLint("ViewConstructor")
 class ScreenStackHeaderSubview(
     context: ReactContext?,
-) : ReactViewGroup(context) {
+) : FabricEnabledHeaderSubviewGroup(context) {
     private var reactWidth = 0
     private var reactHeight = 0
     var type = Type.RIGHT
@@ -37,11 +37,17 @@ class ScreenStackHeaderSubview(
 
     override fun onLayout(
         changed: Boolean,
-        left: Int,
-        top: Int,
-        right: Int,
-        bottom: Int,
-    ) = Unit
+        l: Int,
+        t: Int,
+        r: Int,
+        b: Int,
+    ) {
+       if (changed && BuildConfig.IS_NEW_ARCHITECTURE_ENABLED) {
+           val width = r - l;
+           val height = b - t;
+           updateSubviewFrameState(width, height, l, t)
+       }
+    }
 
     enum class Type {
         LEFT,

android/src/main/java/com/swmansion/rnscreens/ScreenStackHeaderSubviewManager.kt
@@ -2,6 +2,8 @@ package com.swmansion.rnscreens
 
 import com.facebook.react.bridge.JSApplicationIllegalArgumentException
 import com.facebook.react.module.annotations.ReactModule
+import com.facebook.react.uimanager.ReactStylesDiffMap
+import com.facebook.react.uimanager.StateWrapper
 import com.facebook.react.uimanager.ThemedReactContext
 import com.facebook.react.uimanager.ViewGroupManager
 import com.facebook.react.uimanager.ViewManagerDelegate
@@ -37,6 +39,17 @@ class ScreenStackHeaderSubviewManager :
                 "searchBar" -> ScreenStackHeaderSubview.Type.SEARCH_BAR
                 else -> throw JSApplicationIllegalArgumentException("Unknown type $type")
             }
+    }
+
+    override fun updateState(
+        view: ScreenStackHeaderSubview,
+        props: ReactStylesDiffMap?,
+        stateWrapper: StateWrapper?
+    ): Any? {
+        if (BuildConfig.IS_NEW_ARCHITECTURE_ENABLED) {
+            view.setStateWrapper(stateWrapper)
+        }
+        return super.updateState(view, props, stateWrapper)
     }
 
     protected override fun getDelegate(): ViewManagerDelegate<ScreenStackHeaderSubview> = delegate

android/src/paper/java/com/swmansion/rnscreens/FabricEnabledHeaderConfigViewGroup.kt
@@ -16,7 +16,15 @@ abstract class FabricEnabledHeaderConfigViewGroup(
 
     fun setStateWrapper(wrapper: StateWrapper?) = Unit
 
-    fun updatePaddingsFabric(
+    // Do nothing on Paper. This method is used only on Fabric.
+    fun updateHeaderConfigState(
+        width: Int,
+        height: Int,
+        paddingStart: Int,
+        paddingEnd: Int,
+    ) = Unit
+
+    fun updatePaddings(
         paddingStart: Int,
         paddingEnd: Int,
     ) {

commit b1e9c7137a37b9da6ad8e25fcd4934ebd116a7b9

fix(Android,Fabric): prevent Yoga from stretch-fitting height of header subview (#2811)
## Description

When setting header subviews from the rendered component via
`setOptions` the native header enlarges (see the "before" video below
:point_down:).

### Bug mechanism

When `HeaderSubview` is set from `setOptions` its mounted after first
render has happened.
It means that `HeaderConfig` has already been laid out and possibly its
shadow state got
updated with its size. Now, when first layout for the `HeaderSubview` is
computed Yoga
will stretch-fit `HeaderSubview` height to fill available space - the
`HeaderSubview` will
receive height equal to the height of the `HeaderConfig`. Such frame
will be then send
to HostTree, triggering native header layout, which will expand to make
enough space for
such high `HeaderSubview` & additional padding. Thanks to #2696 the
update cycle will be broken
& the issue described in #2675 won't happen.

Note that there is no such buggy behaviour in case the `HeaderSubviews`
is passed directly as option
when defining a `Screen`. This is because Yoga resolves the
`childHeight` (`HeaderSubview` height)
differently depending on whether `containingNode`'s height
(`HeaderConfig`'s height) is defined upfront or not.
When the `containingNode` height is not known (case of initial render
with `HeaderSubview` present) the Yoga will
use `FitContent` or `MaxContent` (not sure here)
[`SizingMode`](https://github.com/facebook/yoga/blob/51e6095005fd713dbfcbaf2c6296009de782d966/yoga/algorithm/SizingMode.h#L21-L45).
In cases, it is known (`HeaderConfig` has received state from HT, case
of `HeaderSubview` rendered via `setOptions`) - `StretchFit` will be
used for some reason (taking into consideration all layout options,
including flex
direction which is `row` for both `HeaderConfig` and `HeaderSubview`).

I believe this regression has been introduced in #2466, where we added
state updates for `HeaderConfig`

common/cpp/react/renderer/components/rnscreens/RNSScreenStackHeaderConfigComponentDescriptor.h
@@ -34,17 +34,21 @@ class RNSScreenStackHeaderConfigComponentDescriptor final
         shadowNode.getState());
     auto stateData = state->getData();
 
-    if (stateData.frameSize.width != 0 && stateData.frameSize.height != 0) {
-      layoutableShadowNode.setSize(stateData.frameSize);
 #ifdef ANDROID
+    if (stateData.frameSize.width != 0) {
+      layoutableShadowNode.setSize({stateData.frameSize.width, YGUndefined});
       layoutableShadowNode.setPadding({
           stateData.paddingStart,
           0,
           stateData.paddingEnd,
           0,
       });
-#endif // ANDROID
     }
+#else
+    if (stateData.frameSize.width != 0 && stateData.frameSize.height != 0) {
+      layoutableShadowNode.setSize(stateData.frameSize);
+    }
+#endif // ANDROID
 
     ConcreteComponentDescriptor::adopt(shadowNode);
 #if !defined(ANDROID) && !defined(NDEBUG)

apps/src/tests/Test2811.tsx
@@ -1,0 +1,60 @@
+import { NavigationContainer } from '@react-navigation/native';
+import { NativeStackNavigationProp, createNativeStackNavigator } from '@react-navigation/native-stack';
+import React from 'react';
+import { Text, View } from 'react-native';
+
+type StackRouteParamList = {
+  Home: undefined;
+};
+
+type StackRouteNavProps = {
+  navigation: NativeStackNavigationProp<StackRouteParamList>;
+};
+
+const Stack = createNativeStackNavigator<StackRouteParamList>();
+
+function Home({ navigation }: StackRouteNavProps) {
+
+  React.useEffect(() => {
+    const timerHandle = setTimeout(() => {
+      navigation.setOptions({
+        headerRight: () => <HeaderSubview size={20} />,
+      });
+    }, 1500);
+
+    return () => {
+      clearTimeout(timerHandle);
+    };
+  }, [navigation]);
+
+  //React.useEffect(() => {
+  //  navigation.setOptions({
+  //    headerRight: () => <HeaderSubview size={20} />,
+  //  });
+  //}, [navigation]);
+
+  return (
+    <View style={{ flex: 1, backgroundColor: 'seagreen' }}>
+      <Text>Home</Text>
+    </View>
+  );
+}
+
+function HeaderSubview({ size }: { size?: number }) {
+  const finalSize = size ?? 20;
+  return (
+    <View style={{ backgroundColor: 'crimson', width: finalSize, height: finalSize, maxHeight: finalSize }} />
+  );
+}
+
+export default function App() {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen name="Home" component={Home} options={{
+          //headerRight: () => <HeaderSubview size={20} />,
+        }} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+}

apps/src/tests/index.ts
@@ -125,6 +125,7 @@ export { default as Test2675 } from './Test2675';
 export { default as Test2717 } from './Test2717';
 export { default as Test2767 } from './Test2767';
 export { default as Test2789 } from './Test2789';
+export { default as Test2811 } from './Test2811';
 export { default as TestScreenAnimation } from './TestScreenAnimation';
 export { default as TestScreenAnimationV5 } from './TestScreenAnimationV5';
 export { default as TestHeader } from './TestHeader';
@@ -136,4 +137,5 @@ export { default as TestMemoryLeak } from './TestMemoryLeak';
 export { default as TestFormSheet } from './TestFormSheet';
 export { default as TestAndroidTransitions } from './TestAndroidTransitions';
 export { default as TestAnimation } from './TestAnimation';
+
 