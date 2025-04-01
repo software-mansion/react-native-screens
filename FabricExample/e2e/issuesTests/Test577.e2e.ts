baedab6a4641fb63eceab2b00f99e2c006564548
Krzysztof Ligarski <krzysztof.ligarski@swmansion.com>
2025-04-01 12:42:37 +0200

    avoid running pointless tests if they are platform-specific

--- FabricExample/e2e/issuesTests/Test577.e2e.ts
+++ FabricExample/e2e/issuesTests/Test577.e2e.ts
@@ -1,6 +1,8 @@
 import { device, expect, element, by } from 'detox';
+import { describeIfiOS } from '../e2e-utils';
 
-describe('Test577', () => {
+// Detox currently supports orientation only on iOS
+describeIfiOS('Test577', () => {
   beforeAll(async () => {
     await device.reloadReactNative();
   });
@@ -15,18 +17,15 @@ describe('Test577', () => {
     await element(by.id('root-screen-tests-Test577')).tap();
   });
 
-  // This issue is related to iOS modal
-  if (device.getPlatform() === 'ios') {
-    it('does not display content underneath modal with gesture disabled when attempting to close it', async () => {
-      await element(by.text('Open modal')).tap();
+  it('does not display content underneath modal with gesture disabled when attempting to close it', async () => {
+    await element(by.text('Open modal')).tap();
 
-      // Original bug was happening after a few consecutive attempts to close the modal.
-      // See this GIF: https://github.com/software-mansion/react-native-screens/issues/577#issue-666185758
-      // Decided to try it a few times.
-      for (let i = 0; i < 5; ++i) {
-        await element(by.text('Modal')).swipe('down', 'fast');
-        await expect(element(by.text('Open modal'))).not.toBeVisible();
-      }
-    });
-  }
+    // Original bug was happening after a few consecutive attempts to close the modal.
+    // See this GIF: https://github.com/software-mansion/react-native-screens/issues/577#issue-666185758
+    // Decided to try it a few times.
+    for (let i = 0; i < 5; ++i) {
+      await element(by.text('Modal')).swipe('down', 'fast');
+      await expect(element(by.text('Open modal'))).not.toBeVisible();
+    }
+  });
 });
