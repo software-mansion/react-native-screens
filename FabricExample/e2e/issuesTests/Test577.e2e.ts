dada4168810efe84e3b72738455e9d489a497ae1
Krzysztof Ligarski <krzysztof.ligarski@swmansion.com>
2025-04-01 12:08:20 +0200

    add explanation for repeating action in Test577

--- FabricExample/e2e/issuesTests/Test577.e2e.ts
+++ FabricExample/e2e/issuesTests/Test577.e2e.ts
@@ -19,6 +19,10 @@ describe('Test577', () => {
   if (device.getPlatform() === 'ios') {
     it('does not display content underneath modal with gesture disabled when attempting to close it', async () => {
       await element(by.text('Open modal')).tap();
+
+      // Original bug was happening after a few consecutive attempts to close the modal.
+      // See this GIF: https://github.com/software-mansion/react-native-screens/issues/577#issue-666185758
+      // Decided to try it a few times.
       for (let i = 0; i < 5; ++i) {
         await element(by.text('Modal')).swipe('down', 'fast');
         await expect(element(by.text('Open modal'))).not.toBeVisible();
