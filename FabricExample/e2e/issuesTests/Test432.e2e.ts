702d4afa1750c7e3a55e8804cd00cffb7562163b
kligarski <63918941+kligarski@users.noreply.github.com>
2025-04-01 18:04:59 +0200

    test(Fabric): add e2e test for issue 577 (#2816)
    ## Description
    
    Check which example screens from issues/PRs can be used in e2e testing
    for tests `Test550, ..., Test577` and implement them if possible for
    Fabric.
    
    ### Test550
    Skipped because the issue is present on the current version (custom
    native header back button image). When it is resolved, we might create
    an e2e test for it.
    
    ### Test556
    Skipped because we can't check flickering nor non-deterministic white
    screen bug.
    
    ### Test564
    Skipped because the issue is still present on the current version
    (headers overlapping when one navigatior has `headerTransparent: true`).
    When it is resolved, we might create an e2e test for it.
    
    ### Test577
    Test created. It checks whether attempting to close a `gestureEnabled:
    false` modal on iOS doesn't break the modal's height allowing the
    content underneath to be visible. The test is present only on iOS
    because of differences in `modal` between iOS and Android.
    
    ## Changes
    
    - add `Test577`
    - add comments for test screen in `apps/src/tests/index.ts` with the
    reason for (not) implementing e2e test for it
    - add `e2e-utils.ts` with `decribeIfiOS` function that will skip running
    the tests if they are run on Android
    
    ## Test code and steps to reproduce
    
    CI
    
    ## Checklist
    
    - [x] Ensured that CI passes

--- FabricExample/e2e/issuesTests/Test432.e2e.ts
+++ FabricExample/e2e/issuesTests/Test432.e2e.ts
@@ -72,39 +72,3 @@ describe('Test432', () => {
     await expect(element(by.id('home-headerRight'))).toBeVisible(100);
   });
 });
-
-// Detox currently supports orientation only on iOS
-if (device.getPlatform() === 'ios') {
-  describe('Test528', () => {
-    beforeAll(async () => {
-      await device.reloadReactNative();
-    });
-
-    it('Test528 should exist', async () => {
-      await waitFor(element(by.id('root-screen-tests-Test528')))
-        .toBeVisible()
-        .whileElement(by.id('root-screen-examples-scrollview'))
-        .scroll(600, 'down', NaN, 0.85);
-
-      await expect(element(by.id('root-screen-tests-Test528'))).toBeVisible();
-      await element(by.id('root-screen-tests-Test528')).tap();
-    });
-
-    it('headerRight button should be visible after orientation change', async () => {
-      await expect(element(by.text('Custom Button'))).toBeVisible(100);
-      await device.setOrientation('landscape');
-      await expect(element(by.text('Custom Button'))).toBeVisible(100);
-      await device.setOrientation('portrait');
-      await expect(element(by.text('Custom Button'))).toBeVisible(100);
-    });
-
-    it('headerRight button should be visible after coming back from horizontal screen', async () => {
-      await element(by.text('Go to Screen 2')).tap();
-      await device.setOrientation('landscape');
-      await element(by.id('BackButton')).tap();
-      await expect(element(by.text('Custom Button'))).toBeVisible(100);
-      await device.setOrientation('portrait');
-      await expect(element(by.text('Custom Button'))).toBeVisible(100);
-    });
-  });
-}
