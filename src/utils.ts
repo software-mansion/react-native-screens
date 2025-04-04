ea2d67f66eb1fd334301e218258d05850e26aa21
Krzysztof Ligarski <krzysztof.ligarski@swmansion.com>
2025-04-04 08:05:16 +0200

    formatting

--- src/utils.ts
+++ src/utils.ts
@@ -3,7 +3,7 @@ import { BackHandler, Platform } from 'react-native';
 export const isSearchBarAvailableForCurrentPlatform = [
   'ios',
   'android',
-  'windows'
+  'windows',
 ].includes(Platform.OS);
 
 export function executeNativeBackPress() {
