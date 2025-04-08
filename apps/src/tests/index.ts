5f0f3690abc654a37e5cd4cb1b8dc87069e8c841
Kacper Kafara <kacperkafara@gmail.com>
2025-04-08 12:30:13 +0200

    Add reproduction for 2714

--- apps/src/tests/index.ts
+++ apps/src/tests/index.ts
@@ -124,6 +124,7 @@ export { default as Test2611 } from './Test2611';
 export { default as Test2631 } from './Test2631';
 export { default as Test2668 } from './Test2668';
 export { default as Test2675 } from './Test2675';
+export { default as Test2714 } from './Test2714';
 export { default as Test2717 } from './Test2717';
 export { default as Test2767 } from './Test2767';
 export { default as Test2789 } from './Test2789';
