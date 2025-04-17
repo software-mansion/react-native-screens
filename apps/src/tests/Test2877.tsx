7dc8de2e3198b8a5c986836fac6ed5d191185d77
Krzysztof Ligarski <krzysztof.ligarski@swmansion.com>
2025-04-17 13:58:22 +0200

    add example screen Test2877

---
+++ apps/src/tests/Test2877.tsx
@@ -0,0 +1,91 @@
+import React, { useEffect, useState } from 'react';
+import { NavigationContainer, ParamListBase } from '@react-navigation/native';
+import {
+  NativeStackNavigationProp,
+  createNativeStackNavigator,
+} from '@react-navigation/native-stack';
+import { Button, Text, View } from 'react-native';
+
+type RouteParamList = {
+  Home: undefined;
+  FormSheet: undefined;
+};
+
+type NavigationProp<ParamList extends ParamListBase> = {
+  navigation: NativeStackNavigationProp<ParamList>;
+};
+
+type StackNavigationProp = NavigationProp<RouteParamList>;
+
+const Stack = createNativeStackNavigator<RouteParamList>();
+
+function Home({ navigation }: StackNavigationProp) {
+  return (
+    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+      <Button
+        title="Open FormSheet"
+        onPress={() => navigation.navigate('FormSheet')}
+      />
+      <Button
+        title="Go back"
+        onPress={() => navigation.pop()}
+      />
+    </View>
+  );
+}
+
+function FormSheet({ navigation }: StackNavigationProp) {
+  const [extended, setExtended] = useState(false);
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setExtended(!extended);
+    }, 1000);
+
+    return () => clearInterval(interval);
+  });
+
+  return (
+    <View>
+      <Button title="Go back" onPress={() => navigation.goBack()} />
+      <View
+        style={{
+          height: 200,
+          flex: 1,
+          alignItems: 'center',
+          justifyContent: 'center',
+        }}>
+        <Text>Every second, sheet's height should change.</Text>
+      </View>
+      {extended && <View
+        style={{
+          height: 200,
+          flex: 1,
+          alignItems: 'center',
+          justifyContent: 'center',
+          backgroundColor: 'lightgreen',
+        }}>
+        <Text>Like this.</Text>
+      </View>}
+    </View>
+  );
+}
+
+export default function App() {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen name="Home" component={Home} />
+        <Stack.Screen
+          name="FormSheet"
+          component={FormSheet}
+          options={{
+            headerShown: false,
+            presentation: 'formSheet',
+            sheetAllowedDetents: 'fitToContents',
+          }}
+        />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+}
