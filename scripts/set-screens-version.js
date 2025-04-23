4384442b3f5ef43e6ddb2c1cff779d2b0f044a6e
Artur Bilski <admin@arturbilski.pl>
2025-04-23 10:33:43 +0200

    chore(CI): add nightly publish workflow
    ## Description
    
    The workflow is inspired by `react-native-reanimated`.
    
    ## Test code and steps to reproduce
    
    N/A
    
    ## Checklist
    
    - [ ] Ensured that CI passes

---
+++ scripts/set-screens-version.js
@@ -0,0 +1,29 @@
+const fs = require('fs');
+const path = require('path');
+const getVersion = require('./releasing').getVersion;
+
+const packageJsonPath = path.resolve(__dirname, '../package.json');
+
+const { currentVersion, newVersion } = getVersion(
+  process.argv,
+  packageJsonPath
+);
+
+const packageJson = JSON.parse(fs.readFileSync(packageJsonPath, 'utf-8'));
+packageJson.version = newVersion;
+const newPackageJson = JSON.stringify(packageJson, null, 2) + '\n';
+fs.writeFileSync(packageJsonPath, newPackageJson, 'utf-8');
+
+const jsVersionPath = path.resolve(
+  __dirname,
+  '../src/platform-specific/jsVersion.ts'
+);
+const before = fs.readFileSync(jsVersionPath, 'utf-8');
+const after = before.replace(
+  /jsVersion = '(.*)';/g,
+  `jsVersion = '${newVersion}';`
+);
+fs.writeFileSync(jsVersionPath, after, 'utf-8');
+
+// Log the current version so it can be restored if needed.
+console.log(currentVersion);
