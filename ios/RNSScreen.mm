69c37c568eddf254068a1fdf5a3d4877faa66544
lodev09 <lodev09@gmail.com>
2025-04-29 04:55:01 +0800

    fix(ios): fixed prop update bug on fabric

--- ios/RNSScreen.mm
+++ ios/RNSScreen.mm
@@ -990,7 +990,7 @@ RNS_IGNORE_SUPER_CALL_END
  * Updates settings for sheet presentation controller.
  * Note that this method should not be called inside `stackPresentation` setter, because on Paper we don't have
  * guarantee that values of all related props had been updated earlier. It should be invoked from `didSetProps`.
- * On Fabric we have control over prop-setting process but it might be reasonable to run it from `finalizeUpdates`.
+ * On Fabric we should call this from `updateProps`.
  */
 - (void)updateFormSheetPresentationStyle
 {
@@ -1326,6 +1326,12 @@ RNS_IGNORE_SUPER_CALL_END
     [self setReplaceAnimation:[RNSConvert RNSScreenReplaceAnimationFromCppEquivalent:newScreenProps.replaceAnimation]];
   }
 
+#if !TARGET_OS_TV && !TARGET_OS_VISION
+  if (self.stackPresentation == RNSScreenStackPresentationFormSheet) {
+    [self updateFormSheetPresentationStyle];
+  }
+#endif // !TARGET_OS_TV
+
   [super updateProps:props oldProps:oldProps];
 }
 
@@ -1350,14 +1356,6 @@ RNS_IGNORE_SUPER_CALL_END
   // pass the dimensions to ui view manager to take into account when laying out
   // subviews
   // Explanation taken from `reactSetFrame`, which is old arch equivalent of this code.
-}
-
-- (void)finalizeUpdates:(RNComponentViewUpdateMask)updateMask
-{
-  [super finalizeUpdates:updateMask];
-#if !TARGET_OS_TV && !TARGET_OS_VISION
-  [self updateFormSheetPresentationStyle];
-#endif // !TARGET_OS_TV
 }
 
 #pragma mark - Paper specific
