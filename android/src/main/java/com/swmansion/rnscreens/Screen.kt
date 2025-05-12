78a2576a66a6a3c7baa5551fc4869a6bdf141ee7
Krzysztof Ligarski <krzysztof.ligarski@swmansion.com>
2025-05-12 13:16:57 +0200

    add check for ScreenStack, rephrase and reformat comment

--- android/src/main/java/com/swmansion/rnscreens/Screen.kt
+++ android/src/main/java/com/swmansion/rnscreens/Screen.kt
@@ -544,12 +544,11 @@ class Screen(
     override fun onAttachedToWindow() {
         super.onAttachedToWindow()
 
-        // Insets handler for formSheet is added onResume but it is often
-        // too late if we use input with autofocus. It happens because onResume is called after
-        // finishing animator animation. onAttachedToWindow is called before onApplyWindowInsets
-        // so we use it to set the handler earlier.
-        // More details: https://github.com/software-mansion/react-native-screens/pull/2911
-        if (usesFormSheetPresentation()) {
+        // Insets handler for formSheet is added onResume but it is often too late if we use input
+        // with autofocus - onResume is called after finishing animator animation.
+        // onAttachedToWindow is called before onApplyWindowInsets so we use it to set the handler
+        // earlier. More details: https://github.com/software-mansion/react-native-screens/pull/2911
+        if (usesFormSheetPresentation() && isNativeStackScreen) {
             (fragment as ScreenStackFragment?)?.sheetDelegate?.let {
                 InsetsObserverProxy.addOnApplyWindowInsetsListener(
                     it,
