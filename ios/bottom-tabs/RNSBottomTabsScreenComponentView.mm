software-mansion/react-native-screens

commit 6576ef2b73f5ebd2286edcb005f6e899e4fc1ef5

feat(bottom-tabs,iOS): add support for title font size (#15)
## Description

Both `TabBar` & `TabBarItem` have `appearance` fields which accept
literally the same properties.

When `TabBarItem` is selected its `appearance` takes precedence over
`TabBar.appearance`.

This leads to situation where literally every appearance related prop
can be specified in both places.

How do I want to sculpture my API then? I see following possibilities:

1. support every option twice on `BottomTabs` and on `BottomTabsScreen`,
2. support customisation options only via props on `BottomTabs`,
3. support customisation options only via props on `BottomTabsScreen`,
4. some mix approach, where certain props are duplicated and certain are
not.

I currently decided to go with no. 1, desipte it requiring most
development effort.

The interaction between options is as follows:

1. If tab specifies an option - it is used (active tab),
2. otherwise if the `BottomTabs` specifies an option - it is used,
3. otherwise default system value is used.

## Changes

- **Make the font settings cascade & add support for title font size**
- **Update JS types**
- **Update example**

## Test code and steps to reproduce

`TestBottomTabs`

## Checklist

- [x] Included code example that can be used to test this change
- [ ] Ensured that CI passes

ios/bottom-tabs/RNSBottomTabsScreenComponentView.mm
@@ -108,14 +108,21 @@ RNS_IGNORE_SUPER_CALL_END
   const auto &oldComponentProps = *std::static_pointer_cast<const react::RNSBottomTabsScreenProps>(_props);
   const auto &newComponentProps = *std::static_pointer_cast<const react::RNSBottomTabsScreenProps>(props);
 
+  bool tabItemNeedsAppearanceUpdate{false};
+
   if (newComponentProps.title != oldComponentProps.title) {
     _title = RCTNSStringFromStringNilIfEmpty(newComponentProps.title);
     _controller.title = _title;
   }
 
+  if (newComponentProps.tabBarItemAppearance.titleFontSize != oldComponentProps.tabBarItemAppearance.titleFontSize) {
+    _titleFontSize = [NSNumber numberWithFloat:newComponentProps.tabBarItemAppearance.titleFontSize];
+    tabItemNeedsAppearanceUpdate = true;
+  }
+
   if (newComponentProps.isFocused != oldComponentProps.isFocused) {
     _isFocused = newComponentProps.isFocused;
-    [_controller tabScreenFocusHasChanged];
+    tabItemNeedsAppearanceUpdate = true;
   }
 
   if (newComponentProps.badgeValue != oldComponentProps.badgeValue) {
@@ -128,6 +135,10 @@ RNS_IGNORE_SUPER_CALL_END
     // Note that this will prevent default color from being set.
     // TODO: support default color by setting nil here.
     NSLog(@"TabsScreen [%ld] update badgeColor to %@", self.tag, _badgeColor);
+    tabItemNeedsAppearanceUpdate = true;
+  }
+
+  if (tabItemNeedsAppearanceUpdate) {
     [_controller tabItemAppearanceHasChanged];
   }
 

apps/src/tests/TestBottomTabs.tsx
@@ -32,8 +32,17 @@ function App() {
 
   return (
     <View style={{ flex: 1 }}>
-      <BottomTabs tabBarBackgroundColor={Colors.NavyLight100}>
-        <BottomTabsScreen isFocused={focusedTab % 3 === 0} badgeValue="1" badgeColor={Colors.NavyDark80} title="Tab1">
+      <BottomTabs
+        tabBarBackgroundColor={Colors.NavyLight100}
+        tabBarAppearance={{ backgroundColor: Colors.NavyLight100 }}
+        tabBarItemAppearance={{ titleFontSize: 28 }}
+      >
+        <BottomTabsScreen
+          isFocused={focusedTab % 3 === 0}
+          badgeValue="1"
+          badgeColor={Colors.NavyDark80}
+          title="Tab1"
+        >
           <LayoutView style={{ backgroundColor: Colors.OffWhite }} tabID={0}>
             <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
               <Text>Hello world from native bottom tab</Text>
@@ -41,13 +50,24 @@ function App() {
             </View>
           </LayoutView>
         </BottomTabsScreen>
-        <BottomTabsScreen isFocused={focusedTab % 3 === 1} badgeValue="2" badgeColor={Colors.PurpleLight100} title="Tab2">
+        <BottomTabsScreen
+          isFocused={focusedTab % 3 === 1}
+          badgeValue="2"
+          badgeColor={Colors.PurpleLight100}
+          title="Tab2"
+        >
           <LayoutView style={{ backgroundColor: Colors.PurpleLight80 }} tabID={1}>
             <Text>Tab2 world from native bottom tab</Text>
             <Button title="Next tab" onPress={selectNextTab} />
           </LayoutView>
         </BottomTabsScreen>
-        <BottomTabsScreen isFocused={focusedTab % 3 === 2} badgeValue="3" badgeColor={Colors.YellowDark120} title="Tab3">
+        <BottomTabsScreen
+          isFocused={focusedTab % 3 === 2}
+          badgeValue="3"
+          badgeColor={Colors.YellowDark120}
+          title="Tab3"
+          tabBarItemAppearance={{ titleFontSize: 16 }}
+        >
           <LayoutView style={{ backgroundColor: Colors.YellowDark80 }} tabID={2}>
             <Text>Tab3 world from native bottom tab</Text>
             <Button title="Next tab" onPress={selectNextTab} />

ios/bottom-tabs/RNSBottomTabsHostComponentView.h
@@ -23,6 +23,7 @@ NS_ASSUME_NONNULL_BEGIN
 
 @property (nonatomic, strong, readonly, nullable) UIColor *tabBarBackgroundColor;
 @property (nonatomic, strong, readonly, nullable) UIBlurEffect *tabBarBlurEffect;
+@property (nonatomic, strong, readonly, nullable) NSNumber *tabBarItemTitleFontSize;
 
 @end
 

ios/bottom-tabs/RNSBottomTabsHostComponentView.mm
@@ -38,15 +38,22 @@ namespace react = facebook::react;
 
 - (void)initState
 {
-  static const auto defaultProps = std::make_shared<const react::RNSBottomTabsProps>();
-  _props = defaultProps;
-  _tabBarBlurEffect = nil;
+  [self resetProps];
 
   _controller = [[RNSTabBarController alloc] init];
   _reactSubviews = [NSMutableArray new];
 
   _hasModifiedReactSubviewsInCurrentTransaction = NO;
   _needsTabBarAppearanceUpdate = NO;
+}
+
+- (void)resetProps
+{
+  static const auto defaultProps = std::make_shared<const react::RNSBottomTabsProps>();
+  _props = defaultProps;
+  _tabBarBlurEffect = nil;
+  _tabBarBackgroundColor = nil;
+  _tabBarItemTitleFontSize = nil;
 }
 
 #pragma mark - UIView methods
@@ -140,6 +147,11 @@ namespace react = facebook::react;
     _needsTabBarAppearanceUpdate = YES;
     _tabBarBlurEffect =
         rnscreens::conversion::RNSUIBlurEffectFromRNSBottomTabsTabBarBlurEffect(newComponentProps.tabBarBlurEffect);
+  }
+
+  if (newComponentProps.tabBarItemAppearance.titleFontSize != oldComponentProps.tabBarItemAppearance.titleFontSize) {
+    _tabBarItemTitleFontSize = [NSNumber numberWithFloat:newComponentProps.tabBarItemAppearance.titleFontSize];
+    _needsTabBarAppearanceUpdate = YES;
   }
 
   // Super call updates _props pointer. We should NOT update it before calling super.

ios/bottom-tabs/RNSBottomTabsScreenComponentView.h
@@ -34,7 +34,9 @@ NS_ASSUME_NONNULL_BEGIN
 @property (nonatomic, readonly) BOOL isFocused;
 @property (nonatomic, readonly, nullable) NSString *badgeValue;
 @property (nonatomic, readonly, nullable) UIColor *badgeColor;
+
 @property (nonatomic, readonly, nullable) NSString *title;
+@property (nonatomic, readonly, nullable) NSNumber *titleFontSize;
 
 @end
 

ios/bottom-tabs/RNSTabBarAppearanceCoordinator.mm
@@ -1,4 +1,5 @@
 #import "RNSTabBarAppearanceCoordinator.h"
+#import <React/RCTFont.h>
 #import "RNSConversions.h"
 #import "RNSTabsScreenViewController.h"
 
@@ -19,6 +20,10 @@
   if (hostComponentView != nil) {
     appearance.backgroundColor = hostComponentView.tabBarBackgroundColor;
     appearance.backgroundEffect = hostComponentView.tabBarBlurEffect;
+
+    [self configureTabBarItemAppearance:appearance.stackedLayoutAppearance withTabsHost:hostComponentView];
+    [self configureTabBarItemAppearance:appearance.compactInlineLayoutAppearance withTabsHost:hostComponentView];
+    [self configureTabBarItemAppearance:appearance.inlineLayoutAppearance withTabsHost:hostComponentView];
   }
 
   // Step 3 - apply general settings to the tab bar
@@ -37,12 +42,18 @@
       continue;
     }
 
+    // Inherit general properties from host
     UITabBarAppearance *tabAppearance = [[UITabBarAppearance alloc] initWithBarAppearance:appearance];
 
     [self configureTabBarItemAppearance:tabAppearance.compactInlineLayoutAppearance
-                 forTabScreenController:tabScreenCtrl];
-    [self configureTabBarItemAppearance:tabAppearance.inlineLayoutAppearance forTabScreenController:tabScreenCtrl];
-    [self configureTabBarItemAppearance:tabAppearance.stackedLayoutAppearance forTabScreenController:tabScreenCtrl];
+                 forTabScreenController:tabScreenCtrl
+                  withHostComponentView:hostComponentView];
+    [self configureTabBarItemAppearance:tabAppearance.inlineLayoutAppearance
+                 forTabScreenController:tabScreenCtrl
+                  withHostComponentView:hostComponentView];
+    [self configureTabBarItemAppearance:tabAppearance.stackedLayoutAppearance
+                 forTabScreenController:tabScreenCtrl
+                  withHostComponentView:hostComponentView];
 
     tabScreenCtrl.tabBarItem.standardAppearance = tabAppearance;
     tabScreenCtrl.tabBarItem.scrollEdgeAppearance = tabAppearance;
@@ -50,15 +61,84 @@
 }
 
 - (void)configureTabBarItemAppearance:(nonnull UITabBarItemAppearance *)tabBarItemAppearance
-               forTabScreenController:(nonnull RNSTabsScreenViewController *)tabScreenCtrl
+                         withTabsHost:(nonnull RNSBottomTabsHostComponentView *)hostComponent
 {
-  tabBarItemAppearance.normal.badgeBackgroundColor = tabScreenCtrl.tabScreenComponentView.badgeColor;
+  NSMutableDictionary *titleTextAttributes = nil;
 
-  tabBarItemAppearance.selected.badgeBackgroundColor = tabScreenCtrl.tabScreenComponentView.badgeColor;
+  if (hostComponent.tabBarItemTitleFontSize != nil) {
+    titleTextAttributes = [[NSMutableDictionary alloc] init];
+    titleTextAttributes[NSFontAttributeName] = [RCTFont updateFont:nil
+                                                        withFamily:nil
+                                                              size:hostComponent.tabBarItemTitleFontSize
+                                                            weight:nil
+                                                             style:nil
+                                                           variant:nil
+                                                   scaleMultiplier:1.0];
+  }
 
-  tabBarItemAppearance.focused.badgeBackgroundColor = tabScreenCtrl.tabScreenComponentView.badgeColor;
+  [self configureTabBarItemStateAppearance:tabBarItemAppearance.normal
+                              withTabsHost:hostComponent
+                   withTitleTextAttributes:titleTextAttributes];
+  [self configureTabBarItemStateAppearance:tabBarItemAppearance.selected
+                              withTabsHost:hostComponent
+                   withTitleTextAttributes:titleTextAttributes];
+  [self configureTabBarItemStateAppearance:tabBarItemAppearance.focused
+                              withTabsHost:hostComponent
+                   withTitleTextAttributes:titleTextAttributes];
+  [self configureTabBarItemStateAppearance:tabBarItemAppearance.disabled
+                              withTabsHost:hostComponent
+                   withTitleTextAttributes:titleTextAttributes];
+}
 
-  tabBarItemAppearance.disabled.badgeBackgroundColor = tabScreenCtrl.tabScreenComponentView.badgeColor;
+- (void)configureTabBarItemAppearance:(nonnull UITabBarItemAppearance *)tabBarItemAppearance
+               forTabScreenController:(nonnull RNSTabsScreenViewController *)tabScreenCtrl
+                withHostComponentView:(nonnull RNSBottomTabsHostComponentView *)tabsHostComponent
+{
+  NSMutableDictionary *titleTextAttributes = nil;
+
+  if (tabScreenCtrl.tabScreenComponentView.titleFontSize != nil) {
+    titleTextAttributes = [[NSMutableDictionary alloc] init];
+    titleTextAttributes[NSFontAttributeName] = [RCTFont updateFont:nil
+                                                        withFamily:nil
+                                                              size:tabScreenCtrl.tabScreenComponentView.titleFontSize
+                                                            weight:nil
+                                                             style:nil
+                                                           variant:nil
+                                                   scaleMultiplier:1.0];
+  }
+
+  [self configureTabBarItemStateAppearance:tabBarItemAppearance.normal
+                    forTabScreenController:tabScreenCtrl
+                   withTitleTextAttributes:titleTextAttributes];
+  [self configureTabBarItemStateAppearance:tabBarItemAppearance.selected
+                    forTabScreenController:tabScreenCtrl
+                   withTitleTextAttributes:titleTextAttributes];
+  [self configureTabBarItemStateAppearance:tabBarItemAppearance.focused
+                    forTabScreenController:tabScreenCtrl
+                   withTitleTextAttributes:titleTextAttributes];
+  [self configureTabBarItemStateAppearance:tabBarItemAppearance.disabled
+                    forTabScreenController:tabScreenCtrl
+                   withTitleTextAttributes:titleTextAttributes];
+}
+
+- (void)configureTabBarItemStateAppearance:(nonnull UITabBarItemStateAppearance *)tabBarItemStateAppearance
+                    forTabScreenController:(nonnull RNSTabsScreenViewController *)tabScreenCtrl
+                   withTitleTextAttributes:(nullable NSDictionary<NSAttributedStringKey, id> *)titleTextAttributes
+{
+  tabBarItemStateAppearance.badgeBackgroundColor = tabScreenCtrl.tabScreenComponentView.badgeColor;
+
+  if (titleTextAttributes != nil) {
+    tabBarItemStateAppearance.titleTextAttributes = titleTextAttributes;
+  }
+}
+
+- (void)configureTabBarItemStateAppearance:(nonnull UITabBarItemStateAppearance *)tabBarItemStateAppearance
+                              withTabsHost:(nonnull RNSBottomTabsHostComponentView *)hostComponent
+                   withTitleTextAttributes:(nullable NSDictionary<NSAttributedStringKey, id> *)titleTextAttributes
+{
+  if (titleTextAttributes != nil) {
+    tabBarItemStateAppearance.titleTextAttributes = titleTextAttributes;
+  }
 }
 
 @end

src/components/BottomTabsScreen.tsx
@@ -1,7 +1,9 @@
 'use client';
 
 import React from 'react';
-import BottomTabsScreenNativeComponent from '../fabric/BottomTabScreenNativeComponent';
+import BottomTabsScreenNativeComponent, {
+  type TabBarItemAppearance,
+} from '../fabric/BottomTabScreenNativeComponent';
 import {
   ColorValue,
   StyleSheet,
@@ -18,12 +20,8 @@ export interface BottomTabsScreenProps {
   badgeValue?: string;
   badgeColor?: ColorValue;
   title?: string;
+  tabBarItemAppearance?: TabBarItemAppearance;
 }
-
-// const LIFECYCLE_STATE_INITIAL = 0;
-// const LIFECYCLE_STATE_CREATED = 1;
-// const LIFECYCLE_STATE_INITIALIZED = 2;
-// const LIFECYCLE_STATE_RESUMED = 3;
 
 function BottomTabsScreen(props: BottomTabsScreenProps) {
   const [nativeViewHasDisappeared, setNativeViewHasDisappeared] =

src/fabric/BottomTabScreenNativeComponent.ts
@@ -4,6 +4,7 @@ import codegenNativeComponent from 'react-native/Libraries/Utilities/codegenNati
 import type { ColorValue, ViewProps } from 'react-native';
 import {
   DirectEventHandler,
+  Float,
   Int32,
 } from 'react-native/Libraries/Types/CodegenTypes';
 
@@ -14,6 +15,10 @@ export type LifecycleStateChangeEvent = Readonly<{
   previousState: Int32;
   newState: Int32;
 }>;
+
+export type TabBarItemAppearance = {
+  titleFontSize?: Float;
+};
 
 export interface NativeProps extends ViewProps {
   // Events
@@ -29,6 +34,8 @@ export interface NativeProps extends ViewProps {
   // Appearance
   badgeValue?: string;
   badgeColor?: ColorValue;
+
+  tabBarItemAppearance?: TabBarItemAppearance;
 
   // General
   title?: string | undefined | null;

commit bc87dd28f0bcc89fe3b7cd0af0dc63984cec2394

fix(bottom-tabs,iOS): regression in JS tab change - wrong signal sent to controller (#19)
## Description


https://github.com/software-mansion-labs/react-native-screens-labs/commit/6576ef2b73f5ebd2286edcb005f6e899e4fc1ef5
introduced a regression causing js tab change to not work.

## Changes

This PR restores correct behaviour on focus change.

## Test code and steps to reproduce

`TestBottomTabs`

## Checklist

- [x] Included code example that can be used to test this change
- [ ] Ensured that CI passes

ios/bottom-tabs/RNSBottomTabsScreenComponentView.mm
@@ -122,7 +122,7 @@ RNS_IGNORE_SUPER_CALL_END
 
   if (newComponentProps.isFocused != oldComponentProps.isFocused) {
     _isSelectedScreen = newComponentProps.isFocused;
-    tabItemNeedsAppearanceUpdate = true;
+    [_controller tabScreenFocusHasChanged];
   }
 
   if (newComponentProps.badgeValue != oldComponentProps.badgeValue) {

ios/bottom-tabs/RNSTabBarController.mm
@@ -49,10 +49,12 @@
 
 - (void)reactMountingTransactionWillMount
 {
+  NSLog(@"TabBarCtrl mountintTransactionWillMount");
 }
 
 - (void)reactMountingTransactionDidMount
 {
+  NSLog(@"TabBarCtrl mountintTransactionDidMount running updates");
   [self updateReactChildrenControllersIfNeeded];
   [self updateSelectedViewControllerIfNeeded];
   [self updateTabBarAppearanceIfNeeded];
@@ -69,6 +71,7 @@
 
 - (void)updateReactChildrenControllers
 {
+  NSLog(@"TabBarCtrl updateReactChildrenControllers");
   _needsUpdateOfReactChildrenControllers = false;
 
   if (_tabScreenControllers == nil) {
@@ -88,6 +91,7 @@
 
 - (void)updateSelectedViewController
 {
+  NSLog(@"TabBarCtrl updateSelectedViewController");
   _needsUpdateOfSelectedTab = false;
 
 #if !defined(NDEBUG)
@@ -122,6 +126,7 @@
 
 - (void)updateTabBarAppearance
 {
+  NSLog(@"TabBarCtrl updateTabBarAppearance");
   _needsUpdateOfTabBarAppearance = false;
 
   UIView *_Nullable maybeHostView = [self.view superview];