1c71ab2d9a89643629ef07660691d7ad08501f6b
kligarski <krzysztof.ligarski@swmansion.com>
2025-06-02 11:23:55 +0200

    add e2e test for Test791

--- apps/src/tests/index.ts
+++ apps/src/tests/index.ts
@@ -32,8 +32,8 @@ export { default as Test750 } from './Test750';
 export { default as Test758 } from './Test758';
 export { default as Test761 } from './Test761';     // [E2E skipped]: can't check animation in a meaningful way
 export { default as Test779 } from './Test779';     // [E2E skipped]: can't check animation in a meaningful way
-export { default as Test780 } from './Test780';
-export { default as Test791 } from './Test791';
+export { default as Test780 } from './Test780';     // [E2E skipped]: can't use native swipe back gesture
+export { default as Test791 } from './Test791';     // [E2E created](iOS): issue related to iOS
 export { default as Test800 } from './Test800';
 export { default as Test817 } from './Test817';
 export { default as Test830 } from './Test830';
