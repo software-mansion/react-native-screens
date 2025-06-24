a987d968943244d83aefc158b0e4d939e0979354
Kacper Kafara <kacperkafara@gmail.com>
2025-06-24 13:05:32 +0200

    refactor(Android): make `TabScreenEventEmitter` inherit from `BaseEventEmitter` (#115)
    Make use of `BaseEventEmitter` introduced in #114

--- android/src/main/java/com/swmansion/rnscreens/gamma/tabs/TabScreenEventEmitter.kt
+++ android/src/main/java/com/swmansion/rnscreens/gamma/tabs/TabScreenEventEmitter.kt
@@ -1,55 +1,36 @@
 package com.swmansion.rnscreens.gamma.tabs
 
 import android.util.Log
-import android.view.View
 import com.facebook.react.bridge.ReactContext
-import com.facebook.react.uimanager.UIManagerHelper
-import com.facebook.react.uimanager.events.EventDispatcher
+import com.swmansion.rnscreens.gamma.common.BaseEventEmitter
 import com.swmansion.rnscreens.gamma.tabs.TabScreenEventEmitter.Companion.TAG
 import com.swmansion.rnscreens.gamma.tabs.event.TabScreenDidAppearEvent
 import com.swmansion.rnscreens.gamma.tabs.event.TabScreenDidDisappearEvent
 import com.swmansion.rnscreens.gamma.tabs.event.TabScreenWillAppearEvent
 import com.swmansion.rnscreens.gamma.tabs.event.TabScreenWillDisappearEvent
 
-class TabScreenEventEmitter(val reactContext: ReactContext) {
-    var viewTag: Int = View.NO_ID
-
-    private val reactEventDispatcher: EventDispatcher?
-        get() {
-            // Lets assert for now to make sure we won't miss event delivery
-            checkValidViewTag()
-            return UIManagerHelper.getEventDispatcherForReactTag(reactContext, viewTag)
-        }
-
-    private val surfaceId: Int
-        get() = UIManagerHelper.getSurfaceId(reactContext)
-
+internal class TabScreenEventEmitter(
+    reactContext: ReactContext,
+    viewTag: Int,
+) : BaseEventEmitter(reactContext, viewTag) {
     fun emitOnWillAppear() {
-        checkValidViewTag()
         logEventDispatch(viewTag, TabScreenWillAppearEvent.EVENT_REGISTRATION_NAME)
-        reactEventDispatcher?.dispatchEvent(TabScreenWillAppearEvent(surfaceId, viewTag))
+        reactEventDispatcher.dispatchEvent(TabScreenWillAppearEvent(surfaceId, viewTag))
     }
 
     fun emitOnDidAppear() {
-        checkValidViewTag()
         logEventDispatch(viewTag, TabScreenDidAppearEvent.EVENT_REGISTRATION_NAME)
-        reactEventDispatcher?.dispatchEvent(TabScreenDidAppearEvent(surfaceId, viewTag))
+        reactEventDispatcher.dispatchEvent(TabScreenDidAppearEvent(surfaceId, viewTag))
     }
 
     fun emitOnWillDisappear() {
-        checkValidViewTag()
         logEventDispatch(viewTag, TabScreenWillDisappearEvent.EVENT_REGISTRATION_NAME)
-        reactEventDispatcher?.dispatchEvent(TabScreenWillDisappearEvent(surfaceId, viewTag))
+        reactEventDispatcher.dispatchEvent(TabScreenWillDisappearEvent(surfaceId, viewTag))
     }
 
     fun emitOnDidDisappear() {
-        checkValidViewTag()
         logEventDispatch(viewTag, TabScreenDidDisappearEvent.EVENT_REGISTRATION_NAME)
-        reactEventDispatcher?.dispatchEvent(TabScreenDidDisappearEvent(surfaceId, viewTag))
-    }
-
-    private fun checkValidViewTag() {
-        check(viewTag != View.NO_ID) { "[RNScreens] Attempt to use viewTag before the value was provided"}
+        reactEventDispatcher.dispatchEvent(TabScreenDidDisappearEvent(surfaceId, viewTag))
     }
 
     companion object {
@@ -57,6 +38,9 @@ class TabScreenEventEmitter(val reactContext: ReactContext) {
     }
 }
 
-private fun logEventDispatch(viewTag: Int, eventName: String) {
+private fun logEventDispatch(
+    viewTag: Int,
+    eventName: String,
+) {
     Log.d(TAG, "TabScreen [$viewTag] emits event: $eventName")
 }
