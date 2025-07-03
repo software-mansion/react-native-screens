987b9791bf4780ea345ea9b43e858fca8b0aa9a9
Tomasz Boroń <tomasz.boron@swmansion.com>
2025-07-03 19:07:36 +0200

    feat(iOS, SplitView): Basic skeleton for SplitView (#150)
    ## Description
    
    This PR adds 2 native components: `SplitViewHost` and `SplitViewScreen`.
    
    `SplitViewHost` - component that creates UISplitViewController. At the
    moment, it's creating SplitView with 3 columns ant it's only attaching
    all subviews controllers to SplitView controller in the order they came
    (leaving for later research which component should represent primary,
    secondary and supplementary view)
    
    `SplitViewScreen` - component that creates child view controller. 
    
    ## Changes
    
    - Adding a new directory for native split-view implementation
    - Adding and exposing new native components from sources
    - Adding a new demo app
    
    ## Test code and steps to reproduce
    
    A new example `SplitViewBaseApp` was added.
    
    ## Checklist
    
    - [X] Included code example that can be used to test this change
    - [X] Ensured that CI passes
    
    Closes
    https://github.com/software-mansion/react-native-screens-labs/issues/49
    
    ---------
    
    Co-authored-by: Kacper Kafara <kacperkafara@gmail.com>

---
+++ ios/gamma/split-view/RNSSplitViewScreenComponentView.mm
@@ -0,0 +1,61 @@
+#import "RNSSplitViewScreenComponentView.h"
+#import <React/RCTAssert.h>
+#import <react/renderer/components/rnscreens/ComponentDescriptors.h>
+
+#import "Swift-Bridging.h"
+
+namespace react = facebook::react;
+
+@implementation RNSSplitViewScreenComponentView {
+    RNSSplitViewScreenController *_Nullable _controller;
+}
+
+- (RNSSplitViewScreenController *)controller {
+    RCTAssert(
+              _controller != nil,
+              @"[RNScreens] Attempt to access RNSSplitViewScreenController before RNSSplitViewScreenComponentView was initialized. (for: %@)",
+              self
+              );
+    return _controller;
+}
+
+- (instancetype)initWithFrame:(CGRect)frame
+{
+    if (self = [super initWithFrame:frame]) {
+        [self initState];
+    }
+    
+    return self;
+}
+
+- (void)initState
+{
+    [self setupController];
+}
+
+- (void)setupController
+{
+    _controller = [[RNSSplitViewScreenController alloc] initWithSplitViewScreenComponentView:self];
+    _controller.view = self;
+}
+
+#pragma mark - RCTViewComponentViewProtocol
+
++ (react::ComponentDescriptorProvider)componentDescriptorProvider
+{
+    return react::concreteComponentDescriptorProvider<react::RNSSplitViewScreenComponentDescriptor>();
+}
+
++ (BOOL)shouldBeRecycled
+{
+    // There won't be tens of instances of this component usually & it's easier for now.
+    // We could consider enabling it someday though.
+    return NO;
+}
+
+@end
+
+Class<RCTComponentViewProtocol> RNSSplitViewScreenCls(void)
+{
+    return RNSSplitViewScreenComponentView.class;
+}
---
+++ src/components/gamma/SplitViewHost.tsx
@@ -0,0 +1,31 @@
+import React from 'react';
+import { StyleSheet } from 'react-native';
+import type { ViewProps } from 'react-native';
+import SplitViewHostNativeComponent from '../../fabric/gamma/SplitViewHostNativeComponent';
+import type { NativeProps } from '../../fabric/gamma/SplitViewHostNativeComponent';
+
+export type SplitViewNativeProps = NativeProps & {
+  // Overrides
+}
+
+type SplitViewHostProps = {
+  children?: ViewProps['children'];
+} & SplitViewNativeProps;
+
+function ScreenStackHost({
+  children,
+}: SplitViewHostProps) {
+  return (
+    <SplitViewHostNativeComponent style={styles.container}>
+      {children}
+    </SplitViewHostNativeComponent>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  }
+})
+
+export default ScreenStackHost;
