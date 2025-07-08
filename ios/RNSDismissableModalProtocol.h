6b052114d905b54b561445ea2410f4f7f5c43468
Jakub Tkacz <jtkacz@expo.io>
2025-07-08 13:33:14 +0200

    refactor: format ios

--- ios/RNSDismissableModalProtocol.h
+++ ios/RNSDismissableModalProtocol.h
@@ -2,7 +2,6 @@ NS_ASSUME_NONNULL_BEGIN
 
 @protocol RNSDismissibleModalProtocol <NSObject>
 
-
 // If NO is returned, the modal will not be dismissed when new modal is presented.
 // Use it on your own responsibility, as it can lead to unexpected behavior.
 - (BOOL)isDismissible;
