51eec957298b48dcc0643449ae0eb3de992b8ee4
Tomasz Boroń <tomasz.boron@swmansion.com>
2025-07-16 18:18:23 +0200

    Add missing property definition

--- ios/gamma/split-view/RNSSplitViewHostComponentView.h
+++ ios/gamma/split-view/RNSSplitViewHostComponentView.h
@@ -22,6 +22,7 @@ NS_ASSUME_NONNULL_BEGIN
 @property (nonatomic, readonly) UISplitViewControllerPrimaryEdge primaryEdge;
 @property (nonatomic, readonly) UISplitViewControllerDisplayMode displayMode;
 @property (nonatomic, readonly) BOOL presentsWithGesture;
+@property (nonatomic, readonly) BOOL showSecondaryToggleButton;
 
 @end
 
