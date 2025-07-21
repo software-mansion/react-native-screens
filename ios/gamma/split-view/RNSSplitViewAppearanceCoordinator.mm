e0d89645c2273f8a05e2ff5d29158129caea51f9
Tomasz Boroń <tomasz.boron@swmansion.com>
2025-07-21 14:49:15 +0200

    Add assertions

--- ios/gamma/split-view/RNSSplitViewAppearanceCoordinator.mm
+++ ios/gamma/split-view/RNSSplitViewAppearanceCoordinator.mm
@@ -8,6 +8,9 @@
 - (void)updateAppearanceOfSplitView:(RNSSplitViewHostComponentView *_Nonnull)splitView
                      withController:(RNSSplitViewHostController *_Nonnull)controller
 {
+  RCTAssert(splitView != nil, @"[RNScreens] Attempt to update appearance of uninitialized splitView host component");
+  RCTAssert(controller != nil, @"[RNScreens] Attempt to update appearance of uninitialized splitView controller");
+
   // Step 1 - general settings
   controller.preferredSplitBehavior = splitView.splitBehavior;
   controller.preferredDisplayMode = splitView.displayMode;
