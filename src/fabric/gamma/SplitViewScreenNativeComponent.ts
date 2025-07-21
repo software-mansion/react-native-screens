3d05a0541a45325e421b6e6ebbb8846db52528af
Tomasz Boroń <tomasz.boron@swmansion.com>
2025-07-21 16:10:17 +0200

    Satisfy linter

--- src/fabric/gamma/SplitViewScreenNativeComponent.ts
+++ src/fabric/gamma/SplitViewScreenNativeComponent.ts
@@ -7,6 +7,7 @@ import {
 } from 'react-native/Libraries/Types/CodegenTypes';
 import codegenNativeComponent from 'react-native/Libraries/Utilities/codegenNativeComponent';
 
+// eslint-disable-next-line @typescript-eslint/ban-types
 type GenericEmptyEvent = Readonly<{}>;
 
 export type SplitViewScreenColumnType = 'column' | 'inspector';
