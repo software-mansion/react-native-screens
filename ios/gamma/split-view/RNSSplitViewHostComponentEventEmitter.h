3c4ab8aa4ad97f9e62301dfcb0c2616dd6e7ddb4
Tomasz Boroń <tomasz.boron@swmansion.com>
2025-07-21 17:57:04 +0200

    Emit event on display mode change

--- ios/gamma/split-view/RNSSplitViewHostComponentEventEmitter.h
+++ ios/gamma/split-view/RNSSplitViewHostComponentEventEmitter.h
@@ -25,6 +25,8 @@ NS_ASSUME_NONNULL_BEGIN
  */
 - (BOOL)emitOnCollapse;
 
+- (BOOL)emitOnDisplayModeWillChange:(UISplitViewControllerDisplayMode)
+                 currentDisplayMode:(UISplitViewControllerDisplayMode)nextDisplayMode;
 /**
  * @brief Emits the onExpand event to notify associated component instance that the SplitView has expanded.
  *
