84026b9a407bf821bf8c7709ba152f6426d11707
Tomasz Boroń <tomasz.boron@swmansion.com>
2025-07-23 11:30:46 +0200

    Standardization

--- src/components/gamma/SplitViewHost.types.ts
+++ src/components/gamma/SplitViewHost.types.ts
@@ -1,7 +1,7 @@
 import type { NativeSyntheticEvent, ViewProps } from 'react-native';
 
 // eslint-disable-next-line @typescript-eslint/ban-types
-export type GenericEmptyEvent = Readonly<{}>;
+type GenericEmptyEvent = Readonly<{}>;
 
 export type SplitViewDisplayModeButtonVisibility =
   | 'always'
@@ -27,75 +27,78 @@ export type SplitViewDisplayMode =
 
 export interface SplitViewColumnMetrics {
   /**
-   * Specifies the minimum width for the primary column in the SplitView layout, typically representing the leftmost sidebar.
+   * @summary Minimum width for the primary sidebar.
    *
-   * @platform ios
+   * Specifies the minimum width for the primary column in the SplitView layout, typically representing the leftmost sidebar.
    */
   minimumPrimaryColumnWidth?: number;
   /**
-   * Specifies the maximum width for the primary column in the SplitView layout, typically representing the leftmost sidebar.
+   * @summary Maximum width for the primary sidebar.
    *
-   * @platform ios
+   * Specifies the maximum width (in points) for the primary column in the SplitView layout, typically representing the leftmost sidebar.
    */
   maximumPrimaryColumnWidth?: number;
   /**
-   * Specifies the preferred width for the primary column in the SplitView layout, typically representing the leftmost sidebar.
+   * @summary Preferred width for the primary sidebar.
    *
-   * @platform ios
+   * Specifies the preferred width (in points) for the primary column in the SplitView layout, typically representing the leftmost sidebar.
    */
   preferredPrimaryColumnWidth?: number;
   /**
-   * Specifies the minimum width for the supplementary column in the SplitView layout, typically representing the intermediate sidebar.
+   * @summary Minimum width for the intermediate sidebar.
    *
-   * @platform ios
+   * Specifies the minimum width (in points) for the supplementary column in the SplitView layout, typically representing the intermediate sidebar.
    */
   minimumSupplementaryColumnWidth?: number;
   /**
-   * Specifies the maximum width for the supplementary column in the SplitView layout, typically representing the intermediate sidebar.
+   * @summary Maximum width for the intermediate sidebar.
    *
-   * @platform ios
+   * Specifies the maximum width (in points) for the supplementary column in the SplitView layout, typically representing the intermediate sidebar.
    */
   maximumSupplementaryColumnWidth?: number;
   /**
-   * Specifies the preferred width for the supplementary column in the SplitView layout, typically representing the intermediate sidebar.
+   * @summary Preferred width for the intermediate sidebar.
    *
-   * @platform ios
+   * Specifies the preferred width (in points) for the supplementary column in the SplitView layout, typically representing the intermediate sidebar.
    */
   preferredSupplementaryColumnWidth?: number;
-
-  // iOS 26 only
   /**
-   * Specifies the minimum width for the secondary column in the SplitView layout, typically for the view with the main content.
+   * @summary Minimum width for the secondary component.
    *
-   * @platform ios
+   * Specifies the minimum width (in points) for the secondary column in the SplitView layout, typically for the view with the main content.
+   * 
    * @supported iOS 26 or higher
    */
   minimumSecondaryColumnWidth?: number;
   /**
-   * Specifies the preferred width for the secondary column in the SplitView layout, typically for the view with the main content.
+   * @summary Preferred width for the secondary component.
    *
-   * @platform ios
+   * Specifies the preferred width (in points) for the secondary column in the SplitView layout, typically for the view with the main content.
+   * 
    * @supported iOS 26 or higher
    */
   preferredSecondaryColumnWidth?: number;
   /**
-   * Specifies the minimum width for the inspector column in the SplitView layout, typically the view which is providing additional data about the secondary column.
+   * @summary Minimum width for the inspector component.
    *
-   * @platform ios
+   * Specifies the minimum width (in points) for the inspector column in the SplitView layout, typically the view which is providing additional data about the secondary column.
+   * 
    * @supported iOS 26 or higher
    */
   minimumInspectorColumnWidth?: number;
   /**
-   * Specifies the maximum width for the inspector column in the SplitView layout, typically the view which is providing additional data about the secondary column.
+   * @summary Maximum width for the inspector component.
    *
-   * @platform ios
+   * Specifies the maximum width (in points) for the inspector column in the SplitView layout, typically the view which is providing additional data about the secondary column.
+   * 
    * @supported iOS 26 or higher
    */
   maximumInspectorColumnWidth?: number;
   /**
-   * Specifies the preferred width for the inspector column in the SplitView layout, typically the view which is providing additional data about the secondary column.
+   * @summary Preferred width for the inspector component.
    *
-   * @platform ios
+   * Specifies the preferred width (in points) for the inspector column in the SplitView layout, typically the view which is providing additional data about the secondary column.
+   * 
    * @supported iOS 26 or higher
    */
   preferredInspectorColumnWidth?: number;
@@ -104,7 +107,7 @@ export interface SplitViewHostProps extends ViewProps {
   children?: React.ReactNode;
 
   /**
-   * An object describing bounds for column widths.
+   * @summary An object describing bounds for column widths.
    *
    * It supports definitions for the following columns:
    *
@@ -115,13 +118,12 @@ export interface SplitViewHostProps extends ViewProps {
    *
    * - `secondary` - the view with the main content
    * - `inspector` - the view which is providing additional data about the secondary column
-   *
-   * @platform ios
    */
   columnMetrics?: SplitViewColumnMetrics;
   /**
-   * Specifies the display mode which will be preferred to use, if the layout requirements are met.
-   * The following values are currently supported (they correspond to [UISplitViewController.DisplayMode](https://developer.apple.com/documentation/uikit/uisplitviewcontroller/displaymode-swift.enum)):
+   * @summary Specifies the display mode which will be preferred to use, if the layout requirements are met.
+   * 
+   * The following values are currently supported:
    *
    * - `automatic` - display mode is chosen by the OS, the appropriate display mode is based on the device and the current app size
    * - `secondaryOnly` – only the secondary column is displayed
@@ -130,85 +132,89 @@ export interface SplitViewHostProps extends ViewProps {
    * - `oneOverSecondary` – a one sidebar is displayed over the secondary column
    * - `twoOverSecondary` – two sidebars are displayed over the secondary column
    * - `twoDisplaceSecondary` – two sidebars are displacind the secondary column, moving it partially offscreen
-   *
-   * @platform ios
+   * 
+   * The supported values corresponds to the official UIKit documentation:
+   * @see {@link https://developer.apple.com/documentation/uikit/uisplitviewcontroller/displaymode-swift.enum|UISplitViewController.DisplayMode}
+   * 
+   * @default automatic
    */
   displayMode?: SplitViewDisplayMode;
   /**
-   * Determines whether the button for changing the SplitView display mode is visible on the screen.
-   * The following values are currently supported (they correspond to [UISplitViewController.DisplayModeButtonVisibility](https://developer.apple.com/documentation/uikit/uisplitviewcontroller/displaymodebuttonvisibility-swift.enum)):
+   * @summary Determines whether the button for changing the SplitView display mode is visible on the screen.
+   * 
+   * The following values are currently supported:
    *
    * - `automatic` - the visibility of the display mode button is set by system
    * - `always` – the display mode button is always visible
    * - `never` – the display mode button is always hidden
-   *
-   * @platform ios
+   * 
+   * The supported values corresponds to the official UIKit documentation:
+   * @see {@link https://developer.apple.com/documentation/uikit/uisplitviewcontroller/displaymodebuttonvisibility-swift.enum|UISplitViewController.DisplayModeButtonVisibility}
+   * 
+   * @default automatic
    */
   displayModeButtonVisibility?: SplitViewDisplayModeButtonVisibility;
   /**
-   * A callback that gets invoked when the SplitView was collapsed to a single column.
-   *
-   * @platform ios
+   * @summary A callback that gets invoked when the SplitView was collapsed to a single column.
    */
   onCollapse?: (e: NativeSyntheticEvent<GenericEmptyEvent>) => void;
   /**
-   * A callback that gets invoked when the SplitView was expanded to multiple columns.
-   *
-   * @platform ios
+   * @summary A callback that gets invoked when the SplitView was expanded to multiple columns.
    */
   onExpand?: (e: NativeSyntheticEvent<GenericEmptyEvent>) => void;
   /**
-   * A callback that gets invoked when the SplitView inspector is either programmatically hidden (in column presentation) or dismissed (in modal presentation).
+   * @summary A callback that gets invoked when the SplitView inspector is either programmatically hidden (in column presentation) or dismissed (in modal presentation).
    *
-   * @remarks
    * The purpose of this callback depends on whether the SplitView is collapsed or expanded.
-   *
-   * @platform ios
+   * 
    * @supported iOS 26 or higher
    */
   onInspectorHide?: (e: NativeSyntheticEvent<GenericEmptyEvent>) => void;
   /**
-   * Determines whether gestures are enabled to change the display mode.
-   *
-   * @platform ios
+   * @summary Determines whether gestures are enabled to change the display mode.
    */
   presentsWithGesture?: boolean;
   /**
-   * Indicates on which side primary sidebar is placed, affecting the split view layout.
-   * The following values are currently supported (they correspond to [UISplitViewController.PrimaryEdge](https://developer.apple.com/documentation/uikit/uisplitviewcontroller/primaryedge-swift.enum)):
+   * @summary Indicates on which side primary sidebar is placed, affecting the split view layout.
+   * 
+   * The following values are currently supported:
    *
    * - `leading` - primary sidebar is placed on the leading edge of the interface
    * - `trailing` - primary sidebar is placed on the trailing edge of the interface
-   *
-   * @platform ios
+   * 
+   * The supported values corresponds to the official UIKit documentation:
+   * @see {@link https://developer.apple.com/documentation/uikit/uisplitviewcontroller/primaryedge-swift.enum|UISplitViewController.PrimaryEdge}
+   * 
+   * @default leading
    */
   primaryEdge?: SplitViewPrimaryEdge;
   /**
-   * Determines whether inspector column should be displayed on the trailing edge of the main (secondary) column (for expanded SplitView) or as a modal (for collapsed SplitView).
-   *
-   * @remarks
+   * @summary Determines whether inspector column should be displayed.
+   * 
+   * Inspector will be displayed on the trailing edge of the main (secondary) column (for expanded SplitView) or as a modal (for collapsed SplitView).
    * The result on the interface for this prop depends on whether the SplitView is collapsed or expanded.
-   *
-   * @platform ios
+   * 
    * @supported iOS 26 or higher
    */
   showInspector?: boolean;
   /**
-   * Determines whether a button to toggle to and from secondaryOnly display mode is visible.
-   *
-   * @platform ios
+   * @summary Determines whether a button to toggle to and from secondaryOnly display mode is visible.
    */
   showSecondaryToggleButton?: boolean;
   /**
-   * Specifies the split behavior which will be preferred to use, if the layout requirements are met.
-   * The following values are currently supported (they correspond to [UISplitViewController.SplitBehavior](https://developer.apple.com/documentation/uikit/uisplitviewcontroller/splitbehavior-swift.enum)):
+   * @summary Specifies the split behavior which will be preferred to use, if the layout requirements are met.
+   * 
+   * The following values are currently supported:
    *
    * - `automatic` - chosen by the OS, the appropriate split behavior is based on the device and the current app size
    * - `displace` – the main column is moved partially offscreen, making a space for sidebars
    * - `overlay` – the sidebars are partially covering main column
    * - `tile` – the sidebars appears side-by-side with the main column
-   *
-   * @platform ios
+   * 
+   * The supported values corresponds to the official UIKit documentation:
+   * @see {@link https://developer.apple.com/documentation/uikit/uisplitviewcontroller/splitbehavior-swift.enum|UISplitViewController.SplitBehavior}
+   * 
+   * @default automatic
    */
   splitBehavior?: SplitViewSplitBehavior;
 }
