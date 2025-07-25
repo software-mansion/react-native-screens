ac1f77e233bddb2e662984f0a2ca714d12f9508d
Konrad Michalik <konrad.michalik@swmansion.com>
2025-07-25 10:42:27 +0200

    Format components in test

--- apps/src/tests/Test3004.tsx
+++ apps/src/tests/Test3004.tsx
@@ -24,72 +24,82 @@ type StackNavigationProp = NavigationProp<StackRouteParamList>;
 const Stack = createNativeStackNavigator<StackRouteParamList>();
 
 function First({ navigation }: StackNavigationProp) {
-  return (<ScrollView contentInsetAdjustmentBehavior='automatic'>
-    <View style={{ flexDirection: 'row' }}>
-      <Button onPress={() => navigation.preload('Second')} title='Preload Second'/>
-      <Button onPress={() => navigation.push('Second')} title='Go to Second'/>
-    </View>
-    <View style={{ flexDirection: 'row' }}>
-      <Button onPress={() => navigation.preload('Third')} title='Preload Third'/>
-      <Button onPress={() => navigation.push('Third')} title='Go to Third'/>
-    </View>
-    <Button onPress={() => navigation.push('Modal')} title='Modal'/>
-    <Button onPress={() => navigation.replace('Modal')} title='Replace Modal'/>
-    <Button onPress={() => navigation.pop()} title='Pop'/>
-    <Button onPress={() => navigation.popToTop()} title='Pop all'/>
-  </ScrollView>)
+  return (
+    <ScrollView contentInsetAdjustmentBehavior='automatic'>
+      <View style={{ flexDirection: 'row' }}>
+        <Button onPress={() => navigation.preload('Second')} title='Preload Second'/>
+        <Button onPress={() => navigation.push('Second')} title='Go to Second'/>
+      </View>
+      <View style={{ flexDirection: 'row' }}>
+        <Button onPress={() => navigation.preload('Third')} title='Preload Third'/>
+        <Button onPress={() => navigation.push('Third')} title='Go to Third'/>
+      </View>
+      <Button onPress={() => navigation.push('Modal')} title='Modal'/>
+      <Button onPress={() => navigation.replace('Modal')} title='Replace Modal'/>
+      <Button onPress={() => navigation.pop()} title='Pop'/>
+      <Button onPress={() => navigation.popToTop()} title='Pop all'/>
+    </ScrollView>
+  )
 }
 
 function Second({ navigation }: StackNavigationProp) {
-  return (<ScrollView contentInsetAdjustmentBehavior='automatic'>
-    <View style={{ flexDirection: 'row' }}>
-      <Button onPress={() => navigation.preload('First')} title='Preload First'/>
-      <Button onPress={() => navigation.push('First')} title='Go to First'/>
-    </View>
-    <View style={{ flexDirection: 'row' }}>
-      <Button onPress={() => navigation.preload('Third')} title='Preload Third'/>
-      <Button onPress={() => navigation.push('Third')} title='Go to Third'/>
-    </View>
-    <Button onPress={() => navigation.replace('Replacement')} title='Replace'/>
-    <Button onPress={() => navigation.pop()} title='Pop'/>
-    <Button onPress={() => navigation.popToTop()} title='Pop all'/>
-  </ScrollView>)
+  return (
+    <ScrollView contentInsetAdjustmentBehavior='automatic'>
+      <View style={{ flexDirection: 'row' }}>
+        <Button onPress={() => navigation.preload('First')} title='Preload First'/>
+        <Button onPress={() => navigation.push('First')} title='Go to First'/>
+      </View>
+      <View style={{ flexDirection: 'row' }}>
+        <Button onPress={() => navigation.preload('Third')} title='Preload Third'/>
+        <Button onPress={() => navigation.push('Third')} title='Go to Third'/>
+      </View>
+      <Button onPress={() => navigation.replace('Replacement')} title='Replace'/>
+      <Button onPress={() => navigation.pop()} title='Pop'/>
+      <Button onPress={() => navigation.popToTop()} title='Pop all'/>
+    </ScrollView>
+  )
 }
 
 function Third({ navigation }: StackNavigationProp) {
-  return (<ScrollView contentInsetAdjustmentBehavior='automatic'>
-    <View style={{ flexDirection: 'row' }}>
-      <Button onPress={() => navigation.preload('First')} title='Preload First'/>
-      <Button onPress={() => navigation.push('First')} title='Go to First'/>
-    </View>
-    <View style={{ flexDirection: 'row' }}>
-      <Button onPress={() => navigation.preload('Second')} title='Preload Second'/>
-      <Button onPress={() => navigation.push('Second')} title='Go to Second'/>
-    </View>
-    <Button onPress={() => navigation.replace('Replacement')} title='Replace'/>
-    <Button onPress={() => navigation.pop()} title='Pop'/>
-    <Button onPress={() => navigation.popToTop()} title='Pop all'/>
-  </ScrollView>)
+  return (
+    <ScrollView contentInsetAdjustmentBehavior='automatic'>
+      <View style={{ flexDirection: 'row' }}>
+        <Button onPress={() => navigation.preload('First')} title='Preload First'/>
+        <Button onPress={() => navigation.push('First')} title='Go to First'/>
+      </View>
+      <View style={{ flexDirection: 'row' }}>
+        <Button onPress={() => navigation.preload('Second')} title='Preload Second'/>
+        <Button onPress={() => navigation.push('Second')} title='Go to Second'/>
+      </View>
+      <Button onPress={() => navigation.replace('Replacement')} title='Replace'/>
+      <Button onPress={() => navigation.pop()} title='Pop'/>
+      <Button onPress={() => navigation.popToTop()} title='Pop all'/>
+    </ScrollView>
+  )
 }
 
 function Modal({ navigation }: StackNavigationProp) {
-  return (<View style={{
-    width: '100%',
-    height: '100%',
-    flexDirection: 'column',
-    alignItems: 'center',
-    justifyContent: 'center',
-    backgroundColor: Colors.NavyLightTransparent
-  }}>
-    <Button onPress={() => navigation.pop()} title='Go Back'/>
-  </View>)
+  return (
+    <View style={{
+      width: '100%',
+      height: '100%',
+      flexDirection: 'column',
+      alignItems: 'center',
+      justifyContent: 'center',
+      backgroundColor: Colors.NavyLightTransparent
+    }}>
+      <Button onPress={() => navigation.pop()} title='Go Back'/>
+    </View>
+  )
 }
 
 function Replacement({ navigation }: StackNavigationProp) {
-  return (<View style={{ backgroundColor: Colors.NavyLight60 }}>
-    <Text style={{ fontSize: 20, color: 'white' }}>Replacement</Text>
-    <Button onPress={() => navigation.pop()} title='Pop'/>
-  </View>)
+  return (
+    <View style={{ backgroundColor: Colors.NavyLight60 }}>
+      <Text style={{ fontSize: 20, color: 'white' }}>Replacement</Text>
+      <Button onPress={() => navigation.pop()} title='Pop'/>
+    </View>
+  )
 }
 
 export default function App() {
