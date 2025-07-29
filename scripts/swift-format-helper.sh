baad802059a67c0dcd165ef5d48ea9c1c8b2ff4a
Konrad Michalik <konrad.michalik@swmansion.com>
2025-07-29 07:04:55 +0200

    Run swift lint on macos runner, split jobs between runners

--- scripts/swift-format-helper.sh
+++ scripts/swift-format-helper.sh
@@ -2,5 +2,5 @@ changed=$(find ios Example/ios FabricExample/ios -type f -name '*.swift' | tr '\
 
 case "$1" in
     "format") swift-format format --in-place --parallel --configuration=.swift-format $changed;;
-    "lint") swift-format lint --strict --parallel --configuration=.swift-format $changed;;
+    "lint") xcrun swift-format lint --strict --parallel --configuration=.swift-format $changed;;
 esac
