e31ca607bcc6121183030a476670a95034064327
Maciej Stosio <maciekstosio@users.noreply.github.com>
2025-07-31 10:01:07 +0200

    allow to pass config through props

--- apps/src/tests/TestScreenStack/index.tsx
+++ apps/src/tests/TestScreenStack/index.tsx
@@ -1,11 +1,38 @@
 import React from 'react';
-
-import { generateStackWithNames } from './helper';
 import { StackContainer } from '../../shared/gamma/containers/stack/StackContainer';
+import { generateStackWithNames } from './helper';
+
+
+const config = generateStackWithNames([
+  {
+    name: 'A',
+    options: {
+      navigationBar: {
+        title: 'Screen A',
+      },
+    },
+  },
+  {
+    name: 'B',
+    options: {
+      navigationBar: {
+        title: 'Screen B',
+      },
+    },
+  },
+  {
+    name: 'C',
+    options: {
+      navigationBar: {
+        title: 'Screen C',
+      },
+    },
+  },
+]);
 
 export default function App() {
   return (
-    <StackContainer pathConfigs={generateStackWithNames(['A', 'B', 'C'])} />
+    <StackContainer pathConfigs={config} />
   );
 }
 
