d6e277bb2a00d88b4712ac6ee703921d431e04b0
Johan Kasperi <johan@kasperi.se>
2025-08-06 11:58:15 +0200

    add BarButtonItems example to example app root

--- apps/App.tsx
+++ apps/App.tsx
@@ -2,10 +2,12 @@ import React from 'react';
 import { enableFreeze } from 'react-native-screens';
 // import Example from './Example';
 import * as Test from './src/tests';
+//import BarButtonItemsExample from './src/screens/BarButtonItems';
 
 enableFreeze(true);
 
 export default function App() {
   // return <Example />;
+  // return <BarButtonItemsExample />;
   return <Test.TestBottomTabs />;
 }
