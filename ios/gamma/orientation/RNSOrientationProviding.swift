f6d63fefbd21bb5f50b00cf856b8e6249931883a
Tomasz Boroń <tomasz.boron@swmansion.com>
2025-08-07 15:57:55 +0200

    Add comment about the necessity of deduplication

--- ios/gamma/orientation/RNSOrientationProviding.swift
+++ ios/gamma/orientation/RNSOrientationProviding.swift
@@ -1,3 +1,5 @@
+// TODO: investigate objc - swift interop and deduplicate this code
+// This class needs to follow the RNSOrientationProviding from objc.
 @objc
 public protocol RNSOrientationProvidingSwift {
   func evaluateOrientation() -> RNSOrientationSwift
