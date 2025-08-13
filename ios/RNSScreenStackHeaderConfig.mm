8f6ca0c4d7a6c26bab2946b97542f3cecb32d5f8
Johan Kasperi <johan@kasperi.se>
2025-08-13 13:31:55 +0200

    remove old debug callback

--- ios/RNSScreenStackHeaderConfig.mm
+++ ios/RNSScreenStackHeaderConfig.mm
@@ -1001,10 +1001,6 @@ RNS_IGNORE_SUPER_CALL_END
   }
 }
 
-- (void)onPressHeaderBarButtonItemHandler:(NSString *)buttonId
-{
-}
-
 static RCTResizeMode resizeModeFromCppEquiv(react::ImageResizeMode resizeMode)
 {
   switch (resizeMode) {
