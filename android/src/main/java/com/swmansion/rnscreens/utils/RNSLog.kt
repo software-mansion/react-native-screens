0dfa75e83f0f1c74d6c7eaded08da3d1d3df2db0
Jakub Tkacz <jtkacz@expo.io>
2025-08-18 17:50:31 +0200

    fix: linting on android

--- android/src/main/java/com/swmansion/rnscreens/utils/RNSLog.kt
+++ android/src/main/java/com/swmansion/rnscreens/utils/RNSLog.kt
@@ -4,13 +4,20 @@ import android.util.Log
 import com.swmansion.rnscreens.BuildConfig
 
 object RNSLog {
-    fun d(tag: String, message: String) {
+    fun d(
+        tag: String,
+        message: String,
+    ) {
         if (BuildConfig.RNS_DEBUG_LOGGING) {
             Log.d(tag, message)
         }
     }
-    
-    fun d(tag: String, message: String, vararg args: Any) {
+
+    fun d(
+        tag: String,
+        message: String,
+        vararg args: Any,
+    ) {
         if (BuildConfig.RNS_DEBUG_LOGGING) {
             Log.d(tag, message.format(*args))
         }
