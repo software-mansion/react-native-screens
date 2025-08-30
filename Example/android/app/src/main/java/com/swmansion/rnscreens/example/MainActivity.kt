929a42e1c7772a7f21fdcb15360b781c0566374f
Kacper Kafara <kacperkafara@gmail.com>
2025-08-30 15:29:07 +0200

    Update imports in example apps

--- Example/android/app/src/main/java/com/swmansion/rnscreens/example/MainActivity.kt
+++ Example/android/app/src/main/java/com/swmansion/rnscreens/example/MainActivity.kt
@@ -5,7 +5,7 @@ import com.facebook.react.ReactActivity
 import com.facebook.react.ReactActivityDelegate
 import com.facebook.react.defaults.DefaultNewArchitectureEntryPoint.fabricEnabled
 import com.facebook.react.defaults.DefaultReactActivityDelegate
-import com.swmansion.rnscreens.RNScreensFragmentFactory
+import com.swmansion.rnscreens.fragment.restoration.RNScreensFragmentFactory
 
 class MainActivity : ReactActivity() {
 
