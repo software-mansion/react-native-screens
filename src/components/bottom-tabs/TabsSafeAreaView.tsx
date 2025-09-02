890133ec4ee7a048a3ccd3e00b821f17fbdfca01
Kacper Kafara <kacperkafara@gmail.com>
2025-09-02 10:04:11 +0200

    Add styles for native component

--- src/components/bottom-tabs/TabsSafeAreaView.tsx
+++ src/components/bottom-tabs/TabsSafeAreaView.tsx
@@ -1,21 +1,31 @@
 import React from 'react';
-import { ViewProps } from 'react-native';
+import { StyleSheet, ViewProps } from 'react-native';
 import TabsSafeAreaViewNativeComponent from '../../fabric/bottom-tabs/TabsSafeAreaViewNativeComponent';
 
 export interface TabsSafeAreaViewProps extends ViewProps {}
 
 function TabsSafeAreaView(props: TabsSafeAreaViewProps) {
+  const [marginBottom, setMarginBottom] = React.useState(0);
+
   return (
     <TabsSafeAreaViewNativeComponent
       {...props}
       collapsable={false}
+      style={[styles.safeAreaView, { marginBottom }]}
       onNativeLayout={event => {
         console.log(
           `Received tabBarHeight on JS side ${event.nativeEvent.tabBarHeight}`,
         );
+        setMarginBottom(event.nativeEvent.tabBarHeight);
       }}
     />
   );
 }
+
+const styles = StyleSheet.create({
+  safeAreaView: {
+    flex: 1,
+  },
+});
 
 export default TabsSafeAreaView;
