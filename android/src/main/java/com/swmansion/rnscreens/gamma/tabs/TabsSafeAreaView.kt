2c0b29195cc89d46e6cfc85ba1335f5bce9e8c4b
Kacper Kafara <kacperkafara@gmail.com>
2025-09-02 10:03:49 +0200

    Stash

--- android/src/main/java/com/swmansion/rnscreens/gamma/tabs/TabsSafeAreaView.kt
+++ android/src/main/java/com/swmansion/rnscreens/gamma/tabs/TabsSafeAreaView.kt
@@ -1,13 +1,19 @@
 package com.swmansion.rnscreens.gamma.tabs
 
+import android.annotation.SuppressLint
 import android.util.Log
 import com.facebook.react.uimanager.ThemedReactContext
 import com.facebook.react.views.view.ReactViewGroup
+import com.swmansion.rnscreens.ext.parentAsView
 
+@SuppressLint("ViewConstructor") // Should never be recreated
 class TabsSafeAreaView(
     private val reactContext: ThemedReactContext,
 ) : ReactViewGroup(reactContext) {
     internal lateinit var eventEmitter: TabsSafeAreaViewEventEmitter
+
+    private val parentScreen: TabScreen?
+        get() = (this.parent as? TabScreen)
 
     override fun onLayout(
         changed: Boolean,
@@ -16,15 +22,13 @@ class TabsSafeAreaView(
         right: Int,
         bottom: Int,
     ) {
-        if (this::eventEmitter.isInitialized) {
-            Log.d("TabsSafeAreaView", "emitting on native layout event")
-            eventEmitter.emitOnNativeLayout(100)
-        }
+        Log.d("TabsSafeAreaView", "emitting on native layout event")
+        eventEmitter.emitOnNativeLayout(100)
     }
 
     internal fun onViewManagerAddEventEmitters() {
         // When this is called from View Manager the view tag is already set
-        check(id != NO_ID) { "[RNScreens] TabsHost must have its tag set when registering event emitters" }
+        check(id != NO_ID) { "[RNScreens] ${TabsSafeAreaView::class.simpleName} must have its tag set when registering event emitters" }
         eventEmitter = TabsSafeAreaViewEventEmitter(reactContext, id)
     }
 }
