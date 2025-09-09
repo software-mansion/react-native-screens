afe1ad8404ac8dc848b3733e0a2cd4d9b5051489
kmichalikk <63911178+kmichalikk@users.noreply.github.com>
2025-09-09 10:16:09 +0200

    fix(iOS): Prevent delay when dismissing transparentModals (#3189)
    ## Description
    
    Closes #3074.
    
    This PR removes the delay when dismissing fullscreen modal-like screens.
    This behavior can be observed on native apps, but makes things look as
    if they are broken. We also observe that modal screens don't take
    transitionDuration into account. We don't plan on supporting it now, but
    might look into it in the future. Docs are updated to reflect the
    current state.
    
    ## Testing
    
    Use Test3074. It has 4 screens in stack - 1. and 2. are a regular
    screen, 3. & 4. are transparentModals. The last one should dismiss
    immediately without animation. Revert changes in RNSScreen to see the
    bug. Checking Debug/Slow animations in simulator makes it even more
    apparent.
    
    ---------
    
    Co-authored-by: Kacper Kafara <kacperkafara@gmail.com>

--- ios/RNSScreen.mm
+++ ios/RNSScreen.mm
@@ -1715,19 +1715,30 @@ Class<RCTComponentViewProtocol> RNSScreenCls(void)
 - (void)setupProgressNotification
 {
   if (self.transitionCoordinator != nil) {
+    if (!self.transitionCoordinator.isAnimated) {
+      // If the transition is not animated, there is no point to set up animation
+      // and completion callbacks. This helps prevent issues with dismissed modals having
+      // "artifical animation duration" instead of being removed instantly.
+      // See: https://github.com/software-mansion/react-native-screens/pull/3189/
+      return;
+    }
+
     _fakeView.alpha = 0.0;
+
+    auto animation = ^(id<UIViewControllerTransitionCoordinatorContext> _Nonnull context) {
+      [[context containerView] addSubview:self->_fakeView];
+      self->_fakeView.alpha = 1.0;
+      self->_animationTimer = [CADisplayLink displayLinkWithTarget:self selector:@selector(handleAnimation)];
+      [self->_animationTimer addToRunLoop:[NSRunLoop currentRunLoop] forMode:NSDefaultRunLoopMode];
+    };
+
     [self.transitionCoordinator
-        animateAlongsideTransition:^(id<UIViewControllerTransitionCoordinatorContext> _Nonnull context) {
-          [[context containerView] addSubview:self->_fakeView];
-          self->_fakeView.alpha = 1.0;
-          self->_animationTimer = [CADisplayLink displayLinkWithTarget:self selector:@selector(handleAnimation)];
-          [self->_animationTimer addToRunLoop:[NSRunLoop currentRunLoop] forMode:NSDefaultRunLoopMode];
-        }
-        completion:^(id<UIViewControllerTransitionCoordinatorContext> _Nonnull context) {
-          [self->_animationTimer setPaused:YES];
-          [self->_animationTimer invalidate];
-          [self->_fakeView removeFromSuperview];
-        }];
+        animateAlongsideTransition:animation
+                        completion:^(id<UIViewControllerTransitionCoordinatorContext> _Nonnull context) {
+                          [self->_animationTimer setPaused:YES];
+                          [self->_animationTimer invalidate];
+                          [self->_fakeView removeFromSuperview];
+                        }];
   }
 }
 
