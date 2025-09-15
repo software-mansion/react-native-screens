62136770c777ee8d1a558d930741717c9fc9777b
Krzysztof Ligarski <krzysztof.ligarski@swmansion.com>
2025-09-15 12:56:43 +0200

    add comments about source

--- android/src/main/java/com/swmansion/rnscreens/safearea/SafeAreaProvider.kt
+++ android/src/main/java/com/swmansion/rnscreens/safearea/SafeAreaProvider.kt
@@ -1,7 +1,5 @@
 package com.swmansion.rnscreens.safearea
 
-import androidx.core.graphics.Insets
-
 interface SafeAreaProvider {
     fun setOnInterfaceInsetsChangeListener(listener: SafeAreaView)
     fun removeOnInterfaceInsetsChangeListener(listener: SafeAreaView)
--- android/src/main/java/com/swmansion/rnscreens/safearea/SafeAreaViewManager.kt
+++ android/src/main/java/com/swmansion/rnscreens/safearea/SafeAreaViewManager.kt
@@ -1,3 +1,5 @@
+// Implementation adapted from `react-native-safe-area-context`:
+// https://github.com/AppAndFlow/react-native-safe-area-context/tree/v5.6.1
 package com.swmansion.rnscreens.safearea
 
 import com.facebook.react.bridge.ReadableMap
