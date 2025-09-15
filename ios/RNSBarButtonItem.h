410a8d49f498d5f58b39fc775913544c47998555
Johan Kasperi <johan@kasperi.se>
2025-09-15 13:34:04 +0200

    buttonId and itemAction as internal properties

--- ios/RNSBarButtonItem.h
+++ ios/RNSBarButtonItem.h
@@ -5,9 +5,6 @@ typedef void (^RNSBarButtonMenuItemAction)(NSString *menuId);
 
 @interface RNSBarButtonItem : UIBarButtonItem
 
-@property (nonatomic, copy) NSString *buttonId;
-@property (nonatomic, copy) RNSBarButtonItemAction itemAction;
-
 - (instancetype)initWithConfig:(NSDictionary<NSString *, id> *)dict
                         action:(RNSBarButtonItemAction)action
                     menuAction:(RNSBarButtonMenuItemAction)menuAction;
