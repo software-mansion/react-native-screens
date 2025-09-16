078c71d29abe148ddbdc04bdc810b71466162ac2
Konrad Michalik <konrad.michalik@swmansion.com>
2025-09-16 07:24:42 +0200

    Restore SplitViewBaseApp

--- apps/src/tests/TestSplitView/SplitViewBaseApp.tsx
+++ apps/src/tests/TestSplitView/SplitViewBaseApp.tsx
@@ -2,23 +2,22 @@ import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 import { SplitViewHost, SplitViewScreen } from 'react-native-screens/experimental';
 import { Colors } from '../../shared/styling/Colors';
-import { TestBottomTabs, TestFoo, TestScreenStack } from '..';
+import { TestBottomTabs, TestScreenStack } from '..';
 import { SplitViewBaseConfig } from './helpers/types';
 
 const SplitViewBaseApp = ({ splitViewBaseConfig }: { splitViewBaseConfig: SplitViewBaseConfig }) => {
   return (
     <SplitViewHost {...splitViewBaseConfig}>
       <SplitViewScreen.Column>
-        {/* <View style={[styles.container, { backgroundColor: Colors.White }]}>
+        <View style={[styles.container, { backgroundColor: Colors.White }]}>
           <Text style={styles.text}>Primary column</Text>
-        </View> */}
-        <TestFoo />
+        </View>
       </SplitViewScreen.Column>
       <SplitViewScreen.Column>
-        <TestFoo />
+        <TestBottomTabs />
       </SplitViewScreen.Column>
       <SplitViewScreen.Column>
-        <TestFoo />
+        <TestScreenStack />
       </SplitViewScreen.Column>
     </SplitViewHost>
   );
