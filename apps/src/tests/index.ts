7b31da1b5168842fd2c4b2f2426943ffe89acc8c
Krzysztof Ligarski <krzysztof.ligarski@swmansion.com>
2025-09-18 10:23:26 +0200

    rename example to TestSafeAreaViewIOS

--- apps/src/tests/index.ts
+++ apps/src/tests/index.ts
@@ -162,4 +162,4 @@ export { default as TestAnimation } from './TestAnimation';
 export { default as TestBottomTabs } from './TestBottomTabs';
 export { default as TestScreenStack } from './TestScreenStack';
 export { default as TestSplitView } from './TestSplitView';
-export { default as TestSafeAreaView } from './TestSafeAreaView';
+export { default as TestSafeAreaViewIOS } from './TestSafeAreaViewIOS';
