cb573bafe3123bacd2f40fd9c6c65512c7602856
Krzysztof Ligarski <krzysztof.ligarski@swmansion.com>
2025-09-18 10:24:58 +0200

    remove redundant imports

--- apps/src/tests/TestSafeAreaViewIOS/BottomTabs/TestTab.tsx
+++ apps/src/tests/TestSafeAreaViewIOS/BottomTabs/TestTab.tsx
@@ -1,12 +1,6 @@
 import React from 'react';
 import { useBottomTabsSAVExampleContext } from './BottomTabsSAVExampleContext';
-import RegularView from '../shared/RegularView';
-import ScrollViewNever from '../shared/ScrollViewNever';
-import ScrollViewAutomatic from '../shared/ScrollViewAutomatic';
-import BottomTabsSAVExample from '.';
-import { Text } from 'react-native';
 import { SafeAreaView } from 'react-native-screens/private';
-import StackV4SAVExample from '../StackV4';
 import { mapContentStringToComponent } from '../shared';
 
 export default function TestTab() {
--- apps/src/tests/TestSafeAreaViewIOS/StackV4/TestScreen.tsx
+++ apps/src/tests/TestSafeAreaViewIOS/StackV4/TestScreen.tsx
@@ -1,11 +1,6 @@
 import React, { useEffect } from 'react';
 import { useStackV4SAVExampleContext } from './StackV4SAVExampleContext';
-import RegularView from '../shared/RegularView';
-import ScrollViewNever from '../shared/ScrollViewNever';
-import ScrollViewAutomatic from '../shared/ScrollViewAutomatic';
-import BottomTabsSAVExample from '../BottomTabs';
-import StackV4SAVExample, { StackNavigationProp } from '.';
-import { Text } from 'react-native';
+import { StackNavigationProp } from '.';
 import { SafeAreaView } from 'react-native-screens/private';
 import { mapContentStringToComponent } from '../shared';
 
