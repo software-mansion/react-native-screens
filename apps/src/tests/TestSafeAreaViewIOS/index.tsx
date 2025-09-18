6a80980c763031239bca544f70b950d6b0049a7b
Krzysztof Ligarski <krzysztof.ligarski@swmansion.com>
2025-09-18 10:31:29 +0200

    fix commented out imports

--- apps/src/tests/TestSafeAreaViewIOS/index.tsx
+++ apps/src/tests/TestSafeAreaViewIOS/index.tsx
@@ -1,8 +1,8 @@
 import React from 'react';
 
 import StackV4SAVExample from './stack-v4';
-// import BottomTabsSAVExample from './BottomTabs';
-// import SplitViewSAVExample from './SplitView';
+// import BottomTabsSAVExample from './bottom-tabs';
+// import SplitViewSAVExample from './split-view';
 
 export default function App() {
   return <StackV4SAVExample />;
