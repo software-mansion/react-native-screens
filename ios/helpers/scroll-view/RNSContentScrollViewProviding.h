0fcf2f341020399077bff8f9d8c59dee785e652f
Konrad Michalik <konrad.michalik@swmansion.com>
2025-09-29 14:56:33 +0200

    Redo documentation for ContentScrollViewProviding

--- ios/helpers/scroll-view/RNSContentScrollViewProviding.h
+++ ios/helpers/scroll-view/RNSContentScrollViewProviding.h
@@ -1,9 +1,9 @@
 @protocol RNSContentScrollViewProviding
+
 /**
  * Finds content ScrollView within provider's hierarchy. The content ScrollView serves as a main interaction on the
- * given screen. Implementations should use `RNSScrollViewFinder.findContentScrollViewWithDelegatingToProvider` to
- * continue continue the search when no more custom logic is necessary (e.g Stack determining the topmost screen (custom
- * part) and forwarding the call directly to its view (going back to regular search)).
+ * given screen. Implementations may use `RNSScrollViewFinder` to continue the search however they see fit.
  */
 - (nullable UIScrollView *)findContentScrollView;
+
 @end
