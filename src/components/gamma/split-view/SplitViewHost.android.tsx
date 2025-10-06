43499290b9201b41e276f13387b328a2c86b624b
Tomasz Boroń <tomasz.boron@swmansion.com>
2025-10-06 11:50:18 +0200

    Add tag

--- src/components/gamma/split-view/SplitViewHost.android.tsx
+++ src/components/gamma/split-view/SplitViewHost.android.tsx
@@ -3,7 +3,7 @@ import { View } from 'react-native';
 const SplitViewHost = View;
 
 console.warn(
-  'SplitView is supported only for iOS. Consider using an alternative layout for Android.',
+  '[RNScreens] SplitView is supported only for iOS. Consider using an alternative layout for Android.',
 );
 
 export default SplitViewHost;
--- src/components/gamma/split-view/SplitViewScreen.android.tsx
+++ src/components/gamma/split-view/SplitViewScreen.android.tsx
@@ -4,7 +4,7 @@ const Column = View;
 const Inspector = View;
 
 console.warn(
-  'SplitView is supported only for iOS. Consider using an alternative layout for Android.',
+  '[RNScreens] SplitView is supported only for iOS. Consider using an alternative layout for Android.',
 );
 
 export default { Column, Inspector };
