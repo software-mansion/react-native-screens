2d2a3d17d19e8d6ce54240270a58a6c1626689d4
Tomasz Boroń <tomasz.boron@swmansion.com>
2025-10-06 13:43:42 +0200

    Enable sync updates in SplitView-Stack example

--- apps/src/tests/TestSplitView/index.tsx
+++ apps/src/tests/TestSplitView/index.tsx
@@ -17,6 +17,9 @@ import {
   SplitViewWithNativeStackSheet
 } from './SplitViewWithNativeStack';
 import { SplitViewBaseConfig } from './helpers/types';
+import { featureFlags } from 'react-native-screens';
+
+featureFlags.experiment.screenSynchronousStateUpdates = true
 
 const App = () => {
   const splitViewBaseConfig: SplitViewBaseConfig = {
@@ -25,7 +28,7 @@ const App = () => {
   }
 
   return (
-    <SplitViewBaseApp splitViewBaseConfig={splitViewBaseConfig} />
+    <SplitViewWithNativeStackBase splitViewBaseConfig={splitViewBaseConfig} />
   );
 }
 
