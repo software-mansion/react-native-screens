c2a358a6116af2961a73a4383add019631b20924
kmichalikk <63911178+kmichalikk@users.noreply.github.com>
2025-10-06 16:41:53 +0200

    feat(iOS): Implement ScrollViewProviding protocol (#3238)
    Closes
    https://github.com/software-mansion/react-native-screens-labs/issues/456
    
    ## Description
    
    This PR implements ScrollViewProviding protocol as per internal
    discussion. The protocol enables us to be smarter about finding content
    ScrollView than simply descending into 0th child. A class that
    implements this protocol can decide how to continue the search. To
    complement the feature, ScrollViewFinder is updated with new method,
    `findContentScrollViewWithDelegatingToProvider` that takes advantage of
    the protocol.
    
    ## Changes
    
    - Added `ScrollViewProviding` protocol, with implementations for
    `RNSScreenStack`, `BottomTabsHostComponentView`. The implementations
    continue search directly from their currently selected screens. This has
    the desired side-effect of completing the search even if the screen is
    not yet mounted in host hierarchy.
    - Modified `ScrollViewFinder` to include a method that takes advantage
    of the new protocol, while leaving the simple version
    - Concrete implementations of the new protocol will be added in future
    PRs
    
    ## Test code and steps to reproduce
    
    Use Test3212; For now, nested containers may not behave as expected,
    because the new "smart" method is not called yet; things will change in
    the following PRs

---
+++ ios/helpers/scroll-view/RNSScrollViewFinder.h
@@ -0,0 +1,26 @@
+#import <UIKit/UIKit.h>
+#import "RNSContentScrollViewProviding.h"
+
+@interface RNSScrollViewFinder : NSObject
+
+/**
+ * Searches for content ScrollView by traversing down the hierarchy using first subview, similar to UIKit behavior.
+ * It will fail if:
+ * - UIScrollView is not a first subview of view or one of its descendants in the hierarchy,
+ * - if UIScrollView's parent is not yet attached.
+ *
+ * When `view == nil`, it should also return `nil`.
+ */
++ (nullable UIScrollView *)findScrollViewInFirstDescendantChainFrom:(nullable UIView *)view;
+
+/**
+ * Looks for UIScrollView in a similar way to `findScrollViewInFirstDescendantChainFrom`, until it finds
+ * `RNSContentScrollViewProviding`. Then, it delegates the task to the provider, and returns the results. This can
+ * overcome the problems of subviews' children not being mounted yet, or ScrollView being mounted at index different
+ * than 0.
+ *
+ * When `view == nil`, it should also return `nil`.
+ */
++ (nullable UIScrollView *)findContentScrollViewWithDelegatingToProvider:(nullable UIView *)view;
+
+@end
