fe2cb69066459e27c4776d8573cf3f2d9424e975
Tomasz Boroń <tomasz.boron@swmansion.com>
2025-10-17 13:37:29 +0200

    jfdja

--- ios/bridging/RNSReactBaseView.h
+++ ios/bridging/RNSReactBaseView.h
@@ -1,18 +1,5 @@
-// Hide C++ symbols from C compiler used when building Swift module
-#pragma mark - New architecture definitions
-#if RCT_NEW_ARCH_ENABLED
-
 #import <React/RCTViewComponentView.h>
 
 @interface RNSReactBaseView : RCTViewComponentView
+
 @end
-
-#else
-#pragma mark - Legacy architecture definitions
-
-#import <React/RCTView.h>
-
-@interface RNSReactBaseView : RCTView
-@end
-
-#endif // RCT_NEW_ARCH_ENABLED
