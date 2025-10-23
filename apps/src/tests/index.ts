aa91f1c2bd75ee272b51252753917c6ee00167d1
Tomasz Boroń <tomasz.boron@swmansion.com>
2025-10-23 13:33:09 +0200

    Add test

--- apps/src/tests/index.ts
+++ apps/src/tests/index.ts
@@ -161,6 +161,7 @@ export { default as Test3342 } from './Test3342';
 export { default as Test3346 } from './Test3346';
 export { default as Test3369 } from './Test3369';
 export { default as Test3379 } from './Test3379';
+export { default as TestXXXX } from './TestXXXX';
 export { default as TestScreenAnimation } from './TestScreenAnimation';
 export { default as TestScreenAnimationV5 } from './TestScreenAnimationV5';
 export { default as TestHeader } from './TestHeader';
