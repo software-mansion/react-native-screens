5c99a35e43969899460c956891769927a05a54ed
Tomasz Boroń <tomasz.boron@swmansion.com>
2025-10-23 17:03:12 +0200

    Remove duplicated SAV logic

--- src/components/ScreenContentWrapper.tsx
+++ src/components/ScreenContentWrapper.tsx
@@ -1,28 +1,9 @@
 import React from 'react';
 import { ViewProps } from 'react-native';
 import ScreenContentWrapperNativeComponent from '../fabric/ScreenContentWrapperNativeComponent';
-import { SafeAreaView } from 'react-native-screens/experimental';
 
-type ScreenContentWrapperProps = ViewProps & {
-  disableSafeAreaViewForSheet?: boolean;
-};
-
-function ScreenContentWrapper({
-  disableSafeAreaViewForSheet,
-  children,
-  ...rest
-}: ScreenContentWrapperProps) {
-  return (
-    <ScreenContentWrapperNativeComponent collapsable={false} {...rest}>
-      {disableSafeAreaViewForSheet ? (
-        children
-      ) : (
-        <SafeAreaView edges={{ top: true, bottom: true }}>
-          {children}
-        </SafeAreaView>
-      )}
-    </ScreenContentWrapperNativeComponent>
-  );
+function ScreenContentWrapper(props: ViewProps) {
+  return <ScreenContentWrapperNativeComponent collapsable={false} {...props} />;
 }
 
 export default ScreenContentWrapper;
