128773ddd0b53d28c5b0d4611f440a4e43c9948d
Konrad Michalik <konrad.michalik@swmansion.com>
2025-10-23 12:10:23 +0200

    Sink event and interactions in nested stack

--- ios/helpers/RNSViewInteractionManager.mm
+++ ios/helpers/RNSViewInteractionManager.mm
@@ -1,5 +1,6 @@
 #import "RNSViewInteractionManager.h"
 #import "RNSBottomTabsScreenComponentView.h"
+#import "RNSViewInteractionAware.h"
 
 @implementation RNSViewInteractionManager {
   __weak UIView *lastRootWithInteractionsDisabled;
@@ -13,28 +14,38 @@
 
 - (void)disableInteractionsForSubtreeWith:(UIView *)view
 {
-  UIView *parent = view.superview;
-  while (parent && ![parent isKindOfClass:UIWindow.class] &&
-         ![parent isKindOfClass:RNSBottomTabsScreenComponentView.class]) {
-    parent = parent.superview;
+  UIView *current = view;
+  while (current && ![current isKindOfClass:UIWindow.class] &&
+         ![current respondsToSelector:@selector(disableInteractions)]) {
+    current = current.superview;
   }
 
-  if (parent) {
-    if (lastRootWithInteractionsDisabled && lastRootWithInteractionsDisabled != parent) {
+  if (current) {
+    if (lastRootWithInteractionsDisabled && lastRootWithInteractionsDisabled != current) {
       // When one view already has interactions disabled, and we request a different view,
       // we need to restore the first one
-      lastRootWithInteractionsDisabled.userInteractionEnabled = YES;
+      [self enableInteractionsForLastSubtree];
     }
 
-    parent.userInteractionEnabled = NO;
-    lastRootWithInteractionsDisabled = parent;
+    if ([current respondsToSelector:@selector(disableInteractions)]) {
+      [static_cast<id<RNSViewInteractionAware>>(current) disableInteractions];
+    } else {
+      current.userInteractionEnabled = NO;
+    }
+
+    lastRootWithInteractionsDisabled = current;
   }
 }
 
 - (void)enableInteractionsForLastSubtree
 {
   if (lastRootWithInteractionsDisabled) {
-    lastRootWithInteractionsDisabled.userInteractionEnabled = YES;
+    if ([lastRootWithInteractionsDisabled respondsToSelector:@selector(enableInteractions)]) {
+      [static_cast<id<RNSViewInteractionAware>>(lastRootWithInteractionsDisabled) enableInteractions];
+    } else {
+      lastRootWithInteractionsDisabled.userInteractionEnabled = YES;
+    }
+
     lastRootWithInteractionsDisabled = nil;
   }
 }
