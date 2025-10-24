edeaeb8bd0fe7fe94f99bc4404735b54767459bb
Konrad Michalik <63911178+kmichalikk@users.noreply.github.com>
2025-10-24 12:59:52 +0200

    chore: Add BottomTabsOrientationTest (#3338)
    ## Description
    
    This PR adds a test for BottomTabs orientation change after removing the
    options from BottomTabsTest in
    https://github.com/software-mansion/react-native-screens/pull/3312 for
    being annoying.
    
    ## Changes
    
    Added BottomTabsOrientationTest.
    
    ---------
    
    Co-authored-by: Kacper Kafara <kacperkafara@gmail.com>

--- apps/src/tests/index.ts
+++ apps/src/tests/index.ts
@@ -166,6 +166,7 @@ export { default as TestFormSheet } from './TestFormSheet';
 export { default as TestAndroidTransitions } from './TestAndroidTransitions';
 export { default as TestAnimation } from './TestAnimation';
 export { default as TestBottomTabs } from './TestBottomTabs';
+export { default as TestBottomTabsOrientation } from './TestBottomTabsOrientation';
 export { default as TestScreenStack } from './TestScreenStack';
 export { default as TestSplitView } from './TestSplitView';
 export { default as TestSafeAreaViewIOS } from './TestSafeAreaViewIOS';
