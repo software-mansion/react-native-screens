3fc921bf1d78aa037b0b55f57ee3e6fd691d397f
Tomasz Boroń <tomasz.boron@swmansion.com>
2025-10-27 13:51:43 +0100

    Add example

--- apps/src/tests/index.ts
+++ apps/src/tests/index.ts
@@ -159,6 +159,7 @@ export { default as Test3282 } from './Test3282';
 export { default as Test3342 } from './Test3342';
 export { default as Test3369 } from './Test3369';
 export { default as Test3379 } from './Test3379';
+export { default as TestXXXX } from './TestXXXX';
 export { default as TestScreenAnimation } from './TestScreenAnimation';
 export { default as TestScreenAnimationV5 } from './TestScreenAnimationV5';
 export { default as TestHeader } from './TestHeader';
