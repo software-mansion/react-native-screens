7351ab10f84de05ab139761f447bfc6a0714b372
Krzysztof Ligarski <krzysztof.ligarski@swmansion.com>
2025-10-27 09:58:10 +0100

    mocks on Android

--- android/src/paper/java/com/facebook/react/viewmanagers/RNSBottomTabsScreenManagerDelegate.java
+++ android/src/paper/java/com/facebook/react/viewmanagers/RNSBottomTabsScreenManagerDelegate.java
@@ -98,6 +98,9 @@ public class RNSBottomTabsScreenManagerDelegate<T extends View, U extends BaseVi
       case "topScrollEdgeEffect":
         mViewManager.setTopScrollEdgeEffect(view, (String) value);
         break;
+      case "unstable_overrideUserInterfaceStyle":
+        mViewManager.setUnstable_overrideUserInterfaceStyle(view, (String) value);
+        break;
       default:
         super.setProperty(view, propName, value);
     }
