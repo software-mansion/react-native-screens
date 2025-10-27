49b5a4e60210f72386a9e188260efeb32e0cfbe6
Krzysztof Ligarski <krzysztof.ligarski@swmansion.com>
2025-10-27 12:24:49 +0100

    update docs

--- src/components/bottom-tabs/BottomTabs.types.ts
+++ src/components/bottom-tabs/BottomTabs.types.ts
@@ -38,6 +38,15 @@ export interface BottomTabsProps extends ViewProps {
   ) => void;
   // #endregion Events
 
+  // #region General
+  /**
+   * @summary Hides the tab bar.
+   *
+   * @default false
+   */
+  tabBarHidden?: boolean;
+  // #endregion General
+
   // #region Android-only appearance
   /**
    * @summary Specifies the background color for the entire tab bar.
@@ -204,14 +213,6 @@ export interface BottomTabsProps extends ViewProps {
    * @supported iOS 18 or higher
    */
   tabBarControllerMode?: TabBarControllerMode;
-  /**
-   * @summary Hides the tab bar.
-   *
-   * @default false
-   *
-   * @platform ios
-   */
-  tabBarHidden?: boolean;
   // #endregion iOS-only appearance
 
   // #region Experimental support
