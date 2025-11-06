7f8292fce4b731235ca4ae001c43fc656b774b47
Tomasz Boroń <tomasz.boron@swmansion.com>
2025-11-06 15:36:14 +0100

    Add multi-scene support

--- FabricExample/App.tsx
+++ FabricExample/App.tsx
@@ -1,8 +1,44 @@
-import App from '../apps';
-import { featureFlags } from '../src';
+import { useState } from "react";
+import { StyleSheet, View, Button, Text } from "react-native";
+import { FullWindowOverlay } from "react-native-screens";
 
-featureFlags.experiment.synchronousScreenUpdatesEnabled = false
-featureFlags.experiment.synchronousHeaderConfigUpdatesEnabled = false
-featureFlags.experiment.synchronousHeaderSubviewUpdatesEnabled = false
+export default function App() {
+  const [modalShown, setModalShown] = useState(false);
+  return (
+    <View style={styles.container}>
+      <Button title="Show" onPress={() => setModalShown(true)} />
+      <Button
+      title="Reset"
+      onPress={()=>setModalShown(false)}
+      />
+      {modalShown && <MyModal onHide={() => setModalShown(false)} />}
+    </View>
+  );
+}
 
-export default App;
+function MyModal({ onHide }) {
+  return (
+    <FullWindowOverlay>
+      <View
+        style={{
+          flex: 1,
+          justifyContent: "center",
+          alignItems: "center",
+          backgroundColor: "red",
+        }}
+      >
+        <Button title="Hide" onPress={onHide} />
+      </View>
+    </FullWindowOverlay>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    backgroundColor: "#ecf0f1",
+    padding: 8,
+    alignItems: 'center'
+  },
+});
---
+++ FabricExample/ios/ReactNativeDelegate.swift
@@ -0,0 +1,17 @@
+import React
+import ReactAppDependencyProvider
+import React_RCTAppDelegate
+
+class ReactNativeDelegate: RCTDefaultReactNativeFactoryDelegate {
+  override func sourceURL(for bridge: RCTBridge) -> URL? {
+    self.bundleURL()
+  }
+
+  override func bundleURL() -> URL? {
+    #if DEBUG
+      RCTBundleURLProvider.sharedSettings().jsBundleURL(forBundleRoot: "index")
+    #else
+      Bundle.main.url(forResource: "main", withExtension: "jsbundle")
+    #endif
+  }
+}
