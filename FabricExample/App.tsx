40489508cef06eacd0d74563acb743e9de235126
Tomasz Boroń <tomasz.boron@swmansion.com>
2025-11-06 17:19:04 +0100

    Restore app

--- FabricExample/App.tsx
+++ FabricExample/App.tsx
@@ -1,44 +1,8 @@
-import { useState } from "react";
-import { StyleSheet, View, Button, Text } from "react-native";
-import { FullWindowOverlay } from "react-native-screens";
+import App from '../apps';
+import { featureFlags } from '../src';
 
-export default function App() {
-  const [modalShown, setModalShown] = useState(false);
-  return (
-    <View style={styles.container}>
-      <Button title="Show" onPress={() => setModalShown(true)} />
-      <Button
-      title="Reset"
-      onPress={()=>setModalShown(false)}
-      />
-      {modalShown && <MyModal onHide={() => setModalShown(false)} />}
-    </View>
-  );
-}
+featureFlags.experiment.synchronousScreenUpdatesEnabled = false
+featureFlags.experiment.synchronousHeaderConfigUpdatesEnabled = false
+featureFlags.experiment.synchronousHeaderSubviewUpdatesEnabled = false
 
-function MyModal({ onHide }) {
-  return (
-    <FullWindowOverlay>
-      <View
-        style={{
-          flex: 1,
-          justifyContent: "center",
-          alignItems: "center",
-          backgroundColor: "red",
-        }}
-      >
-        <Button title="Hide" onPress={onHide} />
-      </View>
-    </FullWindowOverlay>
-  );
-}
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: "center",
-    backgroundColor: "#ecf0f1",
-    padding: 8,
-    alignItems: 'center'
-  },
-});
+export default App;
