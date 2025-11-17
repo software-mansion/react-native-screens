644c1177d7f04b27d2c0463212e05860962b2c3d
Krzysztof Wojnar <krzysztof.wojnar@swmansion.com>
2025-11-17 20:10:14 +0100

    Fix e2e runner
    Co-authored-by: Krzysztof Ligarski <63918941+kligarski@users.noreply.github.com>

---
+++ FabricExample/e2e/component-objects/back-button.ts
@@ -0,0 +1,34 @@
+import { device, element, by } from 'detox';
+import { getIOSVersion } from '../../../scripts/e2e/ios-devices.js';
+import { isVersion } from '../helpers/versionComparator.js';
+
+const IOS_BAR_BUTTON_TYPE = '_UIButtonBarButton';
+const backButtonElement = element(by.id('BackButton'));
+
+export async function tapBarBackButton() {
+  const platform = device.getPlatform();
+  if (platform === 'ios') {
+    return (await getIOSBackButton()).tap();
+  } else if (platform === 'android') {
+    return backButtonElement.tap();
+  } else throw new Error(`Platform "${platform}" not supported`);
+}
+async function getIOSBackButton() {
+  const iosVersion = getIOSVersion().replace('iOS', '').trim();
+  if (isVersion(iosVersion).equalOrHigherThan('26.0')) {
+    const elementsByAttributes =
+      (await backButtonElement.getAttributes()) as unknown as {
+        elements: { className: string }[];
+      };
+    const elements = elementsByAttributes.elements;
+    if (Array.isArray(elements)) {
+      const uiBarButtonIndex = elements.findIndex(
+        elem => elem.className === IOS_BAR_BUTTON_TYPE,
+      );
+      if (uiBarButtonIndex !== -1) {
+        return backButtonElement.atIndex(uiBarButtonIndex);
+      }
+    }
+  }
+  return backButtonElement;
+}
