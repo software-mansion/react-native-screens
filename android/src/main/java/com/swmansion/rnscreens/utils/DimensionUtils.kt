f961fded23a71fb8130ab12bf01a504d20d2e7b1
Krzysztof Ligarski <krzysztof.ligarski@swmansion.com>
2025-11-17 09:57:24 +0100

    android format

--- android/src/main/java/com/swmansion/rnscreens/utils/DimensionUtils.kt
+++ android/src/main/java/com/swmansion/rnscreens/utils/DimensionUtils.kt
@@ -4,12 +4,15 @@ import android.content.Context
 import android.os.Build
 import android.util.TypedValue
 
-fun pxToDp(context: Context, px: Float): Float {
+fun pxToDp(
+    context: Context,
+    px: Float,
+): Float {
     if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.UPSIDE_DOWN_CAKE) {
         return TypedValue.deriveDimension(
             TypedValue.COMPLEX_UNIT_DIP,
             px,
-            context.resources.displayMetrics
+            context.resources.displayMetrics,
         )
     } else {
         val density = context.resources.displayMetrics.density
