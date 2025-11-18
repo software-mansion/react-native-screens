db0a7ab2935c39a9472e575ee6944dad4841cda7
Tomasz Boroń <tomasz.boron@swmansion.com>
2025-11-18 12:30:28 +0100

    Add comment

--- android/src/main/java/com/swmansion/rnscreens/bottomsheet/SheetDelegate.kt
+++ android/src/main/java/com/swmansion/rnscreens/bottomsheet/SheetDelegate.kt
@@ -334,6 +334,12 @@ class SheetDelegate(
 
         val newBottomInset = if (!isImeVisible) prevInsets.bottom else 0
 
+        // Note: We do not manipulate the top inset manually. Therefore, if SafeAreaView has top insets enabled,
+        // we must retain the top inset even if the formSheet does not currently overflow into the status bar.
+        // This is important because in some specific edge cases — for example, when the keyboard slides in —
+        // the formSheet might overlap the status bar. If we ignored the top inset and it suddenly became necessary,
+        // it would result in a noticeable visual content jump. To ensure consistency and avoid layout shifts,
+        // we always include the top inset upfront, which can be disabled from the application perspective.
         return WindowInsetsCompat
             .Builder(insets)
             .setInsets(
