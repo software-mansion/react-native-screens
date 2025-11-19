b169732703a81ceefc1cfb3c0872687239ecf09f
Krzysztof Ligarski <krzysztof.ligarski@swmansion.com>
2025-11-19 13:34:15 +0100

    formatting

--- android/src/main/java/com/swmansion/rnscreens/ScreenStackHeaderHeightUpdateProxy.kt
+++ android/src/main/java/com/swmansion/rnscreens/ScreenStackHeaderHeightUpdateProxy.kt
@@ -3,7 +3,10 @@ package com.swmansion.rnscreens
 class ScreenStackHeaderHeightUpdateProxy {
     var previousHeaderHeightInPx: Int? = null
 
-    fun updateHeaderHeightIfNeeded(config: ScreenStackHeaderConfig, screen: Screen?) {
+    fun updateHeaderHeightIfNeeded(
+        config: ScreenStackHeaderConfig,
+        screen: Screen?,
+    ) {
         val currentHeaderHeightInPx = if (config.isHeaderHidden) 0 else config.toolbar.height
 
         if (currentHeaderHeightInPx != previousHeaderHeightInPx) {
