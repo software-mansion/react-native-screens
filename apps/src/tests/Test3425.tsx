9e6b26efb1ad659da08bef0a144e68a2cc07e8ea
Krzysztof Ligarski <krzysztof.ligarski@swmansion.com>
2025-11-26 13:22:17 +0100

    add test screen

---
+++ apps/src/tests/Test3425.tsx
@@ -0,0 +1,33 @@
+import React, { useState } from 'react';
+import { enableFreeze } from 'react-native-screens';
+import { Button, View } from 'react-native';
+import Colors from '../shared/styling/Colors';
+import { TestBottomTabs } from '.';
+
+enableFreeze(true);
+
+export default function App() {
+  const [index, setIndex] = useState(0);
+  return (
+    <View
+      style={{
+        flex: 1,
+        paddingHorizontal: 20,
+        paddingVertical: 100,
+        backgroundColor: Colors.BlueLight40,
+      }}>
+      {index === 0 ? (
+        <View
+          style={{
+            flex: 1,
+            justifyContent: 'center',
+            alignItems: 'center',
+          }}>
+          <Button title="Go to Bottom Tabs" onPress={() => setIndex(1)} />
+        </View>
+      ) : (
+        <TestBottomTabs />
+      )}
+    </View>
+  );
+}
