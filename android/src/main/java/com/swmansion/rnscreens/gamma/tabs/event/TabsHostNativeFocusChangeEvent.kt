d54e90984ee214077c3b76dec8158819608fff97
Krzysztof Ligarski <krzysztof.ligarski@swmansion.com>
2025-12-01 10:22:18 +0100

    match implementation on Android + fix bug with double onItemSelectedListener call

--- android/src/main/java/com/swmansion/rnscreens/gamma/tabs/event/TabsHostNativeFocusChangeEvent.kt
+++ android/src/main/java/com/swmansion/rnscreens/gamma/tabs/event/TabsHostNativeFocusChangeEvent.kt
@@ -9,18 +9,24 @@ class TabsHostNativeFocusChangeEvent(
     surfaceId: Int,
     viewId: Int,
     val tabKey: String,
+    val tabNumber: Int,
+    val repeatedSelectionHandledBySpecialEffect: Boolean
 ) : Event<TabScreenDidAppearEvent>(surfaceId, viewId),
     NamingAwareEventType {
     override fun getEventName() = EVENT_NAME
 
     override fun getEventRegistrationName() = EVENT_REGISTRATION_NAME
 
-    // All events for a given view can be coalesced.
-    override fun getCoalescingKey(): Short = 0
+    override fun getCoalescingKey(): Short =
+        (tabNumber * 10 + if (repeatedSelectionHandledBySpecialEffect) 1 else 0).toShort()
 
     override fun getEventData(): WritableMap? =
         Arguments.createMap().apply {
             putString(EVENT_KEY_TAB_KEY, tabKey)
+            putBoolean(
+                EVENT_KEY_REPEATED_SELECTION_HANDLED_BY_SPECIAL_EFFECT,
+                repeatedSelectionHandledBySpecialEffect
+            )
         }
 
     companion object : NamingAwareEventType {
@@ -28,6 +34,8 @@ class TabsHostNativeFocusChangeEvent(
         const val EVENT_REGISTRATION_NAME = "onNativeFocusChange"
 
         private const val EVENT_KEY_TAB_KEY = "tabKey"
+        private const val EVENT_KEY_REPEATED_SELECTION_HANDLED_BY_SPECIAL_EFFECT =
+            "repeatedSelectionHandledBySpecialEffect"
 
         override fun getEventName() = EVENT_NAME
 
