5ffe1605299c8e0dffc463b62cc9acb228af8df0
Tomasz Boroń <tomasz.boron@swmansion.com>
2025-12-05 16:21:27 +0100

    Formatting

--- src/fabric/safe-area/SafeAreaViewNativeComponent.ts
+++ src/fabric/safe-area/SafeAreaViewNativeComponent.ts
@@ -1,7 +1,7 @@
 // Implementation adapted from `react-native-safe-area-context`:
 // https://github.com/AppAndFlow/react-native-safe-area-context/blob/v5.6.1/src/specs/NativeSafeAreaView.ts
 
-import { codegenNativeComponent , ViewProps } from 'react-native';
+import { codegenNativeComponent, ViewProps } from 'react-native';
 import { WithDefault } from 'react-native/Libraries/Types/CodegenTypesNamespace';
 
 type InsetType = 'all' | 'system' | 'interface';
