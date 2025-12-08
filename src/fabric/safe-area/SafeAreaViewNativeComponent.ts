5c25bfed50aa1170a88d13d6385338d902146ea9
Tomasz Boroń <tomasz.boron@swmansion.com>
2025-12-08 09:56:05 +0100

    CodegenTypesNamespace deep import removed

--- src/fabric/safe-area/SafeAreaViewNativeComponent.ts
+++ src/fabric/safe-area/SafeAreaViewNativeComponent.ts
@@ -1,8 +1,8 @@
 // Implementation adapted from `react-native-safe-area-context`:
 // https://github.com/AppAndFlow/react-native-safe-area-context/blob/v5.6.1/src/specs/NativeSafeAreaView.ts
 
-import { codegenNativeComponent, ViewProps } from 'react-native';
-import { WithDefault } from 'react-native/Libraries/Types/CodegenTypesNamespace';
+import { codegenNativeComponent } from 'react-native';
+import type { CodegenTypes as CT, ViewProps } from 'react-native';
 
 type InsetType = 'all' | 'system' | 'interface';
 
@@ -14,7 +14,7 @@ export interface NativeProps extends ViewProps {
     left: boolean;
   }>;
   // Android-only
-  insetType?: WithDefault<InsetType, 'all'>;
+  insetType?: CT.WithDefault<InsetType, 'all'>;
 }
 
 export default codegenNativeComponent<NativeProps>('RNSSafeAreaView', {
