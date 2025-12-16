cc3bf1c228083e78c0999a7c68d25d2305efcfb5
Tomasz Boroń <tomasz.boron@swmansion.com>
2025-12-16 15:26:53 +0100

    Cleanup after testing

--- src/components/ScreenStackItem.tsx
+++ src/components/ScreenStackItem.tsx
@@ -282,13 +282,6 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
   },
-  absolute: {
-    position: 'absolute',
-    top: 0,
-    start: 0,
-    end: 0,
-    bottom: 0,
-  },
   absoluteWithNoBottom: {
     position: 'absolute',
     top: 0,
