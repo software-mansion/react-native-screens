5db2608a2956825074a62167142052066e3bfd0b
Tomasz Boroń <tomasz.boron@swmansion.com>
2025-12-18 16:52:47 +0100

    Restore default app

--- apps/App.tsx
+++ apps/App.tsx
@@ -1,11 +1,11 @@
 import React from 'react';
 import { enableFreeze } from 'react-native-screens';
 import Example from './Example';
-import * as Test from './src/tests';
+// import * as Test from './src/tests';
 
 enableFreeze(true);
 
 export default function App() {
-  // return <Example />;
-  return <Test.Test2560 />;
+  return <Example />;
+  // return <Test.TestBottomTabs />;
 }
