76728c2628fb4d05105bafc663086e3161089b09
Tomasz Boroń <tomasz.boron@swmansion.com>
2025-12-18 16:40:51 +0100

    Add description

--- src/types.tsx
+++ src/types.tsx
@@ -504,7 +504,16 @@ export interface ScreenProps extends ViewProps {
    */
   sheetShouldOverflowTopInset?: boolean;
   /**
-   * TODO: @t0maboro - add description
+   * Whether the default native animation should be used when the sheet's with
+   * `fitToContents` content size changes.
+   *
+   * When set to `true`, the sheet uses internal logic to synchronize size updates and
+   * translation animations during entry, exit, or content updates. This ensures a smooth
+   * transition for standard, static content mounting/unmounting.
+   *
+   * When set to `false`, the internal animation and translation logic is ignored. This
+   * allows the sheet to adjust its size dynamically based on the current dimensions of
+   * the content provided by the developer, allowing implementing custom resizing animations.
    *
    * Defaults to `true`.
    *
