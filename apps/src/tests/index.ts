c3cf1dd98193154da8d91bccb39388fb184b3e60
Tomasz Boroń <tomasz.boron@swmansion.com>
2026-01-22 10:25:29 +0100

    Add example

--- apps/src/tests/index.ts
+++ apps/src/tests/index.ts
@@ -173,6 +173,7 @@ export { default as Test3450 } from './Test3450';
 export { default as Test3492 } from './Test3492';
 export { default as Test3521 } from './Test3521';
 export { default as Test3522 } from './Test3522';
+export { default as TestXXXX } from './TestXXXX';
 export { default as TestScreenAnimation } from './TestScreenAnimation';
 // The following test was meant to demo the "go back" gesture using Reanimated
 // but the associated PR in react-navigation is currently put on hold
