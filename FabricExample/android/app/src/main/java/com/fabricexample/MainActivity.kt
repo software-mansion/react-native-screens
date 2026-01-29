b81100a33492d332b4ac30915bd76ba645fb593b
Tomasz Boroń <tomasz.boron@swmansion.com>
2026-01-29 15:48:22 +0100

    Fix crashing setNavigationBarContrastEnforced below API level 29

--- FabricExample/android/app/src/main/java/com/fabricexample/MainActivity.kt
+++ FabricExample/android/app/src/main/java/com/fabricexample/MainActivity.kt
@@ -30,6 +30,9 @@ class MainActivity : ReactActivity() {
   override fun onAttachedToWindow() {
     super.onAttachedToWindow()
     // opt out of having 80% opacity over 3-button navigation
-    getWindow().setNavigationBarContrastEnforced(false)
+    // supported for API level 29 or higher
+    if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.Q) {
+      getWindow().setNavigationBarContrastEnforced(false)
+    }
   }
 }
