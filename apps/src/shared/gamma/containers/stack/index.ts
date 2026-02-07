3aa2199a908b8618f3038158c3dbfaa2315f1d97
Kacper Kafara <kacperkafara@gmail.com>
2026-02-07 11:25:39 +0100

    Expose `StackContainerWithDynamicRouteConfigs` component
    It's a wrapper for StackContainer. It adds `StackRouteConfigContext`,
    which can be used to modify route options stored in route configurations.
    
    This is different from what https://github.com/software-mansion/react-native-screens/pull/3625
    introduced, because this does not impact existing route instances. Only blueprints.
    
    I created separate component to keep the `StackContainer` logic thin.

--- apps/src/shared/gamma/containers/stack/index.ts
+++ apps/src/shared/gamma/containers/stack/index.ts
@@ -5,7 +5,10 @@ export type {
 } from './StackContainer.types';
 
 export type { StackNavigationContextPayload } from './contexts/StackNavigationContext';
+export type { StackRouteConfigContextPayload } from './contexts/StackRouteConfigContext';
 
 export { useStackNavigationContext } from './hooks/useStackNavigationContext';
+export { useStackRouteConfigContext } from './hooks/useStackRouteConfigContext';
 
 export { StackContainer } from './StackContainer';
+export { StackContainerWithDynamicRouteConfigs } from './StackContainerWithDynamicRouteConfigs';
