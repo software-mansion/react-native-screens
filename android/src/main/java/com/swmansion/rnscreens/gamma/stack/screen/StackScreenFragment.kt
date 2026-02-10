f45d99d44596ea1c9c79efed9154680269a6ed42
Kacper Kafara <kacperkafara@gmail.com>
2026-02-10 13:13:34 +0100

    Add basic shared axis material transition

--- android/src/main/java/com/swmansion/rnscreens/gamma/stack/screen/StackScreenFragment.kt
+++ android/src/main/java/com/swmansion/rnscreens/gamma/stack/screen/StackScreenFragment.kt
@@ -1,11 +1,13 @@
 package com.swmansion.rnscreens.gamma.stack.screen
 
+import android.animation.Animator
 import android.os.Bundle
 import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import com.google.android.material.transition.MaterialSharedAxis
 
 internal class StackScreenFragment(
     internal val stackScreen: StackScreen,
@@ -28,6 +30,10 @@ internal class StackScreenFragment(
         requireActivity().onBackPressedDispatcher.addCallback(
             preventNativeDismissBackPressedCallback,
         )
+        enterTransition = MaterialSharedAxis(MaterialSharedAxis.X, true)
+        returnTransition = MaterialSharedAxis(MaterialSharedAxis.X, false)
+        reenterTransition = MaterialSharedAxis(MaterialSharedAxis.X, true)
+        exitTransition = MaterialSharedAxis(MaterialSharedAxis.X, false)
     }
 
     override fun onCreateView(
@@ -54,6 +60,35 @@ internal class StackScreenFragment(
         Log.i("StackScreenFragment", "onDestroy")
         stackScreen.onDismiss()
         preventNativeDismissBackPressedCallback.remove()
+    }
+
+    override fun onCreateAnimator(
+        transit: Int,
+        enter: Boolean,
+        nextAnim: Int,
+    ): Animator? {
+        Log.w("RNScreens", "StackScreenFragment onCreateAnimator ${stackScreen.screenKey}")
+        return super.onCreateAnimator(transit, enter, nextAnim)
+//        return if (enter) {
+//            val animator = ValueAnimator.ofFloat(-stackScreen.width.toFloat(), 0f).apply {
+//                duration = 800
+//                interpolator = FastOutSlowInInterpolator()
+//                addUpdateListener { animator ->
+//                    val animValue = animator.animatedValue as Float
+//                    stackScreen.translationX = animValue
+//                }
+//            }
+//            animator
+//        } else {
+//            val animator = ValueAnimator.ofFloat(0f, stackScreen.width.toFloat()).apply {
+//                duration = 800
+//                addUpdateListener { animator ->
+//                    val animValue = animator.animatedValue as Float
+//                    stackScreen.translationX = animValue
+//                }
+//            }
+//            animator
+//        }
     }
 
     /**
