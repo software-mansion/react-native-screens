e6587a4d6301eae50d3f4643202eb5532d9ed451
Satyajit Sahoo <satyajit.happy@gmail.com>
2026-02-16 23:14:26 +0100

    feat: add new impl for header and toolbar items

---
+++ src/components/bar/index.tsx
@@ -0,0 +1,16 @@
+export { Bar } from './Bar';
+export type {
+  BarBadge,
+  BarBadgeStyle,
+  BarIcon,
+  BarItemPlacement,
+  BarItemProps,
+  BarItemVariant,
+  BarMenuActionProps,
+  BarMenuProps,
+  BarMenuSubmenuProps,
+  BarProps,
+  BarTitleStyle,
+} from './Bar';
+export type { BarSpacerProps } from './BarSpacerNativeComponent';
+export { default as BarView } from './BarViewNativeComponent';
