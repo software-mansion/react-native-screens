9a678f87a9956592b81abb995069bd20c3ac0984
Jakub Tkacz <jtkacz@expo.io>
2026-02-20 11:59:52 +0100

    add test

--- apps/src/tests/single-feature-tests/tabs/index.ts
+++ apps/src/tests/single-feature-tests/tabs/index.ts
@@ -1,6 +1,7 @@
 import type { ScenarioGroup } from '../../shared/helpers';
 
 import BottomAccessoryScenario from './bottom-accessory-layout';
+import OverrideScrollViewContentInsetScenario from './override-scroll-view-content-inset';
 import TabBarHiddenScenario from './tab-bar-hidden';
 import TabsScreenOrientationScenario from './tabs-screen-orientation';
 
@@ -9,6 +10,7 @@ const TabsScenarioGroup: ScenarioGroup = {
   details: 'Single feature tests for tabs',
   scenarios: [
     BottomAccessoryScenario,
+    OverrideScrollViewContentInsetScenario,
     TabBarHiddenScenario,
     TabsScreenOrientationScenario,
   ],
