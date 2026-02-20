2df680c0c666a7a2cdac018c9e476ddfce167a7a
Kacper Kafara <kacperkafara@gmail.com>
2026-02-20 17:03:30 +0100

    Update Test3212
    Shouldn't be affected. Both of these tests **turned off** the
    `freezeContents` explicitly

--- apps/src/tests/issue-tests/Test3212/BottomTabsScenario.tsx
+++ apps/src/tests/issue-tests/Test3212/BottomTabsScenario.tsx
@@ -1,22 +1,31 @@
-import React, { useState } from "react";
-import { SCROLL_EDGE_EFFECT_DEFAULTS, ScrollEdgeEffects, ScrollEdgeEffectsConfigContext } from "./context";
-import { NavigationContainer, NavigationIndependentTree } from "@react-navigation/native";
-import { BottomTabsContainer } from "../../../shared/gamma/containers/bottom-tabs/BottomTabsContainer"
-import { Config } from "./Config";
-import { ScrollViewTemplate } from "./ScrollViewTemplate";
-import { ScrollView } from "react-native";
+import React, { useState } from 'react';
+import {
+  SCROLL_EDGE_EFFECT_DEFAULTS,
+  ScrollEdgeEffects,
+  ScrollEdgeEffectsConfigContext,
+} from './context';
+import {
+  NavigationContainer,
+  NavigationIndependentTree,
+} from '@react-navigation/native';
+import { BottomTabsContainer } from '../../../shared/gamma/containers/bottom-tabs/BottomTabsContainer';
+import { Config } from './Config';
+import { ScrollViewTemplate } from './ScrollViewTemplate';
+import { ScrollView } from 'react-native';
 
 function ConfigComponent() {
   // Add ScrollView for automatic insets which are missing in BottomTabsScreen
   return (
     <ScrollView>
-      <Config title='Stack / scrollEdgeEffects:'/>
+      <Config title="Stack / scrollEdgeEffects:" />
     </ScrollView>
   );
 }
 
 export function BottomTabsScenario() {
-  const [config, setConfig] = useState<ScrollEdgeEffects>({ ...SCROLL_EDGE_EFFECT_DEFAULTS });
+  const [config, setConfig] = useState<ScrollEdgeEffects>({
+    ...SCROLL_EDGE_EFFECT_DEFAULTS,
+  });
 
   return (
     <NavigationIndependentTree>
@@ -24,9 +33,19 @@ export function BottomTabsScenario() {
         <NavigationContainer>
           <BottomTabsContainer
             tabConfigs={[
-              { component: ConfigComponent, tabScreenProps: { tabKey: 'config', title: 'Config' } },
+              {
+                component: ConfigComponent,
+                tabScreenProps: { tabKey: 'config', title: 'Config' },
+              },
               // Using `freezeContents` for testing purposes, to make the ScrollView searching algorithm's success verifiable
-              { component: ScrollViewTemplate, tabScreenProps: { tabKey: 'stack', title: 'Scroll', freezeContents: false, scrollEdgeEffects: config } },
+              {
+                component: ScrollViewTemplate,
+                tabScreenProps: {
+                  tabKey: 'stack',
+                  title: 'Scroll',
+                  scrollEdgeEffects: config,
+                },
+              },
             ]}
           />
         </NavigationContainer>
