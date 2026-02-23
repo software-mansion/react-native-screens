f66d53f61fbd48456b1b1b26151487ca7b627e14
Konrad Michalik <konrad.michalik@swmansion.com>
2026-02-23 09:31:35 +0100

    Simplify single test import in App.tsx

---
+++ apps/src/tests/importer.ts
@@ -0,0 +1,5 @@
+import { COMPONENT_SCENARIOS as CIT } from './component-integration-tests';
+import { COMPONENT_SCENARIOS as SFT } from './single-feature-tests';
+import * as IT from './issue-tests';
+
+export { CIT, SFT, IT };
--- apps/src/tests/single-feature-tests/stack-v5/index.ts
+++ apps/src/tests/single-feature-tests/stack-v5/index.ts
@@ -3,14 +3,16 @@ import PreventNativeDismissSingleStack from './prevent-native-dismiss-single-sta
 import PreventNativeDismissNestedStack from './prevent-native-dismiss-nested-stack';
 import AnimationAndroid from './test-animation-android';
 
-const StackScenarioGroup: ScenarioGroup = {
+const scenarios = {
+  PreventNativeDismissSingleStack,
+  PreventNativeDismissNestedStack,
+  AnimationAndroid,
+};
+
+const StackScenarioGroup: ScenarioGroup<keyof typeof scenarios> = {
   name: 'Stack v5',
   details: 'Single feature tests for new stack implementation',
-  scenarios: [
-    PreventNativeDismissSingleStack,
-    PreventNativeDismissNestedStack,
-    AnimationAndroid,
-  ],
+  scenarios,
 };
 
 export default StackScenarioGroup;
