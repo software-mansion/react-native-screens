software-mansion/react-native-screens

commit 9b387ce05ba63a5083ac0531c29441a2e1db5a10

fix(Android, FormSheet): Add logic for dismissing keyboard when the BottomSheet is presented (#3617)
## Description

Add logic to handle keyboard visibility and focus restoration on sheet
enter/exit

> [!IMPORTANT]  
> It doesn't work well in some edge cases, especially when we have a
Screen with a TextInput and a FormSheet with another TextInput. When
using an HW keyboard in such a case, if we submit the Input inside the
FormSheet (losing focus) and then want to reopen it, the focus search
might select the TextInput on the Screen, as we do not block focus on
the Screen behind the FormSheet.
> Creating ticket for that:
https://github.com/software-mansion/react-native-screens-labs/issues/929

Implementation details:
- I'm checking the IME inset on the DecorView, there's 1 soft keyboard
for the whole application - it's easier to detect whether it was open on
the previous Screen
- I'm restoring the focus, only if the keyboard was visible on the
previous screen - that might be bad for applications that are designed
for keyboard/D-Pad navigation
- I don't want the Screen to be the subject of focus - there is no good
reason for that imo, therefore, I've added FOCUS_AFTER_DESCENDANTS to
focus on the 1st focusable descendant inside the FormSheet
- I'm using onCreate/Destroy callbacks, instead of onStart/Stop
callbacks, because of the timing for autoFocus inside the FormSheet

Closes:
https://github.com/software-mansion/react-native-screens-labs/issues/928

## Changes

- added lifecycle callbacks for sheet creation/destruction that will
show/hide keyboard from Screen and save the latest focus

## Before & after - visual documentation

| Before | After |
| --- | --- |
| <video
src="https://github.com/user-attachments/assets/bd07bc9d-ee8e-4c92-bea8-75117446bb96"
/> | <video
src="https://github.com/user-attachments/assets/5e8cd8e6-6da9-47d0-b138-48332087cc59"
/> |

## Test plan

Test3617

## Checklist

- [x] Included code example that can be used to test this change.

android/src/main/java/com/swmansion/rnscreens/bottomsheet/SheetDelegate.kt
@@ -7,6 +7,7 @@ import android.animation.ValueAnimator
 import android.content.Context
 import android.os.Build
 import android.view.View
+import android.view.ViewGroup
 import android.view.WindowManager
 import android.view.inputmethod.InputMethodManager
 import androidx.coordinatorlayout.widget.CoordinatorLayout
@@ -27,6 +28,7 @@ import com.swmansion.rnscreens.ScreenStackFragment
 import com.swmansion.rnscreens.events.ScreenAnimationDelegate
 import com.swmansion.rnscreens.events.ScreenEventEmitter
 import com.swmansion.rnscreens.transition.ExternalBoundaryValuesEvaluator
+import com.swmansion.rnscreens.utils.isSoftKeyboardVisibleOrNull
 
 class SheetDelegate(
     val screen: Screen,
@@ -63,6 +65,11 @@ class SheetDelegate(
         checkNotNull(screen.reactContext.currentActivity) { "[RNScreens] Attempt to access activity on detached context" }
             .window.decorView
 
+    private var viewToRestoreFocus: View? = null
+
+    private val inputMethodManager: InputMethodManager?
+        get() = screen.reactContext.getSystemService(Context.INPUT_METHOD_SERVICE) as? InputMethodManager
+
     init {
         assert(screen.fragment is ScreenStackFragment) { "[RNScreens] Sheets are supported only in native stack" }
         screen.fragment!!.lifecycle.addObserver(this)
@@ -77,11 +84,17 @@ class SheetDelegate(
         event: Lifecycle.Event,
     ) {
         when (event) {
+            Lifecycle.Event.ON_CREATE -> handleHostFragmentOnCreate()
             Lifecycle.Event.ON_START -> handleHostFragmentOnStart()
             Lifecycle.Event.ON_RESUME -> handleHostFragmentOnResume()
             Lifecycle.Event.ON_PAUSE -> handleHostFragmentOnPause()
+            Lifecycle.Event.ON_DESTROY -> handleHostFragmentOnDestroy()
             else -> Unit
         }
+    }
+
+    private fun handleHostFragmentOnCreate() {
+        preserveBackgroundFocus()
     }
 
     private fun handleHostFragmentOnStart() {
@@ -94,6 +107,10 @@ class SheetDelegate(
 
     private fun handleHostFragmentOnPause() {
         InsetsObserverProxy.removeOnApplyWindowInsetsListener(this)
+    }
+
+    private fun handleHostFragmentOnDestroy() {
+        restoreBackgroundFocus()
     }
 
     private fun onSheetStateChanged(newState: Int) {
@@ -112,6 +129,32 @@ class SheetDelegate(
         if (shouldDismissSheetInState(newState)) {
             stackFragment.dismissSelf()
         }
+    }
+
+    private fun preserveBackgroundFocus() {
+        val activity = screen.reactContext.currentActivity ?: return
+
+        activity.currentFocus?.let { focusedView ->
+            activity.window?.decorView?.let { decorView ->
+                if (isSoftKeyboardVisibleOrNull(decorView) == true) {
+                    viewToRestoreFocus = focusedView
+                }
+            }
+
+            // Note: There's no good reason that Screen should be direct target for focus, we're rather
+            // prefer its children to gain it.
+            screen.descendantFocusability = ViewGroup.FOCUS_AFTER_DESCENDANTS
+            screen.requestFocus()
+            inputMethodManager?.hideSoftInputFromWindow(focusedView.windowToken, 0)
+        }
+    }
+
+    private fun restoreBackgroundFocus() {
+        viewToRestoreFocus?.let { view ->
+            view.requestFocus()
+            inputMethodManager?.showSoftInput(view, 0)
+        }
+        viewToRestoreFocus = null
     }
 
     internal fun updateBottomSheetMetrics(behavior: BottomSheetBehavior<Screen>) {
@@ -555,9 +598,7 @@ class SheetDelegate(
                     // I want to be polite here and request focus before dismissing the keyboard,
                     // however even if it fails I want to try to hide the keyboard. This sometimes works...
                     bottomSheet.requestFocus()
-                    val imm =
-                        screen.reactContext.getSystemService(InputMethodManager::class.java)
-                    imm.hideSoftInputFromWindow(bottomSheet.windowToken, 0)
+                    inputMethodManager?.hideSoftInputFromWindow(bottomSheet.windowToken, 0)
                 }
             }
         }

android/src/main/java/com/swmansion/rnscreens/utils/DecorViewInsetsUtils.kt
@@ -21,3 +21,12 @@ private fun getTopInset(insetsCompat: WindowInsetsCompat): Int =
         .getInsets(
             WindowInsetsCompat.Type.systemBars() or WindowInsetsCompat.Type.displayCutout(),
         ).top
+
+internal fun isSoftKeyboardVisibleOrNull(decorView: View): Boolean? {
+    val insetsCompat = ViewCompat.getRootWindowInsets(decorView) ?: return null
+
+    return insetsCompat
+        .isVisible(
+            WindowInsetsCompat.Type.ime(),
+        )
+}

apps/src/tests/issue-tests/Test3617.tsx
@@ -1,0 +1,160 @@
+import React, { useState } from 'react';
+import { Button, View, Text, StyleSheet, TextInput } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import {
+  createNativeStackNavigator,
+  NativeStackScreenProps,
+} from '@react-navigation/native-stack';
+import Colors from '../../shared/styling/Colors';
+
+type StackParamList = {
+  Home: undefined;
+  FormSheet: undefined;
+  FormSheetWithAutoFocus: undefined;
+  FormSheetWoFocusable: undefined;
+};
+
+const Stack = createNativeStackNavigator<StackParamList>();
+
+const HomeScreen = ({ navigation }: NativeStackScreenProps<StackParamList>) => {
+  const [isFocused, setIsFocused] = useState(false);
+
+  return (
+    <View style={styles.screen}>
+      <Text style={styles.title}>Home Screen</Text>
+      <Text style={styles.text}>Is focused? {isFocused ? 'YES' : 'NO'}</Text>
+      <TextInput
+        style={[
+          styles.input,
+          isFocused
+            ? { borderColor: Colors.GreenDark100 }
+            : { borderColor: Colors.RedDark100 },
+        ]}
+        placeholder="Enter text (no sheet)"
+        onFocus={() => setIsFocused(true)}
+        onBlur={() => setIsFocused(false)}
+      />
+      <Button
+        title="Open Form Sheet"
+        onPress={() => navigation.navigate('FormSheet')}
+      />
+      <Button
+        title="Open Form Sheet (w/ autoFocus)"
+        onPress={() => navigation.navigate('FormSheetWithAutoFocus')}
+      />
+      <Button
+        title="Open Form Sheet (w/o focusable)"
+        onPress={() => navigation.navigate('FormSheetWoFocusable')}
+      />
+    </View>
+  );
+};
+
+const FormSheetScreen = () => {
+  return (
+    <View style={styles.formSheetContainer}>
+      <View style={styles.rectangle} />
+      <TextInput style={styles.input} placeholder="Enter text in FormSheet" />
+    </View>
+  );
+};
+
+const FormSheetScreenWithAutoFocus = () => {
+  return (
+    <View style={styles.formSheetContainer}>
+      <View style={styles.rectangle} />
+      <TextInput
+        style={styles.input}
+        autoFocus
+        placeholder="Enter text in FormSheet"
+      />
+    </View>
+  );
+};
+
+function FormSheetWoFocusable() {
+  return (
+    <View style={styles.formSheetContainer} focusable={false}>
+      <View style={styles.rectangle} focusable={false} />
+    </View>
+  );
+}
+
+export default function App() {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen name="Home" component={HomeScreen} />
+        <Stack.Screen
+          name="FormSheet"
+          component={FormSheetScreen}
+          options={{
+            presentation: 'formSheet',
+            sheetAllowedDetents: 'fitToContents',
+            headerShown: false,
+            contentStyle: {
+              backgroundColor: Colors.YellowLight40,
+            },
+          }}
+        />
+        <Stack.Screen
+          name="FormSheetWithAutoFocus"
+          component={FormSheetScreenWithAutoFocus}
+          options={{
+            presentation: 'formSheet',
+            sheetAllowedDetents: 'fitToContents',
+            headerShown: false,
+            contentStyle: {
+              backgroundColor: Colors.YellowLight40,
+            },
+          }}
+        />
+        <Stack.Screen
+          name="FormSheetWoFocusable"
+          component={FormSheetWoFocusable}
+          options={{
+            presentation: 'formSheet',
+            sheetAllowedDetents: 'fitToContents',
+            headerShown: false,
+            contentStyle: {
+              backgroundColor: Colors.YellowLight40,
+            },
+          }}
+        />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+}
+
+const styles = StyleSheet.create({
+  screen: {
+    flex: 1,
+    alignItems: 'center',
+    padding: 20,
+    gap: 16,
+  },
+  title: {
+    fontSize: 24,
+  },
+  formSheetContainer: {
+    alignItems: 'center',
+    justifyContent: 'center',
+    padding: 20,
+    gap: 20,
+  },
+  text: {
+    fontSize: 16,
+  },
+  rectangle: {
+    width: '100%',
+    backgroundColor: Colors.NavyLight80,
+    height: 200,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  input: {
+    width: '80%',
+    height: 50,
+    borderWidth: 2,
+  },
+});

apps/src/tests/issue-tests/index.ts
@@ -180,6 +180,7 @@ export { default as Test3564 } from './Test3564';
 export { default as Test3566 } from './Test3566';
 export { default as Test3576 } from './Test3576';
 export { default as Test3596 } from './Test3596';
+export { default as Test3617 } from './Test3617';
 export { default as TestScreenAnimation } from './TestScreenAnimation';
 // The following test was meant to demo the "go back" gesture using Reanimated
 // but the associated PR in react-navigation is currently put on hold

commit 010fa85de77a6d6a2c0043781718ccc0eb9b0b15

fix(Android, Stack v4): fix keyboard navigation focus for form sheet (#3245)
## Description

Fixes keyboard navigation focus handling for form sheets on Android.

Previously, it was possible to focus elements in the view displayed
below the form sheet.

Closes
https://github.com/software-mansion/react-native-screens/issues/3188.
Closes
https://github.com/software-mansion/react-native-screens-labs/issues/929.

| before | after |
| --- | --- |
| <video
src="https://github.com/user-attachments/assets/481435f7-5304-4b74-9d2c-65693a33ffed"
/> | <video
src="https://github.com/user-attachments/assets/40b0d027-f982-494c-9b0a-ed9ae6f52670"
/> |

### Context

In `ScreenStack`, there is already logic to handle accessibility focus
via `importantForAccessibility` prop.

I stumbled upon an issue in `react-native` repo [where one of the users
explained](https://github.com/facebook/react-native/issues/30850#issuecomment-1184756886)
that Android handles screen readers focus and keyboard navigation focus
differently. In order to block keyboard navigation focus, you need to
set [`focusable` and
`descendantFocusability`](https://github.com/facebook/react-native/issues/30850#issuecomment-1184958396)
properties - when implemented in `ScreenStack`, this started working
correctly (after clicking `arrow-down`, focus did not leave the
formsheet).

As form sheet isn't a modal form of presentation in current Android
implementation, we decided to block the focusability and set
`importantForAccessibility` to `no` when dimming view is present.

> [!IMPORTANT]
> Change above might be considered a breaking change -
`importantForAccessibility` is not set to `no` if no dimming view is
present.

One problem I noticed is that due to usage of `requestFocus()`
introduced in
https://github.com/software-mansion/react-native-screens/pull/3617,
input is focused instead of e.g. the first button on the screen (as seen
in video at the top of PR description; note that no IME appears). Prior

android/src/main/java/com/swmansion/rnscreens/Screen.kt
@@ -3,6 +3,7 @@ package com.swmansion.rnscreens
 import android.annotation.SuppressLint
 import android.content.pm.ActivityInfo
 import android.graphics.Paint
+import android.os.Build
 import android.os.Parcelable
 import android.util.SparseArray
 import android.view.MotionEvent
@@ -12,6 +13,7 @@ import android.view.WindowInsets
 import android.view.WindowManager
 import android.webkit.WebView
 import android.widget.ImageView
+import androidx.annotation.RequiresApi
 import androidx.coordinatorlayout.widget.CoordinatorLayout
 import androidx.core.view.children
 import androidx.fragment.app.Fragment
@@ -543,6 +545,35 @@ class Screen(
         this.headerConfig?.toolbar?.importantForAccessibility = mode
     }
 
+    // Accepts one of 3 focusable flags and one of 3 descendantFocusability flags.
+    // https://developer.android.com/reference/android/view/View#setFocusable(int)
+    // https://developer.android.com/reference/android/view/ViewGroup#setDescendantFocusability(int)
+    @RequiresApi(Build.VERSION_CODES.O)
+    internal fun changeFocusability(
+        focusable: Int,
+        descendantFocusability: Int,
+    ) {
+        this.focusable = focusable
+        this.headerConfig?.toolbar?.focusable = focusable
+
+        this.descendantFocusability = descendantFocusability
+        this.headerConfig?.toolbar?.descendantFocusability = descendantFocusability
+    }
+
+    // Accepts boolean for focusability and one of 3 descendantFocusability flags.
+    // This method is provided for compatibility reasons only (SDK_API <= 25).
+    // https://developer.android.com/reference/android/view/ViewGroup#setDescendantFocusability(int)
+    internal fun changeFocusabilityCompat(
+        focusable: Boolean,
+        descendantFocusability: Int,
+    ) {
+        this.setFocusable(focusable)
+        this.headerConfig?.toolbar?.setFocusable(focusable)
+
+        this.descendantFocusability = descendantFocusability
+        this.headerConfig?.toolbar?.descendantFocusability = descendantFocusability
+    }
+
     var statusBarStyle: String? = null
         set(statusBarStyle) {
             if (statusBarStyle != null) {
@@ -677,6 +708,13 @@ class Screen(
         if (BuildConfig.IS_NEW_ARCHITECTURE_ENABLED && isStable) {
             onSheetYTranslationChanged()
         }
+
+        // a11y props such as `importantForAccessibility`, `focusable` and `descendantFocusability`
+        // are handled for form sheet depending on dimming view (if dimming view is present, the
+        // background screen is not focusable). When detent changes, dimming view might also change.
+        // That's why we're requesting update of a11y from ScreenStack (which handles those props).
+        check(container is ScreenStack?) { "[RNScreens] Expected native stack screen." }
+        (container as ScreenStack?)?.updateA11yForVisibleScreens()
     }
 
     internal fun onSheetYTranslationChanged() {

android/src/main/java/com/swmansion/rnscreens/ScreenStack.kt
@@ -8,6 +8,8 @@ import com.facebook.react.bridge.ReactContext
 import com.facebook.react.uimanager.UIManagerHelper
 import com.swmansion.rnscreens.Screen.StackAnimation
 import com.swmansion.rnscreens.bottomsheet.requiresEnterTransitionPostponing
+import com.swmansion.rnscreens.bottomsheet.sheetShouldUseDimmingView
+import com.swmansion.rnscreens.bottomsheet.usesFormSheetPresentation
 import com.swmansion.rnscreens.events.StackFinishTransitioningEvent
 import com.swmansion.rnscreens.stack.views.ChildrenDrawingOrderStrategy
 import com.swmansion.rnscreens.stack.views.ReverseFromIndex
@@ -27,6 +29,7 @@ class ScreenStack(
     private var drawingOps: MutableList<DrawingOp> = ArrayList()
     private var topScreenWrapper: ScreenStackFragmentWrapper? = null
     private var removalTransitionStarted = false
+    private var currentVisibleBottom: ScreenFragmentWrapper? = null
 
     private var childrenDrawingOrderStrategy: ChildrenDrawingOrderStrategy? = null
     private var disappearingTransitioningChildren: MutableList<View> = ArrayList()
@@ -161,6 +164,7 @@ class ScreenStack(
                 .dropWhile { it.isTranslucent() }
                 .firstOrNull()
                 ?.takeUnless { it === newTop }
+        currentVisibleBottom = visibleBottom
 
         var shouldUseOpenAnimation = true
         var stackAnimation: StackAnimation? = null
@@ -294,34 +298,68 @@ class ScreenStack(
                     .filter { it.screen.activityState == Screen.ActivityState.INACTIVE }
                     .toList()
 
-            turnOffA11yUnderTransparentScreen(visibleBottom)
+            updateA11yForVisibleScreens()
+
             transaction.commitNowAllowingStateLoss()
         }
     }
 
     // only top visible screen should be accessible
-    private fun turnOffA11yUnderTransparentScreen(visibleBottom: ScreenFragmentWrapper?) {
-        if (screenWrappers.size > 1 && visibleBottom != null) {
+    internal fun updateA11yForVisibleScreens() {
+        if (screenWrappers.size > 1 && currentVisibleBottom != null) {
             topScreenWrapper?.let {
-                if (it.isTranslucent()) {
-                    val screenFragmentsBeneathTop =
-                        screenWrappers.slice(0 until screenWrappers.size - 1).asReversed()
-                    // go from the top of the stack excluding the top screen
-                    for (fragmentWrapper in screenFragmentsBeneathTop) {
-                        fragmentWrapper.screen.changeAccessibilityMode(
-                            IMPORTANT_FOR_ACCESSIBILITY_NO_HIDE_DESCENDANTS,
-                        )
+                val shouldDisableFocusability = shouldDisableFocusabilityBeneathTopScreen()
+                val screenFragmentsBeneathTop =
+                    screenWrappers.slice(0 until screenWrappers.size - 1).asReversed()
+                // go from the top of the stack excluding the top screen
+                for (fragmentWrapper in screenFragmentsBeneathTop) {
+                    val accessibilityMode =
+                        if (shouldDisableFocusability) IMPORTANT_FOR_ACCESSIBILITY_NO_HIDE_DESCENDANTS else IMPORTANT_FOR_ACCESSIBILITY_AUTO
+                    fragmentWrapper.screen.changeAccessibilityMode(accessibilityMode)
 
-                        // don't change a11y below non-transparent screens
-                        if (fragmentWrapper == visibleBottom) {
-                            break
-                        }
+                    // Keyboard navigation focus is separate from screen reader focus, that's
+                    // why we need to use focusable and descendantFocusability.
+                    changeScreenFocusability(fragmentWrapper.screen, !shouldDisableFocusability)
+
+                    // don't change a11y below non-transparent screens
+                    if (fragmentWrapper == currentVisibleBottom) {
+                        break
                     }
                 }
             }
         }
 
         topScreen?.changeAccessibilityMode(IMPORTANT_FOR_ACCESSIBILITY_AUTO)
+        topScreen?.let { changeScreenFocusability(it, true) }
+    }
+
+    private fun shouldDisableFocusabilityBeneathTopScreen(): Boolean {
+        topScreenWrapper?.let {
+            return if (it.screen.usesFormSheetPresentation()) {
+                it.screen.sheetShouldUseDimmingView()
+            } else {
+                it.isTranslucent()
+            }
+        }
+        return false
+    }
+
+    private fun changeScreenFocusability(
+        screen: Screen,
+        focusable: Boolean,
+    ) {
+        val descendantFocusability =
+            if (focusable) FOCUS_AFTER_DESCENDANTS else FOCUS_BLOCK_DESCENDANTS
+
+        // On API >= 26, we use FOCUSABLE_AUTO.
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            screen.changeFocusability(
+                if (focusable) FOCUSABLE_AUTO else NOT_FOCUSABLE,
+                descendantFocusability,
+            )
+        } else {
+            screen.changeFocusabilityCompat(focusable, descendantFocusability)
+        }
     }
 
     override fun notifyContainerUpdate() {

android/src/main/java/com/swmansion/rnscreens/bottomsheet/DimmingView.kt
@@ -31,6 +31,7 @@ internal class DimmingView(
 
     init {
         pointerEventsProxy.pointerEventsImpl = DimmingViewPointerEventsImpl(this)
+        setFocusable(false)
     }
 
     internal val blockGestures

android/src/main/java/com/swmansion/rnscreens/bottomsheet/SheetDelegate.kt
@@ -7,7 +7,6 @@ import android.animation.ValueAnimator
 import android.content.Context
 import android.os.Build
 import android.view.View
-import android.view.ViewGroup
 import android.view.WindowManager
 import android.view.inputmethod.InputMethodManager
 import androidx.coordinatorlayout.widget.CoordinatorLayout
@@ -141,9 +140,6 @@ class SheetDelegate(
                 }
             }
 
-            // Note: There's no good reason that Screen should be direct target for focus, we're rather
-            // prefer its children to gain it.
-            screen.descendantFocusability = ViewGroup.FOCUS_AFTER_DESCENDANTS
             screen.requestFocus()
             inputMethodManager?.hideSoftInputFromWindow(focusedView.windowToken, 0)
         }

android/src/main/java/com/swmansion/rnscreens/bottomsheet/SheetUtils.kt
@@ -8,6 +8,7 @@ import com.google.android.material.bottomsheet.BottomSheetBehavior.STATE_HALF_EX
 import com.google.android.material.bottomsheet.BottomSheetBehavior.STATE_HIDDEN
 import com.swmansion.rnscreens.BuildConfig
 import com.swmansion.rnscreens.Screen
+import com.swmansion.rnscreens.ext.asScreenStackFragment
 
 object SheetUtils {
     /**
@@ -159,6 +160,13 @@ fun Screen.requiresEnterTransitionPostponing(): Boolean {
     }
     // Assumes that formSheet uses content wrapper
     return !this.isLaidOutOrHasCachedLayout() || this.contentWrapper?.isLaidOutOrHasCachedLayout() != true
+}
+
+fun Screen.sheetShouldUseDimmingView(): Boolean {
+    val currentDetentIndex =
+        fragment?.asScreenStackFragment()?.sheetDelegate?.lastStableDetentIndex
+            ?: sheetInitialDetentIndex
+    return currentDetentIndex > sheetLargestUndimmedDetentIndex
 }
 
 /**