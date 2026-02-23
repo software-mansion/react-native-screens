724b37627e21bf21fa7a28f092dc8a572859d06b
Kacper Kafara <kacperkafara@gmail.com>
2026-02-23 15:43:59 +0100

    Format Android

--- android/src/main/java/com/swmansion/rnscreens/gamma/scrollviewmarker/ScrollViewSeeking.kt
+++ android/src/main/java/com/swmansion/rnscreens/gamma/scrollviewmarker/ScrollViewSeeking.kt
@@ -6,5 +6,8 @@ interface ScrollViewSeeking {
     // scrollView is a ViewGroup, because there is no universal ScrollView component on Android.
     // It might a be ScrollView, NestedScrollView (different inheritance hierarchies) or any other
     // ViewGroup that implements appropriate scrolling interfaces.
-    fun registerScrollView(maker: ScrollViewMarker, scrollView: ViewGroup)
+    fun registerScrollView(
+        maker: ScrollViewMarker,
+        scrollView: ViewGroup,
+    )
 }
