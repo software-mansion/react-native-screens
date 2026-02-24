086ea4a744c167892a06320c6eb2d73b871700e6
Kacper Kafara <kacperkafara@gmail.com>
2026-02-24 11:56:17 +0100

    Mark ScrollViewSeeking interface as `internal`
    There is no reason to publish it atm. It if turns out to be needed
    we'll make it public.

--- android/src/main/java/com/swmansion/rnscreens/gamma/scrollviewmarker/ScrollViewSeeking.kt
+++ android/src/main/java/com/swmansion/rnscreens/gamma/scrollviewmarker/ScrollViewSeeking.kt
@@ -2,12 +2,12 @@ package com.swmansion.rnscreens.gamma.scrollviewmarker
 
 import android.view.ViewGroup
 
-interface ScrollViewSeeking {
+internal interface ScrollViewSeeking {
     // scrollView is a ViewGroup, because there is no universal ScrollView component on Android.
     // It might a be ScrollView, NestedScrollView (different inheritance hierarchies) or any other
     // ViewGroup that implements appropriate scrolling interfaces.
     fun registerScrollView(
-        maker: ScrollViewMarker,
+        marker: ScrollViewMarker,
         scrollView: ViewGroup,
     )
 }
