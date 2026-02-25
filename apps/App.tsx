bfdfdc80a84fa47e571976a420e8f5406b0a4308
Szymon Gaczoł <szymon.gaczol@swmansion.com>
2026-02-25 13:33:14 +0100

    test update in app.tsx

--- apps/App.tsx
+++ apps/App.tsx
@@ -1,11 +1,12 @@
 import React from 'react';
 import { enableFreeze } from 'react-native-screens';
-import Example from './Example';
-// import * as Test from './src/tests/issue-tests';
+//import Example from './Example';
+import * as Test from './src/tests/issue-tests';
 
 enableFreeze(true);
 
 export default function App() {
-  return <Example />;
+  //return <Example />;
   // return <Test.TestBottomTabs />;
+  return <Test.TestSplit />;
 }
