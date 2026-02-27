5b9803d67a6329c90f216c2b4fdf89eb049311ce
Tomasz Boroń <tomasz.boron@swmansion.com>
2026-02-27 17:36:00 +0100

    Add comments

--- src/components/tabs/TabsHost.d.ts
+++ src/components/tabs/TabsHost.d.ts
@@ -1,3 +1,11 @@
+/**
+ * TS module resolution does not support this RN platform extension pattern out of the box.
+ * Without a base .tsx file, TS will throw a "Cannot find module" error.
+ *
+ * This file satisfies the TS compiler by providing the correct type signatures,
+ * whereas Metro will handle the proper runtime file resolution.
+ */
+
 import React from 'react';
 import { TabsHostProps } from './TabsHost.types';
 
