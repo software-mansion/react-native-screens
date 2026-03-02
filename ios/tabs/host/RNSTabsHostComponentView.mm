9f0266b3605e9887e176886969ab24c8bf02e8aa
Krzysztof Ligarski <krzysztof.ligarski@swmansion.com>
2026-03-02 19:02:11 +0100

    add support for layout direction changes in runtime on iOS

--- ios/tabs/host/RNSTabsHostComponentView.mm
+++ ios/tabs/host/RNSTabsHostComponentView.mm
@@ -598,6 +598,12 @@ RNS_IGNORE_SUPER_CALL_END
 #endif // RNS_IPHONE_OS_VERSION_AVAILABLE(17_0)
   {
     _controller.needsLayoutDirectionUpdate = YES;
+
+    // If controller is already attached to parent VC, we should update layout direction
+    // immediately as controller updates layoutDirection only on `didMoveToParentViewController`.
+    if (_controller.parentViewController != nil) {
+      [_controller updateLayoutDirectionIfNeeded];
+    }
   }
 }
 
