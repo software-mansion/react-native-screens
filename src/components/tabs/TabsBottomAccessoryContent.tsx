49ecbebe4b24959a61c670fac7a6f6c63a687a21
Krzysztof Ligarski <krzysztof.ligarski@swmansion.com>
2026-03-02 15:24:48 +0100

    rename TabsAccessory -> TabsBottomAccessory

--- src/components/tabs/TabsBottomAccessoryContent.tsx
+++ src/components/tabs/TabsBottomAccessoryContent.tsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import { StyleSheet } from 'react-native';
-import { TabsAccessoryContentProps } from './TabsAccessoryContent.types';
+import { TabsBottomAccessoryContentProps } from './TabsBottomAccessoryContent.types';
 import TabsBottomAccessoryContentNativeComponent from '../../fabric/tabs/TabsBottomAccessoryContentNativeComponent';
 
 /**
  * EXPERIMENTAL API, MIGHT CHANGE W/O ANY NOTICE
  */
-export default function TabsAccessoryContent(props: TabsAccessoryContentProps) {
+export default function TabsBottomAccessoryContent(
+  props: TabsBottomAccessoryContentProps,
+) {
   return (
     <TabsBottomAccessoryContentNativeComponent
       {...props}
