f02b9df547a09136423f465f1810f15d0dd8ba86
Konrad Michalik <konrad.michalik@swmansion.com>
2026-03-02 12:24:24 +0100

    Remove Example

--- Example/App.tsx
+++
@@ -1,8 +0,0 @@
-import App from '../apps';
-
-import { featureFlags } from 'react-native-screens';
-
-featureFlags.experiment.iosPreventReattachmentOfDismissedScreens = true;
-featureFlags.experiment.ios26AllowInteractionsDuringTransition = true;
-
-export default App;
