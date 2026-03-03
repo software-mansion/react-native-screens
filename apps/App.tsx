c5ee409fffec0cc33e49b1a11c566fd3d8933387
Kacper Kafara <kacperkafara@gmail.com>
2026-03-03 15:38:23 +0100

    Simplify test usage in Appjs

--- apps/App.tsx
+++ apps/App.tsx
@@ -1,20 +1,17 @@
 import React from 'react';
 import { enableFreeze } from 'react-native-screens';
-// import { NavigationContainer } from '@react-navigation/native';
 
-// import { COMPONENT_SCENARIOS as Integration } from './src/tests/component-integration-tests';
-// import { COMPONENT_SCENARIOS as Feature } from './src/tests/single-feature-tests';
-// import * as Issue from './src/tests/issue-tests';
 import Example from './Example';
+// import { NavigationContainer } from '@react-navigation/native';
+// import { Tests } from './src/tests';
 
 enableFreeze(true);
 
 export default function App() {
   return <Example />;
-  // return <Issue.TestBottomTabs />;
   // return (
   //   <NavigationContainer>
-  //     <Integration.Orientation.scenarios.StackInTabs.AppComponent />
+  //     <Tests.Issue.TestBottomTabs />
   //   </NavigationContainer>
   // );
 }
