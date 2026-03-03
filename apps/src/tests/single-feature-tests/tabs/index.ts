b3f4d7f2347058a81374f892f3d266d4d8ee32cf
Krzysztof Ligarski <krzysztof.ligarski@swmansion.com>
2026-03-03 15:06:14 +0100

    change test to be single feature test

--- apps/src/tests/single-feature-tests/tabs/index.ts
+++ apps/src/tests/single-feature-tests/tabs/index.ts
@@ -4,6 +4,7 @@ import BottomAccessoryScenario from './bottom-accessory-layout';
 import OverrideScrollViewContentInsetScenario from './override-scroll-view-content-inset';
 import TabBarHiddenScenario from './tab-bar-hidden';
 import TabsScreenOrientationScenario from './tabs-screen-orientation';
+import TestTabsLayoutDirection from './test-tabs-layout-direction';
 
 const TabsScenarioGroup: ScenarioGroup = {
   name: 'Tabs',
@@ -13,6 +14,7 @@ const TabsScenarioGroup: ScenarioGroup = {
     OverrideScrollViewContentInsetScenario,
     TabBarHiddenScenario,
     TabsScreenOrientationScenario,
+    TestTabsLayoutDirection,
   ],
 };
 
