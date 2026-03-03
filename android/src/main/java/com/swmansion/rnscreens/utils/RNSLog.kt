de2e6141354cd86b18e3d4d7d8b2bf5d3f208748
Szymon Gaczoł <szymon.gaczol@swmansion.com>
2026-03-03 15:07:00 +0100

    RNSLog android

--- android/src/main/java/com/swmansion/rnscreens/utils/RNSLog.kt
+++ android/src/main/java/com/swmansion/rnscreens/utils/RNSLog.kt
@@ -22,4 +22,99 @@ object RNSLog {
             Log.d(tag, message.format(*args))
         }
     }
+
+    fun e(
+        tag: String,
+        message: String,
+    ) {
+        if (BuildConfig.RNS_DEBUG_LOGGING) {
+            Log.e(tag, message)
+        }
+    }
+
+    fun e(
+        tag: String,
+        message: String,
+        vararg args: Any,
+    ) {
+        if (BuildConfig.RNS_DEBUG_LOGGING) {
+            Log.e(tag, message.format(*args))
+        }
+    }
+
+    fun i(
+        tag: String,
+        message: String,
+    ) {
+        if (BuildConfig.RNS_DEBUG_LOGGING) {
+            Log.i(tag, message)
+        }
+    }
+
+    fun i(
+        tag: String,
+        message: String,
+        vararg args: Any,
+    ) {
+        if (BuildConfig.RNS_DEBUG_LOGGING) {
+            Log.i(tag, message.format(*args))
+        }
+    }
+
+    fun v(
+        tag: String,
+        message: String,
+    ) {
+        if (BuildConfig.RNS_DEBUG_LOGGING) {
+            Log.v(tag, message)
+        }
+    }
+
+    fun v(
+        tag: String,
+        message: String,
+        vararg args: Any,
+    ) {
+        if (BuildConfig.RNS_DEBUG_LOGGING) {
+            Log.v(tag, message.format(*args))
+        }
+    }
+
+    fun w(
+        tag: String,
+        message: String,
+    ) {
+        if (BuildConfig.RNS_DEBUG_LOGGING) {
+            Log.w(tag, message)
+        }
+    }
+
+    fun w(
+        tag: String,
+        message: String,
+        vararg args: Any,
+    ) {
+        if (BuildConfig.RNS_DEBUG_LOGGING) {
+            Log.w(tag, message.format(*args))
+        }
+    }
+
+    fun wtf(
+        tag: String,
+        message: String,
+    ) {
+        if (BuildConfig.RNS_DEBUG_LOGGING) {
+            Log.wtf(tag, message)
+        }
+    }
+
+    fun wtf(
+        tag: String,
+        message: String,
+        vararg args: Any,
+    ) {
+        if (BuildConfig.RNS_DEBUG_LOGGING) {
+            Log.wtf(tag, message.format(*args))
+        }
+    }
 }
