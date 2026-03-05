1506219890d996a4e9234d5ea05e171705a79212
Szymon Gaczoł <167703022+sgaczol@users.noreply.github.com>
2026-03-05 10:45:26 +0100

    feat(Android): configure `RNSLog` utility (#3718)
    ## Description
    
    Introduce automatic `RNSLog` activation in debug mode and deactivation
    in release mode. This behavior can be manually overridden using the
    `rnsDebugLogsEnabled` flag.
    
    Example usage:
    `yarn run android --mode release --extra-params
    "-PrnsDebugLogsEnabled=true"` activates `RNSLog` in release mode.
    `yarn run android --mode debug --extra-params
    "-PrnsDebugLogsEnabled=false"` deactivates `RNSLog` in debug mode.
    
    Extend `RNSLog` functionality.
    
    ## Changes
    
    - update `gradle.properties` and `build.gradle` 
    - add more functionality to `RNSLog` analogous to native Android `Log`
    
    ## Before & after - visual documentation
    
    Not applicable.
    
    ## Test plan
    
    Run app in both modes and check if it works properly.
    
    ## Checklist
    
    - [ ] Included code example that can be used to test this change.
    - [ ] Updated / created local changelog entries in relevant test files.
    - [ ] For visual changes, included screenshots / GIFs / recordings
    documenting the change.
    - [ ] For API changes, updated relevant public types.
    - [x] Ensured that CI passes

--- android/src/main/java/com/swmansion/rnscreens/utils/RNSLog.kt
+++ android/src/main/java/com/swmansion/rnscreens/utils/RNSLog.kt
@@ -4,22 +4,90 @@ import android.util.Log
 import com.swmansion.rnscreens.BuildConfig
 
 object RNSLog {
-    fun d(
+    private inline fun logIfEnabled(
         tag: String,
-        message: String,
+        msg: String,
+        logger: (String, String) -> Int,
     ) {
         if (BuildConfig.RNS_DEBUG_LOGGING) {
-            Log.d(tag, message)
+            logger(tag, msg)
+        }
+    }
+
+    private inline fun logIfEnabled(
+        tag: String,
+        msg: String,
+        tr: Throwable,
+        logger: (String, String, Throwable) -> Int,
+    ) {
+        if (BuildConfig.RNS_DEBUG_LOGGING) {
+            logger(tag, msg, tr)
         }
     }
 
     fun d(
         tag: String,
-        message: String,
-        vararg args: Any,
-    ) {
-        if (BuildConfig.RNS_DEBUG_LOGGING) {
-            Log.d(tag, message.format(*args))
-        }
-    }
+        msg: String,
+    ) = logIfEnabled(tag, msg, Log::d)
+
+    fun d(
+        tag: String,
+        msg: String,
+        tr: Throwable,
+    ) = logIfEnabled(tag, msg, tr, Log::d)
+
+    fun e(
+        tag: String,
+        msg: String,
+    ) = logIfEnabled(tag, msg, Log::e)
+
+    fun e(
+        tag: String,
+        msg: String,
+        tr: Throwable,
+    ) = logIfEnabled(tag, msg, tr, Log::e)
+
+    fun i(
+        tag: String,
+        msg: String,
+    ) = logIfEnabled(tag, msg, Log::i)
+
+    fun i(
+        tag: String,
+        msg: String,
+        tr: Throwable,
+    ) = logIfEnabled(tag, msg, tr, Log::i)
+
+    fun v(
+        tag: String,
+        msg: String,
+    ) = logIfEnabled(tag, msg, Log::v)
+
+    fun v(
+        tag: String,
+        msg: String,
+        tr: Throwable,
+    ) = logIfEnabled(tag, msg, tr, Log::v)
+
+    fun w(
+        tag: String,
+        msg: String,
+    ) = logIfEnabled(tag, msg, Log::w)
+
+    fun w(
+        tag: String,
+        msg: String,
+        tr: Throwable,
+    ) = logIfEnabled(tag, msg, tr, Log::w)
+
+    fun wtf(
+        tag: String,
+        msg: String,
+    ) = logIfEnabled(tag, msg, Log::wtf)
+
+    fun wtf(
+        tag: String,
+        msg: String,
+        tr: Throwable,
+    ) = logIfEnabled(tag, msg, tr, Log::wtf)
 }
