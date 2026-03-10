54473d6b085fe328e9a7252e6da44b08ad2f92dc
Tomasz Boroń <tomasz.boron@swmansion.com>
2026-03-10 16:10:59 +0100

    Consistency

--- src/components/tabs/bottom-accessory/index.ts
+++ src/components/tabs/bottom-accessory/index.ts
@@ -1,5 +1,5 @@
 export { default as TabsBottomAccessory } from './TabsBottomAccessory';
 export { default as TabsBottomAccessoryContent } from './TabsBottomAccessoryContent';
 
-export * from './TabsBottomAccessory.types';
-export * from './TabsBottomAccessoryContent.types';
+export type * from './TabsBottomAccessory.types';
+export type * from './TabsBottomAccessoryContent.types';
