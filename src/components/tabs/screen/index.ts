28cf6b0d862547d18dfd8d1855ff277727a0ebad
Tomasz Boroń <tomasz.boron@swmansion.com>
2026-03-10 19:03:49 +0100

    Consistency

--- src/components/tabs/screen/index.ts
+++ src/components/tabs/screen/index.ts
@@ -1,6 +1,6 @@
 export { default as TabsScreen } from './TabsScreen';
 
-export * from './TabsScreen.types';
+export type * from './TabsScreen.types';
 
 export type * from './TabsScreen.android.types';
 export type * from './TabsScreen.ios.types';
