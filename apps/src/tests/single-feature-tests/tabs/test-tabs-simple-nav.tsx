498d9ad21b4232918aa93c6a0907b2d18d5ecac3
Kacper Kafara <kacperkafara@gmail.com>
2026-03-15 10:01:37 +0100

    Create simple test with basic navigation scenario

--- apps/src/tests/single-feature-tests/tabs/test-tabs-simple-nav.tsx
+++ apps/src/tests/single-feature-tests/tabs/test-tabs-simple-nav.tsx
@@ -1,6 +1,13 @@
 import React from 'react';
 import type { Scenario } from '../../shared/helpers';
-import { Text, View } from 'react-native';
+import { Text } from 'react-native';
+import {
+  TabsContainer,
+  type TabRouteConfig,
+  DEFAULT_TAB_ROUTE_OPTIONS,
+  useTabsNavigationContext,
+} from '../../../shared/gamma/containers/tabs';
+import { CenteredLayoutView } from '../../../shared/CenteredLayoutView';
 
 const SCENARIO: Scenario = {
   name: 'Test simple navigation',
@@ -12,10 +19,33 @@ const SCENARIO: Scenario = {
 
 export default SCENARIO;
 
-export function App() {
+function ContentView() {
+  const { routeKey } = useTabsNavigationContext();
   return (
-    <View>
-      <Text>Hello world</Text>
-    </View>
+    <CenteredLayoutView>
+      <Text style={{ fontWeight: 'bold', textAlign: 'center' }}>{routeKey}</Text>
+    </CenteredLayoutView>
   );
+}
+
+const ROUTE_CONFIGS: TabRouteConfig[] = [
+  {
+    name: 'First',
+    Component: ContentView,
+    options: { ...DEFAULT_TAB_ROUTE_OPTIONS, title: 'First' },
+  },
+  {
+    name: 'Second',
+    Component: ContentView,
+    options: { ...DEFAULT_TAB_ROUTE_OPTIONS, title: 'Second' },
+  },
+  {
+    name: 'Third',
+    Component: ContentView,
+    options: { ...DEFAULT_TAB_ROUTE_OPTIONS, title: 'Third' },
+  },
+];
+
+export function App() {
+  return <TabsContainer routeConfigs={ROUTE_CONFIGS} />;
 }
