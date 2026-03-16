a5661146c8fd7cea37c27c00069f65ff4fcfbdc9
Krzysztof Ligarski <krzysztof.ligarski@swmansion.com>
2026-03-16 16:15:29 +0100

    update e2e test to handle iOS 26+

--- FabricExample/e2e/issue-tests/Test758.e2e.ts
+++ FabricExample/e2e/issue-tests/Test758.e2e.ts
@@ -1,5 +1,18 @@
 import { device, expect, element, by } from 'detox';
 import { describeIfiOS, selectIssueTestScreen } from '../e2e-utils';
+import isVersionEqualOrHigherThan from '../helpers/isVersionEqualOrHigherThan';
+
+const { getIOSVersionNumber } = require('../../../scripts/e2e/ios-devices.js');
+
+// On iOS 26+ cancel button does not contain any text.
+function getSearchBarCloseButton() {
+  const iosVersion = getIOSVersionNumber();
+  if (isVersionEqualOrHigherThan(iosVersion, '26.0')) {
+    return element(by.label('Close').and(by.traits(['button'])));
+  } else {
+    return element(by.text('Cancel text'));
+  }
+}
 
 // PR related to iOS search bar
 describeIfiOS('Test758', () => {
@@ -12,31 +25,31 @@ describeIfiOS('Test758', () => {
   });
 
   it('search bar should not be initially visible', async () => {
-    await expect(element(by.type('UISearchBarTextField'))).not.toBeVisible();
+    await expect(element(by.traits(['searchField']))).not.toBeVisible();
   });
 
   it('search bar should appear on scroll', async () => {
     await element(by.text('Stuff')).swipe('down');
-    await expect(element(by.type('UISearchBarTextField'))).toBeVisible();
+    await expect(element(by.traits(['searchField']))).toBeVisible();
   });
 
   it('search bar should have correct placeholder text', async () => {
     await expect(element(by.text('Placeholder text'))).toBeVisible();
   });
 
-  it('search bar should show correct cancel button text when focused', async () => {
-    await element(by.type('UISearchBarTextField')).tap();
-    await expect(element(by.text('Cancel text'))).toBeVisible();
+  it('search bar should show correct cancel button when focused', async () => {
+    await element(by.traits(['searchField'])).tap();
+    await expect(getSearchBarCloseButton()).toBeVisible();
   });
 
   it('cancel button should disappear after being clicked', async () => {
-    await element(by.text('Cancel text')).tap();
-    await expect(element(by.text('Cancel text'))).not.toBeVisible();
+    await getSearchBarCloseButton().tap();
+    await expect(getSearchBarCloseButton()).not.toBeVisible();
   });
 
   it('search bar should show correct results', async () => {
-    await element(by.type('UISearchBarTextField')).tap();
-    await element(by.type('UISearchBarTextField')).typeText('th');
+    await element(by.traits(['searchField'])).tap();
+    await element(by.traits(['searchField'])).typeText('th');
 
     await expect(element(by.text('Other'))).toBeVisible();
     await expect(element(by.text('The'))).toBeVisible();
@@ -45,7 +58,7 @@ describeIfiOS('Test758', () => {
 
   it('search bar should not be visible on another screen', async () => {
     await element(by.text('Tap me for second screen')).tap();
-    await expect(element(by.type('UISearchBarTextField'))).not.toBeVisible();
+    await expect(element(by.traits(['searchField']))).not.toBeVisible();
   });
 
   it('search bar query should still be present after coming back from another screen', async () => {
@@ -60,7 +73,7 @@ describeIfiOS('Test758', () => {
   });
 
   it('search should still be responsive after coming back from another screen', async () => {
-    await element(by.type('UISearchBarTextField')).clearText();
+    await element(by.traits(['searchField'])).clearText();
     await expect(element(by.text('Nuggets'))).toBeVisible();
   });
 });
