18f4f6e803efd28421a2c733c6add07468cea59e
Konrad Michalik <konrad.michalik@swmansion.com>
2026-03-17 08:14:00 +0100

    Add RNSStackOperation

--- ios/gamma/stack-queue/host/RNSStackOperation.h
+++ ios/gamma/stack-queue/host/RNSStackOperation.h
@@ -1,1 +1,50 @@
+#pragma once
 
+#import "RNSStackScreenComponentView.h"
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface RNSStackOperation : NSObject
+@end
+
+@interface RNSPushOperation : RNSStackOperation
+
+@property (nonatomic, strong, readonly) RNSStackScreenComponentView *screen;
+
+- (instancetype)initWithScreen:(RNSStackScreenComponentView *)screen;
+
+@end
+
+@implementation RNSPushOperation
+
+- (instancetype)initWithScreen:(RNSStackScreenComponentView *)screen
+{
+  if (self = [super init]) {
+    _screen = screen;
+  }
+  return self;
+}
+
+@end
+
+@interface RNSPopOperation : RNSStackOperation
+
+@property (nonatomic, strong, readonly) RNSStackScreenComponentView *screen;
+
+- (instancetype)initWithScreen:(RNSStackScreenComponentView *)screen;
+
+@end
+
+@implementation RNSPopOperation
+
+- (instancetype)initWithScreen:(RNSStackScreenComponentView *)screen
+{
+  if (self = [super init]) {
+    _screen = screen;
+  }
+  return self;
+}
+
+@end
+
+NS_ASSUME_NONNULL_END
