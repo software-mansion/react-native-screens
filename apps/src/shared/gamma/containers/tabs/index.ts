09180ff86a8e435ad3389b44e2716c5a2b391399
Kacper Kafara <kacperkafara@gmail.com>
2026-03-23 20:26:04 +0100

    Remove unused `TabsContainerWithDynamicRouteConfigs` & related

--- apps/src/shared/gamma/containers/tabs/index.ts
+++ apps/src/shared/gamma/containers/tabs/index.ts
@@ -13,13 +13,9 @@ export type {
 
 export type { TabsNavigationContextPayload } from './contexts/TabsNavigationContext';
 export type { TabsHostConfigContextPayload } from './contexts/TabsHostConfigContext';
-export type { TabsRouteConfigContextPayload } from './contexts/TabsRouteConfigContext';
-
 export { useTabsNavigationContext } from './hooks/useTabsNavigationContext';
 export { useTabsHostConfig } from './hooks/useTabsHostConfig';
-export { useTabsRouteConfigContext } from './hooks/useTabsRouteConfigContext';
 
 export { TabsContainer } from './TabsContainer';
-export { TabsContainerWithDynamicRouteConfigs } from './TabsContainerWithDynamicRouteConfigs';
 export { TabsContainerWithHostConfigContext } from './TabsContainerWithHostConfigContext';
 export { DEFAULT_TAB_ROUTE_OPTIONS } from './presets';
