30151c6c2704b7435be71a5cb2b6d2cd8d88783d
Tomasz Boroń <tomasz.boron@swmansion.com>
2026-04-09 10:12:30 +0200

    Use Unit

--- android/src/main/java/com/swmansion/rnscreens/utils/ScreenDummyLayoutHelper.kt
+++ android/src/main/java/com/swmansion/rnscreens/utils/ScreenDummyLayoutHelper.kt
@@ -263,20 +263,20 @@ internal class ScreenDummyLayoutHelper(
                 override fun onActivityCreated(
                     activity: Activity,
                     savedInstanceState: Bundle?,
-                ) {}
+                ) = Unit
 
-                override fun onActivityStarted(activity: Activity) {}
+                override fun onActivityStarted(activity: Activity) = Unit
 
-                override fun onActivityResumed(activity: Activity) {}
+                override fun onActivityResumed(activity: Activity) = Unit
 
-                override fun onActivityPaused(activity: Activity) {}
+                override fun onActivityPaused(activity: Activity) = Unit
 
-                override fun onActivityStopped(activity: Activity) {}
+                override fun onActivityStopped(activity: Activity) = Unit
 
                 override fun onActivitySaveInstanceState(
                     activity: Activity,
                     outState: Bundle,
-                ) {}
+                ) = Unit
             }
 
         activity.application.registerActivityLifecycleCallbacks(activityLifecycleCallbacks)
