10dbfed38d86a05b05c52c6db74f0a5960cdc753
Szymon Gaczoł <szymon.gaczol@swmansion.com>
2026-04-14 12:07:42 +0200

    log remove in useReactNavigationTheme

--- apps/src/shared/styling/adapter/react-navigation/useReactNavigationTheme.ts
+++ apps/src/shared/styling/adapter/react-navigation/useReactNavigationTheme.ts
@@ -5,7 +5,6 @@ import { DarkTheme, LightTheme } from '../../Colors';
 export function useReactNavigationTheme() {
   const theme = React.useContext(ThemeContext);
   if (theme == null) {
-    console.log('Returning undefined from useReactNavigationTheme');
     return undefined;
   }
   return theme.dark ? DarkTheme : LightTheme;
