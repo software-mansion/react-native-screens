79967311620c9a1ca56ba260c00baa750276ddb3
Szymon Gaczoł <167703022+sgaczol@users.noreply.github.com>
2026-04-15 17:49:12 +0200

    chore(TVOSExample): bump RN to 0.85.0 (#3882)
    ## Description
    
    
    https://react-native-community.github.io/upgrade-helper/?from=0.84.0&to=0.85.0
    
    This PR bumps also: `react-native-reanimated` to 4.3.0,
    `react-native-worklets` to 0.8.1 and `react-native-gesture-handler` to
    2.31.1 in `TVOSExample`.
    
    Closes
    https://github.com/software-mansion/react-native-screens-labs/issues/1132
    
    ## Changes
    
    - update configuration files
    
    ## Before & after - visual documentation
    
    N/A
    
    ## Test plan
    
    Build `TVOSExample` and check run-time.
    
    ## Checklist
    
    - [ ] Ensured that CI passes

--- TVOSExample/android/gradlew.bat
+++ TVOSExample/android/gradlew.bat
@@ -74,7 +74,7 @@ set CLASSPATH=
 
 
 @rem Execute Gradle
-"%JAVA_EXE%" %DEFAULT_JVM_OPTS% %JAVA_OPTS% %GRADLE_OPTS% "-Dorg.gradle.appname=%APP_BASE_NAME%" -classpath "%CLASSPATH%" -jar "%APP_HOME%\gradle\wrapper\gradle-wrapper.jar" %*
+"%JAVA_EXE%" %DEFAULT_JVM_OPTS% %JAVA_OPTS% %GRADLE_OPTS% "-Dorg.gradle.appname=%APP_BASE_NAME%" -jar "%APP_HOME%\gradle\wrapper\gradle-wrapper.jar" %*
 
 :end
 @rem End local scope for the variables with windows NT shell
