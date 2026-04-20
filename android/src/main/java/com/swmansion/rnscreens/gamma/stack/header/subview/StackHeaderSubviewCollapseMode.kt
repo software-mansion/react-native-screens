3b3e51b6ee698e8df9a4b75a62265684f6a6ae29
Krzysztof Ligarski <krzysztof.ligarski@swmansion.com>
2026-04-20 14:11:16 +0200

    add comment in SubviewCollapseMode

--- android/src/main/java/com/swmansion/rnscreens/gamma/stack/header/subview/StackHeaderSubviewCollapseMode.kt
+++ android/src/main/java/com/swmansion/rnscreens/gamma/stack/header/subview/StackHeaderSubviewCollapseMode.kt
@@ -2,6 +2,11 @@ package com.swmansion.rnscreens.gamma.stack.header.subview
 
 import com.google.android.material.appbar.CollapsingToolbarLayout
 
+// Currently, PIN collapse mode is unsupported for background
+// header subviews. To enable flex layouts within the background
+// we use absolute fill, which effectively makes PIN mode equivalent
+// to OFF.
+// More details: https://github.com/software-mansion/react-native-screens/pull/3796.
 enum class StackHeaderSubviewCollapseMode {
     OFF,
     PARALLAX,
