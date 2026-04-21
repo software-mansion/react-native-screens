12edf296f786257d4b9bcb1b1efff9c6ff651f65
Krzysztof Ligarski <63918941+kligarski@users.noreply.github.com>
2026-04-21 17:23:24 +0200

    chore(types): remove nullability from some `children` props in unstable/experimental components (#3918)
    ## Description
    
    This PR is a follow-up to
    https://github.com/software-mansion/react-native-screens/pull/3719. We
    adjust `children` property to be not nullable for `Host` components
    (Tabs, Split and Stack v5) and subviews in Stack v5 implementation.
    
    This change is applied only to unstable/experimental components so it
    shouldn't be considered a breaking change.
    
    ## Changes
    
    - wrap `ReactNode`/`ViewProps['children']` in `NonNullable` for:
      - `StackHost`
      - `TabsHost`
      - `SplitHost`
      - `StackHeaderSubview` and related places in `StackHeaderConfig`
    
    ## Before & after - visual documentation
    
    N/A
    
    ## Test plan
    
    `yarn check-types`
    
    ## Checklist
    
    - [x] For API changes, updated relevant public types.
    - [x] Ensured that CI passes

--- src/components/gamma/stack/host/StackHost.types.ts
+++ src/components/gamma/stack/host/StackHost.types.ts
@@ -3,7 +3,7 @@ import type { ReactNativeElement, ViewProps } from 'react-native';
 import { type NativeProps } from '../../../../fabric/gamma/stack/StackHostNativeComponent';
 
 export type StackHostProps = {
-  children: ViewProps['children'];
+  children: NonNullable<ViewProps['children']>;
   // TODO: Work on these types
   ref?:
     | React.RefObject<
