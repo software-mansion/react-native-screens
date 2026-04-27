1f2b5bdc7e0a09cef9dcca4698a4cb0e3453bb99
Tomasz Boroń <tomasz.boron@swmansion.com>
2026-04-27 15:26:03 +0200

    Cleanup

--- apps/src/shared/Toast.tsx
+++ apps/src/shared/Toast.tsx
@@ -37,7 +37,7 @@ const Toast = ({
       remove(id);
     }, DISAPPEAR_AFTER);
     return () => clearTimeout(timer);
-  }, [id, remove]); // Added dependencies
+  }, []);
 
   return (
     <TouchableOpacity
