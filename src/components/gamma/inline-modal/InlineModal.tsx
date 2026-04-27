174b9a14409329a293405817ac9b1ec27204f446
Tomasz Boroń <tomasz.boron@swmansion.com>
2026-04-27 12:40:27 +0200

    Fix content flicker with detaching modal

--- src/components/gamma/inline-modal/InlineModal.tsx
+++ src/components/gamma/inline-modal/InlineModal.tsx
@@ -1,7 +1,10 @@
 import React from 'react';
 import type { InlineModalProps } from './InlineModal.types';
 import InlineModalNativeComponent from '../../../fabric/gamma/inline-modal/InlineModalNativeComponent';
+import { StyleSheet } from 'react-native';
 
 export function InlineModal(props: InlineModalProps) {
-  return <InlineModalNativeComponent {...props} />;
+  return (
+    <InlineModalNativeComponent {...props} style={StyleSheet.absoluteFill} />
+  );
 }
