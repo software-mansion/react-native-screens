2a56c01adbe9d4f849ecdeb5ec50906a72bf6dcf
Tomasz Boroń <tomasz.boron@swmansion.com>
2026-04-27 12:26:24 +0200

    Expand ModalProvider with the available space

--- src/components/gamma/inline-modal/InlineModalProvider.tsx
+++ src/components/gamma/inline-modal/InlineModalProvider.tsx
@@ -1,7 +1,22 @@
 import React from 'react';
+import { StyleSheet } from 'react-native';
 import InlineModalProviderNativeComponent from '../../../fabric/gamma/inline-modal/InlineModalProviderNativeComponent';
 import type { InlineModalProviderProps } from './InlineModalProvider.types';
 
-export function InlineModalProvider(props: InlineModalProviderProps) {
-  return <InlineModalProviderNativeComponent {...props} />;
+export function InlineModalProvider({
+  style,
+  ...rest
+}: InlineModalProviderProps) {
+  return (
+    <InlineModalProviderNativeComponent
+      style={[style, styles.container]}
+      {...rest}
+    />
+  );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+});
