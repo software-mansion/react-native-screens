837ebbb9ce83afc17fe1ab96d5c7ba74a5289842
Kacper Kafara <kacperkafara@gmail.com>
2026-05-07 20:26:54 +0200

    fix(iOS, Tabs): correctly pass action origin to `progressNavigationState`
    It has been hardcoded to `RNSTabsActionOriginProgrammaticJs`, but that's
    not only option right now. It can also be
    `RNSTabsActionOriginProgrammaticNative`.

--- ios/tabs/host/RNSTabBarController.mm
+++ ios/tabs/host/RNSTabBarController.mm
@@ -266,6 +266,7 @@ static void rns_pushViewController(__unsafe_unretained id self,
  */
 - (BOOL)updateSelectedViewControllerTo:(nullable UIViewController *)nextSelectedViewController
                                withKey:(nullable NSString *)screenKey
+                          actionOrigin:(RNSTabsActionOrigin)actionOrigin
 {
   if (nextSelectedViewController == nil) {
     return NO;
@@ -276,7 +277,7 @@ static void rns_pushViewController(__unsafe_unretained id self,
   RCTAssert(![NSString rnscreens_isBlankOrNull:screenKey],
             @"[RNScreens] The screenKey MUST NOT be null if the view controller is not null");
 
-  [self progressNavigationState:screenKey withOrigin:RNSTabsActionOriginProgrammaticJs];
+  [self progressNavigationState:screenKey withOrigin:actionOrigin];
 
   if (currSelectedViewController == nextSelectedViewController) {
     return YES;
@@ -544,7 +545,8 @@ static void rns_pushViewController(__unsafe_unretained id self,
 
   RNSLog(@"Change selected view controller to: %@", nextSelectedViewControllerKey);
   BOOL hasStateProgressed = [self updateSelectedViewControllerTo:nextSelectedViewController
-                                                         withKey:nextSelectedViewControllerKey];
+                                                         withKey:nextSelectedViewControllerKey
+                                                    actionOrigin:_pendingStateUpdate.actionOrigin];
 
   if (hasStateProgressed) {
     RNSTabsNavigationStateUpdateContext *context =
