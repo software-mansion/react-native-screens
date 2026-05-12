077b4ab6c0bdd152a626f97a76870eaf90b635d7
lkuchno <luseeku@gmail.com>
2026-05-12 13:34:32 +0200

    name change - lifcycle added

--- apps/src/tests/single-feature-tests/tabs/test-tabs-lifecycle-events/index.tsx
+++ apps/src/tests/single-feature-tests/tabs/test-tabs-lifecycle-events/index.tsx
@@ -14,7 +14,7 @@ import { Colors } from '@apps/shared/styling';
 
 const scenarioDescription: ScenarioDescription = {
   name: 'Tabs lifecycle events',
-  key: 'test-tabs-events',
+  key: 'test-tabs-lifecycle-events',
   details:
     'Verify lifecycle events (onWillAppear, etc.) fire on tab switch',
   platforms: ['ios', 'android'],
