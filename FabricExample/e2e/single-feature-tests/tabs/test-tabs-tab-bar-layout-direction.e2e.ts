6117eef6f9bb395ada40c6b2b204e7afdc611375
lkuchno <luseeku@gmail.com>
2026-05-14 15:05:36 +0200

    removing redundant code, adding new function for launching app with args

--- FabricExample/e2e/single-feature-tests/tabs/test-tabs-tab-bar-layout-direction.e2e.ts
+++ FabricExample/e2e/single-feature-tests/tabs/test-tabs-tab-bar-layout-direction.e2e.ts
@@ -45,6 +45,13 @@ async function expectTab1ToBeLeftOfTab2(shouldBeLeft: boolean) {
   }
 }
 
+async function launchAppWithAttributes(launchArgs?: Record<string, any>) {
+  await device.launchApp({
+    newInstance: true,
+    ...(launchArgs && { launchArgs }),
+  });
+}
+
 describe('Tab Bar Layout Direction - system settings: LTR', () => {
   beforeEach(async () => {
     await device.reloadReactNative();
@@ -135,39 +142,14 @@ describe('Tab Bar Layout Direction - system settings: RTL', () => {
 
   afterAll(async () => {
     if (device.getPlatform() === 'ios') {
-      await device.launchApp({
-        newInstance: true,
-        launchArgs: {
-          AppleTextDirection: 'NO',
-          NSForceRightToLeftWritingDirection: 'NO',
-          I18NIsRTL: 'NO',
-        },
+      await launchAppWithAttributes({
+        AppleTextDirection: 'NO',
+        NSForceRightToLeftWritingDirection: 'NO',
+        I18NIsRTL: 'NO',
       });
     } else {
-      await device.launchApp({ newInstance: true });
-      await selectSingleFeatureTestsScreen(
-        'Tabs',
-        'test-tabs-tab-bar-layout-direction',
-      );
-      try {
-        await element(by.id('react-force-rtl-picker')).tap();
-        await waitFor(element(by.id('react-force-rtl-picker')))
-          .toHaveLabel('forceRTL: true')
-          .withTimeout(3000);
-      } finally {
-        const attrs = await element(
-          by.id('react-force-rtl-picker'),
-        ).getAttributes();
-        const currentLabel =
-          'elements' in attrs ? attrs.elements[0]?.label : attrs.label;
-        if (currentLabel === 'forceRTL: true') {
-          await element(by.id('react-force-rtl-picker')).tap();
-          await waitFor(element(by.id('react-force-rtl-picker')))
-            .toHaveLabel('forceRTL: false')
-            .withTimeout(3000);
-        }
-        await device.reloadReactNative();
-      }
+      await launchAppWithAttributes();
+      await device.reloadReactNative();
     }
   });
 
