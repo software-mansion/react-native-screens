664ddaa788f775dfbe04033f2421eaa4cd41b5b0
lkuchno <luseeku@gmail.com>
2026-05-19 13:46:32 +0200

    removing additional quotes

--- src/components/tabs/screen/TabsScreen.ios.types.ts
+++ src/components/tabs/screen/TabsScreen.ios.types.ts
@@ -223,7 +223,7 @@ export interface TabsScreenPropsIOS {
    * If this property is `undefined`, UIKit uses `standardAppearance`, modified to
    * have a transparent background.
    *
-   * When `tabBarBackgroundColor` is transparent and `tabBarBlurEffect` is `'none'`,
+   * When `tabBarBackgroundColor` is transparent and `tabBarBlurEffect` is `none`,
    * the `tabBarBackgroundColor` from `standardAppearance` will show through from beneath.
    * @platform ios
    */
