dc3f9ac3a6fd02d193cdb927a0bbbe8bbaa04011
Tomasz Boroń <tomasz.boron@swmansion.com>
2026-05-22 13:14:21 +0200

    Expose via providers, drop properties

--- ios/gamma/modals/form-sheet/RNSFormSheetHostComponentView.h
+++ ios/gamma/modals/form-sheet/RNSFormSheetHostComponentView.h
@@ -16,17 +16,6 @@ NS_ASSUME_NONNULL_BEGIN
 
 @interface RNSFormSheetHostComponentView ()
 
-#if defined(__cplusplus)
-- (const std::vector<double> &)detents;
-#endif
-
-@property (nonatomic, readonly) BOOL isOpen;
-@property (nonatomic, readonly) BOOL prefersGrabberVisible;
-@property (nonatomic, readonly) CGFloat preferredCornerRadius;
-@property (nonatomic, readonly) NSInteger largestUndimmedDetentIndex;
-@property (nonatomic, readonly) NSInteger initialDetentIndex;
-@property (nonatomic, readonly) BOOL prefersScrollingExpandsWhenScrolledToEdge;
-
 @end
 
 NS_ASSUME_NONNULL_END
