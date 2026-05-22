2cb5b92735b484c34caa4a4fe65f173f1f10a5af
lkuchno <luseeku@gmail.com>
2026-05-22 14:51:41 +0200

    chore(test): adding new screen and scenario for Tab Bar Layout Appearances (iOS)

--- apps/src/tests/single-feature-tests/tabs/index.ts
+++ apps/src/tests/single-feature-tests/tabs/index.ts
@@ -17,7 +17,7 @@ import TestTabsTabBarControllerMode from './test-tabs-tab-bar-controller-mode-io
 import TestTabsSpecialEffectsScrollToTop from './test-tabs-special-effects-scroll-to-top';
 import TestTabsTabBarExperimentalUserInterfaceStyle from './test-tabs-tab-bar-experimental-user-interface-style-ios';
 import TestTabsLifecycleEvents from './test-tabs-lifecycle-events';
-import TestTabsDisplayModeIOS from './test-tabs-display-mode-ios';
+import TestTabsLayoutAppearances from './test-tabs-layout-appearances-ios';
 
 const scenarios = {
   TestTabBottomAccessory,
@@ -37,7 +37,7 @@ const scenarios = {
   TestTabsSpecialEffectsScrollToTop,
   TestTabsTabBarExperimentalUserInterfaceStyle,
   TestTabsLifecycleEvents,
-  TestTabsDisplayModeIOS,
+  TestTabsLayoutAppearances,
 };
 
 const TabsScenarioGroup: ScenarioGroup<keyof typeof scenarios> = {
