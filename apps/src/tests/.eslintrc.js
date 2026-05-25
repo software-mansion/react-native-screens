e46c4d98577e8f8413d076003071b64d62888c51
Szymon Gaczoł <szymon.gaczol@swmansion.com>
2026-05-25 16:48:38 +0200

    add dev dependency to package.json, move plugin directory to apps, create yarn.lock, update .eslintrc.js

--- apps/src/tests/.eslintrc.js
+++ apps/src/tests/.eslintrc.js
@@ -1,17 +1,3 @@
-const path = require('path');
-const Module = require('module');
-
-const pluginPath = require.resolve(
-  path.join(__dirname, '..', 'eslint-plugin-local-rules'),
-);
-const originalResolve = Module._resolveFilename;
-Module._resolveFilename = function (request, ...args) {
-  if (request === 'eslint-plugin-local-rules') {
-    return pluginPath;
-  }
-  return originalResolve.call(this, request, ...args);
-};
-
 module.exports = {
   overrides: [
     {
