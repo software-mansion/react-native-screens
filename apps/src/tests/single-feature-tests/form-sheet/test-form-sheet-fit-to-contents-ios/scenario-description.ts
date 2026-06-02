569fbd19269c5a52436a098bb4a8ba91f52370da
Tomasz Boroń <tomasz.boron@swmansion.com>
2026-06-02 11:31:25 +0200

    format

--- apps/src/tests/single-feature-tests/form-sheet/test-form-sheet-fit-to-contents-ios/scenario-description.ts
+++ apps/src/tests/single-feature-tests/form-sheet/test-form-sheet-fit-to-contents-ios/scenario-description.ts
@@ -3,7 +3,8 @@ import type { ScenarioDescription } from '@apps/tests/shared/helpers';
 export const scenarioDescription: ScenarioDescription = {
   name: 'Fit to contents',
   key: 'test-form-sheet-fit-to-contents-ios',
-  details: 'Allows to test the fitToContents detent and dynamic height changes of the FormSheet component.',
+  details:
+    'Allows to test the fitToContents detent and dynamic height changes of the FormSheet component.',
   platforms: ['ios'],
   e2eCoverage: 'tbd',
   smokeTest: false,
