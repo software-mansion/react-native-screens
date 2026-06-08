eaa00c7e12d7e8aa29c4a22bfd6b42a27346f977
Szymon Gaczol <gaczol.szymon@gmail.com>
2026-06-08 12:28:24 +0200

    ContainedModal and ContainedModalProvider codegen setup

---
+++ src/fabric/ContainedModalNativeComponent.ts
@@ -0,0 +1,8 @@
+import { codegenNativeComponent } from 'react-native';
+import type { ViewProps } from 'react-native';
+
+export interface NativeProps extends ViewProps {}
+
+export default codegenNativeComponent<NativeProps>('RNSContainedModal', {
+  interfaceOnly: true,
+});
