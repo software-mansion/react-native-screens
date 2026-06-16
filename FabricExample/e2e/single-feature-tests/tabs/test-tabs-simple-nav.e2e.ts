876d5887b429554d67d8df06fa5c0018488c134a
lkuchno <luseeku@gmail.com>
2026-06-16 13:30:02 +0200

    adding @smoke tag to describe test title

--- FabricExample/e2e/single-feature-tests/tabs/test-tabs-simple-nav.e2e.ts
+++ FabricExample/e2e/single-feature-tests/tabs/test-tabs-simple-nav.e2e.ts
@@ -4,7 +4,7 @@ import {
   selectSingleFeatureTestsScreen,
 } from '../../e2e-utils';
 
-describe('Tabs: simple navigation', () => {
+describe('Tabs: simple navigation @smoke', () => {
   beforeAll(async () => {
     await device.reloadReactNative();
     await selectSingleFeatureTestsScreen('Tabs', 'test-tabs-simple-nav');
