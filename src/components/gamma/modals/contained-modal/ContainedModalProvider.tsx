fc9e7e8fef5248a19992fa30f49531c61add4b38
Szymon Gaczol <gaczol.szymon@gmail.com>
2026-06-16 11:36:59 +0200

    rename codegen files

--- src/components/gamma/modals/contained-modal/ContainedModalProvider.tsx
+++ src/components/gamma/modals/contained-modal/ContainedModalProvider.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
-import ContainedModalProviderNativeComponent from '../../../../fabric/gamma/modals/contained-modal/ContainedModalProviderNativeComponent';
+import ContainedModalProviderHostNativeComponent from '../../../../fabric/gamma/modals/contained-modal/ContainedModalProviderHostNativeComponent';
 import type { ContainedModalProviderProps } from './ContainedModalProvider.types';
 
 export function ContainedModalProvider(props: ContainedModalProviderProps) {
-  return <ContainedModalProviderNativeComponent {...props} />;
+  return <ContainedModalProviderHostNativeComponent {...props} />;
 }
