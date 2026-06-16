138f7da4e840b30623fcdc7ab0eb501e58844172
Szymon Gaczol <gaczol.szymon@gmail.com>
2026-06-16 13:21:44 +0200

    codegen name fix

--- src/fabric/gamma/modals/contained-modal/ContainedModalHostNativeComponent.ts
+++ src/fabric/gamma/modals/contained-modal/ContainedModalHostNativeComponent.ts
@@ -7,7 +7,7 @@ export interface NativeProps extends ViewProps {
   isOpen?: CT.WithDefault<boolean, false>;
 }
 
-export default codegenNativeComponent<NativeProps>('RNSContainedModal', {
+export default codegenNativeComponent<NativeProps>('RNSContainedModalHost', {
   interfaceOnly: true,
   excludedPlatforms: ['android'],
 });
