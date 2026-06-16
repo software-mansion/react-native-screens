5beb11ebbe30b6dc9510aac68b3fc494d07c3b7e
Szymon Gaczol <gaczol.szymon@gmail.com>
2026-06-16 11:25:23 +0200

    import type

--- src/components/gamma/modals/contained-modal/ContainedModalProvider.tsx
+++ src/components/gamma/modals/contained-modal/ContainedModalProvider.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import ContainedModalProviderNativeComponent from '../../../../fabric/gamma/modals/contained-modal/ContainedModalProviderNativeComponent';
-import { ContainedModalProviderProps } from './ContainedModalProvider.types';
+import type { ContainedModalProviderProps } from './ContainedModalProvider.types';
 
 export function ContainedModalProvider(props: ContainedModalProviderProps) {
   return <ContainedModalProviderNativeComponent {...props} />;
