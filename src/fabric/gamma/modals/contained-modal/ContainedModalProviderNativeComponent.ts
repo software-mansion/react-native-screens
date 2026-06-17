93288c6ee939d03c6a79579ec75949657be637c3
Szymon Gaczol <gaczol.szymon@gmail.com>
2026-06-17 12:51:24 +0200

    provider name change

--- src/fabric/gamma/modals/contained-modal/ContainedModalProviderNativeComponent.ts
+++ src/fabric/gamma/modals/contained-modal/ContainedModalProviderNativeComponent.ts
@@ -8,7 +8,7 @@ export interface NativeProps extends ViewProps {
 }
 
 export default codegenNativeComponent<NativeProps>(
-  'RNSContainedModalProviderHost',
+  'RNSContainedModalProvider',
   {
     excludedPlatforms: ['android'],
   },
