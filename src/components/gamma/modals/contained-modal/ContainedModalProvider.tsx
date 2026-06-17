f7a346badee3968c3e80a06f2f219f456b9b972f
Szymon Gaczol <gaczol.szymon@gmail.com>
2026-06-17 12:51:24 +0200

    provider name change

--- src/components/gamma/modals/contained-modal/ContainedModalProvider.tsx
+++ src/components/gamma/modals/contained-modal/ContainedModalProvider.tsx
@@ -1,13 +1,13 @@
 import React from 'react';
-import ContainedModalProviderHostNativeComponent from '../../../../fabric/gamma/modals/contained-modal/ContainedModalProviderHostNativeComponent';
+import ContainedModalProviderNativeComponent from '../../../../fabric/gamma/modals/contained-modal/ContainedModalProviderNativeComponent';
 import type { ContainedModalProviderProps } from './ContainedModalProvider.types';
 
 export function ContainedModalProvider(props: ContainedModalProviderProps) {
   const { children, style, ...rest } = props;
 
   return (
-    <ContainedModalProviderHostNativeComponent style={style} {...rest}>
+    <ContainedModalProviderNativeComponent style={style} {...rest}>
       {children}
-    </ContainedModalProviderHostNativeComponent>
+    </ContainedModalProviderNativeComponent>
   );
 }
