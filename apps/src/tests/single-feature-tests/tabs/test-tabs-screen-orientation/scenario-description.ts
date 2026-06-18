418bcaee9b6cb45bdb68b451690f26858a7a5591
lkuchno <luseeku@gmail.com>
2026-06-18 11:17:03 +0200

    adding android to scenario-descirption as this scenario is intend to work on both platform just waiting for android implementation

--- apps/src/tests/single-feature-tests/tabs/test-tabs-screen-orientation/scenario-description.ts
+++ apps/src/tests/single-feature-tests/tabs/test-tabs-screen-orientation/scenario-description.ts
@@ -3,7 +3,7 @@ import type { ScenarioDescription } from '@apps/tests/shared/helpers';
 export const scenarioDescription: ScenarioDescription = {
   name: 'Tabs Screen Orientation',
   key: 'test-tabs-screen-orientation',
-  platforms: ['ios'],
+  platforms: ['ios', 'android'],
   e2eCoverage: 'tbd',
   smokeTest: false,
 };
