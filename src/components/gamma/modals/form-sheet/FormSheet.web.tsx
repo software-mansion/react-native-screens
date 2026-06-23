b56873d1f98ea6c3377fa12e5ff5031507b3934e
Tomasz Boroń <tomasz.boron@swmansion.com>
2026-06-23 15:54:48 +0200

    Update warn

--- src/components/gamma/modals/form-sheet/FormSheet.web.tsx
+++ src/components/gamma/modals/form-sheet/FormSheet.web.tsx
@@ -3,7 +3,7 @@ import warnOnce from 'warn-once';
 export const FormSheet = () => {
   warnOnce(
     true,
-    '[RNScreens] As of now, FormSheet component is supported only for iOS.',
+    '[RNScreens] As of now, FormSheet component is not supported for web.',
   );
   return null;
 };
