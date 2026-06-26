16c8060f6fbfdb889b21d648ff6f5454d7252cfa
Tomasz Boroń <tomasz.boron@swmansion.com>
2026-06-26 10:34:41 +0200

    Prevent animation, when already hidden

--- android/src/main/java/com/swmansion/rnscreens/gamma/modals/formsheet/FormSheetAnimationCoordinator.kt
+++ android/src/main/java/com/swmansion/rnscreens/gamma/modals/formsheet/FormSheetAnimationCoordinator.kt
@@ -5,6 +5,7 @@ import android.animation.AnimatorListenerAdapter
 import android.animation.AnimatorSet
 import android.animation.ValueAnimator
 import android.view.View
+import com.google.android.material.bottomsheet.BottomSheetBehavior
 import com.swmansion.rnscreens.gamma.modals.dimmingview.DimmingViewManager
 
 internal class FormSheetAnimationCoordinator(
@@ -17,17 +18,19 @@ internal class FormSheetAnimationCoordinator(
     internal fun runEnterAnimation(view: View) {
         view.translationY = view.height.toFloat()
 
-        val slideAnimator = ValueAnimator.ofFloat(view.translationY, 0f).apply {
-            addUpdateListener { animation ->
-                view.translationY = animation.animatedValue as Float
+        val slideAnimator =
+            ValueAnimator.ofFloat(view.translationY, 0f).apply {
+                addUpdateListener { animation ->
+                    view.translationY = animation.animatedValue as Float
+                }
             }
-        }
 
-        val alphaAnimator = ValueAnimator.ofFloat(0f, dimmingManager.maxAlpha).apply {
-            addUpdateListener { animation ->
-                dimmingManager.updateAlpha(animation.animatedValue as Float)
+        val alphaAnimator =
+            ValueAnimator.ofFloat(0f, dimmingManager.maxAlpha).apply {
+                addUpdateListener { animation ->
+                    dimmingManager.updateAlpha(animation.animatedValue as Float)
+                }
             }
-        }
 
         AnimatorSet().apply {
             playTogether(slideAnimator, alphaAnimator)
@@ -42,28 +45,38 @@ internal class FormSheetAnimationCoordinator(
             return
         }
 
+        val behavior = BottomSheetBehavior.from(view)
+        if (behavior.state == BottomSheetBehavior.STATE_HIDDEN) {
+            onDismissEnd()
+            return
+        }
+
         view.animate().cancel()
 
-        val slideAnimator = ValueAnimator.ofFloat(view.translationY, view.height.toFloat()).apply {
-            addUpdateListener { animation ->
-                view.translationY = animation.animatedValue as Float
+        val slideAnimator =
+            ValueAnimator.ofFloat(view.translationY, view.height.toFloat()).apply {
+                addUpdateListener { animation ->
+                    view.translationY = animation.animatedValue as Float
+                }
             }
-        }
 
-        val alphaAnimator = ValueAnimator.ofFloat(dimmingManager.currentAlpha, 0f).apply {
-            addUpdateListener { animation ->
-                dimmingManager.updateAlpha(animation.animatedValue as Float)
+        val alphaAnimator =
+            ValueAnimator.ofFloat(dimmingManager.currentAlpha, 0f).apply {
+                addUpdateListener { animation ->
+                    dimmingManager.updateAlpha(animation.animatedValue as Float)
+                }
             }
-        }
 
         AnimatorSet().apply {
             playTogether(slideAnimator, alphaAnimator)
             duration = animationDuration
-            addListener(object : AnimatorListenerAdapter() {
-                override fun onAnimationEnd(animation: Animator) {
-                    onDismissEnd()
-                }
-            })
+            addListener(
+                object : AnimatorListenerAdapter() {
+                    override fun onAnimationEnd(animation: Animator) {
+                        onDismissEnd()
+                    }
+                },
+            )
             start()
         }
     }
