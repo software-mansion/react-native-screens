50fee388c0e18f18cdd1fe34c6f3c59cd694be04
Tomasz Boroń <tomasz.boron@swmansion.com>
2026-07-08 14:25:15 +0200

    Fix key

--- apps/src/tests/single-feature-tests/form-sheet/test-form-sheet-initial-detent-index/scenario-description.ts
+++ apps/src/tests/single-feature-tests/form-sheet/test-form-sheet-initial-detent-index/scenario-description.ts
@@ -2,7 +2,7 @@ import type { ScenarioDescription } from '@apps/tests/shared/helpers';
 
 export const scenarioDescription: ScenarioDescription = {
   name: 'Sheet initial detent index',
-  key: 'test-form-sheet-initial-detent-index-ios',
+  key: 'test-form-sheet-initial-detent-index',
   details: 'Allows to test initialDetentIndex prop of FormSheet component.',
   platforms: ['android', 'ios'],
   e2eCoverage: 'tbd',
