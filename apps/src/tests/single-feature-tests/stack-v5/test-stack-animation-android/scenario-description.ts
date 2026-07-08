dc9f3315ae2e58099b0cd49cc54c72290de9f7b1
lkuchno <luseeku@gmail.com>
2026-07-08 13:19:12 +0200

    correct key name to contains stack

--- apps/src/tests/single-feature-tests/stack-v5/test-stack-animation-android/scenario-description.ts
+++ apps/src/tests/single-feature-tests/stack-v5/test-stack-animation-android/scenario-description.ts
@@ -2,7 +2,7 @@ import type { ScenarioDescription } from '@apps/tests/shared/helpers';
 
 export const scenarioDescription: ScenarioDescription = {
   name: 'Animation Android',
-  key: 'test-animation-android',
+  key: 'test-stack-animation-android',
   details: 'High contrast screens to test animations on Android',
   platforms: ['android'],
   e2eCoverage: 'incomplete',
