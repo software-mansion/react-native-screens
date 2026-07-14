7234b4e91a7cb55f3b501ad8980a2ebf2900359c
Konrad Michalik <konrad.michalik@swmansion.com>
2026-07-14 13:39:31 +0200

    chore(Stack v5, iOS): Add missing imports for iOS SFTs

--- apps/src/tests/single-feature-tests/stack-v5/index.ts
+++ apps/src/tests/single-feature-tests/stack-v5/index.ts
@@ -30,8 +30,11 @@ export { default as TestStackAnimationAndroid } from './test-stack-animation-and
 export { default as TestStackSimpleNav } from './test-stack-simple-nav';
 export { default as TestStackSubviewsAndroid } from './test-stack-subviews-android';
 export { default as TestStackSubviewsIOS } from './test-stack-subviews-ios';
-export { default as TestStackHeaderMenuIOS } from './test-stack-header-menu-ios';
 export { default as TestStackHeaderIconIOS } from './test-stack-header-icon-ios';
+export { default as TestStackHeaderMenuIOS } from './test-stack-header-menu-ios';
+export { default as TestStackHeaderMenuOptionsIOS } from './test-stack-header-menu-options-ios';
+export { default as TestStackHeaderSelectiveUpatesIOS } from './test-stack-header-selective-updates-ios';
+export { default as TestStackHeaderSubviewOnPressIOS } from './test-stack-header-subview-onpress-ios';
 export { default as TestStackBackButton } from './test-stack-back-button-android';
 export { default as TestStackToolbarMenuCommands } from './test-stack-toolbar-menu-commands-android';
 export { default as TestStackToolbarMenuDisabled } from './test-stack-toolbar-menu-disabled-android';
@@ -40,7 +43,6 @@ export { default as TestStackToolbarMenuShowAsAction } from './test-stack-toolba
 export { default as TestStackToolbarMenuTitle } from './test-stack-toolbar-menu-title-android';
 export { default as TestStackToolbarMenuIcon } from './test-stack-toolbar-menu-icon-android';
 export { default as TestStackToolbarNestedMenu } from './test-stack-toolbar-nested-menu-android';
-export { default as TestStackHeaderMenuOptionsIOS } from './test-stack-header-menu-options-ios';
 
 const scenarios = {
   TestStackPreventNativeDismissSingleStack,
