6430ff7b961956400999c64d8526f4bc3b106c75
Krzysztof Ligarski <krzysztof.ligarski@swmansion.com>
2026-07-21 10:21:28 +0200

    expand comment about the component being unstable

--- src/components/scroll-to-top-guard/ScrollToTopGuard.ios.tsx
+++ src/components/scroll-to-top-guard/ScrollToTopGuard.ios.tsx
@@ -6,6 +6,11 @@ import ScrollToTopGuardNativeComponent from '../../fabric/scroll-to-top-guard/Sc
 
 /**
  * EXPERIMENTAL API, MIGHT CHANGE W/O ANY NOTICE
+ *
+ * This is an **experimental, unstable** component which might be subject to
+ * breaking changes and will be removed in the future. It is meant to be used
+ * only as a temporary workaround until the issue is fixed on `react-native`'s
+ * side. The issue report: https://github.com/react/react-native/issues/56061.
  */
 export function ScrollToTopGuard(props: ViewProps) {
   return <ScrollToTopGuardNativeComponent {...props} collapsable={false} />;
--- src/components/scroll-to-top-guard/ScrollToTopGuard.tsx
+++ src/components/scroll-to-top-guard/ScrollToTopGuard.tsx
@@ -1,5 +1,13 @@
 import { View } from 'react-native';
 
+/**
+ * EXPERIMENTAL API, MIGHT CHANGE W/O ANY NOTICE
+ *
+ * This is an **experimental, unstable** component which might be subject to
+ * breaking changes and will be removed in the future. It is meant to be used
+ * only as a temporary workaround until the issue is fixed on `react-native`'s
+ * side. The issue report: https://github.com/react/react-native/issues/56061.
+ */
 const ScrollToTopGuard = View;
 
 export { ScrollToTopGuard };
