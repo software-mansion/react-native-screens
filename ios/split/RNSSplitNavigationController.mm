9aafdcfbd04661dbfae50aaf71c6a87a05a26b62
Tomasz Boroń <tomasz.boron@swmansion.com>
2026-07-30 16:24:27 +0200

    Extract frame observer helper

--- ios/split/RNSSplitNavigationController.mm
+++ ios/split/RNSSplitNavigationController.mm
@@ -1,9 +1,8 @@
 #import "RNSSplitNavigationController.h"
-
-static void *RNSSplitNavigationControllerViewFrameContext = &RNSSplitNavigationControllerViewFrameContext;
+#import "RNSSplitNavigationControllerFrameObserver.h"
 
 @implementation RNSSplitNavigationController {
-  BOOL _isObservingViewFrame;
+  RNSSplitNavigationControllerFrameObserver *_frameObserver;
 }
 
 /**
@@ -16,53 +15,15 @@ static void *RNSSplitNavigationControllerViewFrameContext = &RNSSplitNavigationC
 {
   [super viewDidLoad];
 
-  if (_isObservingViewFrame) {
-    [self.view removeObserver:self forKeyPath:@"frame" context:RNSSplitNavigationControllerViewFrameContext];
-    _isObservingViewFrame = NO;
+  if (_frameObserver == nil) {
+    _frameObserver = [[RNSSplitNavigationControllerFrameObserver alloc] initWithSplitNavigationController:self];
   }
-
-  [self.view addObserver:self
-              forKeyPath:@"frame"
-                 options:NSKeyValueObservingOptionOld | NSKeyValueObservingOptionNew
-                 context:RNSSplitNavigationControllerViewFrameContext];
-  _isObservingViewFrame = YES;
-}
-
-- (void)observeValueForKeyPath:(NSString *)keyPath
-                      ofObject:(id)object
-                        change:(NSDictionary<NSKeyValueChangeKey, id> *)change
-                       context:(void *)context
-{
-  if (context != RNSSplitNavigationControllerViewFrameContext) {
-    [super observeValueForKeyPath:keyPath ofObject:object change:change context:context];
-    return;
-  }
-
-  id oldValue = change[NSKeyValueChangeOldKey];
-  id newValue = change[NSKeyValueChangeNewKey];
-  if (![oldValue isKindOfClass:[NSValue class]] || ![newValue isKindOfClass:[NSValue class]]) {
-    return;
-  }
-
-  // This cast should be safe, as only our RNSSplitNavigationControllerViewFrameContext can reach that.
-  CGRect oldFrame = [oldValue CGRectValue];
-  CGRect newFrame = [newValue CGRectValue];
-
-  if (!CGPointEqualToPoint(oldFrame.origin, newFrame.origin)) {
-    [self onViewOriginChange];
-  }
-}
-
-- (void)onViewOriginChange
-{
-  [self.viewFrameOriginChangeObserver splitNavCtrlViewDidChangeFrameOrigin:self];
+  [_frameObserver registerForViewFrameChanges];
 }
 
 - (void)dealloc
 {
-  if (_isObservingViewFrame) {
-    [self.viewIfLoaded removeObserver:self forKeyPath:@"frame" context:RNSSplitNavigationControllerViewFrameContext];
-  }
+  [_frameObserver unregisterForViewFrameChanges];
 }
 
 @end
