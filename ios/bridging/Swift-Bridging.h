efb9942faba16e888596afb28015784dfe613f65
Tomasz Boroń <tomasz.boron@swmansion.com>
2026-07-31 11:09:02 +0200

    chore(iOS, Split): Migrate RNSSplitNavigationController to obj-c (#4422)
    Next part of dropping Swift from our codebase; here I am migrating the
    RNSSplitNavigationController. The code should look similar to the
    previous implementation; the main change was around key-value observer
    syntax.
    
    Closes:
    https://github.com/software-mansion/react-native-screens-labs/issues/1702
    
    - RNSSplitNavigationController: swift -> obj-c
    
    N/A
    
    Played for a while with SplitBaseApp and TestSplitPressables (on this
    one focused on pressables)
    
    - [ ] Included code example that can be used to test this change.
    - [ ] For visual changes, included screenshots / GIFs / recordings
    documenting the change.
    - [ ] For API changes, updated relevant public types.
    - [ ] Ensured that CI passes
    
    (cherry picked from commit 93875dbafadf5ef61071435c79e244f6d79e142e)

--- ios/bridging/Swift-Bridging.h
+++ ios/bridging/Swift-Bridging.h
@@ -1,6 +1,8 @@
 #pragma once
 
 #import "RNSReactMountingTransactionObserving.h"
+#import "RNSSplitNavigationController.h"
+#import "RNSSplitNavigationControllerFrameOriginChangeDelegate.h"
 
 #if __has_include(<RNScreens/RNScreens-Swift.h>)
 #import <RNScreens/RNScreens-Swift.h>
