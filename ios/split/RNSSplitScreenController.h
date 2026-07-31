27c04d91042cc7026c9f21cd34c8abbf540eb637
Tomasz Boroń <tomasz.boron@swmansion.com>
2026-07-31 17:33:32 +0200

    chore(iOS, Split): Migrate RNSSplitScreenController to obj-c (#4424)
    ## Description
    
    Next part of dropping the Swift sources from our codebase. I've left
    some todos in the code for the next PR - these will be resolved once I
    migrate SplitHostController.
    
    Closes
    https://github.com/software-mansion/react-native-screens-labs/issues/1703
    
    ## Changes
    
    - RNSSplitScreenController: swift -> obj-c
    
    ## Before & after - visual documentation
    
    N/A
    
    ## Test plan
    
    Played for a while with Split examples
    
    ## Checklist
    
    - [ ] Included code example that can be used to test this change.
    - [ ] For visual changes, included screenshots / GIFs / recordings
    documenting the change.
    - [ ] For API changes, updated relevant public types.
    - [ ] Ensured that CI passes

---
+++ ios/split/RNSSplitScreenController.h
@@ -0,0 +1,47 @@
+#pragma once
+
+#import <UIKit/UIKit.h>
+
+@class RNSSplitScreenComponentView;
+
+NS_ASSUME_NONNULL_BEGIN
+
+/**
+ * @class RNSSplitScreenController
+ * @brief A UIViewController subclass that manages a Split column in a UISplitViewController.
+ *
+ * Associated with a RNSSplitScreenComponentView, it handles layout synchronization with the
+ * Shadow Tree, emits React lifecycle events, and interacts with the SplitHost hierarchy.
+ */
+@interface RNSSplitScreenController : UIViewController
+
+- (instancetype)initWithSplitScreenComponentView:(RNSSplitScreenComponentView *)splitScreenComponentView;
+
+#pragma mark - Signals
+
+/**
+ * @brief Determines if this controller is nested inside a SplitHost hierarchy.
+ *
+ * Used to differentiate between screens embedded in the native host and modal presentations.
+ *
+ * @return true if inside RNSSplitHostController, false otherwise.
+ */
+- (BOOL)isInSplitHostSubtree;
+
+- (void)setNeedsLifecycleStateUpdate;
+
+#pragma mark - Layout
+
+/**
+ * @brief Request ShadowNode state update when the Split screen frame origin has changed.
+ *
+ * @param splitViewController The UISplitViewController whose layout positioning changed, represented by
+ * RNSSplitHostController.
+ */
+- (void)columnPositioningDidChangeInSplitViewController:(UISplitViewController *)splitViewController
+    // TODO: @t0maboro - NS_SWIFT_NAME is temporal and will be removed while working on RNSSplitHostController migration
+    NS_SWIFT_NAME(columnPositioningDidChangeIn(splitViewController:));
+
+@end
+
+NS_ASSUME_NONNULL_END
