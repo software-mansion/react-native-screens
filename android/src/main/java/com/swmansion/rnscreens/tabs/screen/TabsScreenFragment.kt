1ce5fd71cdbee975fd15694f9d0588d163faf989
Ngoc Le <ngoc1997307@gmail.com>
2026-08-22 19:07:14 +0700

    fix(Android): make fragment restoration R8-safe

--- android/src/main/java/com/swmansion/rnscreens/tabs/screen/TabsScreenFragment.kt
+++ android/src/main/java/com/swmansion/rnscreens/tabs/screen/TabsScreenFragment.kt
@@ -6,10 +6,12 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import com.swmansion.rnscreens.fragment.restoration.RNScreensFragment
 
 class TabsScreenFragment(
     internal val tabsScreen: TabsScreen,
-) : Fragment() {
+) : Fragment(),
+    RNScreensFragment {
     internal val requireScreenKey: String by tabsScreen::requireScreenKey
     internal val isPreventNativeSelectionEnabled: Boolean by tabsScreen::preventNativeSelection
 
