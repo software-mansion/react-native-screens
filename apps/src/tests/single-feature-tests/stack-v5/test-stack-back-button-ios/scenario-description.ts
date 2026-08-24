99670dd30fe9eedef142f6cdfcffe3beb2f0071d
Konrad Michalik <konrad.michalik@swmansion.com>
2026-08-24 11:49:44 +0200

    update naming of scenarios

--- apps/src/tests/single-feature-tests/stack-v5/test-stack-back-button-ios/scenario-description.ts
+++ apps/src/tests/single-feature-tests/stack-v5/test-stack-back-button-ios/scenario-description.ts
@@ -1,7 +1,7 @@
 import type { ScenarioDescription } from '@apps/tests/shared/helpers';
 
 export const scenarioDescription: ScenarioDescription = {
-  name: 'Back button config (iOS)',
+  name: 'Stack Back Button (iOS)',
   key: 'test-stack-back-button-ios',
   details: 'Tests back button configuration and layout with respect to changing widths of elements in header. Back button props are configured on the screen that renders the back button, but the native configuration is performed on the screen below.',
   platforms: ['ios'],
