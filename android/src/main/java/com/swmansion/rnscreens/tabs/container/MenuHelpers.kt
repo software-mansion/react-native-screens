c2d338d7f5775733dc893b596d7009ee9191ead1
Loloekk <karolbie@interia.pl>
2026-08-26 20:29:35 +0200

    refactor: use ViewIdGenerator to identify menuItems

--- android/src/main/java/com/swmansion/rnscreens/tabs/container/MenuHelpers.kt
+++ android/src/main/java/com/swmansion/rnscreens/tabs/container/MenuHelpers.kt
@@ -2,30 +2,22 @@ package com.swmansion.rnscreens.tabs.container
 
 import android.view.Menu
 import android.view.MenuItem
-import com.swmansion.rnscreens.tabs.screen.TabsScreen
+import com.swmansion.rnscreens.tabs.screen.TabsScreenFragment
 
-// MenuItem ids are offset by one, because 0 has special meaning in the BottomNavigationMenuView API.
+internal fun Menu.getOrCreateMenuItemForFragment(fragment: TabsScreenFragment): MenuItem =
+    this.findItem(fragment.menuItemId) ?: this.add(
+        Menu.NONE,
+        fragment.menuItemId,
+        Menu.NONE,
+        fragment.tabsScreen.tabTitle,
+    )
 
-/**
- * Compute MenuItem id for a fragment at given index in "tabsModel" of TabsContainer
- */
-internal fun menuItemIdForFragmentAtIndex(fragmentIndex: Int): Int = fragmentIndex + 1
-
-/**
- * Compute fragment index in "tabsModel" of TabsContainer for a MenuItem with given id.
- */
-internal fun fragmentIndexForMenuItemId(menuItemId: Int): Int {
-    check(menuItemId >= 1) { "[RNScreens] MenuItem id must not be less than 1" }
-    return menuItemId - 1
+internal fun Menu.indexOfItem(item: MenuItem): Int {
+    // We can iterate because we know that there are a few items at most
+    for (i in 0 until size()) {
+        if (getItem(i) === item) return i
+    }
+    return -1
 }
 
-internal fun Menu.getOrCreateMenuItemForFragmentAt(
-    index: Int,
-    tabsScreen: TabsScreen,
-): MenuItem =
-    this.findItem(menuItemIdForFragmentAtIndex(index)) ?: this.add(
-        Menu.NONE,
-        menuItemIdForFragmentAtIndex(index),
-        Menu.NONE,
-        tabsScreen.tabTitle,
-    )
+
