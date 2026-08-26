2f0e68f842eb13fd6319bf199769e08c7719caea
Konrad Michalik <63911178+kmichalikk@users.noreply.github.com>
2026-08-26 08:26:02 +0200

    fix(iOS, Stack v5): Refactor header coordination to hide header when no headerConfig is provided (#4535)
    Closes
    https://github.com/software-mansion/react-native-screens-labs/issues/1699
    
    ## Description
    
    Currently on v5 when no headerConfig is provided native header is
    rendered empty, with back button present. Additionally, the header is
    set to large variant by default, and that looks bad. This PR hides the
    header entirely when no config is provided, matching the behavior in v4.
    
    > [!important]
    > Without visible back button, screen dismiss gestures are disabled and
    swiping results in dismissing from outer stack. This is native behavior
    but indesirable in our case. We prevented this in v4 with a custom
    gesture recognizer, which is not implemented yet for v5, but will be in
    the future
    (https://github.com/software-mansion/react-native-screens-labs/issues/824).
    
    ## Changes
    
    - moved the header coordinator wiring header config earlier so we can
    catch the fact when screen `viewWillAppear` fires
    - added method for setting header visibility
    - added `screen.viewWillAppear` that hides the header if no config
    provider is present
    
    ## Before & after - visual documentation
    
    n/a
    
    ## Test plan
    
    Use simple navigation SFT, hide the header on one screen and navigate;
    use test-stack-header-subviews-ios, hide / show the header via prop
    updates. Verify that it hides.
    
    ## Checklist
    
    - [ ] Included code example that can be used to test this change.
    - [ ] For visual changes, included screenshots / GIFs / recordings
    documenting the change.
    - [ ] For API changes, updated relevant public types.
    - [ ] Ensured that CI passes

--- ios/stack/screen/RNSStackScreenController.mm
+++ ios/stack/screen/RNSStackScreenController.mm
@@ -56,6 +56,7 @@
 - (void)viewWillAppear:(BOOL)animated
 {
   [super viewWillAppear:animated];
+  [self.headerCoordinator updateNavigationBarVisibilityAnimated:animated];
   [[self reactEventEmitter] emitOnWillAppear];
 }
 
