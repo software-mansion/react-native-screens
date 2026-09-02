39b79f444c4a2973f53cdd13e94f0f7e08300282
Konrad Michalik <konrad.michalik@swmansion.com>
2026-09-02 16:32:23 +0200

    Mark android version explicitly

--- apps/src/tests/single-feature-tests/stack-v5/index.ts
+++ apps/src/tests/single-feature-tests/stack-v5/index.ts
@@ -28,7 +28,7 @@ import TestStackHeaderSubviewOnPress from './test-stack-header-subview-onpress-i
 import TestStackHeaderSelectiveUpdates from './test-stack-header-selective-updates-ios';
 import TestStackHeaderMenuOptionsIOS from './test-stack-header-menu-options-ios';
 import TestStackHeaderItemIdentifierIOS from './test-stack-header-item-identifier-ios';
-import TestStackHeaderTitleAppearance from './test-stack-header-title-appearance-android';
+import TestStackHeaderTitleAppearanceAndroid from './test-stack-header-title-appearance-android';
 import TestStackHeaderTitleAppearanceIOS from './test-stack-header-title-appearance-ios';
 import TestStackHeaderContentInsets from './test-stack-header-content-insets-android';
 import TestStackHeaderBackground from './test-stack-header-background-android';
@@ -63,7 +63,7 @@ export { default as TestStackToolbarMenuIcon } from './test-stack-toolbar-menu-i
 export { default as TestStackToolbarNestedMenu } from './test-stack-toolbar-nested-menu-android';
 export { default as TestStackToolbarMenuBatchCommands } from './test-stack-toolbar-menu-batch-commands-android';
 export { default as TestStackToolbarMenuA11y } from './test-stack-toolbar-menu-a11y-android';
-export { default as TestStackHeaderTitleAppearance } from './test-stack-header-title-appearance-android';
+export { default as TestStackHeaderTitleAppearanceAndroid } from './test-stack-header-title-appearance-android';
 export { default as TestStackHeaderTitleAppearanceIOS } from './test-stack-header-title-appearance-ios';
 export { default as TestStackHeaderContentInsets } from './test-stack-header-content-insets-android';
 export { default as TestStackHeaderBackground } from './test-stack-header-background-android';
@@ -97,7 +97,7 @@ const scenarios = {
   TestStackToolbarNestedMenu,
   TestStackToolbarMenuBatchCommands,
   TestStackToolbarMenuA11y,
-  TestStackHeaderTitleAppearance,
+  TestStackHeaderTitleAppearanceAndroid,
   TestStackHeaderTitleAppearanceIOS,
   TestStackHeaderContentInsets,
   TestStackHeaderBackground,
