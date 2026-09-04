cdf741f8763153aa68a47d12c6b03519a50d63bb
Krzysztof Ligarski <krzysztof.ligarski@swmansion.com>
2026-09-04 15:13:09 +0200

    add comment for isContentScrolled()

--- android/src/main/java/com/swmansion/rnscreens/stack/header/StackHeaderCoordinatorLayout.kt
+++ android/src/main/java/com/swmansion/rnscreens/stack/header/StackHeaderCoordinatorLayout.kt
@@ -252,6 +252,7 @@ internal class StackHeaderCoordinatorLayout(
         onMaybeHeaderLayoutChanged()
     }
 
+    // If the content scroll view can scroll further up, we consider it scrolled.
     private fun isContentScrolled() = stackScreen.findContentScrollView()?.canScrollVertically(-1) == true
 
     // endregion
